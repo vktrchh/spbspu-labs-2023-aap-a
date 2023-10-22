57f438b3477b602d8819da69f669f78e921bab0e
Rail (Susanoo) <zhalilov.rsh@edu.spbstu.ru>
2023-10-22 19:44:34 +0300

    Fixed mistakes, added input checking and functional object

--- zhalilov.rail/P1/main.cpp
+++ zhalilov.rail/P1/main.cpp
@@ -1,30 +1,54 @@
 #include <iostream>
 
+class Counter
+{
+  size_t count;
+  size_t maxSeq;
+public:
+  Counter():
+    count(1),
+    maxSeq(0)
+  {}
+
+  void operator()(int prev, int curr)
+  {
+    if(curr == prev)
+    {
+      count++;
+    }
+    else
+    {
+      count = 1;
+    }
+
+    if(count > maxSeq)
+    {
+      maxSeq = count;
+    }
+  }
+  
+  size_t operator()() const
+  {
+    return maxSeq;
+  }
+};
+
 int main()
 {
   int prev = 0;
   int curr = 0;
-
-  size_t counter = 1;
-  size_t maxSeq = 0;
+  
+  Counter counter;
   while(std::cin >> curr && curr)
   {
-    if(curr == prev)
+    if(!std::cin)
     {
-      counter++;
+      std::cout << "Bad input";
+      return 1;
     }
-    else
-    {
-      counter = 1;
-    }
-
-    if(counter > maxSeq)
-    {
-      maxSeq = counter;
-      counter = 1;
-    }
+    counter(prev, curr);
     prev = curr;
   }
 
-  std::cout << maxSeq;
+  std::cout << counter();
 }
