c1eaeb0c0647516cf86c5dce54ccda58837a64e3
Piyavva <piyavkin05@bk.ru>
2023-10-22 21:41:19 +0300

    while

--- piyavkin.anton/P1/main.cpp
+++ piyavkin.anton/P1/main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 
-int main(){
-  int num = 0;
-  std::cin >> num;
-  if (!std::cin){
-    return 1;
-  }
+int main() {
+    long long int num = 1;
+    long long int prev = 1000000000000;
+    size_t count = 0;
+    size_t maxc = 0;
+    while (num != 0) {
+        std::cin >> num;
+        if (!std::cin) {
+            std::cout << "error\n";
+            return 1;
+        }
+        else if (num == 0) {
+            std::cout << maxc;
+        }
+        else if (num <= prev) {
+            ++count;
+        }
+        else {
+            if (count > maxc) {
+                maxc = count;
+            }
+            count = 1;
+        }
+        prev = num;
+    }
 }
-
