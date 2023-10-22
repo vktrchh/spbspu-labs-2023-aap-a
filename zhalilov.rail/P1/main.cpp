1d3bdfa82525ce0a51f547686446e1d12db1df13
Rail (Susanoo) <zhalilov.rsh@edu.spbstu.ru>
2023-10-22 17:50:28 +0300

    Added sequence check

--- zhalilov.rail/P1/main.cpp
+++ zhalilov.rail/P1/main.cpp
@@ -2,5 +2,29 @@
 
 int main()
 {
-  return 0;
+  int prev = 0;
+  int curr = 0;
+
+  size_t counter = 1;
+  size_t maxSeq = 0;
+  while(std::cin >> curr && curr)
+  {
+    if(curr == prev)
+    {
+      counter++;
+    }
+    else
+    {
+      counter = 1;
+    }
+    
+    if(counter > maxSeq)
+    {
+      maxSeq = counter;
+      counter = 1;
+    }
+    prev = curr;
+  }
+  
+  std::cout << maxSeq;
 }
