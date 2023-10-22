bc2eed04945cca94b9bb8eeddfb9a0b07ef3b705
osakasonyboy@gmail.com <sensh0god@gmail.com>
2023-10-22 16:57:12 +0300

    added check for sequence of one element

--- arakelyan.daniil/P1/main.cpp
+++ arakelyan.daniil/P1/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <limits>
 
 int main()
 {
-  unsigned long long count = 0;
+  size_t count = 0;
   int num = 0;
+  size_t lenght = 0;
   do {
     int past = num;
     std::cin >> num;
@@ -12,10 +14,20 @@ int main()
       return 1;
     }else if (num != 0)
     {
-      if(num % past == 0)
+      size_t max_lenght  = std::numeric_limits<size_t>::max();
+      if(lenght == max_lenght)
       {
+        std::cerr << "Sequence is too long\n";
+        return 2;
+      }else if(num % past == 0){
         ++count;
+        ++lenght;
       }
+    }
+    else
+    {
+      std::cout << "Sequence is too small\n";
+      return 2;
     }
   }while (num != 0);
   std::cout << count << "\n";
