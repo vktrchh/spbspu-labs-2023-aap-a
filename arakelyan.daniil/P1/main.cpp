90810e920d4b14ff3e64eacb89af1d54399f1eab
osakasonyboy@gmail.com <sensh0god@gmail.com>
2023-10-22 16:00:06 +0300

    набросок с while циклом

--- arakelyan.daniil/P1/main.cpp
+++ arakelyan.daniil/P1/main.cpp
@@ -4,9 +4,22 @@ int main()
 {
     int num = 0;
     std::cin >> num;
-    if(!std::cin){
+    if(!std::cin)
+    {
         std::cerr << "Is not a number\n";
         return 1;
     }
+    int count = 0;
+    while(num != 0)
+    {
+        int past = num;
+        std::cin >> num;
+        if (num % past == 0)
+        {
+            std::cout << num << ":" << past  << ":" << count << "\n";
+            count += 1;
+        }
+    }
+    std::cout << count << "\n";
     return 0;
 }
