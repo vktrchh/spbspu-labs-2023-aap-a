e1dc46d9d2be37dc36d68bc22b1d0921ef819175
osakasonyboy@gmail.com <sensh0god@gmail.com>
2023-10-22 17:54:18 +0300

    CG

--- arakelyan.daniil/P1/main.cpp
+++ arakelyan.daniil/P1/main.cpp
@@ -8,36 +8,38 @@ int main()
   size_t count = 0;
   int num = 0;
   size_t lenght = 0;
-  do {
+  do
+  {
     int past = num;
     std::cin >> num;
-    if(!std::cin){
+    if (!std::cin) {
       std::cerr << "Not a num.\n";
       return 1;
-    }else if (num != 0)
-    {
+    }
+    else if (num != 0) {
       size_t max_lenght  = std::numeric_limits<size_t>::max();
       if(lenght == max_lenght)
       {
         std::cerr << "Sequence is too long.\n";
         return 2;
-      }else if(quotientOfNumbers(num,past)){
+      } else if (quotientOfNumbers(num,past)) {
         ++count;
         ++lenght;
       }
     }
-  }while (num != 0);
-  if(count == 0){
+  }
+  while (num != 0);
+  if (count == 0) {
     std::cerr << "Sequence is too small.\n";
     return 2;
-  }else{
+  } else {
     std::cout << count << "\n";
   }
   return 0;
 }
 
 bool quotientOfNumbers(int a,int b){
-  if(a % b == 0){
+  if (a % b == 0) {
     return true;
   }
   return false;
