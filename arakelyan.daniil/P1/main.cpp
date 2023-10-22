d481f632aef94a4e72bf0e68caee7681bc1209a8
osakasonyboy@gmail.com <sensh0god@gmail.com>
2023-10-22 15:46:06 +0300

    проверка на ввод

--- arakelyan.daniil/P1/main.cpp
+++ arakelyan.daniil/P1/main.cpp
@@ -1,2 +1,12 @@
+#include <iostream>
+
 int main()
-{}
+{
+    int num = 0;
+    std::cin >> num;
+    if(!std::cin){
+        std::cerr << "Is not a number\n";
+        return 1;
+    }
+    return 0;
+}
