e63ad12309fbf95a1d6989f7ba553209a99cd837
SlivkaSadovka <strelyaev_ra@spbstu.ru>
2023-10-22 23:01:07 +0300

    basic sequence input and error message

--- strelyaev.roman/P1/main.cpp
+++ strelyaev.roman/P1/main.cpp
@@ -1,4 +1,19 @@
+#include <iostream>
+
 int main()
 {
+  int currentNumber = 0;
+/* В голову ничего кроме самого обычно do-while цикла не пришло. Извиняйте */
+  do 
+  {
+    std::cin >> currentNumber;
+
+    if (!std::cin)
+    {
+    std::cerr << "AN ERROR HAS OCCURRED\n";
+    return 1;
+    }
+  }
+  while (currentNumber != 0); 
 }
 
