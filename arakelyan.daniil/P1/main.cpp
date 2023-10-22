5788bc176b49bd214e347d5f6e737e4df26a81e1
osakasonyboy@gmail.com <sensh0god@gmail.com>
2023-10-22 22:54:11 +0300

    сделал набросок с классом

--- arakelyan.daniil/P1/main.cpp
+++ arakelyan.daniil/P1/main.cpp
@@ -1,46 +1,75 @@
 #include <iostream>
 #include <limits>
 
-bool quotientOfNumbers(int a,int b);
-
-int main()
+struct Sequens
 {
-  size_t count = 0;
-  int num = 0;
-  size_t lenght = 0;
-  do
+  Sequens():
+    count(0),
+    lenght(0)
+  {}
+  bool number_choto(int a, int b)
   {
-    int past = num;
-    std::cin >> num;
-    if (!std::cin) {
-      std::cerr << "Not a num.\n";
-      return 1;
+    if (a % b == 0) {
+      return true;
     }
-    else if (num != 0) {
-      size_t max_lenght  = std::numeric_limits<size_t>::max();
-      if(lenght == max_lenght)
-      {
-        std::cerr << "Sequence is too long.\n";
-        return 2;
-      } else if (quotientOfNumbers(num,past)) {
+    return false;
+  }
+  
+  void seqLenghtAndCount(int a, int b){
+    size_t max_lenght = std::numeric_limits<size_t>::max();
+    if (lenght == max_lenght) {
+      throw std::logic_error("Sequence is too long.");
+    } else if (a == 0 && lenght < 3) {
+      throw std::logic_error("Sequence is short.");
+    } else {
+      if (number_choto(a,b)){
         ++count;
         ++lenght;
       }
     }
   }
-  while (num != 0);
-  if (count == 0) {
-    std::cerr << "Sequence is too small.\n";
-    return 2;
-  } else {
-    std::cout << count << "\n";
+  
+  size_t get_count() {
+    return count;
   }
+  
+  size_t get_lenght() {
+    return lenght;
+  }
+
+private:
+  size_t count;
+  size_t lenght;
+};
+
+
+int main()
+{
+  int num = 0;
+  // int min_len = 0;
+  Sequens counter;
+  do
+  {
+    int past = num;
+    std::cin >> num;
+    // ++min_len;
+    if (!std::cin) {
+      std::cerr << "Not a num.\n";
+      return 1;
+    } else if (num == 0 &&  counter.get_lenght() < 2) {
+      std::cout << "Sequence is too short.\n";
+      return 2;
+    } else if (num != 0) {
+      try {
+        counter.seqLenghtAndCount(num,past); 
+      } 
+      catch (const std::exception & e) {
+        std::cout << "Error: " << e.what() << "\n";
+        return 2;
+      }
+    }
+  }
+  while (num != 0);
+  std::cout << counter.get_count() <<  "\n";
   return 0;
 }
-
-bool quotientOfNumbers(int a,int b){
-  if (a % b == 0) {
-    return true;
-  }
-  return false;
-}; 
