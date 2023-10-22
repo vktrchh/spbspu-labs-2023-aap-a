99f0e1dec23e63367a08c0d2e78e6b1eb2377902
Rail (Susanoo) <zhalilov.rsh@edu.spbstu.ru>
2023-10-22 20:29:07 +0300

    Fixed input checking

--- zhalilov.rail/P1/main.cpp
+++ zhalilov.rail/P1/main.cpp
@@ -9,13 +9,14 @@ int main()
   Counter counter;
   while(std::cin >> curr && curr)
   {
-    if(!std::cin)
-    {
-      std::cout << "Bad input";
-      return 1;
-    }
     counter(prev, curr);
     prev = curr;
+  }
+
+  if(!std::cin.good())
+  {
+    std::cout << "Bad input";
+    return 1;
   }
 
   std::cout << counter();
