4b10866161d29438c36d0048cabd97adc8dd5170
osakasonyboy@gmail.com <sensh0god@gmail.com>
2023-10-22 17:40:51 +0300

    added a func for test the condition

--- arakelyan.daniil/P1/main.cpp
+++ arakelyan.daniil/P1/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <limits>
 
+bool quotientOfNumbers(int a,int b);
+
 int main()
 {
   size_t count = 0;
@@ -10,26 +12,33 @@ int main()
     int past = num;
     std::cin >> num;
     if(!std::cin){
-      std::cerr << "Not a num\n";
+      std::cerr << "Not a num.\n";
       return 1;
     }else if (num != 0)
     {
       size_t max_lenght  = std::numeric_limits<size_t>::max();
       if(lenght == max_lenght)
       {
-        std::cerr << "Sequence is too long\n";
+        std::cerr << "Sequence is too long.\n";
         return 2;
-      }else if(num % past == 0){
+      }else if(quotientOfNumbers(num,past)){
         ++count;
         ++lenght;
       }
     }
   }while (num != 0);
   if(count == 0){
-    std::cerr << "Sequence is too small\n";
+    std::cerr << "Sequence is too small.\n";
     return 2;
   }else{
     std::cout << count << "\n";
   }
   return 0;
 }
+
+bool quotientOfNumbers(int a,int b){
+  if(a % b == 0){
+    return true;
+  }
+  return false;
+}; 
