b1bc28306f57dc1d63addb7efee225021d2d622c
Steuger7 <steuger7@mail.ru>
2023-10-24 00:24:15 +0300

    Overlong sequence processing and cosmetic changes

--- grechishnikov.roman/P1/main.cpp
+++ grechishnikov.roman/P1/main.cpp
@@ -3,19 +3,25 @@
 
 int main()
 {
+  size_t max_length = std::numeric_limits<size_t>::max();
   size_t number_count = 0;
   long long number_min = std::numeric_limits<long long>::max();
-  int number_current = 0;
+  long long  number_current = 0;
   do
   {
     std::cin >> number_current;
     if (!std::cin)
     {
-      std::cerr << "Not a sequense\n";
+      std::cerr << "Not a sequence\n";
       return 1;
     }
     if (number_current != 0)
     {
+      if (number_count == max_length)
+      {
+        std::cout << "Sequence is too long\n";
+        return 2;
+      }
       if (number_current == number_min)
       {
         number_count += 1;
