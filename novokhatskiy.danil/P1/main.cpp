9a9a8aa1d0666ce296931aec824c717275fb884e
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2023-10-23 22:53:42 +0300

    Add struct

--- novokhatskiy.danil/P1/main.cpp
+++ novokhatskiy.danil/P1/main.cpp
@@ -2,22 +2,86 @@
 #include <limits>
 #include <stdexcept>
 
-int main() {
+struct SequenceCounter
+{
+  SequenceCounter():
+   count_(0)
+  {}
+  void count(int num, size_t max)
+  {
+    size_t max_size = std::numeric_limits< size_t >::max();
+    if (count_ == max_size)
+    {
+      throw std::logic_error("Sequence is too long");
+     }
+     if (num > max)
+     {
+	max = num;
+        count_ = 0;
+     }
+     else
+     {
+       ++count_;
+     }
+
+
+	}
+       size_t get_result() const
+       {
+         return count_;
+       }
+
+private:
+  size_t count_;
+};
+
+
+
+int main()
+{
+  SequenceCounter counter;
   size_t count = 0;
   int num = 0;
+  size_t max = 0;
   std::cin >> num;
-  if (!std::cin) {
-	  std::cerr << "It is not a number\n";
-	  return 1;
+  if (!std::cin)
+  {
+    std::cerr << "It is not a number\n";
+    return 1;
   }
-  while (num != 0) {
-	  std::cin >> num;
-	  if (!std::cin) {
-		  std::cerr << "It is not a number\n";
-		  return 1;
-	  }
-	  ++count;
+  while (num != 0)
+  {
+    if (num > max )
+    {
+      max = num;
+      count = 0;
+    }
+    else
+    {
+      ++count;
+     }
+
+    std::cin >> num;
+    if (!std::cin)
+    {
+      std::cerr << "It is not a number\n";
+      return 1;
+    }
+    else if (num != 0)
+    {
+      try
+      {
+        counter.count(num,max);
+      }
+      catch (const std::exception& e)
+      {
+        std::cerr << "Error: " << e.what() << "\n";
+        return 2;
+      }
+
+    }
   }
-  std::cout << count << "\n";	
+  std::cout << counter.get_result() << "\n";
   return 0;
 }
+
