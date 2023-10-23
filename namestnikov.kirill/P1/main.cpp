c657e5dfa20893fffbc38a072f076ddcee69b6be
kirillnamestnikov <kir.namestnikov@gmail.com>
2023-10-23 11:52:54 +0300

    Input one number

--- namestnikov.kirill/P1/main.cpp
+++ namestnikov.kirill/P1/main.cpp
@@ -1,2 +1,17 @@
+#include <iostream>
+
 int main()
-{}
+{
+  int lastnumber = 0;
+  std::cin >> lastnumber;
+  if (!std::cin)
+  {
+    std::cerr << "Is not a  sequence\n";
+    return 1;
+  }
+  else if (lastnumber == 0)
+  {
+    std::cerr << "There is no way to find a local min\n";
+    return 2;
+  }
+}
