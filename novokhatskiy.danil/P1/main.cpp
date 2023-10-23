940fec055cc0bc66e76b212e738df5e9b1583176
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2023-10-23 18:55:11 +0300

    Add test cycle while

--- novokhatskiy.danil/P1/main.cpp
+++ novokhatskiy.danil/P1/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <limits>
+#include <stdexcept>
 
-int main()
-{
+int main() {
+  size_t count = 0;
+  int num = 0;
+  std::cin >> num;
+  if (!std::cin) {
+	  std::cerr << "It is not a number\n";
+	  return 1;
+  }
+  while (num != 0) {
+	  std::cin >> num;
+	  if (!std::cin) {
+		  std::cerr << "It is not a number\n";
+		  return 1;
+	  }
+	  ++count;
+  }
+  std::cout << count << "\n";	
   return 0;
-]
+}
