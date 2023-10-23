1d21a00f447d68f8f52c14cc5e622ae2a156bb77
SlivkaSadovka <strelyaev_ra@spbstu.ru>
2023-10-23 12:16:22 +0300

    hotfix

--- strelyaev.roman/P1/main.cpp
+++ strelyaev.roman/P1/main.cpp
@@ -1,34 +1,3 @@
-#include <iostream>
-#include "mondec.h"
-
 int main()
 {
-  using namespace strelyaev;
-  int currentNumber;
-  Mondec count;
-/* В голову ничего кроме самого обычно do-while цикла не пришло. Извиняйте */
-  do
-  {
-    std::cin >> currentNumber;
-    if (!std::cin)
-    {
-      std::cerr << "AN ERROR HAS OCCURRED: NOT A NUMBER\n";
-      return 1;
-    }
-    else if (currentNumber != 0)
-    {
-      try
-      {
-        count(currentNumber);
-        count();
-      }
-      catch (std::exception & e)
-      {
-        std::cerr << "ERROR: " << e.what() << "\n";
-        return 2;
-      }
-    }
-  }
-  while (currentNumber != 0);
-  std::cout << count.resultLog() << "\n";
 }
