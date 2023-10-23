d88373b103f10f893dd6113733fa78a42c1423a3
Steuger7 <steuger7@mail.ru>
2023-10-24 00:07:28 +0300

    single-file solution

--- grechishnikov.roman/P1/main.cpp
+++ grechishnikov.roman/P1/main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
+#include <limits>
 
 int main()
 {
-  int namber_count = 0;
-  int namber_min = 0;
-  int namber_current = 0;
-  std::cin >> namber_current;
+  size_t number_count = 0;
+  long long number_min = std::numeric_limits<long long>::max();
+  int number_current = 0;
   do
   {
-    std::cin >> namber_current;
+    std::cin >> number_current;
     if (!std::cin)
     {
       std::cerr << "Not a sequense\n";
       return 1;
     }
+    if (number_current != 0)
+    {
+      if (number_current == number_min)
+      {
+        number_count += 1;
+      }
+      else if (number_current < number_min)
+      {
+        number_min = number_current;
+        number_count = 1;
+      }
+    }
   }
-  while (namber_current != 0);
+  while (number_current != 0);
+  std::cout << number_count << "\n";
 }
