5bafaa556a6d34ee1a2ebb41ff72462cfb4ef37e
kirillnamestnikov <kir.namestnikov@gmail.com>
2023-10-23 12:58:33 +0300

    Main function done

--- namestnikov.kirill/P1/main.cpp
+++ namestnikov.kirill/P1/main.cpp
@@ -14,4 +14,45 @@ int main()
     std::cerr << "There is no way to find a local min\n";
     return 2;
   }
+  int currentnumber = 0;
+  std::cin >> currentnumber;
+  if (!std::cin)
+  {
+    std::cerr << "Is not a  sequence\n";
+    return 1;
+  }
+  else if (currentnumber == 0)
+  {
+    std::cerr << "There is no way to find a local min\n";
+    return 2;
+  }
+  int futurenumber = 0;
+  std::cin >> futurenumber;
+  if (!std::cin)
+  {
+    std::cerr << "Is not a  sequence\n";
+    return 1;
+  }
+  else if (futurenumber == 0)
+  {
+    std::cerr << "There is no way to find a local min\n";
+    return 2;
+  }
+  size_t counter = 0;
+  while (futurenumber != 0)
+  {
+    if ((currentnumber < lastnumber) && (currentnumber < futurenumber))
+    {
+      ++counter;
+    }
+    lastnumber = currentnumber;
+    currentnumber = futurenumber;
+    std::cin >> futurenumber;
+    if (!std::cin)
+    {
+      std::cerr << "Is not a sequence\n";
+      return 1;
+    }
+  }
+  std::cout << counter << std::endl;
 }
