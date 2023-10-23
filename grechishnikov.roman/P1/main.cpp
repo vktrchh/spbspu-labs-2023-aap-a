ed9f22a265cbb625a188a8bdf69a8bdc0389162a
Steuger7 <steuger7@mail.ru>
2023-10-23 21:55:53 +0300

    basic processing of the entered number

--- grechishnikov.roman/P1/main.cpp
+++ grechishnikov.roman/P1/main.cpp
@@ -1,2 +1,14 @@
+#include <iostream>
+
 int main()
-{}
+{
+  int namber_count = 0;
+  int namber_min = 0;
+  int namber_current = 0;
+  std::cin >> namber_current;
+  if (!std::cin)
+  {
+    std::cerr << "Not a sequense\n";
+    return 1;
+  }
+}
