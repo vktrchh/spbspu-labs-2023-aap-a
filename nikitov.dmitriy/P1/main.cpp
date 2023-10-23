6150142ee955990025f351c379331856c3c935a3
Dmitriy <Dmitry5335@yandex.ru>
2023-10-23 21:18:16 +0300

    Added input cycle

--- nikitov.dmitriy/P1/main.cpp
+++ nikitov.dmitriy/P1/main.cpp
@@ -3,9 +3,12 @@
 int main()
 {
   int number = 0;
-  std::cin >> number;
-  if (!std::cin){
-    std::cerr << "This is not a sequence\n";
-    return 1;
+  do{
+    std::cin >> number;
+    if (!std::cin){
+      std::cerr << "This is not a sequence\n";
+      return 1;
+    }
   }
+  while (number != 0);
 }
