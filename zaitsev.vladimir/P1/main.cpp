c97ed24499e03020e4ee1f038743adacb188291c
v-zaycev <70699650+v-zaycev@users.noreply.github.com>
2023-10-23 18:17:09 +0300

    Input with verification is added

--- zaitsev.vladimir/P1/main.cpp
+++ zaitsev.vladimir/P1/main.cpp
@@ -1,2 +1,17 @@
+#include <iostream>
+
 int main()
-{}
+{
+	int curr_element = 1;
+
+	while ((std::cin >> curr_element) && curr_element != 0)
+	{}
+
+	if (!std::cin)
+	{
+		std::cerr << "The input data is not a sequence\n";
+		return 1;
+	}
+
+	return 0;
+}
