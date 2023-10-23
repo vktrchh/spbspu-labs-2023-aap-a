d0a4b560a8a8068b8c9f5e953d012c8997ae945f
v-zaycev <70699650+v-zaycev@users.noreply.github.com>
2023-10-23 19:08:00 +0300

    Sequence processing is added

--- zaitsev.vladimir/P1/main.cpp
+++ zaitsev.vladimir/P1/main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <algorithm>
 
 int main()
 {
   int curr_element = 1;
+  int seq_beg_element = 0;
+  int curr_len = 0;
+  int max_len = 0;
 
   while ((std::cin >> curr_element) && curr_element != 0)
   {
+    if (curr_element == seq_beg_element)
+    {
+      ++curr_len;
+    }
+    else
+    {
+      max_len = std::max(max_len, curr_len);
+      curr_len = 1;
+      seq_beg_element = curr_element;
+    }
   }
 
   if (!std::cin)
@@ -13,6 +27,8 @@ int main()
     std::cerr << "The input data is not a sequence\n";
     return 1;
   }
+  
+  std::cout << std::max(curr_len, max_len) << '\n';
 
   return 0;
 }
