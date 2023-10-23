14c01c21e502d1f95f783ee7f7639b27f81756a4
Dmitriy <Dmitry5335@yandex.ru>
2023-10-23 21:56:30 +0300

    Added function to check second maximum of sequence

--- nikitov.dmitriy/P1/main.cpp
+++ nikitov.dmitriy/P1/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include <limits>
 
+void isMax (int now, int& actual, int& pre)
+{
+  if (now >= actual){
+    pre = actual;
+    actual = now;
+  }
+}
+
 int main()
 {
   int number = 0;
+  int actual_max = 0;
+  int &act = actual_max;
+  int pre_max = 0;
+  int &pre = pre_max;
   size_t count = 0;
   do{
     std::cin >> number;
@@ -18,10 +30,14 @@ int main()
         return 2;
       }
       ++count;
+      isMax(number, act, pre);
     }
   }
   while (number != 0);
   if (count == 0 || count == 1){
     std::cerr << "This sequence is too short\n";
+    return 2;
   }
+  std::cout << pre_max << '\n';
+  return 0;
 }
