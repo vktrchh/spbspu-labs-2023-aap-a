ffb86e5b65002856174e80804d12e4b1f36e6238
Z0ltaan <gregnov69@gmail.com>
2023-10-24 20:18:06 +0300

    added input and input check

--- isaychev.vladislav/P1/main.cpp
+++ isaychev.vladislav/P1/main.cpp
@@ -1,4 +1,21 @@
+#include <iostream>
+
 int main()
 {
-
+  int c_number = 0;
+  std::cin >> c_number;
+  if (!std::cin)
+  {
+   std::cerr << "not a number was inputed\n";
+   return 1; 
+  }
+  while (c_number != 0)
+  {
+    std::cin >> c_number;
+    if (!std::cin)
+    {
+      std::cerr << "not a number was inputed\n"; 
+      return 1; 
+    }
+  }
 }
