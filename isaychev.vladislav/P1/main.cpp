35cd88f4732702279c525a5fb113d011aa461a8b
Z0ltaan <gregnov69@gmail.com>
2023-10-24 22:00:20 +0300

    prototype without classes in main only

--- isaychev.vladislav/P1/main.cpp
+++ isaychev.vladislav/P1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 
 int main()
 {
@@ -7,15 +8,42 @@ int main()
   if (!std::cin)
   {
    std::cerr << "not a number was inputed\n";
-   return 1; 
+   return 1;
   }
+  int p_number = 0;
+  unsigned long long int count = 0, max_count = 0;
   while (c_number != 0)
   {
-    std::cin >> c_number;
     if (!std::cin)
     {
-      std::cerr << "not a number was inputed\n"; 
-      return 1; 
+      std::cerr << "not a number was inputed\n";
+      return 1;
     }
+    else if (std::cin)
+    {
+      if (p_number != 0 && (p_number % 2 == 0 && c_number % 2 == 0))
+      {
+        ++count;
+        if (count > max_count)
+        {
+          max_count = count;
+        }
+      }
+      else if (p_number != 0 && (p_number % 2 != 0 || c_number % 2 != 0))
+      {
+        count = 0;
+      }
+      p_number = c_number;
+    }
+    std::cin >> c_number;
+  }
+  const int ptn = 1;
+  if (max_count > 0)
+  {
+    std::cout << max_count + ptn << "\n";
+  }
+  else
+  {
+    std::cout << max_count << "\n";
   }
 }
