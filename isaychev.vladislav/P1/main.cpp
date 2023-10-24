990f17330a18e02855b91859d1d2b4d54f11184d
Z0ltaan <gregnov69@gmail.com>
2023-10-24 23:52:18 +0300

    added max length of sequence check

--- isaychev.vladislav/P1/main.cpp
+++ isaychev.vladislav/P1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 
 int main()
 {
@@ -12,14 +13,20 @@ int main()
   unsigned long long int count = 0, max_count = 0;
   while (c_number != 0)
   {
-    if (c_number % 2 == 0) 
+    unsigned long long int u_border = std::numeric_limits <unsigned long long int>::max();
+    if (count == u_border)
+    {
+      std::cerr << "sequence is too long\n";
+      return 2;
+    }
+    else if (c_number % 2 == 0)
     {
       ++count;
       if (count > max_count)
       {
         max_count = count;
       }
-    }  
+    }
     else if (c_number % 2 != 0)
     {
       count = 0;
@@ -29,7 +36,7 @@ int main()
     {
       std::cerr << "not a number was inputed\n";
       return 1;
-    } 
+    }
   }
   std::cout << max_count << "\n";
 }
