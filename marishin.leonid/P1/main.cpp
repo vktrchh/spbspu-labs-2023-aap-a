720387bd149996a31a427b829edf67edd72111cc
marlions <marishin.lv@edu.spbstu.ru>
2023-10-24 23:14:25 +0300

    add class

--- marishin.leonid/P1/main.cpp
+++ marishin.leonid/P1/main.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include <limits>
+#include <stdexcept>
+
+namespace marishin {
+  class Sequence {
+  
+  }
+}
 
 int main() {
   long long int num;
@@ -7,10 +15,11 @@ int main() {
     std::cerr << "Is not a sequence\n";
     return 1;
   }
-  while (num != 0) {
+  while (true) {
     if (!std::cin) {
       std::cerr << "Is not a sequence\n";
       return 1;
     }
+    std::cin >> num;
   }
 }
