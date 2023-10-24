38a9f8374bf29de7d363471a963ea806a18e5cc8
Steuger7 <steuger7@mail.ru>
2023-10-24 22:44:56 +0300

    multi-file solution and operator overloading

--- grechishnikov.roman/P1/main.cpp
+++ grechishnikov.roman/P1/main.cpp
@@ -1,38 +1,33 @@
 #include <iostream>
-#include <limits>
-
+#include <stdexcept>
+#include <cstddef>
+#include "sequence.hpp"
+ 
 int main()
 {
-  size_t max_length = std::numeric_limits<size_t>::max();
-  size_t number_count = 0;
-  long long number_min = std::numeric_limits<long long>::max();
-  long long  number_current = 0;
+  sequence seq;
+  long long num;
   do
   {
-    std::cin >> number_current;
+    std::cin >> num;
     if (!std::cin)
     {
       std::cerr << "Not a sequence\n";
       return 1;
     }
-    if (number_current != 0)
+    if (num != 0)
     {
-      if (number_count == max_length)
+      try
       {
-        std::cout << "Sequence is too long\n";
+        seq.doCount(num);
+      }
+      catch (const std::exception & e)
+      {
+        std::cerr << "Error: " << e.what() << "\n";
         return 2;
-      }
-      if (number_current == number_min)
-      {
-        number_count += 1;
-      }
-      else if (number_current < number_min)
-      {
-        number_min = number_current;
-        number_count = 1;
       }
     }
   }
-  while (number_current != 0);
-  std::cout << number_count << "\n";
+  while (num != 0);
+  std::cout << seq.getRez() << "\n";
 }
---
+++ grechishnikov.roman/P1/sequence.cpp
@@ -0,0 +1,34 @@
+#include "sequence.hpp"
+#include <stdexcept>
+ 
+void sequence::doCount(long long num)
+{
+  if (num == number_min)
+  {
+    sequence::changeNum();
+  }
+  if (num < number_min)
+  {
+    sequence::changeNum(num);
+  }
+}
+ 
+void sequence::changeNum(long long num)
+{
+  number_min = num;
+  number_count = 1;
+}
+ 
+void sequence::changeNum()
+{
+  if (number_count == max_length)
+  {
+    throw std::overflow_error("sequence is too long");
+  }
+  number_count += 1;
+}
+ 
+int sequence::getRez()
+{
+  return number_count;
+}
