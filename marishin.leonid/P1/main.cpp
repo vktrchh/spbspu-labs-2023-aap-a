24ccdf1942d75806a221dbf84b1065c4630e70c5
marlions <marishin.lv@edu.spbstu.ru>
2023-10-24 23:35:40 +0300

    size_t

--- marishin.leonid/P1/main.cpp
+++ marishin.leonid/P1/main.cpp
@@ -2,21 +2,39 @@
 #include <limits>
 #include <stdexcept>
 
-namespace marishin {
-  class Sequence {
-  
+namespace marishin
+{
+  class Sequence
+  {
+  public:
+    void operator()(int num)
+    {
+      size_t max_size = std::numeric_limits< size_t >::max();
+      if (global_count == max_size)
+      {
+        throw std::logic_error("sequence is too long");
+      }
+    }
+    size_t operator()() const
+    {
+      return global_count;
+    }
   }
 }
 
-int main() {
+int main()
+{
   long long int num;
   std::cin >> num;
-  if (!std::cin) {
+  if (!std::cin)
+  {
     std::cerr << "Is not a sequence\n";
     return 1;
   }
-  while (true) {
-    if (!std::cin) {
+  while (true)
+  {
+    if (!std::cin)
+    {
       std::cerr << "Is not a sequence\n";
       return 1;
     }
