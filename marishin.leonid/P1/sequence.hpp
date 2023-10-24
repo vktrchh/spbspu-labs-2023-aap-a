1026d86a1b22e8a32f217a7d7edb98282538782e
marlions <marishin.lv@edu.spbstu.ru>
2023-10-25 00:17:50 +0300

    add hpp

---
+++ marishin.leonid/P1/sequence.hpp
@@ -0,0 +1,21 @@
+#ifndef SEQUENCE_HPP
+#define SEQUENCE_HPP
+
+#include <cstddef>
+namespace marishin
+{
+  class Sequence
+  {
+  public:
+    Sequence();
+    void operator()(int num);
+    size_t operator()() const;
+    size_t get_global_count() const;
+  private:
+    size_t count;
+    size_t global_count;
+    long long int prev1;
+    long long int prev2;
+  };
+}
+#endif
