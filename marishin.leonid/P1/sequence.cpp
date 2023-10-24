485d5ada0923deddb733c43d19458f7d6279cf6e
marlions <marishin.lv@edu.spbstu.ru>
2023-10-25 00:32:11 +0300

    sequence processing

--- marishin.leonid/P1/sequence.cpp
+++ marishin.leonid/P1/sequence.cpp
@@ -10,3 +10,18 @@ marishin::Sequence::Sequence():
   count(0)
 {}
 
+void marishin::Sequence::operator()(int num)
+{
+  size_t max_size = std::numeric_limits< size_t >::max();
+  if (global_count == max_size)
+  {
+    throw std::logic_error("sequence is too long");
+  }
+  if ((global_count > 2) && (num == prev1 + prev2))
+  {
+    count++;
+  }
+  prev2 = prev1;
+  prev1 = num;
+  ++global_count;
+}
