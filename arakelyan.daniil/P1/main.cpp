ff07d1e7920de0577cf97ef816fc51b908d692e6
osakasonyboy@gmail.com <sensh0god@gmail.com>
2023-10-24 18:56:53 +0300

    итоговый вариант первой лабораторной работы (до просмотра ВА)

--- arakelyan.daniil/P1/main.cpp
+++ arakelyan.daniil/P1/main.cpp
@@ -1,41 +1,6 @@
 #include <iostream>
 #include "sequenceofnums.hpp"
 
-
-// класс последовательности
-/*
-struct SequensOfNums
-{
-  SequensOfNums():
-    count()
-  {}
-private:
-  size_t count;
-
-public:
-  bool lenghtCheck (size_t length,int curr) {
-    size_t max_length = std::numeric_limits<size_t>::max();
-    if (length == max_length) {
-      throw std::logic_error("Sequense is too long\n");
-    } else if ((length == 2 && curr == 0) || (curr == 0 && length == 1)) {
-      throw std::logic_error("Sequence is too short\n");
-    } else {
-      return 0;
-    }
-  }
-  void countOfque(int a,int b) {
-    if (a % b == 0) {
-      ++count;
-    }
-  }
-
-  size_t get_count() {
-    return count;
-  }
-};
-*/
-
-
 int main()
 {
   int currentNum = 0;
