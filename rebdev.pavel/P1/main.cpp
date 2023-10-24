2ad011985a1ce2d5e3c36e525e50e9d107b081bc
User1235321 <pavreb1235321@gmail.com>
2023-10-24 19:07:53 +0300

    Initial commit for P1

---
+++ rebdev.pavel/P1/main.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+
+int main()
+{
+  int arrayElements[3] = {0, 0, 0};
+  for (int i = 0; i < 3; ++i)
+  {
+    std::cin >> arrayElements[i];
+    if (std::cin.fail() || (arrayElements[i] == 0))
+    {
+      std::cout << "0\n";
+      return 0;
+    }
+  }
+  
+  int counter = 0;
+  while (!std::cin.eof() && (arrayElements[2] != 0))
+  {
+    counter += (arrayElements[0] > arrayElements[1]) && (arrayElements[1] > arrayElements[2]);
+    arrayElements[0] = arrayElements[1];
+    arrayElements[1] = arrayElements[2];  	
+    std::cin >> arrayElements[2];
+  }
+  
+  if (arrayElements[2] == 0)
+  {
+    std::cout << counter << '\n';
+    return 0;
+  }
+  else
+  {
+    std::cout << "0\n";
+    return 0;
+  }
+}
