d9fd866f3dee9e69add1475a507186d73188887c
Z0ltaan <gregnov69@gmail.com>
2023-10-24 23:14:19 +0300

    tried different approach and fixed input check mistakes

--- isaychev.vladislav/P1/main.cpp
+++ isaychev.vladislav/P1/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <limits>
 
 int main()
 {
@@ -7,43 +6,32 @@ int main()
   std::cin >> c_number;
   if (!std::cin)
   {
-   std::cerr << "not a number was inputed\n";
-   return 1;
+    std::cerr << "not a number was inputed\n";
+    return 1;
   }
-  int p_number = 0;
   unsigned long long int count = 0, max_count = 0;
   while (c_number != 0)
   {
+    if (c_number % 2 == 0) 
+    {
+      ++count;
+      if (count > max_count)
+      {
+        max_count = count;
+      }
+    }  
+    else if (c_number % 2 != 0)
+    {
+      count = 0;
+    }
+    std::cin >> c_number;
     if (!std::cin)
     {
       std::cerr << "not a number was inputed\n";
       return 1;
-    }
-    else if (std::cin)
-    {
-      if (p_number != 0 && (p_number % 2 == 0 && c_number % 2 == 0))
-      {
-        ++count;
-        if (count > max_count)
-        {
-          max_count = count;
-        }
-      }
-      else if (p_number != 0 && (p_number % 2 != 0 || c_number % 2 != 0))
-      {
-        count = 0;
-      }
-      p_number = c_number;
-    }
-    std::cin >> c_number;
+    } 
   }
-  const int ptn = 1;
-  if (max_count > 0)
-  {
-    std::cout << max_count + ptn << "\n";
-  }
-  else
-  {
-    std::cout << max_count << "\n";
-  }
+  std::cout << max_count << "\n";
 }
+
+
