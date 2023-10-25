8f4406c669ff89f35cd2776f93feabd1aac3f695
jiggerman <liggerman@mail.ru>
2023-10-26 00:48:24 +0300

    Add class

--- chistyakov.vladislav/P1/main.cpp
+++ chistyakov.vladislav/P1/main.cpp
@@ -1,45 +1,98 @@
-#include<iostream>
+#include <iostream>
+#include <limits>
+#include <stdexcept>
+
+struct SequenceNumOfMax
+{
+  SequenceNumOfMax():
+    counter(0)
+  {}
+
+  void start_max(size_t nowNum)
+  {
+    maxNum = nowNum;
+    counter = 1;
+  }
+
+  void count(size_t nowNum)
+  {
+    size_t max_size = std::numeric_limits< size_t >::max();
+    if (counter == max_size)
+    {
+      throw std::logic_error("sequence is too long\n");
+    }
+    else if (maxNum == max_size)
+    {
+      throw std::logic_error("max numder is too big\n");
+    }
+    else if (nowNum == max_size)
+    {
+      throw std::logic_error("input numder is too big\n");
+    }
+
+    if (nowNum > maxNum)
+    {
+      maxNum = nowNum;
+      counter = 1;
+    }
+    else if (nowNum == maxNum)
+    {
+      ++counter;
+    }
+  }
+  
+  size_t get_result() const
+  {
+    return counter;
+  }
+
+private:
+  size_t counter;
+  size_t maxNum;
+  size_t nowNum;
+};
+
 
 int main()
 {
-  int max_num = 0;;
-  int num = 0;
+  int nowNum = 0;
+  std::cin >> nowNum;
 
-  std::cin >> num;
-  // Input first num and put it in max_num
-  if (!std::cin){
-      std::cout << "Bad input, try again!\n";
-      return 1;
-  }
-  if (num == 0){
-      std::cout << "There are no numbers in the sequence\n";
-      return 2;
-  }
-
-  max_num = num;
-  int counter = 1;
-
-  while (true)
+  if (!std::cin)
   {
-    std::cin >> num;
+    std::cerr << "Bad input, try again\n";
+    return 1;
+  }
+  else if (nowNum == 0)
+  {
+    std::cout << "There are no numbers in the sequence, the number of maximum numbers: 0\n"; 
+    return 0;
+  }
+  SequenceNumOfMax counter;
+  counter.start_max(nowNum);
 
-    if (!std::cin){
-      std::cout << "Bad input, try again!\n";
+  do
+  {
+    std::cin >> nowNum;
+    if (!std::cin)
+    {
+      std::cerr << "Bad input, try again\n";
       return 1;
     }
-
-    if (num == max_num){
-      counter += 1;
-    }
-    else if (num > max_num){
-      max_num = num;
-      counter = 1;
-    }
-
-    if (num == 0){
-      std::cout << counter << "\n";
-      break;
+    else if (nowNum != 0)
+    {
+      try
+      {
+        counter.count(nowNum);
+      }
+      catch (const std::exception & e)
+      {
+        std::cerr << "Error: " << e.what() << "\n";
+        return 2;
+      }
     }
   }
+  while (nowNum != 0);
+  std::cout << counter.get_result() << "\n";
   return 0;
 }
