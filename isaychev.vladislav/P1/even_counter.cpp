fda5b840f256d130461959b4ca313ebe99c1ddb9
Z0ltaan <gregnov69@gmail.com>
2023-10-25 23:22:57 +0300

    fixed wrong names

--- isaychev.vladislav/P1/even_counter.cpp
+++ isaychev.vladislav/P1/even_counter.cpp
@@ -3,14 +3,14 @@
 #include <stdexcept>
 
 isaychev::Even_counter::Even_counter():
-  count(0),
-  max_count(0)
+  count_(0),
+  max_count_(0)
 {}
 
 void isaychev::Even_counter::operator()(int c_num)
 {
   unsigned long long int u_border = std::numeric_limits <unsigned long long int>::max();
-  if (count == u_border)
+  if (count_ == u_border)
   {
     throw std::logic_error("sequence is too long");
   }
