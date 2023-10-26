f1b030e029e8e76c8405807e2fc86e4aaf62c994
MayaSakovskaya <zloyejik05@gmail.com>
2023-10-27 00:20:54 +0300

    Count limit

--- sakovskaia.maiia/P1/main.cpp
+++ sakovskaia.maiia/P1/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <limits>
 int main()
 {
   int number = 0;
@@ -14,6 +14,12 @@ int main()
     }
     else if (number != 0)
     {
+      size_t max_size = std::numeric_limits< size_t >::max();
+      if (count == max_size)
+      {
+        std::cerr << "Sequence is too long\n";
+        return 2;
+      }
       ++count;
     }
   }
