90a84d0ba1c74762f75fd63ff2a4d78b6895fd6f
puzz13 <puzz13one@gmail.com>
2023-10-26 20:45:09 +0300

    Count sequence

--- kovtun.alexander/P1/main.cpp
+++ kovtun.alexander/P1/main.cpp
@@ -2,5 +2,24 @@
 
 int main()
 {
+    int number = 0;
+    size_t size = 0;
 
+    do 
+    {
+        std::cin >> number;
+        if (!std::cin) {
+            std::cerr << "sequence is not valid\n";
+            return 1;
+        }
+
+        if (number != 0) {
+            size++;
+        }
+    }
+    while (number != 0);
+    
+    std::cout << size << std::endl;
+
+    return 0;
 }
