4134b496337c7a7342b8d1347ee3fb2c9a647402
MayaSakovskaya <zloyejik05@gmail.com>
2023-10-27 00:13:34 +0300

    Count sequence

--- sakovskaia.maiia/P1/main.cpp
+++ sakovskaia.maiia/P1/main.cpp
@@ -3,6 +3,7 @@
 int main()
 {
   int number = 0;
+  size_t count = 0;
   do
   {
     std::cin >> number;
@@ -11,6 +12,11 @@ int main()
       std::cerr << "Is not a sequence\n";
       return 1;
     }
+    else if (number != 0)
+    {
+      ++count;
+    }
   }
   while (number != 0);
+  std::cout << count << "\n";
 }
