71b112bf11747a324c492cf5f3fd0edd2ee0ef86
pashko2303 <baranov-p.b@yandex.ru>
2023-10-26 21:41:41 +0300

    Add reaction for too short sequences

--- baranov.pavel/P1/main.cpp
+++ baranov.pavel/P1/main.cpp
@@ -2,15 +2,42 @@
 
 int main()
 {
-  long long int number = 0;
+  long long int prevNumber = 1;
+  long long int currentNumber = 1;
+  long long int nextNumber = 1;
+
+  std::cin >> prevNumber;
+  if (!std::cin)
+  {
+    std::cerr << "Is not a sequence\n";
+    return 1;
+  }
+  else if (prevNumber == 0)
+  {
+    std::cerr << "A sequence is too short \n";
+    return 2;
+  }
+
+  std::cin >> currentNumber;
+  if (!std::cin)
+  {
+    std::cerr << "Is not a sequence\n";
+    return 1;
+  }
+  else if (currentNumber == 0)
+  {
+    std::cout << 0 << '\n';
+    return 0;
+  }
+
   do
   {
-    std::cin >> number;
+    std::cin >> nextNumber;
     if (!std::cin)
     {
       std::cerr << "Is not a sequence\n";
       return 1;
     }
   }
-  while (number != 0);
+  while (nextNumber != 0);
 }
