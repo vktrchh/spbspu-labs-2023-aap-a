5c8396622d2a5393d102739c07e447550c42f0cc
jiggerman <liggerman@mail.ru>
2023-10-26 20:50:48 +0300

    Add grtlss.hpp

--- chistyakov.vladislav/P1/grtlss.hpp
+++ chistyakov.vladislav/P1/grtlss.hpp
@@ -1,1 +1,19 @@
+#ifndef GRTLSS_HPP
+#define GRTLSS_HPP
 
+#include <cstddef>
+struct grt-lss
+{
+  grt-lss();
+  void firstNumber(size_t number);
+  void middleNumber(size_t number);
+  void countGRT(size_t number);
+  size_t get_result-grt() const;
+private:
+  size_t fstNum;
+  size_t midNum;
+  size_t endNum;
+  size_t counter;
+}
+
+#endif
