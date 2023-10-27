6bf6f89c48b158d41e86e66610d4382fcb0254b1
WarriorVortex <erohinvova01010111@gmail.com>
2023-10-27 13:25:53 +0300

    added handling input of non-numbers

--- erohin.vladimir/P1/main.cpp
+++ erohin.vladimir/P1/main.cpp
@@ -6,6 +6,15 @@ int main() {
   do
   {
     std::cin >> current;
+    if (!std::cin)
+    {
+      std::cerr << "Input data is not a number sequence\n";
+      return 1;
+    }
+    else if (current != 0)
+    {
+      
+    }
   }
   while (current != 0);
 }
