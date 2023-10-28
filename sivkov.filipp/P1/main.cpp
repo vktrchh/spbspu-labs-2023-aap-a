be54266cd1fc0f03002225ef5d68a84286dcef8e
exxiiint <filipp.sivkov@yandex.ru>
2023-10-28 14:41:16 +0300

    count limits

--- sivkov.filipp/P1/main.cpp
+++ sivkov.filipp/P1/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-
+#include <limits>
 int main()
 {
   int num = 0;
-  int k;
+  size_t k =0;
   do
   {
     std::cin >> num;
@@ -12,6 +12,16 @@ int main()
       std::cerr << "is not a seq...\n";
       return 1;
     }
+    else if (num !=0)
+    {
+      size_t max_size = std::numeric_limits < size_t > ::max();
+      if (k == max_size)
+      {
+        std::cout << "seq is too long\n";
+        return 2;
+      }
+    }
   }
   while(num != 0);
+  std::cout << k << std::endl;
 }
