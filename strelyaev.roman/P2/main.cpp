b68c7b776b9b7c1dbf8b59fe26dfba02069db207
SlivkaSadovka <strelyaev_ra@spbstu.ru>
2023-10-29 19:44:02 +0300

    stoll function

--- strelyaev.roman/P2/main.cpp
+++ strelyaev.roman/P2/main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <fstream>
 
+
+
 int main(int argc, char * argv[])
 {
   std::fstream input(argv[2]);
   std::fstream output(argv[3]);
-  int i = 0;
-  input >> i;
+  size_t rows = 0;
+  size_t columns = 0;
+
+  try
+  {
+    int n = std::stoll(argv[1]);
+  }
+  catch(const std::invalid_argument &)
+  {
+    std::cerr << "UNABLE TO PARSE FIRST ARGUMENT";
+  }
+
+  input >> rows;
+  input >> columns;
   if (!input)
   {
     std::cerr << "CANNOT READ A NUMBER\n";
     return 2;
   }
-
-  output << i;
-
 }
