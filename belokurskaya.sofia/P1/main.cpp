976fe0d1c3f0d5cb4f6e4a7a47356c25e614bd38
sofabelokurskaya@yandex.ru <sofabelokurskaya@yandex.ru>
2023-10-29 18:09:14 +0300

    count sequence

--- belokurskaya.sofia/P1/main.cpp
+++ belokurskaya.sofia/P1/main.cpp
@@ -3,6 +3,8 @@
 int main()
 {
   int number = 0;
+  size_t count = 0;
+  int max = 0;
   do
   {
     std::cin >> number;
@@ -11,6 +13,19 @@ int main()
       std::cerr << " Is not a sequence\n";
       return 1;
     }
+    else if (number != 0)
+    {
+      if (number > max)
+        {
+          max = number;
+          count = 1;
+        }
+        else if (number == max)
+        {
+          ++count;
+        }
+    }
   }
   while (number != 0);
+  std::cout << count << "\n";
 }
