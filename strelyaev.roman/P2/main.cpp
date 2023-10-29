a82186ec092303ef1a3d5b1885ff45ec14ae552e
SlivkaSadovka <strelyaev_ra@spbstu.ru>
2023-10-30 02:03:28 +0300

    dynamic and satic array function (depends on argv[1])

--- strelyaev.roman/P2/main.cpp
+++ strelyaev.roman/P2/main.cpp
@@ -1,29 +1,59 @@
 #include <iostream>
 #include <fstream>
 
+void sMatrix (std::istream & in, int * a, int e)
+{
+  for (int i = 0; i < e; i++)
+  {
+    in >> a[i];
+  }
+}
+
+void dMatrix (std::istream & in, int * a, int e)
+{
+  for (int i = 0; i < e; i++)
+  {
+    in >> a[i];
+  }
+}
 
 
 int main(int argc, char * argv[])
 {
   std::fstream input(argv[2]);
-  std::fstream output(argv[3]);
-  size_t rows = 0;
-  size_t columns = 0;
 
-  try
-  {
-    int n = std::stoll(argv[1]);
-  }
-  catch(const std::invalid_argument &)
-  {
-    std::cerr << "UNABLE TO PARSE FIRST ARGUMENT";
-  }
+
+  int rows = 0;
+  int columns = 0;
+  int n = 0;
 
   input >> rows;
   input >> columns;
-  if (!input)
+  try 
   {
-    std::cerr << "CANNOT READ A NUMBER\n";
+    n = std::stoll(argv[1]);
+  }
+  catch (const std::invalid_argument &)
+  {
+    return 3;
+  }
+  
+  if (n == 1)
+  {
+    int matrix[rows * columns] = {};
+    sMatrix(input, matrix, rows*columns); 
+    std::cout << matrix[1 * columns + 0];
+  }
+  else if (n == 2)
+  {
+    int * matrix = new int[rows*columns];
+    dMatrix (input, matrix, rows*columns);
+    std::cout << matrix[1 * columns + 0];
+    delete [] matrix;
+  } 
+  else 
+  {
     return 2;
   }
+
 }
