b895c234867c49e6898c6fd2cb2d8358f4bb399b
ZakozhurnikovaKristina <kristinazak150@gmail.com>
2023-10-30 01:50:26 +0300

    before splitting into translation units

--- zakozhurnikova.kristina/P1/main.cpp
+++ zakozhurnikova.kristina/P1/main.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
 #include <limits>
+#include <stdexcept>
+
+struct SequenceCounter
+{
+  SequenceCounter():
+   counts(0)
+  {}
+
+  void count(int number)
+  {
+    int max = std::numeric_limits< int >::min();
+    size_t max_size = std::numeric_limits< size_t >::max();
+    if (counts == max_size)
+    {
+      throw std::logic_error("sequence is too long");
+    }
+    else if (number > max)
+    {
+      counts = 0;
+      max = number;
+    }
+    else
+    {
+      ++counts;
+    }
+  }
+  size_t get_result() const
+  {
+    return counts;
+  }
+private:
+  size_t counts;
+};
 int main ()
 {
   int number = 0;
-  size_t count = 0;
-  int max = std::numeric_limits< int >::min();
+  SequenceCounter counter;
   do
   {
     std::cin >> number;
@@ -15,23 +47,17 @@ int main ()
     }
     else if (number != 0)
     {
-      size_t max_size = std::numeric_limits< size_t >::max();
-      if (count == max_size)
+      try
       {
-        std::cerr << "Sequence is too long\n";
+        counter.count(number);
+      }
+      catch (const std::exception & e)
+      {
+        std::cerr << "Error: " << e.what() << "\n";
         return 2;
-      }
-      else if (number > max)
-      {
-        count = 0;
-        max = number;
-      }
-      else
-      {
-        ++count;
       }
     }
   }
   while (number != 0);
-  std::cout << count << "\n";
+  std::cout << counter.get_result() << "\n";
 }
