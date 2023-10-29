7f41d134bc13c587ad336fd1d9ea549948b97172
ZakozhurnikovaKristina <kristinazak150@gmail.com>
2023-10-29 18:13:57 +0300

    Cod with incorrect operation, outputs the same thing

--- zakozhurnikova.kristina/P1/main.cpp
+++ zakozhurnikova.kristina/P1/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <limits>
 int main ()
 {
   int number = 0;
+  size_t count = -1;
+  int max = 0;
   do
   {
     std::cin >> number;
@@ -10,6 +13,25 @@ int main ()
       std::cerr << "Is not sequence\n";
       return 1;
     }
+     else if (number != 0)
+     {
+       size_t max_size = std::numeric_limits< size_t >::max();
+       if (count == max_size)
+       {
+         std::cerr << "Sequence is too long\n";
+         return 2;
+       }
+       else if (number > max)
+       {
+         count = 0;
+         max = number;
+       }
+       else
+       {
+         ++count;
+       }
+     }
   }
   while (number != 0);
+  std::cout << count << "\n";
 }
