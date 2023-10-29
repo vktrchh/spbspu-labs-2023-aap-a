4ef822e299bc21ffcb281b582dfb44aa2e43e5d9
SofiyLop <lopatina.sa@edu.spbstu.ru>
2023-10-30 01:48:43 +0300

    Add do-while loop for sequence

--- lopatina.sofia/P1/main.cpp
+++ lopatina.sofia/P1/main.cpp
@@ -1,2 +1,13 @@
-int main()
-{}
+#include <iostream>
+
+int main() {
+  int number = 0;
+  do {
+    std::cin >> number;
+    if (!std::cin) {
+      std::cerr << "Is not a sequence\n";
+      return 1;
+    }
+  }
+  while (number != 0);
+}
