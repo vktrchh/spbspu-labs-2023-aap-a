c6ee435e8eddb33e66581cb6d4ade323dbe12fed
ZakozhurnikovaKristina <kristinazak150@gmail.com>
2023-10-29 22:54:21 +0300

    new code without a loop with "i"

--- zakozhurnikova.kristina/P1/main.cpp
+++ zakozhurnikova.kristina/P1/main.cpp
@@ -4,39 +4,33 @@ int main ()
 {
   int number = 0;
   size_t count = 0;
-  int max = 0;
-  int i = 0;
+  int max = std::numeric_limits< int >::min();
   do
   {
     std::cin >> number;
-    if (i<1)
-    {
-      max = number - 1;
-      i = 1;
-    }
     if (!std::cin)
     {
       std::cerr << "Is not sequence\n";
       return 1;
     }
-     else if (number != 0)
-     {
-       size_t max_size = std::numeric_limits< size_t >::max();
-       if (count == max_size)
-       {
-         std::cerr << "Sequence is too long\n";
-         return 2;
-       }
-       else if (number > max)
-       {
-         count = 0;
-         max = number;
-       }
-       else
-       {
-         ++count;
-       }
-     }
+    else if (number != 0)
+    {
+      size_t max_size = std::numeric_limits< size_t >::max();
+      if (count == max_size)
+      {
+        std::cerr << "Sequence is too long\n";
+        return 2;
+      }
+      else if (number > max)
+      {
+        count = 0;
+        max = number;
+      }
+      else
+      {
+        ++count;
+      }
+    }
   }
   while (number != 0);
   std::cout << count << "\n";
