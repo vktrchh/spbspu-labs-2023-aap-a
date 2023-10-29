562f22332500658e2f4fcdd9217f6098dfaac823
pashko2303 <baranov-p.b@yandex.ru>
2023-10-29 12:38:31 +0300

    Add class LocalMaxCounter

--- baranov.pavel/P1/main.cpp
+++ baranov.pavel/P1/main.cpp
@@ -1,62 +1,100 @@
 #include <iostream>
 #include <limits>
+#include <stdexcept>
+
+class LocalMaxCounter
+{
+public:
+  LocalMaxCounter(long long int firstNumber, long long int secondNumber);
+  void count(long long int number);
+  size_t get_result() const;
+private:
+  long long int prevNumber;
+  long long int currentNumber;
+  long long int nextNumber;
+  size_t count_;
+};
+
+LocalMaxCounter::LocalMaxCounter(long long int firstNumber, long long int secondNumber):
+  prevNumber(firstNumber),
+  currentNumber(secondNumber),
+  nextNumber(1),
+  count_(0)
+{}
+
+void LocalMaxCounter::count(long long int number)
+{
+  nextNumber = number;
+  if (currentNumber > prevNumber && currentNumber > nextNumber)
+  {
+    size_t max_size = std::numeric_limits< size_t >::max();
+    if (count_ == max_size)
+    {
+      throw std::logic_error("sequence is too long");
+    }
+    ++count_;
+  }
+prevNumber = currentNumber;
+currentNumber = nextNumber;
+}
+
+size_t LocalMaxCounter::get_result() const
+{
+  return count_;
+}
 
 int main()
 {
-  long long int prevNumber = 1;
-  long long int currentNumber = 1;
-  long long int nextNumber = 1;
-  size_t count = 0;
+  long long int firstNumber = 1;
+  long long int secondNumber = 1;
+  long long int number = 1;
 
-  std::cin >> prevNumber;
+  std::cin >> firstNumber;
   if (!std::cin)
   {
     std::cerr << "Is not a sequence\n";
     return 1;
   }
-  else if (prevNumber == 0)
+  else if (firstNumber == 0)
   {
     std::cerr << "A sequence is too short \n";
     return 2;
   }
 
-  std::cin >> currentNumber;
+  std::cin >> secondNumber;
   if (!std::cin)
   {
     std::cerr << "Is not a sequence\n";
     return 1;
   }
-  else if (currentNumber == 0)
+  else if (secondNumber == 0)
   {
     std::cout << 0 << '\n';
     return 0;
   }
-
+  LocalMaxCounter counter(firstNumber, secondNumber);
   do
   {
-    std::cin >> nextNumber;
+    std::cin >> number;
     if (!std::cin)
     {
       std::cerr << "Is not a sequence\n";
       return 1;
     }
-    else if (nextNumber != 0)
+    else if (number != 0)
     {
-      if (currentNumber > prevNumber && currentNumber > nextNumber)
+      try
       {
-        size_t max_size = std::numeric_limits< size_t >::max();
-        if (count == max_size)
-        {
-          std::cerr << "Sequence is too long\n";
-          return 2;
-        }
-        ++count;
+        counter.count(number);
+      }
+      catch (const std::exception & e)
+      {
+        std::cerr << "Error: " << e.what() << '\n';
+        return 2;
       }
     }
-  prevNumber = currentNumber;
-  currentNumber = nextNumber;
   }
-  while (nextNumber != 0);
+  while (number != 0);
 
-  std::cout << count << '\n';
+  std::cout << counter.get_result()  << '\n';
 }
