c6e8a6aef693309d8eb6335ac931fe33caf6e5c0
dmitei <dima9990860349@icloud.com>
2023-10-29 14:28:04 +0300

    Error test

--- shabalin.dmitrij/P1/main.cpp
+++ shabalin.dmitrij/P1/main.cpp
@@ -1,4 +1,13 @@
-int main() 
+#include <iostream>
+
+int main()
 {
+  int CurNum = 0;
+  std::cin >> CurNum;
+  if (!std::cin)
+  {
+    std::cerr << ("Is not a sequence\n");
+    return 1;
+  }
 
 }
