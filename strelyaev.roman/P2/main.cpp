8fa17e211afa218c084800940d7662a87cbdb549
SlivkaSadovka <strelyaev_ra@spbstu.ru>
2023-10-29 16:49:31 +0300

    file stream input

--- strelyaev.roman/P2/main.cpp
+++ strelyaev.roman/P2/main.cpp
@@ -1,2 +1,17 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char * argv[])
+{
+  std::fstream input(argv[2]);
+  int i = 0;
+  input >> i;
+  if (!input)
+  {
+    std::cerr << "CANNOT READ A NUMBER\n";
+    return 2;
+  }
+
+  std::cout << i << "\n";
+
+}
