a6bf78656544ce4603e256fdd0f72471691debc5
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2023-10-29 18:12:11 +0300

    add size

--- vyzhanov.daniil/P1/main.cpp
+++ vyzhanov.daniil/P1/main.cpp
@@ -3,14 +3,20 @@
 int main()
 {
   int num = 0;
+  size_t size = 0;
   do
   {
     std::cin >> num;
-    if(!std::cin)
+    if (!std::cin)
     {
       std::cerr << "Is not a sequance\n";
       return 1;
     }
+    else if (num != 0)
+    {
+      ++size;
+    }
   }
-  while(num != 0);
+  while (num != 0);
+  std::cout << size << "\n";
 }
