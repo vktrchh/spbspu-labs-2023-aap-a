024ff2a69f3237ae265b568c0773932d15d8506e
CA21lmFlame <chernov.ar@spbstu.ru>
2023-10-29 22:55:33 +0300

    add dowhile

--- chernov.artem/P1/main.cpp
+++ chernov.artem/P1/main.cpp
@@ -2,10 +2,14 @@
 int main()
 {
   int number = 0;
-  std::cin >> number;
-  if (!std::cin)
+  do
   {
-    std::cerr << "Is not a sequence\n";
-    return 1;
+    std::cin >> number;
+    if (!std::cin)
+    {
+      std::cerr << "Is not a sequence\n";
+      return 1;
+    }
   }
+  while (number != 0);
 }
