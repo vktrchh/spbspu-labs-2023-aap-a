ce61b92031ddda194d02a4f485db40a7343718cc
dmitei <dima9990860349@icloud.com>
2023-10-29 15:46:27 +0300

    sequence overflow

--- shabalin.dmitrij/P1/main.cpp
+++ shabalin.dmitrij/P1/main.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <limits>
+
+struct SequenceCounter
+{
+  SequenceCounter():
+   count_(0)
+  {}
+
+  void count(int)
+  {
+    size_t max_size = std::numeric_limits<size_t>::max();
+    if (count_ == max_size)
+    {
+      throw std::logic_error("sequence is too long");
+    }
+    ++count_;
+  }
+  size_t get_result() const
+  {
+    return count_;
+  }
+  private:
+  size_t count_;
+};
+
 
 int main()
 {
+  size_t count = 0;
   int CurNum = 0;
+  int first; 
+  int third;
+  int kount = 0;
+  SequenceCounter counter;
   do
   {
     std::cin >> CurNum;
@@ -10,6 +40,18 @@ int main()
     {
       std::cerr << ("Is not a sequence\n");
       return 1;
+      }
+    else if(CurNum != 0)
+    {
+      try 
+      {
+        counter.count(CurNum);
+      }
+      catch (const std::exception & e)
+      {
+        std::cerr << ("Error: ") << e.what() << "\n";
+        return 2;
+      }
     }
   }
   while (CurNum != 0);
