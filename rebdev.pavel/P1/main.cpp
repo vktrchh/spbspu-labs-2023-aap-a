de2880f46d2336bf667f65c398e78fb86c8d7ec5
User1235321 <pavreb1235321@gmail.com>
2023-10-30 22:07:33 +0300

    Correction in main.cpp[C

--- rebdev.pavel/P1/main.cpp
+++ rebdev.pavel/P1/main.cpp
@@ -5,51 +5,29 @@ int main()
 {
   using namespace rebdev;
 
-  int firstElement = 0, secondElement = 0;
-  std::cin >> firstElement;
+  int number = 0;
 
-  if (!std::cin)
+  ElementCounter objectOfElementCounter;
+
+  do
   {
-    std::cerr << "Input error!\n";
-    return 1;
-  }
-  else if (firstElement == 0)
-  {
-    std::cout << "0\n";
-    return 0;
-  }
-
-  std::cin >> secondElement;
-  if (!std::cin)
-  {
-    std::cerr << "Input error!\n";
-    return 1;
-  }
-
-  element_counter objectOfElementCounter(firstElement, secondElement);
-
-  while (secondElement != 0)
-  {
-    std::cin >> secondElement;
-
+    std::cin >> number;
     if (!std::cin)
     {
       std::cerr << "Input error!\n";
       return 1;
     }
-    else if (secondElement != 0)
+    try
     {
-      try
-      {
-        objectOfElementCounter(secondElement);
-      }
-      catch (const std::exception & e)
-      {
-        std::cerr << "Error: " << e.what() << '\n';
-        return 2;
-      }
+      objectOfElementCounter(number);
+    }
+    catch (const std::exception & e)
+    {
+      std::cerr << "Error: " << e.what() << '\n';
+      return 2;
     }
   }
+  while (number != 0);
 
   std::cout << objectOfElementCounter() << '\n';
   return 0;
