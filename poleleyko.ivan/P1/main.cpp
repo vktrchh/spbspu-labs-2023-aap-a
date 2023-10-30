c9c74b4596ad6b33761dee8404dda87d4dd76360
seedlair <poleleyko.ivan@gmail.com>
2023-10-30 20:12:30 +0300

    Добавляем проерку на пустую последовательность

--- poleleyko.ivan/P1/main.cpp
+++ poleleyko.ivan/P1/main.cpp
@@ -4,8 +4,17 @@ int main ()
 {
   std::cout << "Введите последовательность чисел: ";
   int num;
-  while (std::cin >> num){
+  bool hasInput = false;
 
+  while (std::cin >> num){
+    hasInput = true;
+    if (num == 0) {
+        if(!hasInput) {
+            std::cerr << "Ошибка! Пустая последовательность." << std::endl;
+            return 2;
+        }
+        break;
+    }
   }
 
   return 0;
