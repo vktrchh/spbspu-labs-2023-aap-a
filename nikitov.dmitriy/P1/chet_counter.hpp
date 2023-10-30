83f4ab490d00eb408b0da95a54b53dfc072ba785
Dmitriy <Dmitry5335@yandex.ru>
2023-10-30 18:01:32 +0300

    Added chet_counter function The return codes of my functions do not match, so chet_counter just outputs the answer without binding to the function of the second maximum

---
+++ nikitov.dmitriy/P1/chet_counter.hpp
@@ -0,0 +1,17 @@
+#include <cstddef>
+
+void chet_counter(int now, size_t& max, size_t& actual)
+{
+  if (now % 2 == 0)
+  {
+    ++actual;
+    if (actual > max){
+      max = actual;
+    }
+  }
+  else
+  {
+    actual = 0;
+  }
+}
+
--- nikitov.dmitriy/P1/main.cpp
+++ nikitov.dmitriy/P1/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include "sequence_counter.hpp"
 #include "ismax.hpp"
+#include "chet_counter.hpp"
 
 int main()
 {
   int number = 0;
   int actual_max = 0;
   int pre_max = 0;
+  size_t max_chet_count = 0;
+  size_t current_chet_count = 0;
   using namespace nikitov;
   SequenceCounter counter;
 
@@ -30,13 +33,15 @@ int main()
         return 2;
       }
       isMax(number, actual_max, pre_max);
+      chet_counter(number, max_chet_count, current_chet_count);
     }
   }
   while (number != 0);
 
+  std::cout << max_chet_count << '\n';
   if (counter() == 0 || counter() == 1)
   {
-    std::cerr << "Error: This sequence is too short\n";
+    std::cerr << "Error: This sequence is too short for SUB-MAX\n";
     return 2;
   }
   std::cout << pre_max << '\n';
