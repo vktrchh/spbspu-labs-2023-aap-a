5a9ae52b145864bb78bb00a69e2e370ba1d61d8d
seedlair <poleleyko.ivan@gmail.com>
2023-10-30 20:45:57 +0300

    Дополняем программу, чтобы она производила поиск максимального и второго по величине значения.

--- poleleyko.ivan/P1/main.cpp
+++ poleleyko.ivan/P1/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <climits>
 
 int main ()
 {
+  int max = INT_MIN;
+  int secMax = INT_MIN;
+
   std::cout << "Введите последовательность чисел: ";
   int num;
   bool hasInput = false;
@@ -9,13 +13,20 @@ int main ()
   while (std::cin >> num){
     hasInput = true;
     if (num == 0) {
-        if(!hasInput) {
-            std::cerr << "Ошибка! Пустая последовательность." << std::endl;
-            return 2;
-        }
-        break;
+      if(!hasInput) {
+        std::cerr << "Ошибка! Пустая последовательность." << std::endl;
+        return 2;
+      }
+      break;
+    }
+    if (num > max){
+      secMax = max;
+      max = num;
+
+    } else if(num > secMax && num != max){
+      secMax = num;
     }
   }
-
+  std::cout << secMax;
   return 0;
 }
