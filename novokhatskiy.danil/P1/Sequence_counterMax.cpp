47cd2b72c849e69a64991f31815983eb4168eddf
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2023-11-03 18:56:20 +0300

    Add const, changed names, changed if

---
+++ novokhatskiy.danil/P1/Sequence_counterMax.cpp
@@ -0,0 +1,29 @@
+#include <limits>
+#include <stdexcept>
+#include "Sequence_counterMax.hpp"
+
+novokhatskiy::SequenceCounterMax::SequenceCounterMax():
+  count_(0),
+  max_(0)
+{}
+void novokhatskiy::SequenceCounterMax::operator()(int num)
+{
+  const size_t max_size = std::numeric_limits< size_t >::max();
+  if (num > max_)
+  {
+    if (count_ == max_size)
+    {
+      throw std::logic_error("The sequence is too long!");
+    {
+    max_ = num;
+    count_ = 0;
+  }
+  else
+  {
+    ++count_;
+  }
+}
+size_t novokhatskiy::SequenceCounterMax::operator()() const
+{
+  return count_;
+}
--- novokhatskiy.danil/P1/main.cpp
+++ novokhatskiy.danil/P1/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stdexcept>
-#include "Sequence_counter.hpp"
+#include "Sequence_counterMax.hpp"
 
 int main()
 {
