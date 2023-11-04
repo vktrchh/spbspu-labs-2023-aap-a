1b0c769cdc1e44b6e4a7e8c2e85d6b20d49eac72
SlivkaSadovka <strelyaev_ra@spbstu.ru>
2023-11-04 23:14:04 +0300

    Subtraction funcion, minor CG fix, static array fix

--- strelyaev.roman/P2/main.cpp
+++ strelyaev.roman/P2/main.cpp
@@ -3,9 +3,17 @@
 #include <exception>
 #include "matrix.h"
 #include "checkargs.h"
+#include <algorithm>
 
+void matrixSubtraction(int * original, int * reverse, int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    original[i] -= reverse[i];
+  }
+}
 
-void printMatrix(int * a, int amount, int columns)
+void printMatrix(int * a, const int amount, const int columns)
 {
   for (int i = 0; i < amount; i++)
   {
@@ -17,29 +25,29 @@ void printMatrix(int * a, int amount, int columns)
   }
 }
 
-void Clockwise(int * a, int rows, int columns)
+void clockwise(int * a, int rows, int columns)
 {
-  int start = (rows-1) * columns + 0; 
+  int start = (rows-1) * columns + 0;
   int num = 1;
-  for (int delta = 0; delta < rows - 2; delta++)
+  for (int delta = 0; delta < std::max(rows - 2, 1); delta++)
   {
-  for (int i = (rows-1)*columns - delta*(columns-1); i > delta*(columns+1); i-=columns)
-  {
-    a[i] = num++;
-  }
-  for (int i  = 0 + delta*(columns+1); i < columns + delta*(columns-1); i+=1)
-  {
-    a[i] = num++;
-  }
-  --num;
-  for (int i  = (columns-1) + delta*(columns-1); i < rows*columns - delta*(columns+1) - 1; i+=columns)
-  {
-    a[i] = num++;
-  }
-  for (int i  = rows*columns - 1 - delta*(columns+1); i > (rows-1) * columns - delta*(columns-1); i-=1)
-  {
-    a[i] = num++;
-  }
+    for (int i = (rows-1)*columns - delta*(columns-1); i > delta*(columns+1); i-=columns)
+    {
+      a[i] = num++;
+    }
+    for (int i  = 0 + delta*(columns+1); i < columns + delta*(columns-1); i+=1)
+    {
+      a[i] = num++;
+    }
+    --num;
+    for (int i  = (columns-1) + delta*(columns-1); i < rows*columns - delta*(columns+1) - 1; i+=columns)
+    {
+      a[i] = num++;
+    }
+    for (int i  = rows*columns - 1 - delta*(columns+1); i > (rows-1) * columns - delta*(columns-1); i-=1)
+    {
+      a[i] = num++;
+    }
   }
 }
 
@@ -48,7 +56,7 @@ int main(int argc, char * argv[])
   std::fstream input(argv[2]);
 
   int n = 0;
-  try 
+  try
   {
     n = std::stoll(argv[1]);
     checkArgs(argc, argv, n, input);
@@ -63,7 +71,6 @@ int main(int argc, char * argv[])
     std::cerr << e.what() << "\n";
     return 2;
   }
-  
   int rows = 0;
   int columns = 0;
 
@@ -73,12 +80,14 @@ int main(int argc, char * argv[])
 
   if (n == 1)
   {
-    try 
+    try
     {
       int matrix[rows * columns] = {};
-      sMatrix(input, matrix, rows*columns); 
-      Clockwise(matrix, rows, columns);
-      printMatrix(matrix,rows*columns,columns);      
+      sMatrix(input, matrix, rows*columns);
+      int clock_matrix[rows*columns];
+      clockwise(clock_matrix, rows, columns);
+      matrixSubtraction(matrix, clock_matrix, rows*columns);
+      printMatrix(matrix, rows*columns, columns);
     }
     catch (const std::logic_error & e)
     {
@@ -96,12 +105,8 @@ int main(int argc, char * argv[])
     }
     catch(const std::logic_error& e)
     {
-      std::cerr << e.what() << '\n';
+      std::cerr << e.what() << "\n";
       return 2;
     }
-    
-  
-  } 
-  
-
+  }
 }
