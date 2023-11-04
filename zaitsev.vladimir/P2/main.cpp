395325a13cc1f3af0f78474d6ed958b1bc926d0f
v-zaycev <70699650+v-zaycev@users.noreply.github.com>
2023-11-04 17:49:24 +0300

    Initial checking for input file added

--- zaitsev.vladimir/P2/main.cpp
+++ zaitsev.vladimir/P2/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <string>
+#include <cstdlib>
 #include <stdexcept>
+#include <fstream>
 
 
 int main(int argc, char* argv[])
@@ -16,25 +17,54 @@ int main(int argc, char* argv[])
     return 1;
   }
 
-  int task_nmb = 1;
-  try
-  {
-    task_nmb = std::stoi(argv[1]);
-  }
-  catch (const std::out_of_range&)
-  {
-    std::cerr << "First parameter is out of range\n";
-    return 1;
-  }
-  catch (const std::logic_error&)
+  char* ptr = nullptr;
+  long task_nmb = std::strtol(argv[1], &ptr, 10);
+
+  if (task_nmb == 0 && ptr == argv[1])
   {
     std::cerr << "First parameter is not a number\n";
     return 1;
   }
-  if (task_nmb < 1 || task_nmb>2)
+  if (task_nmb > 2 || task_nmb < 1)
   {
     std::cerr << "First parameter is out of range\n";
     return 1;
+  }
+
+  std::ifstream input(argv[2]);
+  if (!input.is_open())
+  {
+    std::cerr << "The input file can't be opened\n";
+    return 2;
+  }
+
+  if (task_nmb == 1)
+  {
+    const size_t capacity = 10000;
+    double matrix[capacity];
+    for (size_t i = 0; i < capacity; ++i)
+    {
+      matrix[i] = 0;
+    }
+    size_t cols = 0;
+    size_t rows = 0;
+    input >> cols >> rows;
+    if (!input)
+    {
+      std::cerr << "Matrix can't be read\n";
+      return 2;
+    }
+  }
+  else
+  {
+    size_t cols = 0;
+    size_t rows = 0;
+    input >> cols >> rows;
+    if (!input)
+    {
+      std::cerr << "Matrix can't be read\n";
+      return 2;
+    }
   }
   return 0;
 }
