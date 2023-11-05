69092065bdd2942b500de0dd6373b24b27da803c
magggottt <gladyshevmatvey@gmail.com>
2023-11-06 00:52:17 +0700

    public0

--- gladyshev.matvey/P1/counterreversal.h
+++ gladyshev.matvey/P1/counterreversal.h
@@ -7,12 +7,11 @@ namespace gladyshev
 {
   struct CounterReversal
   {
-  public:
     CounterReversal();
     void operator()(long long int number_);
     size_t operator()() const;
-    size_t counter_sign_;
   private:
+    size_t counter_sign_;
     long long int number_;
     long long int sup_number_;
   };
