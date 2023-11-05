40d6f31bbe72dca0368a3e4b0488a94489005f3b
fncg <stepanov.gr@edu.spbstu.ru>
2023-11-05 23:14:40 +0300

    Count limit

--- stepanov.gena/P1/main.cpp
+++ stepanov.gena/P1/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 int main()
 {
   int number = 0;
@@ -8,11 +9,17 @@ int main()
     std::cin>>number;
     if (!std::cin)
     {
-      std::cerr<<"Is not a sequence\n";
+      std::cerr << "Is not a sequence\n";
       return 1;
     }
     else if (number != 0)
     {
+      size_t max_size = std::numeric_limits< size_t >::max();
+      if (count == max_size)
+      {
+        std::cerr << "Sequence is too long\n";
+        return 2;
+      }
       ++count;
     }
   }
