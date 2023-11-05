e1c484b4b47fb100295e871381bfd8f5b89cde8e
fncg <stepanov.gr@edu.spbstu.ru>
2023-11-06 02:39:12 +0300

    Sequence Counter Class

--- stepanov.gena/P1/main.cpp
+++ stepanov.gena/P1/main.cpp
@@ -1,28 +1,54 @@
 #include <iostream>
 #include <limits>
+#include <stdexcept>
+
+struct SequenceCounter
+{
+  SequenceCounter():
+   count_(0)
+  {}
+
+  void count(int)
+  {
+    size_t max_size = std::numeric_limits< size_t >::max();
+    if (count_ == max_size)
+    {
+      throw std::logic_error("Sequence is too long");
+    }
+    ++count_;
+  }
+  size_t get_result() const
+  {
+    return count_;
+  }
+private:
+  size_t count_;
+};
 int main()
 {
   int number = 0;
-  size_t count = 0; 
+  SequenceCounter counter;
   do
   {
-    std::cin>>number;
+    std::cin >> number;
     if (!std::cin)
     {
-      std::cerr << "Is not a sequence\n";
+      std::cerr <<"Is not a sequence\n";
       return 1;
     }
     else if (number != 0)
     {
-      size_t max_size = std::numeric_limits< size_t >::max();
-      if (count == max_size)
+      try
       {
-        std::cerr << "Sequence is too long\n";
+        counter.count(number);
+      }
+      catch (const std::exception & e)
+      {
+        std::cerr << "Error:" << e.what() << "\n";
         return 2;
       }
-      ++count;
     }
   }
-  while(number != 0);
-  std::cout<<count<<"\n";
+  while (number != 0);
+  std::cout << counter.get_result() << "\n";
 }
