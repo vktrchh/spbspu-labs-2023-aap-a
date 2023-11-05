a83d79e79362cacc62b5513b98e93c304db19aaf
fncg <stepanov.gr@edu.spbstu.ru>
2023-11-05 23:00:40 +0300

    Count sequence

--- stepanov.gena/P1/main.cpp
+++ stepanov.gena/P1/main.cpp
@@ -2,6 +2,7 @@
 int main()
 {
   int number = 0;
+  size_t count = 0; 
   do
   {
     std::cin>>number;
@@ -10,6 +11,11 @@ int main()
       std::cerr<<"Is not a sequence\n";
       return 1;
     }
+    else if (number != 0)
+    {
+      ++count;
+    }
   }
   while(number != 0);
+  std::cout<<count<<"\n";
 }
