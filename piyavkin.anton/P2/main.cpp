61e63a073f095b2cc81708aad59b0797ba6d9688
Piyavva <piyavkin05@bk.ru>
2023-11-05 18:40:27 +0300

    Processing the first argument

--- piyavkin.anton/P2/main.cpp
+++ piyavkin.anton/P2/main.cpp
@@ -1,3 +1,26 @@
-int main()
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+int main(int argc, char * argv[])
 {
+  if (argc != 4)
+  {
+    std::cerr << "Arguments entered incorrectly\n";
+    return 1;
+  }
+  try
+  {
+    int num = std::stoll(argv[1]);
+      if (num != 1 && num != 2)
+      {
+        std::cerr << "The first parameter is incorrectly specified\n";
+        return 3;
+      }
+  }
+  catch (...)
+  {
+    std::cerr << "The first parameter is incorrectly specified\n";
+    return 3;
+  }
 }
