a4a019f63c911256bca271e2e5cb3dffe25dbefd
Piyavva <piyavkin05@bk.ru>
2023-11-06 20:44:49 +0300

    Added input variations

--- piyavkin.anton/P4/main.cpp
+++ piyavkin.anton/P4/main.cpp
@@ -2,6 +2,18 @@
 #include <cstdlib>
 #include <fstream>
 
+size_t InputArray(std::istream & in, int * a, size_t s, size_t toread)
+{
+  for (size_t i = 0; i < std::min(toread, s); ++ i)
+  {
+    if (!(in >> a[i]))
+    {
+      return i;
+    }
+  }
+  return std::min(toread, s);
+}
+
 int main(int argc, char * argv[])
 {
   if (argc != 4)
@@ -23,30 +35,57 @@ int main(int argc, char * argv[])
     std::cerr << "The first parameter is incorrectly specified\n";
     return 3;
   }
+  int num = std::stoll(argv[1]);
+  if (num == 1)
+  {
+    int n = 0;
+    int m = 0;
+    bool flag = true;
+    std::ifstream input(argv[2]);
+    input >> n >> m;
+    int a[10000];
+    for (int i = 0; i < n * m; ++i)
+    {
+      input >> a[i];
+    }
+    if (!input)
+    {
+      std::cerr << "Can not read number\n";
+      return 2;
+    }
+    std::ofstream output(argv[3]);
+    if (n == m && n > 1)
+    {
+      for (int i = 0; i < n * m; ++i)
+      {
+        if (i > ((i / m) * (m + 1) + 1) && a[i] != 0)
+        {
+          flag = false;
+          break;
+        }
+      }
+    }
+    else
+    {
+      flag = false;
+    }
+    output << flag << "\n";
+  }
+  if (num == 2)
+  {
   int n = 0;
   int m = 0;
-  bool flag = true;
   std::ifstream input(argv[2]);
-  input >> n;
-  input >> m;
-  int a[10000];
-  for (int i = 0; i < n * m; ++i)
-  {
-    input >> a[i];
-  }
+  input >> n >> m;
+  int * matrix = new int [n*m];
+  size_t result = InputArray(input, matrix, n * m, n * m);
   if (!input)
   {
-    std::cerr << "Can not read number\n";
+    std::cerr << "Can not read\n";
+    delete [] matrix;
     return 2;
   }
   std::ofstream output(argv[3]);
-  for (int i = 0; i < n * m; ++i)
-  {
-    if (i > ((i / m) * (m + 1) + 1) && a[i] != 0)
-    {
-      flag = false;
-      break;
-    }
+  output << result;
   }
-  output << flag;
 }
