734c1eb8247961f7fbb843c38107a8bcc6183454
Rail (Susanoo) <zhalilov.rsh@edu.spbstu.ru>
2023-11-06 16:07:08 +0300

    Added arguments line errors checking

--- zhalilov.rail/P4/main.cpp
+++ zhalilov.rail/P4/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
 
-int main()
+int main(int argc, char * argv[])
 {
+  if (argc != 4)
+  {
+    std::cerr << "Bad arguments";
+    return 1;
+  }
+  
+  int num = 0;
+  try
+  {
+    num = std::stoi(argv[1]);
+  }
+  catch (const std::exception &e)
+  {
+    std::cerr << "Invalid argument of task number\n";
+    return 1;
+  }
+
+  if (num != 1 && num != 2)
+  {
+    std::cerr << "Invalid argument of task number\n";
+    return 1;
+  }
   return 0;
 }
