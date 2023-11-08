a119fef132c8db11efb79ff46a33197d3d14cb9d
puzz13 <puzz13one@gmail.com>
2023-11-08 20:40:02 +0300

    Add SGN-CHG function

--- kovtun.alexander/P1/main.cpp
+++ kovtun.alexander/P1/main.cpp
@@ -2,17 +2,30 @@
 #include <limits>
 
 
+void countSignChanges(int &previousNumber, int &currentNumber, size_t &counter)
+{
+  if ((currentNumber > 0 && previousNumber < 0) || (currentNumber < 0 && previousNumber > 0))
+  {
+    counter++;
+  }
+
+  previousNumber = currentNumber;
+}
+
 int main()
 {
   int number = 0;
   size_t size = 0;
+
+  size_t signChangeCounter = 0;
+  int previousNumber = 0;
 
   do
   {
     std::cin >> number;
     if (!std::cin)
     {
-      std::cerr << "sequence is not valid\n";
+      std::cerr << "sequence is not valid";
       return 1;
     }
 
@@ -21,16 +34,18 @@ int main()
       size_t max_size = std::numeric_limits<size_t>::max();
       if (size == max_size)
       {
-        std::cout << "sequence is too long";
+        std::cerr << "sequence is too long";
         return 2;
       }
-
       size++;
+    
+      countSignChanges(previousNumber, number, signChangeCounter);
     }
   }
   while (number != 0);
 
-  std::cout << size << std::endl;
+  std::cout << signChangeCounter << '\n';
+
   return 0;
 }
 
