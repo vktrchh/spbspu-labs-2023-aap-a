0fd1d70e5afcd52c7c3c0ae6e82dd72e3115afdb
osakasonyboy@gmail.com <sensh0god@gmail.com>
2023-11-08 15:42:28 +0300

    fix

--- arakelyan.daniil/P1/counterofnums.cpp
+++ arakelyan.daniil/P1/counterofnums.cpp
@@ -15,21 +15,22 @@ void arakelyan::CounterOfNumsThatDevideByThePrev::operator()(int currentNum)
 
   if (currentNum != 0)
   {
-    if (length_ == maxLength)
-    {
-      throw std::logic_error("Sequense is too long");
-    }
-    if ((currentNum % pastNum_ == 0) && (pastNum_ != 0))
+    if ((pastNum_ != 0) && (currentNum % pastNum_ == 0))
     {
       ++count_;
     }
     pastNum_ = currentNum;
+    if (length_ == maxLength)
+    {
+      throw std::logic_error("Sequense is too long");
+    }
   }
   else if ((length_ == 2 && currentNum == 0) || (length_ == 1 && currentNum == 0))
   {
     throw std::logic_error("Sequence is too short");
   }
 }
+
 size_t arakelyan::CounterOfNumsThatDevideByThePrev::operator()() const
 {
   return count_;
