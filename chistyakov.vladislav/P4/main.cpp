928ca7823577913f371b4f40be1092248cfd67aa
jiggerman <liggerman@mail.ru>
2023-11-08 16:13:04 +0300

    Read text for file and output

--- chistyakov.vladislav/P4/main.cpp
+++ chistyakov.vladislav/P4/main.cpp
@@ -1,2 +1,26 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char** argv)
+{
+  if (argc != 4)
+  {
+    std::cerr << "Bad input arguments\n";
+    return 1;
+  }
+
+  int taskNumber = std::stoll(argv[1]);
+  if (taskNumber == 1)
+  {
+    {
+      std::ifstream input(argv[2]);
+      while (!input.eof())
+      {
+        int i = 0;
+        input >> i;
+        std::cout << i << "\n";
+      }
+    }
+  }
+}
