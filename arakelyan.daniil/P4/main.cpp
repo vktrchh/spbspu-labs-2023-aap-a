bd37c42c2b79da7b0504916861317cf1994b135f
osakasonyboy@gmail.com <sensh0god@gmail.com>
2023-11-10 01:27:15 +0300

    trash

--- arakelyan.daniil/P4/main.cpp
+++ arakelyan.daniil/P4/main.cpp
@@ -48,53 +48,7 @@ int main (int argc, char * argv[])
       return 2;
     }
 
-
-    //считываю изначальную матрицу из входного файла
-    int matrix[rows][cols];
-    for (int i = 0; i < rows; i++)
-    {
-      for (int j = 0; j < cols; j++)
-      {
-        input >> matrix[i][j];
-      }
-    }
-
     input.close();
-
-    //создаю и инициализирую сглаженную матрицу на основе имеющейся
-    double smoothedMatrix[rows][cols];
-    for (int i = 0; i < rows; i++)
-    {
-      for (int j = 0; j < cols; j++)
-      {
-        double sum = 0.0;
-        int count = 0;
-
-        for (int x = i - 1; x <= i + 1; x++)
-        {
-          for (int y = j - 1; y <= j + 1; y++)
-          {
-            if ((x >= 0 && y >= 0) && (x < rows && y < cols) && (x != i || y != j))
-            {
-              sum += matrix[x][y];
-              count++;
-            }
-          }
-        }
-        smoothedMatrix[i][j] = sum / count;
-      }
-    }
-
-   std::cout << "smoothedMatrix\n";
-    for(int i = 0; i < rows; ++i)
-    {
-      std::cout << "[";
-      for(int j = 0; j < cols; ++j)
-      {
-        std::cout << " " << smoothedMatrix[i][j] << " ";
-      }
-      std::cout << "]\n";
-    }
 
     //файл вывода
     std::ofstream output(argv[3]);
@@ -102,16 +56,6 @@ int main (int argc, char * argv[])
     {
       std::cerr << "Cannot open the input file\n";
       return 2;
-    }
-
-    for (int i = 0; i < rows; ++i)
-    {
-      output << "|";
-      for (int j = 0; j < cols; ++j)
-      {
-        output << std::fixed << std::setprecision(1) <<" " << smoothedMatrix[i][j] << " ";
-      }
-      output << "|\n";
     }
   }
 
