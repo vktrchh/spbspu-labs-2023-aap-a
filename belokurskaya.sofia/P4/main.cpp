02d90f2677813ca721e5d3596f006e30afb159b5
sofabelokurskaya@yandex.ru <sofabelokurskaya@yandex.ru>
2023-11-09 21:54:48 +0300

    add strtoll

--- belokurskaya.sofia/P4/main.cpp
+++ belokurskaya.sofia/P4/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 
 int main(int argc, char * argv[])
 {
@@ -11,6 +12,22 @@ int main(int argc, char * argv[])
   {
     std::cerr << "Error in command line arguments\n";
     return 1;
+  }
+
+  char * endOfParsing = nullptr;
+  int num = std::strtoll(argv[1], &endOfParsing, 10);
+  if (num == 0 && endOfParsing == argv[1])
+  {
+    std::cerr << "Cannot parse a value\n";
+    return 2;
+  }
+  else if (num == 0 && endOfParsing != argv[1])
+  {
+    std::cout << "Num is zero\n";
+  }
+  else
+  {
+    std::cout << "Number is" << num << "\n";
   }
   int i = 0;
   {
