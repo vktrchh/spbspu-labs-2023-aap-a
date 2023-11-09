252bc9b702f82164205d1f6029dc50b31375f02f
kirillnamestnikov <kir.namestnikov@gmail.com>
2023-11-09 22:12:31 +0300

    one - dimensional array in-main realization

---
+++ namestnikov.kirill/P4/main.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+
+void freeMatrix(int ** m, size_t rows, size_t cols)
+{
+  if (m == nullptr)
+  {
+    return;
+  }
+  for (size_t i = 0; i < rows; ++i)
+  {
+    delete [] m[i];
+  }
+  delete [] m;
+}
+
+int ** createMatrix(size_t rows, size_t cols)
+{
+  int ** rowsptrs = new int *[rows];
+  for (size_t i = 0; i < rows; ++i)
+  {
+    rowsptrs[i] = nullptr;
+  }
+  try
+  {
+    for (size_t i = 0; i < rows; ++i)
+    {
+      rowsptrs[i] = new int[cols];
+    }
+    return rowsptrs;
+  }
+  catch (const std::bad_alloc &)
+  {
+    freeMatrix(rowsptrs, rows, cols);
+    throw;
+  }
+}
+
+size_t inputArray(std::istream & in, int * a, size_t s, size_t toread)
+{
+  for (size_t i = 0; i < std::min(toread, s); ++i)
+  {
+    if (!(in >> a[i]))
+    {
+      return i;
+    }
+  }
+  return std::min(toread, s);
+}
+
+int main(int argc, char ** argv)
+{
+  if (argc != 4)
+  {
+    std::cerr << "Error in command line arguments\n";
+    return 1;
+  }
+  int num = 0;
+  try
+  {
+    num = std::stoll(argv[1]);
+    if ((num != 1) && (num != 2))
+    {
+      std::cerr << "Wrong number in command line\n";
+      return 3;
+    }
+
+  }
+  catch (const std::out_of_range &)
+  {
+    std::cerr << "Value of first CLA is too large\n";
+    return 3;
+  }
+  catch (const std::invalid_argument &)
+  {
+    std::cerr << "Cannot parse a value\n";
+    return 3;
+  }
+  if (num == 1)
+  {
+    size_t rows = 0;
+    size_t cols = 0;
+    std::ifstream input(argv[2]);
+    input >> rows >> cols;
+    if (!input)
+    {
+      std::cerr << "Can not read a number\n";
+      return 2;
+    }
+    int matrix[10000];
+    for (int i = 0; i < rows * cols; ++i)
+    {
+      input >> matrix[i];
+      if (!input)
+      {
+        std::cerr << "Can not read a number\n";
+        return 2;
+      }
+    }
+    std::ofstream output(argv[3]);
+    int minRow[10000];
+    int maxCol[10000];
+    for (int i = 0; i < rows * cols; i = i + 4)
+    {
+      minRow[i] = matrix[i];
+      for (int j = 0; j < cols; ++j)
+      {
+        if (matrix[i + j] < minRow[i])
+        {
+          minRow[i] = matrix[i + j];
+        }
+      }
+    }
+    for (int j = 0; j < cols; ++j)
+    {
+      maxCol[j] = matrix[j];
+      for (int i = 0; i < rows * cols; i = i + 4)
+      {
+        if (matrix[j + i] > maxCol[j])
+        {
+          maxCol[j] = matrix[j + i];
+        }
+      }
+    }
+    size_t count = 0;
+    for (int i = 0; i < rows * cols; i = i + 4)
+    {
+      for (int j = 0; j < cols; ++j)
+      {
+        if ((matrix[i+j] == minRow[i]) && (matrix[i+j] == maxCol[j]))
+        {
+          ++count;
+        }
+      }
+    }
+    output << count << "\n";
+  }
+}
