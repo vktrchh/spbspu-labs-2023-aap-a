157af34f9fa77bc8d620cf5b0ddcb3da17d03400
sofabelokurskaya@yandex.ru <sofabelokurskaya@yandex.ru>
2023-11-09 21:30:58 +0300

    add ifstream/ofstream

--- belokurskaya.sofia/P4/main.cpp
+++ belokurskaya.sofia/P4/main.cpp
@@ -1,9 +1,29 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
 
-int main(int argc, char ** argv)
+int main(int argc, char * argv[])
 {
   for (int i = 0; i < argc; ++i)
   {
     std::cout << argv[i] << "\n";
   }
+  if (argc != 4)
+  {
+    std::cerr << "Error in command line arguments\n";
+    return 1;
+  }
+  int i = 0;
+  {
+    std::fstream input(argv[2]);
+    input >> i;
+    if (!input)
+    {
+      std::cerr << "Cannot read a number\n";
+      return 2;
+    }
+  }
+  std::cout << i << "\n";
+
+  std::ofstream output(argv[3]);
+  output << i << "\n";
 }
