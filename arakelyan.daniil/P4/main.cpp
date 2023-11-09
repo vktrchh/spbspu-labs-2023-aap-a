7a43d0e157221cd96592cdf3a805f44a1935d001
osakasonyboy@gmail.com <sensh0god@gmail.com>
2023-11-09 22:23:45 +0300

    first steps, argument check added

--- arakelyan.daniil/P4/main.cpp
+++ arakelyan.daniil/P4/main.cpp
@@ -1,4 +1,27 @@
-int main()
-{
+#include <iostream>
+#include <string>
 
+
+int main (int argc, char * argv[])
+{
+  //проверка кол-ва аргументов передаваем. при вызове программы
+  if (argc != 4)
+  {
+    std::cerr << "Must be 3 arguments: number 1 or 2; name of input file; name of output file\n";
+    return 1;
+  }
+
+  //проверка первого аргумента
+  char * endOfParse = nullptr;
+  int firstArgument = std::strtoll(argv[1],&endOfParse,10);
+  if (firstArgument == 0 && endOfParse == argv[1])
+  {
+    std::cerr << "Cannot parse the first argument!\n";
+    return 1;
+  }
+  else
+  {
+    std::cout << "all good!\n";
+  }
+  return 0;
 }
