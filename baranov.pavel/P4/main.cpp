8de1d7c27f161548ea7ca15325e8117e4d6bb4bc
pashko2303 <baranov-p.b@yandex.ru>
2023-11-09 21:03:36 +0300

    Command line arguments

--- baranov.pavel/P4/main.cpp
+++ baranov.pavel/P4/main.cpp
@@ -1,2 +1,11 @@
-int main()
-{}
+#include <iostream>
+
+int main(int argc, char ** argv)
+{
+  if (argc != 4)
+  {
+    std::cerr << "Error in command line arguments\n";
+    return 1;
+  }
+  std::cout << argv[1] << argv[2] << argv[3] << '\n';
+}
