76bf1826fe09a5bd90a55e2ba53d6d535f2ee50a
WarriorVortex <erohinvova01010111@gmail.com>
2023-11-10 03:21:59 +0300

    Input of the first argument

--- erohin.vladimir/P4/main.cpp
+++ erohin.vladimir/P4/main.cpp
@@ -1,2 +1,25 @@
-int main()
-{}
+#include <iostream>
+#include <string>
+int main(int argc, char * arcv [])
+{
+  if (argc != 4)
+  {
+    std::cerr << "Wrong number of line arguments\n";
+    return 1;
+  }
+  int num = 0;
+  try
+  {
+    num = stoi(argv[1]);
+  }
+  catch (const std::out_of_range &)
+  {
+    std::cerr << "Number is out of range\n";
+    return 3;
+  }
+  catch (const std::invalid_argument &)
+  {
+    std::cerr << "Cannot parse a value\n";
+    return 3;
+  }
+}
