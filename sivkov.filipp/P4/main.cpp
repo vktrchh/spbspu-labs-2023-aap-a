39583b7a19bac348e7621c1c7352cc299042faa4
exxiiint <filipp.sivkov@yandex.ru>
2023-11-11 00:25:07 +0300

    add translation units

--- sivkov.filipp/P4/main.cpp
+++ sivkov.filipp/P4/main.cpp
@@ -4,37 +4,7 @@
 #include <stdexcept>
 #include <cstdlib>
 #include <string>
-
-void freeMatrix(int** m, size_t rows)
-{
-  for (size_t i = 0; i < rows; ++i)
-  {
-    delete[] m[i];
-  }
-  delete[] m;
-}
-
-int ** createMatrix(size_t rows, size_t cols)
-{
-  int ** rowsptrs = new int* [rows];
-  for (size_t i = 0; i < rows; ++i)
-  {
-    rowsptrs[i] = nullptr;
-  }
-  try
-  {
-    for (size_t i = 0; i < rows; ++i)
-    {
-      rowsptrs[i] = new int[cols];
-    }
-    return rowsptrs;
-  }
-  catch (const std::bad_alloc&)
-  {
-    freeMatrix(rowsptrs, rows);
-    throw;
-  }
-}
+#include "matrix.hpp"
 
 int main(int argc, char * argv[])
 {
