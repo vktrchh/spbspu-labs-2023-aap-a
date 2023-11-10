e0db6f6280ce5fd2431f621d11e57daaf4b7cc15
pashko2303 <baranov-p.b@yandex.ru>
2023-11-10 22:26:14 +0300

    Matrix input

--- baranov.pavel/P4/main.cpp
+++ baranov.pavel/P4/main.cpp
@@ -2,6 +2,17 @@
 #include <fstream>
 #include <cstdlib>
 
+void inputMatrix(std::istream & input, int * matrix, size_t size)
+{
+  for (size_t i = 0; i < size; ++i)
+  {
+    if (!(input >> matrix[i]))
+    {
+      throw std::length_error("Can not read matrix element");
+    }
+  }
+}
+
 int main(int argc, char ** argv)
 {
   if (argc != 4)
@@ -14,7 +25,12 @@ int main(int argc, char ** argv)
   {
     task = std::stoll(argv[1]);
   }
-  catch (...)
+  catch (const std::out_of_range &)
+  {
+    std::cerr << "Valuue of first CLA is too large\n";
+    return 1;
+  }
+  catch (const std::invalid_argument &)
   {
     std::cerr << "Cannot parse a value\n";
     return 1;
@@ -34,6 +50,24 @@ int main(int argc, char ** argv)
     {
       std::cerr << "Incorrect parametrs for matrix\n";
       return 2;
+    }
+
+    if (task == 1)
+    {
+      int matrix[rows * columns] = {0};
+      try
+      {
+        inputMatrix(input, matrix, rows * columns);
+      }
+      catch (...)
+      {
+        std::cerr << "Can not read matrix\n";
+        return 2;
+      }
+      for (unsigned int i = 0; i < rows * columns; ++i)
+      {
+        std::cout << matrix[i] << '\n';
+      }
     }
   }
 }
