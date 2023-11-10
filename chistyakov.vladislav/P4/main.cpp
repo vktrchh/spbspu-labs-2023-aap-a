05da6adf9a31cb7e0a98341ce19288fb1af036a3
jiggerman <liggerman@mail.ru>
2023-11-10 14:16:14 +0300

    Input numbers from file. 1 and 2

--- chistyakov.vladislav/P4/main.cpp
+++ chistyakov.vladislav/P4/main.cpp
@@ -2,6 +2,20 @@
 #include <fstream>
 #include <string>
 
+size_t inputArray(std::ifstream & input, int * array, size_t size, size_t sizeToRead)
+{
+  for (size_t i = 0; i < std::min(size, sizeToRead); ++i)
+  {
+    input >> array[i];
+    if (!input)
+    {
+      return i;
+    }
+  }
+  return std::min(size, sizeToRead);
+}
+
+
 int main(int argc, char** argv)
 {
   if (argc != 4)
@@ -10,17 +24,80 @@ int main(int argc, char** argv)
     return 1;
   }
 
-  int taskNumber = std::stoll(argv[1]);
+  // Чтение первого числа
+  int taskNumber = 0;
+  try
+  {
+    taskNumber = std::stoll(argv[1]);
+  }
+  catch (...)
+  {
+    std::cerr << "Cannot parse values\n";
+    return 1;
+  }
+
   if (taskNumber == 1)
   {
     {
+      // Чтение из файла и записть в файл статический массив
       std::ifstream input(argv[2]);
-      while (!input.eof())
+      size_t col = 0;
+      size_t row = 0;
+
+      input >> col;
+      input >> row;
+      if (!input)
       {
-        int i = 0;
-        input >> i;
-        std::cout << i << "\n";
+        std::cerr << "Cannot parse number of column or rows\n";
+        return 2;
       }
+
+      int size = col * row;
+      int array[size];
+      int number = 0;
+      for (int i = 0; i < size; ++i)
+      {
+        input >> number;
+        if (!input)
+        {
+          std::cerr << "Cannot parse number from file\n";
+          return 2;
+        }
+        array[i] = number;
+      }
+      // решение задачи
     }
+  }
+  else if (taskNumber == 2)
+  {
+    std::ifstream input(argv[2]);
+
+    size_t col = 0;
+    size_t row = 0;
+
+    input >> col >> row;
+
+    if (!input)
+    {
+      std::cerr << "Cannot parse number of column or rows\n";
+      return 2;
+    }
+
+    int * array = new int[row * col];
+    int number = 0;
+    size_t numOfElements = 0;
+    numOfElements = inputArray(input, array, (row * col), (row * col));
+    if (numOfElements != (row * col))
+    {
+      std::cout << numOfElements << "\n";
+      std::cerr << "Cannot parse numbers from file\n";
+      delete [] array;
+      return 2;
+    }
+    std::cout << numOfElements;
+  }
+  else
+  {
+
   }
 }
