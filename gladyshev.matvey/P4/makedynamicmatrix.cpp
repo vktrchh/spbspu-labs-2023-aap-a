ee61a71ebcae620d226517261e1ecd89572bd5df
magggottt <gladyshevmatvey@gmail.com>
2023-11-10 23:29:41 +0700

    im crying

--- gladyshev.matvey/P4/makedynamicmatrix.cpp
+++ gladyshev.matvey/P4/makedynamicmatrix.cpp
@@ -3,34 +3,26 @@
 int ** gladyshev::goDynamicMatrix(std::fstream &input, size_t rows, size_t cols)
 {
   int ** array = new int *[rows];
-  try
+  for (size_t i = 0; i < rows; i++)
   {
-    for (size_t i = 0; i < rows; i++)
+    array[i] = new int[cols];
+    for (size_t j = 0; j < cols; j++)
     {
-      array[i] = new int[cols];
-      for (size_t j = 0; j < cols; j++)
+      if (!input.eof())
       {
-        if (!input.eof())
+        if (!input)
         {
-          if (!input)
-          {
-            throw std::bad_alloc("Error in reading");
-          }
+          throw std::bad_alloc("Error in reading");
         }
-        else
-        {
-          throw std::runtime_error("Out of data");
-        }
-        input >> array[i][j];
       }
+      else
+      {
+        throw std::runtime_error("Out of data");
+      }
+      input >> array[i][j];
     }
-    return array;
   }
-  catch (const std::bad_alloc & e)
-  {
-    gladyshev::freeMatrix(array, rows, cols);
-    throw;
-  }
+  return array;
 }
 
 void gladyshev::freeMatrix(int ** matrix, size_t rows)
