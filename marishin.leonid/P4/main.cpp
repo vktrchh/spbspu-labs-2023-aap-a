d8931b23e8a06c7d3cb9ab00de8bc4403ff41997
marlions <marishin.lv@edu.spbstu.ru>
2023-11-11 02:21:33 +0300

    checking the number of arguments passed

--- marishin.leonid/P4/main.cpp
+++ marishin.leonid/P4/main.cpp
@@ -1,56 +1,17 @@
 #include <iostream>
 
-int main(int argc, char* argv[]))
+int main(int argc, char* argv[])
 {
-  size_t rows = 0;
-  size_t cols = 0;
-  std::cin >> rows >> cols;
-  size_t matrixSize = rows * cols;
-  int* matrix = new int[matrixSize];
-  for (size_t i = 0; i < matrixSize; i++)
+  using namespace marishin;
+  if (argc < 4)
   {
-    std::cin >> matrix[i];
+    std::cerr << "Not enough arguments\n";
+    return 1;
   }
-  int topRow = 0;
-  int bottomRow = rows - 1;
-  int leftCol = 0;
-  int rightCol = cols - 1;
-  int count = 1;
-
-  while (topRow <= bottomRow && leftCol <= rightCol)
+  else if (argc > 4)
   {
-    for (int row = bottomRow; row >= topRow; row--)
-    {
-      matrix[row * cols + leftCol] -= count;
-      ++count;
-    }
-    leftCol++;
-    for (int col = leftCol; col <= rightCol; col++)
-    {
-      matrix[topRow * cols + col] -= count;
-      ++count;
-    }
-    topRow++;
-    for (int row = topRow; row <= bottomRow; row++)
-    {
-      matrix[row * cols + rightCol] -= count;
-      ++count;
-    }
-    rightCol--;
-    if (topRow <= bottomRow)
-    {
-      for (int col = rightCol; col >= leftCol; col--)
-      {
-        matrix[bottomRow * cols + col] -= count;
-        ++count;
-      }
-      bottomRow--;
-    }
+    std::cerr << "Too many arguments\n";
+    return 1;
   }
-
-  for (size_t j = 0; j < matrixSize; j++)
-  {
-    std::cout << matrix[j] << " ";
-  }
-  delete[] matrix;
+  
 }
