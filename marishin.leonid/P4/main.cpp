a83890e904acd59ee19167651cc0d2ddca2aab68
marlions <marishin.lv@edu.spbstu.ru>
2023-11-11 21:43:52 +0300

    main refactor

--- marishin.leonid/P4/main.cpp
+++ marishin.leonid/P4/main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 #include "arraycheck.hpp"
+#include "matrixtransformation.hpp"
 
 int main(int argc, char* argv[])
 {
@@ -36,9 +37,9 @@ int main(int argc, char* argv[])
     std::cerr << "First parameter is out of range\n";
     return 1;
   }
-  size_t rows = 0;
-  size_t cols = 0;
-  std::ifstream inputFile(argv[2]);
+  int rows = 0;
+  int cols = 0;
+  std::ifstream input(argv[2]);
   input >> rows >> cols;
   if (!input)
   {
@@ -47,15 +48,33 @@ int main(int argc, char* argv[])
   }
   if (num == 1)
   {
-    const arraySize = 10000;
-    int matrix = [arraySize]{};
-    if (arrayCheck(input, matrix, rows, cols))
+    const int arraySize = 10000;
+    int matrix[arraySize]{};
+    if (arrayCheck(input, matrix, rows, cols, num))
     {
-      // ....
-      return 0;
+      matrixTransformation(matrix, rows, cols);
+      std::ofstream output(argv[3]);
+      matrixEntry(output, matrix, rows, cols);
     }
     else
     {
+      std::cerr << "The contents of the file cannot be interpreted\n";
+      return 2;
+    }
+  }
+  else if (num == 2)
+  {
+    int* matrix = new int[rows * cols];
+    if (arrayCheck(input, matrix, rows, cols, num))
+    {
+      matrixTransformation(matrix, rows, cols);
+      std::ofstream output(argv[3]);
+      matrixEntry(output, matrix, rows, cols);
+      delete[] matrix;
+    }
+    else
+    {
+      delete[] matrix;
       std::cerr << "The contents of the file cannot be interpreted\n";
       return 2;
     }
