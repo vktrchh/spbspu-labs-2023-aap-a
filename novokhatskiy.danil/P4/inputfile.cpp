be3243e8a78ac2447665e462f5104322ea25fae4
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2023-11-12 00:23:53 +0300

    stupid mistake, but it won't help

--- novokhatskiy.danil/P4/inputfile.cpp
+++ novokhatskiy.danil/P4/inputfile.cpp
@@ -1,6 +1,6 @@
 #include "inputfile.hpp"
 
-int novokhatskiy::inputfile(int matrix[], int rows, int cols, std::ifstream &input);
+int novokhatskiy::inputfile(int matrix[], int rows, int cols, std::ifstream &input)
 {
   for (int i = 0; i < rows * cols; i++)
   {
