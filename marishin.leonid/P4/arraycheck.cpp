13743d88f1e3a37a84fee57955c2fdb08bd91228
marlions <marishin.lv@edu.spbstu.ru>
2023-11-11 04:03:12 +0300

    arraycheck.cpp

---
+++ marishin.leonid/P4/arraycheck.cpp
@@ -0,0 +1,13 @@
+#include "arraycheck.hpp"
+
+bool arrayCheck(std::istream& n, int* matrix, size_t rows, size_t cols)
+{
+  for (int i = 0; i < rows * cols; ++i)
+  {
+    if (!(n >> matrix[i]))
+    {
+      return false;
+    }
+    return true;
+  }
+}
