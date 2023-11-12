6ab3b7c9a2e2bd534d2014b9e2b8dc039755f04a
WarriorVortex <erohinvova01010111@gmail.com>
2023-11-12 04:51:32 +0300

    Changed logic of handling matrix element overflow

--- erohin.vladimir/P4/circle_fill.cpp
+++ erohin.vladimir/P4/circle_fill.cpp
@@ -3,17 +3,12 @@
 #include <limits>
 #include <stdexcept>
 
-size_t erohin::module(const int & num)
-{
-  return static_cast< size_t >(std::abs(num));
-}
-
 void erohin::circleFill(int * matrix, const size_t & rows, const size_t & cols)
 {
   size_t dist_edge_rows = 0;
   size_t dist_edge_cols = 0;
   size_t add = 0;
-  size_t max_size = std::numeric_limits< int > ::max();
+  size_t max_size = std::numeric_limits< int >::max();
   for (size_t i = 0; i < rows; ++i)
   {
     dist_edge_rows = std::min(i, rows - i - 1);
@@ -21,11 +16,7 @@ void erohin::circleFill(int * matrix, const size_t & rows, const size_t & cols)
     {
       dist_edge_cols = std::min(j, cols - j - 1);
       add = std::min(dist_edge_rows, dist_edge_cols) + 1;
-      if (matrix[rows * i + j] >= 0 && erohin::module(matrix[rows * i - j]) > max_size - add)
-      {
-        throw std::logic_error("Matrix element is too large");
-      }
-      else if (matrix[rows * i + j] < 0 && (-erohin::module(matrix[rows * i + j])) > max_size - add)
+      if (max_size - matrix[rows * i + j] < add)
       {
         throw std::logic_error("Matrix element is too large");
       }
--- erohin.vladimir/P4/circle_fill.hpp
+++ erohin.vladimir/P4/circle_fill.hpp
@@ -4,7 +4,6 @@
 #include <cstddef>
 namespace erohin
 {
-  size_t module(const int & num);
   void circleFill(int * matrix, const size_t & rows, const size_t & cols);
 }
 #endif
