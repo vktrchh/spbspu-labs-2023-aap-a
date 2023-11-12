7b2fba855c4cf90b51dc666e43a4194763ecd1d9
Z0ltaan <gregnov69@gmail.com>
2023-11-12 20:55:58 +0300

    addded dynamic array

--- isaychev.vladislav/P4/main.cpp
+++ isaychev.vladislav/P4/main.cpp
@@ -14,60 +14,60 @@ int main(int argc, char * argv[])
     std::cerr << "Too many arguments\n";
     return 1;
   }
-  else if (argc == 4)
+  char * endPtr = nullptr;
+  long long int ctrl_parameter = std::strtoll(argv[1], &endPtr, 10);
+  if ((ctrl_parameter > 2 || ctrl_parameter < 1) && endPtr != argv[1])
   {
-    char * endPtr = nullptr;
-    long long int ctrl_parameter = std::strtoll(argv[1], &endPtr, 10);
-    if ((ctrl_parameter > 2 || ctrl_parameter < 1) && endPtr != argv[1])
+    std::cerr << "First parameter is out of range\n";
+    return 1;
+  }
+  else if ((ctrl_parameter > 2 || ctrl_parameter < 1) && endPtr == argv[1])
+  {
+    std::cerr << "First parameter is not a number\n";
+    return 1;
+  }
+  size_t rows = 0;
+  size_t columns = 0;
+  if (ctrl_parameter == 1)
+  {
+    // starting input
+    long long int Matrix[10000] = {};
+    std::ifstream input(argv[2]);
+    input >> rows >> columns;
+    if (!input)
     {
-      std::cerr << "First parameter is out of range\n";
-      return 1;
+      std::cerr << "Number of rows and columns in array must be described as positive integer number\n";
+      return 2;
     }
-    else if ((ctrl_parameter > 2 || ctrl_parameter < 1) && endPtr == argv[1])
+    size_t count = 0;
+    while (count < rows * columns)
     {
-      std::cerr << "First parameter is not a number\n";
-      return 1;
-    }
-    else if (ctrl_parameter == 1)
-    {
-      // starting input
-      size_t rows = 0;
-      size_t columns = 0;
-      long long int Matrix[10000] = {};
+      input >> Matrix[count];
+      if (!input)
       {
-        std::ifstream input(argv[2]);
-        input >> rows >> columns;
-        if (!input)
-        {
-          std::cerr << "Number of rows and columns in array must be described as positive integer number\n";
-          return 2;
-        }
-        size_t count = 0;
-        while (count < rows * columns)
-        {
-          input >> Matrix[count];
-          if (!input)
-          {
-            std::cerr << "Element of array must be described as integer\n";
-            return 2;
-          }
-          ++count;
-        }
+        std::cerr << "Element of array must be described as integer\n";
+        return 2;
       }
-
-/*      {
-          std::ofstream output(argv[3]);
-          for (size_t i = 0; i < rows * columns; ++i)
-          {
-            output << Matrix[i] << " ";
-          }
-        }*/
-      }
-
+      ++count;
     }
-    else if (ctrl_parameter == 2)
+  }
+
+  else if (ctrl_parameter == 2)
+  {
+    std::ifstream input(argv[2]);
+    input >> rows >> columns;
+    if (!input)
     {
-      return 0;
+      std::cerr << "Number of rows and columns in array must be described as positive integer number\n";
+      return 2;
     }
+    long long int * DynMatrix = new long long int[rows * columns]();
+    size_t count = 0;
+    while (count < rows * columns)
+    {
+      input >> DynMatrix[count];
+      ++count;
+    }
+    delete[] DynMatrix;
   }
 }
