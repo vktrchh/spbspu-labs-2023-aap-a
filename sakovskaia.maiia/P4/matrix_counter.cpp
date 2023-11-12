c1e6207742c84391bd72f558b61b95f75db59fa5
MayaSakovskaya <zloyejik05@gmail.com>
2023-11-12 21:13:41 +0300

    Method of counting conterclock-wise matrix the same size as input matrix

---
+++ sakovskaia.maiia/P4/matrix_counter.cpp
@@ -0,0 +1,39 @@
+#include "matrix.hpp"
+#include <math.h>
+
+void sakovskaia::counterClockwiseCircleMatrix(int * a, int rows, int columns)
+{
+  int n = 0;
+  int cnt = 0;
+  int cntrows = rows;
+  int cntcolumns = columns;
+  for (int circle = 1; circle <= (ceil((std::min(rows, columns)) / 2)); ++circle)
+  {
+    for (int index = cnt; index <= (rows * columns - columns * circle + cnt); columns)
+    {
+      a[index] = ++n;
+    }
+    for (int index = (rows * columns - columns * circle + cnt + 1); index <= (rows * columns - columns * circle + cnt + cntcolumns - 1); ++index)
+    {
+      a[index] = ++n;
+    }
+    for (int index = (rows * columns - columns - circle); index <= (circle * (columns - 1)); index = index - columns)
+    {
+      a[index] = ++n;
+    }
+    for (int index = (circle * (columns - 1) - 1); index <= (cnt * columns + circle); --index)
+    {
+      a[index] = ++n;
+    }
+    ++cnt;
+    cntcolumns = cntcolumns - 2;
+  }
+}
+
+void sakovskaia::roadToAnswer(int * inputmatrix, int * counterclockwisematrix, int size)
+{
+  for (int index = 0; index < size; ++index)
+  {
+    inputmatrix[index] += counterclockwisematrix[index];
+  }
+}
