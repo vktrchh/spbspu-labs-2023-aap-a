4efd22970aaf631724eef93b568be2266cd3feeb
Z0ltaan <gregnov69@gmail.com>
2023-11-12 15:41:20 +0300

    basic input output

--- isaychev.vladislav/P4/main.cpp
+++ isaychev.vladislav/P4/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
   if (argc < 4)
   {
@@ -15,11 +16,10 @@ int main(int argc, char *argv[])
   }
   else if (argc == 4)
   {
-    char *endPtr = nullptr;
+    char * endPtr = nullptr;
     long long int ctrl_parameter = std::strtoll(argv[1], &endPtr, 10);
     if ((ctrl_parameter > 2 || ctrl_parameter < 1) && endPtr != argv[1])
     {
-      std::cout << ctrl_parameter << "\n";
       std::cerr << "First parameter is out of range\n";
       return 1;
     }
@@ -28,6 +28,23 @@ int main(int argc, char *argv[])
       std::cerr << "First parameter is not a number\n";
       return 1;
     }
+    else if (ctrl_parameter == 1 || ctrl_parameter == 2)
+    {
+      // starting input
+      unsigned long long int rows = 0;
+      {
+        std::ifstream input(argv[2]);
+        input >> rows;
+        if (!input)
+        {
+          std::cerr << "Number of rows in array must be described as positive integer number\n";
+          return 2;
+        }
+      }
+      {
+        std::ofstream output(argv[3]);
+        output << rows << "\n";
+      }
+    }
   }
 }
-
