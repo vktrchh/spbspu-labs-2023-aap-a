e06a7def32534eeb0b5f641ecb2ca01c2d19806f
Z0ltaan <gregnov69@gmail.com>
2023-11-12 17:56:26 +0300

    added array with static size

--- isaychev.vladislav/P4/main.cpp
+++ isaychev.vladislav/P4/main.cpp
@@ -28,23 +28,46 @@ int main(int argc, char * argv[])
       std::cerr << "First parameter is not a number\n";
       return 1;
     }
-    else if (ctrl_parameter == 1 || ctrl_parameter == 2)
+    else if (ctrl_parameter == 1)
     {
       // starting input
-      unsigned long long int rows = 0;
+      size_t rows = 0;
+      size_t columns = 0;
+      long long int Matrix[10000] = {};
       {
         std::ifstream input(argv[2]);
-        input >> rows;
+        input >> rows >> columns;
         if (!input)
         {
-          std::cerr << "Number of rows in array must be described as positive integer number\n";
+          std::cerr << "Number of rows and columns in array must be described as positive integer number\n";
           return 2;
         }
+        size_t count = 0;
+        while (count < rows * columns)
+        {
+          input >> Matrix[count];
+          if (!input)
+          {
+            std::cerr << "Element of array must be described as integer\n";
+            return 2;
+          }
+          ++count;
+        }
       }
-      {
-        std::ofstream output(argv[3]);
-        output << rows << "\n";
+
+/*      {
+          std::ofstream output(argv[3]);
+          for (size_t i = 0; i < rows * columns; ++i)
+          {
+            output << Matrix[i] << " ";
+          }
+        }*/
       }
+
+    }
+    else if (ctrl_parameter == 2)
+    {
+      return 0;
     }
   }
 }
