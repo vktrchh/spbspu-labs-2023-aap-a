551b569be0f6e45b22501f6b11376cdaeb17e7a0
Dmitriy <Dmitry5335@yandex.ru>
2023-11-18 22:55:38 +0300

    Do-while + buffering I tried to split the commit into several parts, but nothing would work individually.

--- nikitov.dmitriy/P3/main.cpp
+++ nikitov.dmitriy/P3/main.cpp
@@ -3,25 +3,62 @@
 
 int main()
 {
-  char * buffer = new char[25]{};
+  const size_t bufferSize = 25;
+  size_t arraySize = 0;
 
   char symb = 0;
   size_t i = 0;
+  bool status = true;
+  char* actualArray = new char[arraySize + bufferSize]{};
 
   std::cin >> std::noskipws;
-  while ((std::cin >> symb) && (i < 24))
+  do
   {
-    buffer[i++] = symb;
-    if (symb == '\n')
+    char* buffer = new char[bufferSize]{};
+    while ((i != bufferSize) && (std::cin >> symb))
     {
-      buffer[i - 1] = 0;
-      break;
+      buffer[i++] = symb;
+      if (symb == '\n')
+      {
+        buffer[i - 1] = 0;
+        status = false;
+        break;
+      }
+    }
+
+    size_t position = arraySize;
+    for (size_t j = 0; j != bufferSize; ++j)
+    {
+      actualArray[position] = buffer[j];
+      ++position;
+    }
+    arraySize += bufferSize;
+
+    char* tempArray = new char[arraySize]{};
+    for (size_t j = 0; j != arraySize; ++j)
+    {
+      tempArray[j] = actualArray[j];
+    }
+    delete [] actualArray;
+
+    actualArray = new char[arraySize + bufferSize]{};
+    for (size_t j = 0; j != arraySize; ++j)
+    {
+      actualArray[j] = tempArray[j];
+    }
+    delete [] tempArray;
+
+    i = 0;
+
+    if (!status)
+    {
+      std::cout << actualArray << '\n';
+      delete [] actualArray;
+      actualArray = nullptr;
     }
   }
+  while (status);
   std::cin >> std::skipws;
-
-  std::cout << buffer << "\n";
-  delete [] buffer;
 
   return 0;
 }
