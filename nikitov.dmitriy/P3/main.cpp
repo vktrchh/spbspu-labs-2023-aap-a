d2a2094efd3dc78058f50f1a562a460b2fca0d54
Dmitriy <Dmitry5335@yandex.ru>
2023-11-18 18:13:18 +0300

    Buffer array and cycle for input

--- nikitov.dmitriy/P3/main.cpp
+++ nikitov.dmitriy/P3/main.cpp
@@ -1,2 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
 int main()
-{}
+{
+  char * buffer = new char[25]{};
+
+  char symb = 0;
+  size_t i = 0;
+
+  while ((std::cin >> symb) && (i < 24))
+  {
+    buffer[i++] = symb;
+  }
+
+  std::cout << buffer << "\n";
+}
