4ad8e038df1f98cb49b5077c52736a44c48edc4b
Dmitriy <Dmitry5335@yandex.ru>
2023-11-18 18:27:58 +0300

    Stop by \n and spaces reading

--- nikitov.dmitriy/P3/main.cpp
+++ nikitov.dmitriy/P3/main.cpp
@@ -8,10 +8,17 @@ int main()
   char symb = 0;
   size_t i = 0;
 
+  std::cin >> std::noskipws;
   while ((std::cin >> symb) && (i < 24))
   {
     buffer[i++] = symb;
+    if (symb == '\n')
+    {
+      buffer[i - 1] = 0;
+      break;
+    }
   }
+  std::cin >> std::skipws;
 
   std::cout << buffer << "\n";
 }
