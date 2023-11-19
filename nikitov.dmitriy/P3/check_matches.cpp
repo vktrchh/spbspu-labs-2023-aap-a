fa92040f154c7d3713bc987b95cb24b3cc3456c1
Dmitriy <Dmitry5335@yandex.ru>
2023-11-19 14:51:49 +0300

    Check matches realisation

--- nikitov.dmitriy/P3/check_matches.cpp
+++ nikitov.dmitriy/P3/check_matches.cpp
@@ -1,7 +1,27 @@
 #include "check_matches.hpp"
-#include <cstring>
 
 int nikitov::checkMatches(char* firstArray, char* secondArray, size_t firstSize, size_t secondSize)
 {
+  for (size_t i = 0; i != firstSize; ++i)
+  {
+    char symb1 = firstArray[i];
+    if (symb1 == '\0')
+    {
+      break;
+    }
+    for (size_t j = 0; j != secondSize; ++j)
+    {
+      char symb2 = secondArray[j];
+      if (symb2 == '\0')
+      {
+        break;
+      }
+      else if (symb1 == symb2)
+      {
+        return 1;
+      }
+    }
+  }
+
   return 0;
 }
