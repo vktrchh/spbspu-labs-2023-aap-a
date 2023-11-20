f43b430df6f44ce43247e09be7f46b1f578f4064
CA21lmFlame <chernov.ar@spbstu.ru>
2023-11-20 19:00:30 +0300

    added argc and argv

--- chernov.artem/P4/main.cpp
+++ chernov.artem/P4/main.cpp
@@ -1,3 +1,25 @@
-int main()
+#include <iostream>
+#include <ifstream>
+#include <ofstream>
+
+int main(int argc, char * argv[])
 {
+  if (argc != 4)
+  {
+    std::cerr << "Error in command line arguments\n";
+    return 1;
+  }
+  size_t rows = 0;
+  size_t cols = 0;
+  std::ifstream input(argv[2]);
+  std::ofstream output(argv[3]);
+  input >> rows >> cols;
+  if (!input)
+  {
+    std::cerr << "Cannot read a number\n";
+    return 2;
+  }
+  output << "\n";
+  output.close();
+  return 0;
 }
