33c0caeceea67fd1b48fa86a50abb63a9440711a
Z0ltaan <gregnov69@gmail.com>
2023-11-21 00:34:34 +0300

    try input string

--- isaychev.vladislav/P3/main.cpp
+++ isaychev.vladislav/P3/main.cpp
@@ -2,5 +2,12 @@
 
 int main()
 {
-
+  int capacity = 10;
+  char * str = new char[capacity]();
+  int i = 0;
+  while ((std::cin >> str[i]) && (i < capacity))
+  {
+    ++i;
+  }
+  delete[] str;
 }
