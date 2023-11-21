e57f3b0ffe9149654f7a691609a08fe6e9908246
NovokhatskiyDanil <karuk.vova2005@mail.ru>
2023-11-21 20:42:26 +0300

    Add function

--- novokhatskiy.danil/P3/main.cpp
+++ novokhatskiy.danil/P3/main.cpp
@@ -1,6 +1,26 @@
+#include <cctype>
+#include <string>
 #include <iostream>
+
+void result(const char* in, char* out)
+{
+  do
+  {
+    if(!std::isalpha(*in))
+    {
+      *out++ = *in;
+    }
+  }
+  while(*++in);
+}
 
 int main()
 {
-
+  std::string s;
+  std::getline(std::cin, s);
+  char * output = new char[s.size()];
+  result(s.c_str(),output);
+  std::cout << output << "\n";
+  delete[] output;
 }
+
