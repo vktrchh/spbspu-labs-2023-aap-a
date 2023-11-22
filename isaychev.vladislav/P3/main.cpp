13ecf86d14f3fdcba722ea457ca1804722024832
Z0ltaan <gregnov69@gmail.com>
2023-11-23 01:14:08 +0300

    input string

--- isaychev.vladislav/P3/main.cpp
+++ isaychev.vladislav/P3/main.cpp
@@ -1,13 +1,46 @@
 #include <iostream>
+#include <iomanip>
 
 int main()
 {
   int capacity = 10;
-  char * str = new char[capacity]();
+  char * str = nullptr;
+  str = new char[capacity]{};
   int i = 0;
-  while ((std::cin >> str[i]) && (i < capacity))
+  char curr_char = 0;
+  std::cin >> std::noskipws;
+  while ((std::cin >> curr_char) && (i < capacity))
   {
+    str[i] = curr_char;
+    if (curr_char == '\n')
+    {
+      str[i] = '\0';
+      break;
+    }
+    if (i == capacity - 1)
+    {
+      char * str_temp = new char[capacity]{};
+      for (int j = 0; j < i; ++j)
+      {
+        str_temp[j] = str[j];
+      }
+      delete [] str;
+      capacity += 10;
+      str = new char[capacity]{};
+      for (int j = 0; j < i; ++j)
+      {
+        str[j] = str_temp[j];
+      }
+      delete [] str_temp;
+    }
     ++i;
   }
-  delete[] str;
+// for tests
+
+/*  for (int k = 0; k < capacity; ++k)
+  {
+    std::cout << str[k] << " ";
+  }
+  std::cout << "\n" << capacity << "\n";*/
+  delete [] str;
 }
