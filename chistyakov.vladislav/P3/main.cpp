cf7f9cd070ef1f87177f48c95eaf307462c2c2f3
jiggerman <liggerman@mail.ru>
2023-11-22 10:57:50 +0300

    Append dynamic array with out checks

--- chistyakov.vladislav/P3/main.cpp
+++ chistyakov.vladislav/P3/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 
+char * enlargeArray(char * array, size_t size)
+{
+  char * newArray = new char[size + 10]{};
+  for (int i = 0; i < size; ++i)
+  {
+    newArray[i] = array[i];
+  }
+  delete [] array;
+  return newArray;
+}
+
 int main()
 {
-  int size = 10;
-  char array[size]{};
+  size_t size = 10;
+  char * array = new char[size]{};
 
   char input = 0;
   int i = 0;
@@ -11,7 +22,18 @@ int main()
 
   while ((std::cin >> input) && (i < size))
   {
+    if (input == '\n')
+    {
+      break;
+    }
+
     array[i++] = input;
+
+    if (i == size)
+    {
+      array = enlargeArray(array, size);
+      size += 10;
+    }
   }
 
   std::cin >> std::skipws;
