ec6c257317d74bc7d249f65253a2a3a303f49f3a
jiggerman <liggerman@mail.ru>
2023-11-22 10:42:20 +0300

    Read with ws from stream cin and print it in cmd

--- chistyakov.vladislav/P3/main.cpp
+++ chistyakov.vladislav/P3/main.cpp
@@ -1,3 +1,24 @@
+#include <iostream>
+
 int main()
 {
+  int size = 10;
+  char array[size]{};
+
+  char input = 0;
+  int i = 0;
+  std::cin >> std::noskipws;
+
+  while ((std::cin >> input) && (i < size))
+  {
+    array[i++] = input;
+  }
+
+  std::cin >> std::skipws;
+
+  for (int i = 0; i < size; ++i)
+  {
+    std::cout << array[i];
+  }
+  return 0;
 }
