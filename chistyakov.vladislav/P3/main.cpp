46813c4e93b0da7c7ed53a213df3109aaaa9b76a
jiggerman <liggerman@mail.ru>
2023-11-22 21:43:18 +0300

    Append checks to sequence (overflow, problem with dynamic memary)

--- chistyakov.vladislav/P3/main.cpp
+++ chistyakov.vladislav/P3/main.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
+#include <limits>
+#include <stdexcept>
 
 char * enlargeArray(char * array, size_t size)
 {
   char * newArray = new char[size + 10]{};
-  for (int i = 0; i < size; ++i)
+
+  try
   {
-    newArray[i] = array[i];
+    for (int i = 0; i < size; ++i)
+    {
+      newArray[i] = array[i];
+    }
+    delete [] array;
+    return newArray;
   }
-  delete [] array;
-  return newArray;
+  catch (std::logic_error & e)
+  {
+    std::cerr << e.what() << "\n";
+    delete [] newArray;
+    return nullptr;
+  }
+}
+
+bool overflow(size_t number)
+{
+  size_t max_size = std::numeric_limits< size_t >::max();
+  return number == max_size;
 }
 
 int main()
@@ -26,12 +44,30 @@ int main()
     {
       break;
     }
+    else if (!std::cin)
+    {
+      std::cerr << "Bad input!\n";
+      delete [] array;
+      return 1;
+    }
+
+    if (overflow)
+    {
+      delete [] array;
+      std::cerr << "Sequecne is too long\n";
+      return 2;
+    }
 
     array[i++] = input;
 
     if (i == size)
     {
-      array = enlargeArray(array, size);
+      do
+      {
+        array = enlargeArray(array, size);
+      }
+      while (array != nullptr);
+
       size += 10;
     }
   }
