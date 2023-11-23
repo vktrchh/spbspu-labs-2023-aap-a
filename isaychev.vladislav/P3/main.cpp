c3ca23e7a75219d04d017e2082bcd2624e243979
Z0ltaan <gregnov69@gmail.com>
2023-11-23 23:53:48 +0300

    added result string for 1st task, added counters for lengths of strings

--- isaychev.vladislav/P3/main.cpp
+++ isaychev.vladislav/P3/main.cpp
@@ -1,40 +1,50 @@
 #include <iostream>
 #include <iomanip>
+#include <cstddef>
 
 int main()
 {
   int capacity = 10;
   char * str = nullptr;
   str = new char[capacity]{};
-  int i = 0;
   char curr_char = 0;
+  size_t length1 = 0;
   std::cin >> std::noskipws;
-  while ((std::cin >> curr_char) && (i < capacity))
+  while ((std::cin >> curr_char) && (length1 < capacity))
   {
-    str[i] = curr_char;
+    str[length1] = curr_char;
     if (curr_char == '\n')
     {
-      str[i] = '\0';
+      str[length1] = '\0';
       break;
     }
-    if (i == capacity - 1)
+    if (length1 == capacity - 1)
     {
       char * str_temp = new char[capacity]{};
-      for (int j = 0; j < i; ++j)
+      for (int j = 0; j < length1; ++j)
       {
         str_temp[j] = str[j];
       }
       delete [] str;
       capacity += 10;
       str = new char[capacity]{};
-      for (int j = 0; j < i; ++j)
+      for (int j = 0; j < length1; ++j)
       {
         str[j] = str_temp[j];
       }
       delete [] str_temp;
     }
-    ++i;
+    length1++;
   }
+  const char * str2 = "AFD12hd";
+  size_t length2 = 0;
+  while (str2[length2] != '\0')
+  {
+    length2++;
+  }
+  char * str_after_extr = nullptr;
+  str_after_extr = new char[capacity]{};
+
 // for tests
 
 /*  for (int k = 0; k < capacity; ++k)
@@ -42,5 +52,6 @@ int main()
     std::cout << str[k] << " ";
   }
   std::cout << "\n" << capacity << "\n";*/
+  delete [] str_after_extr;
   delete [] str;
 }
