20133cb0a9feeffbf7a90eafb474e93612b9e714
SlivkaSadovka <strelyaev_ra@spbstu.ru>
2023-11-23 19:25:37 +0300

    function fix

--- strelyaev.roman/P3/main.cpp
+++ strelyaev.roman/P3/main.cpp
@@ -12,12 +12,12 @@ void moveArray(char * old_array, size_t old_array_size, char * new_array)
 
 char * inputArray(char * old_array, size_t old_array_size)
 {
-  char * new_array = new char [old_array_size + 50];
+  char * new_array = new char [old_array_size + 10];
   moveArray(old_array, old_array_size, new_array);
 
   char c = 0;
-  size_t i = 0;
-  while ((c != '\n') && (i < old_array_size + 49))
+  size_t i = old_array_size - 1;
+  while ((c != '\n') && (i < old_array_size + 9))
   {
     std::cin >> c;
     new_array[i] = c;
@@ -28,8 +28,7 @@ char * inputArray(char * old_array, size_t old_array_size)
   {
     return new_array;
   }
-
-  return inputArray(new_array, old_array_size+50);
+  return inputArray(new_array, old_array_size + 10);
 }
 
 int main()
@@ -38,6 +37,5 @@ int main()
   char * start_array = new char [1];
   char * end_array = inputArray(start_array, 1);
   std::cin >> std::skipws;
-
-  std::cout << end_array;
+  std::cout << end_array << "\n";
 }
