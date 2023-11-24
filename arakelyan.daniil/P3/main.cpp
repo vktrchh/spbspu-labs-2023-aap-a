d4a7ec2eec8e984e9ba832e1bd5030ece3967070
osakasonyboy@gmail.com <sensh0god@gmail.com>
2023-11-24 05:26:21 +0300

    practice, added reading, but it doesnt work

--- arakelyan.daniil/P3/main.cpp
+++ arakelyan.daniil/P3/main.cpp
@@ -1,2 +1,63 @@
+#include <exception>
+#include <iostream>
+#include <stdexcept>
+#include <cctype>
+
+char * inputArray(size_t & arrSize);
+
 int main()
-{}
+{
+  size_t arrSize = 0;
+  char * outString = nullptr;
+  try {
+    outString = inputArray(arrSize);
+    std::cout << outString << "\n";
+  } catch (const std::exception & e) {
+    std::cerr << "Error: " << e.what() << "\n";
+    delete [] outString;
+    return 1;
+  }
+
+}
+
+
+char * inputArray(size_t & arrSize)
+{
+  const size_t defBufferSize = 10;
+  arrSize = defBufferSize;
+
+  size_t index = 0;
+  char sym = 0;
+
+  char * mainBuffer = new char[arrSize];
+  if (!mainBuffer)
+  {
+    throw std::logic_error("Can't create mainBuffer");
+  }
+
+  while ((std::cin >> sym) && sym != '\n')
+  {
+    if (index == arrSize - 1)
+    {
+      arrSize *= 2;
+      
+      char * tempBuffer = new char[arrSize];
+      if (!tempBuffer)
+      {
+        throw std::logic_error("Cant't create tempBuffer");
+      }
+
+      for (size_t j = 0; j < index; j++)
+      {
+        tempBuffer[j] = mainBuffer[j];
+      }
+      
+      delete [] mainBuffer;
+
+      mainBuffer = tempBuffer;
+    }
+    mainBuffer[index] = sym;
+  }
+  
+  return mainBuffer;
+}
