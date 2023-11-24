9ec1d733ff3f8f40ee176e7ab3da050096a210cc
Steuger7 <steuger7@mail.ru>
2023-11-24 17:35:09 +0300

    initial input

--- grechishnikov.roman/P3/main.cpp
+++ grechishnikov.roman/P3/main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <cstddef>
+#include <exception>
 
 int main(int argc, char ** argv)
 {
-  if (argc != 2)
+  size_t len = 10;
+  char * curStr = nullptr;
+  try
   {
-    std::cerr << "Incorrect number of elements in command line\n";
+    curStr = new char[len];
+  }
+  catch (const std::bad_alloc)
+  {
+    std::cerr << "Cannot allocate memory\n";
     return 1;
   }
+
+  char chInput = '0';
+  size_t curPos = 0;
+  std::cin >> std::noskipws;
+  while (chInput != '\n')
+  {
+    std::cin >> chInput;
+    curStr[curPos] = chInput;
+    curPos++;
+  }
+  std::cin >> std::skipws;
 }
