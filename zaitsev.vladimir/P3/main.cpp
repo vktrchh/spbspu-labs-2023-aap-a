de68c10da4f2acd35ec4e0d4743a637e03f1962c
v-zaycev <70699650+v-zaycev@users.noreply.github.com>
2023-11-24 19:39:23 +0300

    Main with strings input

--- zaitsev.vladimir/P3/main.cpp
+++ zaitsev.vladimir/P3/main.cpp
@@ -1,2 +1,24 @@
+#include <iostream>
+#include <stdexcept>
+#include "input.h"
+
 int main()
-{}
+{
+  char* string1 = nullptr;
+  char* string2 = nullptr;
+
+  try 
+  {
+    string1 = read_str();
+    string2 = read_str();
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+  std::cout << string1<<" "<<string2;
+  delete[] string1;
+  delete[] string2;
+  return 0;
+}
