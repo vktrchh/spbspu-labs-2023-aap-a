7d6de51fa313f2f479223a88d3f834086b52ca21
osakasonyboy@gmail.com <sensh0god@gmail.com>
2023-11-24 17:30:26 +0300

    added arrays for the second task with two lines

--- arakelyan.daniil/P3/main.cpp
+++ arakelyan.daniil/P3/main.cpp
@@ -2,65 +2,33 @@
 #include <iostream>
 #include <cctype>
 #include <stdexcept>
-
-char * inputArray(size_t & arrSize);
+#include "inputArray.hpp"
 
 int main()
 {
   size_t arrSize = 0;
-  char * ourString = nullptr;
+  char * taskOneString = nullptr;
+
+  size_t arrSizeTaskTwo = 0;
+  char * taskTwoStringOne = nullptr;
+  char * taskTwoStringTwo = nullptr;
+
   try
   {
-   ourString = inputArray(arrSize);
+    taskOneString = ara::inputArray(arrSize);
+    taskTwoStringOne = ara::inputArray(arrSizeTaskTwo);
+    taskTwoStringTwo = ara::inputArray(arrSizeTaskTwo);
   }
   catch (const std::exception & e)
   {
     std::cerr << "Error: " << e.what() << "\n";
-    delete [] ourString;
+    delete [] taskOneString;
+    delete [] taskTwoStringOne;
+    delete [] taskTwoStringTwo;
     return 1;
   }
-  std::cout << ourString;
+  std::cout << "taks 1 str - " << taskOneString << "\n";
+  std::cout << "taks 2 str 1 - " << taskTwoStringOne << "\n";
+  std::cout << "taks 2 str 2 - " << taskTwoStringTwo << "\n";
   return 0;
-}
-
-
-char * inputArray(size_t & arrSize)
-{
-  const size_t defBufferSize = 10;
-  arrSize = defBufferSize;
-
-  size_t index = 0;
-  char sym = 0;
-
-  char * mainBuffer = new char[arrSize];
-  if (!mainBuffer)
-  {
-    throw std::logic_error("Can't create mainBuffer");
-  }
-  std::cin >> std::noskipws;
-  while ((std::cin >> sym) && (sym != '\n'))
-  {
-    if (index == arrSize - 1)
-    {
-      arrSize *= 2;
-
-      char * tempBuffer = new char[arrSize];
-      if (!tempBuffer)
-      {
-        throw std::logic_error("Cant't create tempBuffer");
-      }
-
-      for (size_t j = 0; j < index; j++)
-      {
-        tempBuffer[j] = mainBuffer[j];
-      }
-
-      delete [] mainBuffer;
-
-      mainBuffer = tempBuffer;
-    }
-    mainBuffer[index++] = sym;
-  }
-  mainBuffer[index] = '\0';
-  return mainBuffer;
 }
