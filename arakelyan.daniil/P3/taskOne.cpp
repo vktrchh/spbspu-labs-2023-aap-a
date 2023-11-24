5e68257a0df5e80f050e5c1890d418cd8943bc66
osakasonyboy@gmail.com <sensh0god@gmail.com>
2023-11-24 23:45:43 +0300

    commented

--- arakelyan.daniil/P3/taskOne.cpp
+++ arakelyan.daniil/P3/taskOne.cpp
@@ -4,7 +4,7 @@ size_t ara::countOfdifferentLetters(char *myArray, size_t arraySize)
 {
   size_t arr[arraySize];
   size_t countOfDifferentLet = 0;
-  int index = 0;
+  // int index = 0;
   for (size_t i = 0; myArray[i] != '\0'; i++)
   {
     if (std::isalpha(myArray[i]))
@@ -13,7 +13,7 @@ size_t ara::countOfdifferentLetters(char *myArray, size_t arraySize)
     }
   }
 
-  for (size_t i = 0; myArray[i] != '\0'; i++)
+  for (size_t i = 0; arr[i] != '\0'; i++)
   {
     ++countOfDifferentLet;
   }
