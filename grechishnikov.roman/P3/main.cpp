3883fbe96673e05aa77cbc8c8f97f75fd9473bd3
Steuger7 <steuger7@mail.ru>
2023-11-24 17:06:53 +0300

    number of elem check

--- grechishnikov.roman/P3/main.cpp
+++ grechishnikov.roman/P3/main.cpp
@@ -1,2 +1,10 @@
-int main()
-{}
+#include <iostream>
+
+int main(int argc, char ** argv)
+{
+  if (argc != 2)
+  {
+    std::cerr << "Incorrect number of elements in command line\n";
+    return 1;
+  }
+}
