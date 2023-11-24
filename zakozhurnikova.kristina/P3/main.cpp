a075313b822f5bc5ee1c97b81232bafdcc281a90
ZakozhurnikovaKristina <kristinazak150@gmail.com>
2023-11-24 14:06:27 +0300

    enterning variables

--- zakozhurnikova.kristina/P3/main.cpp
+++ zakozhurnikova.kristina/P3/main.cpp
@@ -1,1 +1,11 @@
-main {}
+#include <iostream>
+#include <cctype>
+
+int main()
+{
+  size_t size = 20;
+  char *buff = new char[size]{};
+  char *string = nullptr;
+  char **strPtr = nullptr;
+  char c = '\0';
+}
