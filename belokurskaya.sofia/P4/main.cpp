ffff7472bb255a64a07b504424953c5365ab8334
sofabelokurskaya@yandex.ru <sofabelokurskaya@yandex.ru>
2023-11-26 00:34:56 +0300

    fixed

--- belokurskaya.sofia/P4/main.cpp
+++ belokurskaya.sofia/P4/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
+
 
 void freeMatrix(int ** m, size_t rows)
 {
@@ -11,39 +11,54 @@ void freeMatrix(int ** m, size_t rows)
   delete [] m;
 }
 
-void freeMatrix(int ** m, size_t rows, size_t)
-{
-  freeMatrix(m, rows);
-}
-
 int ** createMatrix(size_t rows, size_t cols)
 {
   int ** rowsptrs = new int *[rows];
   for (size_t i = 0; i < rows; ++i)
     {
-      rowsptrs[i] = nullptr;
-    }
-  try
-  {
-    for (size_t i = 0; i < rows; ++i)
-    {
       rowsptrs[i] = new int[cols];
     }
     return rowsptrs;
-  }
-  catch (const std::bad_alloc &)
+}
+
+size_t findMaxRow(int * matrix, size_t rows, size_t cols)
+{
+  size_t maxRow = 0;
+  size_t max = 0;
+
+  for (size_t i = 0; i < rows; ++i)
   {
-    freeMatrix(rowsptrs, rows, cols);
-    throw "Error connected with free store\n";
+    size_t localCount = 1;
+    for (size_t j = 0; j < cols - 1; ++j)
+    {
+      if (matrix[i * cols + j] == matrix[i * cols + j + 1])
+      {
+        ++localCount;
+        if (localCount > max)
+        {
+          max = localCount;
+          maxRow = i + 1;
+        }
+      }
+      else
+      {
+        localCount = 1;
+      }
+    }
   }
+  return maxRow;
+}
+
+void processMatrix(int ** matrix, size_t rows, size_t cols, const char * number)
+{
+  size_t maxRow = findMaxRow(& matrix[0][0], rows, cols);
+
+  std::ofstream output(number);
+  output << maxRow;
 }
 
 int main(int argc, char * argv[])
 {
-  for (int i = 0; i < argc; ++i)
-  {
-    std::cout << argv[i] << "\n";
-  }
   if (argc != 4)
   {
     std::cerr << "Error in command line arguments\n";
@@ -53,28 +68,29 @@ int main(int argc, char * argv[])
   int num = 0;
   try
   {
-    num = std::stoll(argv[1]);
+    num = std::atoi(argv[1]);
   }
   catch (const std::out_of_range &)
   {
-    std::cerr << "Value of first comand line argument is too large\n";
+    std::cerr << "Value of the first comand line argument is too large\n";
     return 1;
   }
   catch (const std::invalid_argument &)
   {
-    std::cerr << "First argument is not a number\n";
+    std::cerr << "The first argument is not a number\n";
     return  1;
   }
 
-  if (num > 2)
+  if (num != 1 && num != 2)
   {
-    std::cerr << "Number of task is wrong\n";
+    std::cerr << "Number of the task is wrong\n";
     return 1;
   }
 
   size_t rows = 0, cols = 0;
-  std::fstream input(argv[2]);
+  std::ifstream input(argv[2]);
   input >> rows >> cols;
+
   if (!input)
   {
     std::cerr << "Cannot read a number\n";
@@ -83,49 +99,29 @@ int main(int argc, char * argv[])
 
   if (num == 1)
   {
-    int matrix[10000];
-    size_t count = 1;
-    size_t maxRow = 0;
-    size_t max = 0;
+    if (rows * cols > 10000)
+    {
+      std::cerr << "Size of the matrix exceeds the limit for a static array\n";
+      return 1;
+    }
+
+    int * matrix = new int[rows * cols];
+
     for (size_t i = 0; i < rows; ++i)
     {
       for (size_t j = 0; j < cols; ++j)
       {
-        input >> matrix[i * rows + j];
-      }
-      if (matrix[i] == matrix[i + 1])
-      {
-        ++count;
-        if (count > max)
-        {
-          max = count;
-          maxRow = i + 1;
-        }
-      }
-      else
-      {
-        count = 1;
+        input >> matrix[i * cols + j];
       }
     }
-    std::ofstream output(argv[3]);
-    output << maxRow;
-
-    input.close();
-    return 0;
+    processMatrix(& matrix, rows, cols, argv[3]);
+    delete [] matrix;
   }
+
   else if (num == 2)
   {
-    int* matrix = new int[rows * cols];
-    try
-    {
-       matrix = new int[rows * cols];
-    }
-    catch (const std::exception& e)
-    {
-      delete [] matrix;
-      std::cerr << e.what();
-      return 4;
-    }
+    int *  matrix = new int[rows * cols];
+
     for (size_t i = 0; i < rows; ++i)
     {
       for (size_t j = 0; j < cols; ++j)
@@ -133,50 +129,9 @@ int main(int argc, char * argv[])
         input >> matrix[i * rows + j];
       }
     }
-    input.close();
-    if (!input)
-    {
-      std::cerr << "Invalid value of matrix argyment\n";
-      return 2;
-    }
-    size_t maxRow = 0;
-    size_t count = 1;
-    size_t max = 0;
-    for (size_t i = 0; i < rows; ++i)
-    {
-      for (size_t j = 0; j < cols - 1; ++j)
-      {
-        if (matrix[i * rows + j] == matrix[i * rows + j + 1])
-        {
-          ++count;
-          if (count > max)
-          {
-            max = count;
-            maxRow = i + 1;
-          }
-          else
-          {
-            max = 0;
-          }
-        }
-        else
-        {
-          count = 1;
-        }
-        if (count > max)
-        {
-          max = count;
-          maxRow = i + 1;
-        }
-        else
-        {
-          max = 0;
-        }
-      }
-    }
+    processMatrix(& matrix, rows, cols, argv[3]);
     delete [] matrix;
-
-    std::ofstream output(argv[3]);
-    output << maxRow;
   }
+  input.close();
+  return 0;
 }
