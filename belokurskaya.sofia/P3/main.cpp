18a6cbcd87efb5ad4c72aadcfb9406318296bfd9
sofabelokurskaya@yandex.ru <sofabelokurskaya@yandex.ru>
2023-11-25 21:31:13 +0300

    try to do my first variant

--- belokurskaya.sofia/P3/main.cpp
+++ belokurskaya.sofia/P3/main.cpp
@@ -1,20 +1,47 @@
 #include <iostream>
 #include <iomanip>
+#include <cstring>
+#include <cctype>
+
 
 char * transformString(const char * input)
 {
   char * result = new char[128];
+
   for (int i = 0; i < 128; ++i)
   {
-    
-  }
+    char c = i;
+    result[i] = c;
 
+    for (size_t j = 0; j < std::strlen(input); ++j)
+    {
+      if (input[j] == c)
+      {
+        result[i] = '\0';
+        break;
+      }
+    }
+  }
 }
+
+
 int main()
 {
-  std::cin >> std::noskipws;
+  try
+  {
+    std::cin >> std::noskipws;
 
-  std::cin >> std::skipws;
-  std::cout << result << "\n";
-  delete [] result;
+    std::cin >> std::skipws;
+    char * result = transformString(input.c_str());
+
+    std::cout << result << "\n";
+    delete [] result;
+    return 0;
+  }
+
+  catch (const std::bad_alloc& e)
+  {
+    std::cerr << "Memory allocation faild\n";
+    return 1;
+  }
 }
