29652b0219cdaa6a26060dc6365cbd1b0b9ebe68
jiggerman <liggerman@mail.ru>
2023-11-26 02:32:12 +0300

    Task solution in the one unit of translation

--- chistyakov.vladislav/P3/main.cpp
+++ chistyakov.vladislav/P3/main.cpp
@@ -29,45 +29,120 @@ bool overflow(size_t number)
   return number == max_size;
 }
 
-void frqTop(char * array, size_t size)
+char * sort(char * array)
 {
-  char * dictChar = new char[128]{};
-  size_t * dictNumsOfChar = new size_t[128]{};
+  char max = 0;
+  char min = 127;
+  char mid = 0;
 
-  char nowChar = 0;
-
-  for (size_t i = 0; i < size; ++i)
+  for (int i = 0; i < 3; ++i)
   {
-    nowChar = array[i];
-    if (nowChar == 0)
+    if (array[i] > max)
     {
-      break;
+      max = array[i];
     }
 
-    for (int j = 0; j < 128; ++j)
+    if (array[i] < min)
     {
-      if (dictChar[j] == 0)
+      min = array[i];
+    }
+  }
+
+  for (int i = 0; i < 3; ++i)
+  {
+    if (array[i] != max && array[i] != min)
+    {
+      mid = array[i];
+      break;
+    }
+  }
+
+  array[0] = min;
+  array[1] = mid;
+  array[2] = max;
+
+  return array;
+}
+
+char * frqTop(char * array, size_t size)
+{
+  char * dictChar = nullptr;
+  size_t * dictNumsOfChar = nullptr;
+  char nowChar = 0;
+
+  try
+  {
+    dictChar = new char[128]{};
+    dictNumsOfChar = new size_t[128]{};
+
+    for (size_t i = 0; i < size; ++i)
+    {
+      nowChar = array[i];
+      if (nowChar == 0)
       {
-        dictChar[j] = nowChar;
-        dictNumsOfChar[j] += 1;
         break;
       }
-      else if (dictChar[j] == nowChar)
+
+      for (int j = 0; j < 128; ++j)
       {
-        dictNumsOfChar[j] += 1;
-        break;
+        if (dictChar[j] == 0)
+        {
+          dictChar[j] = nowChar;
+          dictNumsOfChar[j] += 1;
+          break;
+        }
+        else if (dictChar[j] == nowChar)
+        {
+          dictNumsOfChar[j] += 1;
+          break;
+        }
       }
     }
   }
+  catch(std::logic_error & e)
+  {
+    std::cerr << e.what() << "\n";
+    delete [] dictChar;
+    delete [] dictNumsOfChar;
+
+    return nullptr;
+  }
+
+  char * result = new char[3]{};
+  size_t max = 0;
+  size_t max1 = 0;
+  size_t max2 = 0;
 
   for (int d = 0; d < 128; ++d)
   {
-    if (dictChar[d] == 0)
-    {
-      break;
+    if (dictNumsOfChar[d] > max){
+      result[2] = result[1];
+      result[1] = result[0];
+      result[0] = dictChar[d];
+      max2 = max1;
+      max1 = max;
+      max = dictNumsOfChar[d];
     }
-    std::cout << dictChar[d] << " " << dictNumsOfChar[d] << "\n";
+    else if (dictNumsOfChar[d] > max1)
+    {
+      result[2] = result[1];
+      result[1] = dictChar[d];
+      max2 = max1;
+      max1 = dictNumsOfChar[d];
+    }
+    else if (dictNumsOfChar[d] > max2)
+    {
+      result[2] = dictChar[d];
+      max2 = dictNumsOfChar[d];
+    }
   }
+
+  delete [] dictChar;
+  delete [] dictNumsOfChar;
+
+  result = sort(result);
+
+  return result;
 }
 
 int main()
@@ -116,7 +191,21 @@ int main()
 
   std::cin >> std::skipws;
 
-  frqTop(array, size);
+  char * result = nullptr;
+
+  do
+  {
+    result = frqTop(array, size);
+  }
+  while(result == nullptr);
+
+  for (int i = 0; i < 3; ++i)
+  {
+    std::cout << result[i];
+  }
+  std::cout << "\n";
+
+  delete [] result;
 
   return 0;
 }
