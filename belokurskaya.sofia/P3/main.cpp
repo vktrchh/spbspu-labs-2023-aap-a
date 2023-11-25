00194b4fe6541b2509e7213ed9ffd6dc28e798a8
sofabelokurskaya@yandex.ru <sofabelokurskaya@yandex.ru>
2023-11-25 19:25:49 +0300

    added rtansfornString

--- belokurskaya.sofia/P3/main.cpp
+++ belokurskaya.sofia/P3/main.cpp
@@ -1,19 +1,20 @@
+#include <iostream>
+#include <iomanip>
+
+char * transformString(const char * input)
+{
+  char * result = new char[128];
+  for (int i = 0; i < 128; ++i)
+  {
+    
+  }
+
+}
 int main()
 {
-  char * input = new char[20]{};
-  char c = 0;
-  size_t i = 0;
   std::cin >> std::noskipws;
-  while ((std::cin >> c) && (i < 19))
-  {
-    input[i++] = c;
-    if (c == '\n')
-    {
-      input[i - 1] = 0;
-      break;
-    }
-  }
+
   std::cin >> std::skipws;
-  std::cout << input << "\n";
-  delete [] input;
+  std::cout << result << "\n";
+  delete [] result;
 }
