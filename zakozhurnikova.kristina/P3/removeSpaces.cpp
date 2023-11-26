0eaa9f73907d7375ef2718b1b3187f730d976f68
ZakozhurnikovaKristina <kristinazak150@gmail.com>
2023-11-26 22:17:54 +0300

    broadcast units

---
+++ zakozhurnikova.kristina/P3/removeSpaces.cpp
@@ -0,0 +1,27 @@
+#include "removeSpaces.h"
+
+void removeSpaces(char *dest, const char *src, size_t buff)
+{
+  size_t index = 0;
+  for (size_t i = 1; i < buff; i++)
+  {
+    if (src[i] == '\0')
+    {
+      break;
+    }
+    if (src[i - 1] != ' ')
+    {
+      dest[index] = src[i - 1];
+      index++;
+    }
+    if (src[i] == ' ' && src[i - 1] != ' ')
+    {
+      dest[index] = src[i];
+      index++;
+    }
+  }
+  if (dest[index - 1] == ' ')
+  {
+    dest[index - 1] = '\0';
+  }
+}
---
+++ zakozhurnikova.kristina/P3/removeSpaces.h
@@ -0,0 +1,7 @@
+#ifndef REMOVESPACES_H
+#define REMOVESPACES_H
+#include <cstddef>
+
+void removeSpaces(char *dest, const char *src, size_t buff);
+
+#endif
