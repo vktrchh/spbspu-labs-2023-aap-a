fd0dfc8cf502fd4238fc395d408bd373f4713ecc
exxiiint <filipp.sivkov@yandex.ru>
2023-11-27 02:07:36 +0300

    add delete space before string

--- sivkov.filipp/P3/main.cpp
+++ sivkov.filipp/P3/main.cpp
@@ -53,25 +53,30 @@ int main()
   int isSpace = 0;
   int indexArr = 0;
   for (int j = 0; j < k; j++)
+  {
+    if (std::isspace(string[j]))
     {
-      if (std::isspace(string[j]))
+      if (isSpace == 0)
       {
-        if (isSpace == 0)
-        {
-          arrayWithOutSpace[indexArr++] = ' ';
-          isSpace = 1;
-        }
-      }
-      else
-      {
-        arrayWithOutSpace[indexArr++] = string[j];
-        isSpace = 0;
+        arrayWithOutSpace[indexArr++] = ' ';
+        isSpace = 1;
       }
     }
-
-
-
-
+    else
+    {
+      arrayWithOutSpace[indexArr++] = string[j];
+      isSpace = 0;
+    }
+  }
+  if (indexArr > 0 && arrayWithOutSpace[0] == ' ')
+  {
+    for (int j = 0; j < indexArr - 1; j++)
+    {
+      arrayWithOutSpace[j] = arrayWithOutSpace[j + 1];
+    }
+    indexArr--;
+  }
+  arrayWithOutSpace[indexArr] = '\0';
   std::cout << arrayWithOutSpace << "\n";
   delete[] string;
   delete[] arrayWithOutSpace;
