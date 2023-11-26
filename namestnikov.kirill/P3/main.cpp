0ccd9a9f48aecc7d22955ae42474a328cf21f23b
kirillnamestnikov <kir.namestnikov@gmail.com>
2023-11-26 23:36:17 +0300

    Translation units + namespace

--- namestnikov.kirill/P3/main.cpp
+++ namestnikov.kirill/P3/main.cpp
@@ -1,29 +1,11 @@
 #include <iostream>
 #include <iomanip>
 #include <algorithm>
-
-
-void uniqueSymArray(char * result, const char * str1, const char * str2, size_t num1, size_t num2)
-{
-  for (size_t i = 0; i < num1; ++i)
-  {
-     if ((std::find(str1, str1 + num1, str1[i]) != str1 + num1) && (std::find(str2, str2 + num2, str1[i]) == str2 + num2))
-     {
-       *result++ = str1[i];
-     }
-  }
-  for (size_t i = 0; i < num2; ++i)
-  {
-    if ((std::find(str1, str1 + num1, str2[i]) == str1 + num1) && (std::find(str2, str2 + num2, str2[i]) != str2 + num2))
-    {
-      *result++ = str2[i];
-    }
-  }
-}
-
+#include "unique_sym_array.hpp"
 
 int main()
 {
+  using namespace namestnikov;
   char * input1 = new char[20];
   char c1 = 0;
   size_t i1 = 0;
---
+++ namestnikov.kirill/P3/unique_sym_array.hpp
@@ -0,0 +1,11 @@
+#ifndef UNIQUE_SYM_ARRAY_HPP
+#define UNIQUE_SYM_ARRAY_HPP
+
+#include <algorithm>
+namespace namestnikov
+{
+  void uniqueSymArray(char * result, const char * str1, const char * str2, size_t num1, size_t num2);
+}
+
+#endif
+
