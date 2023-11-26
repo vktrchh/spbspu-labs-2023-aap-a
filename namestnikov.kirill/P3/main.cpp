9fb78725565527252ce00eaa77a0892b8fdfedef
kirillnamestnikov <kir.namestnikov@gmail.com>
2023-11-26 20:34:41 +0300

    simple input & output with 2 arrays

--- namestnikov.kirill/P3/main.cpp
+++ namestnikov.kirill/P3/main.cpp
@@ -4,20 +4,36 @@
 
 int main()
 {
-  char * input = new char[20];
-  char c = 0;
-  size_t i = 0;
+  char * input1 = new char[20];
+  char c1 = 0;
+  size_t i1 = 0;
   std::cin >> std::noskipws;
-  while ((std::cin >> c) && (i < 19))
+  while ((std::cin >> c1) && (i1 < 19))
   {
-    input[i++] = c;
-    if (c == '\n')
+    input1[i1++] = c1;
+    if (c1 == '\n')
     {
-      input[i - 1] = 0;
+      input1[i1 - 1] = 0;
       break;
     }
   }
   std::cin >> std::skipws;
-  std::cout << input << "\n";
-  delete[] input;
+  std::cin >> std::noskipws;
+  char * input2 = new char[20];
+  char c2 = 0;
+  size_t i2 = 0;
+  while ((std::cin >> c2) && (i2 < 19))
+  {
+    input2[i2++] = c2;
+    if (c2 == '\n')
+    {
+      input2[i2 - 1] = 0;
+      break;
+    }
+  }
+  std::cin >> std::skipws;
+  std::cout << input1 << "\n";
+  std::cout << input2 << "\n";
+  delete[] input1;
+  delete[] input2;
 }
