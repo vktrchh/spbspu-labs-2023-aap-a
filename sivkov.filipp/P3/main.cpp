e5c2086ec08ac532d4eafac36af29d42d757892e
exxiiint <filipp.sivkov@yandex.ru>
2023-11-26 18:48:35 +0300

    add input string

--- sivkov.filipp/P3/main.cpp
+++ sivkov.filipp/P3/main.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 
-int main
+int main()
 {
+  char c = 0;
+  size_t k = 0;
+  int size = 20;
+  char* string = new char[size];
+
+  if (!string)
+  {
+    std::cerr << "Error with array" << "\n";
+    return 1;
+  }
+
+  std::cin >> std::noskipws;
+  while (std::cin >> c)
+  {
+    string[k++] = c;
+    if (c == '\n')
+    {
+      break;
+    }
+  }
+  std::cout << string << "\n";
+  delete[] string;
+  return 0;
 }
