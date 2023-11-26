e01364ece92a123af41afa030cec65e41c99b7fc
exxiiint <filipp.sivkov@yandex.ru>
2023-11-27 01:46:53 +0300

    add copy-paste array

--- sivkov.filipp/P3/main.cpp
+++ sivkov.filipp/P3/main.cpp
@@ -16,11 +16,38 @@ int main()
   std::cin >> std::noskipws;
   while (std::cin >> c)
   {
+    if ((k == 0) && (c == '\n'))
+    {
+      std::cerr << "Very small string\n";
+      return 1;
+    }
+
     string[k++] = c;
     if (c == '\n')
     {
       break;
     }
+    else
+    {
+      if (k == size - 1)
+      {
+        char* newstring = new char[size * 2];
+        if (!newstring)
+        {
+          std::cerr << "Error with array\n";
+          delete[] string;
+          return 1;
+        }
+        for (int j = 0; j < size - 1; j++)
+        {
+          newstring[j] = string[j];
+        }
+        delete[] string;
+        string = newstring;
+        size *= 2;
+      }
+    }
+
   }
   std::cout << string << "\n";
   delete[] string;
