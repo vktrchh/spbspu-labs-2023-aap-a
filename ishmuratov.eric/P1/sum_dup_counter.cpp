89658bee2e3438548961189654b61068aea15fcb
seep-code <seepox7@gmail.com>
2023-11-26 17:28:38 +0300

    ishmuratov.eric/P1

---
+++ ishmuratov.eric/P1/sum_dup_counter.cpp
@@ -0,0 +1,38 @@
+#include "sum_dup_counter.hpp"
+
+#include <limits>
+#include <stdexcept>
+
+ishmuratov::SumDupCounter::SumDupCounter():
+  sum_count_(0),
+  first_num_(0),
+  second_num_(0),
+  cur_num_(0)
+{}
+
+void ishmuratov::SumDupCounter::operator()(long long int num)
+{
+  first_num_ = second_num_;
+  second_num_ = cur_num_;
+  cur_num_ = num;
+  if (cur_num_ == (first_num_ + second_num_))
+  {
+    size_t max_size = std::numeric_limits< size_t >::max();
+    if (sum_count_ == max_size)
+    {
+      throw std::logic_error("Sequence is too long!");
+    }
+    ++sum_count_;
+  }
+}
+
+size_t ishmuratov::SumDupCounter::operator()() const
+{
+  if ((first_num_ == 0) || (second_num_ == 0) || (cur_num_ == 0))
+  {
+    throw std::logic_error("Sequence is too short!");
+  }
+  return sum_count_;
+}
+
+
