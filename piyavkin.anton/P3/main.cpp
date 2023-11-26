f18ff23ef7453baa5a9eeb205617da49983ce377
Piyavva <piyavkin05@bk.ru>
2023-11-26 20:40:27 +0300

    Error processing

--- piyavkin.anton/P3/main.cpp
+++ piyavkin.anton/P3/main.cpp
@@ -10,17 +10,31 @@ int main()
   while (c != '\n')
   {
     char * a = input;
-    input = new char [n+1];
+    try
+    {
+      input = new char [n+1];
+    }
+    catch (...)
+    {
+      std::cerr << "Dynamic memory overflow";
+      return 1;
+    }
     for (size_t j = 0; j < n; ++j)
     {
       input[j] = a[j];
     }
     delete [] a;
     std::cin >> c;
+    if (!std::cin)
+    {
+      std::cerr << "Line not read";
+      return 1;
+    }
     input[i] = c;
     ++i;
     ++n;
   }
+  std::cin >> std::skipws;
   for (size_t i = 0; i < n; ++i)
   {
     std::cout << input[i];
