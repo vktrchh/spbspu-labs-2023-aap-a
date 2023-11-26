d5701c3993db8cfab05b628acdfa402e5658b5f6
pashko2303 <baranov-p.b@yandex.ru>
2023-11-26 12:16:32 +0300

    Input buffer array

--- baranov.pavel/P3/main.cpp
+++ baranov.pavel/P3/main.cpp
@@ -1,2 +1,36 @@
+#include <iostream>
+
+char * inputString(std::istream & input, const size_t bufferSize)
+{
+  char * buffer = new char[bufferSize]{0};
+  char c;
+  size_t i = 0;
+  while ((input >> c) && (i < bufferSize))
+  {
+    buffer[i++] = c;
+    if (c == '\n')
+    {
+      buffer[i - 1] = 0;
+      break;
+    }
+  }
+  return buffer;
+}
+
 int main()
-{}
+{
+  std::cin >> std::noskipws;
+  try
+  {
+    char * buffer = inputString(std::cin, 20);
+    std::cout << buffer << '\n';
+    delete[] buffer;
+  }
+  catch(std::bad_alloc & e)
+  {
+    std::cerr << "Error: " << e.what() << '\n';
+    return 1;
+  }
+  std::cin >> std::skipws;
+}
+
