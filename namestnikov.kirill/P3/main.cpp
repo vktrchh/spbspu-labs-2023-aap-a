999a7efb3bdbeb83c5c4951eab5f6dc4f901f61d
kirillnamestnikov <kir.namestnikov@gmail.com>
2023-11-26 20:23:34 +0300

    Tried simple input & output

--- namestnikov.kirill/P3/main.cpp
+++ namestnikov.kirill/P3/main.cpp
@@ -1,2 +1,23 @@
+#include <iostream>
+#include <iomanip>
+
+
 int main()
-{}
+{
+  char * input = new char[20];
+  char c = 0;
+  size_t i = 0;
+  std::cin >> std::noskipws;
+  while ((std::cin >> c) && (i < 19))
+  {
+    input[i++] = c;
+    if (c == '\n')
+    {
+      input[i - 1] = 0;
+      break;
+    }
+  }
+  std::cin >> std::skipws;
+  std::cout << input << "\n";
+  delete[] input;
+}
