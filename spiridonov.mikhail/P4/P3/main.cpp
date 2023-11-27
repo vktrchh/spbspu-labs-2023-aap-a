20618a29e995a804eff25c70d737826bd85e9a2c
mordw1n <lolpovar0@gmail.com>
2023-11-27 07:03:45 +0300

    add try catch

--- spiridonov.mikhail/P4/P3/main.cpp
+++ spiridonov.mikhail/P4/P3/main.cpp
@@ -7,15 +7,41 @@ using namespace spiridonov;
 
 int main()
 {
-  const int max_size = 100;
+  const size_t max_size = 100;
   char str[max_size];
+  try
+  {
+    std::cout << "Enter a string: ";
+    std::cin.getline(str, max_size);
+  }
 
-  std::cout << "Enter a string: ";
-  std::cin.getline(str, max_size);
+  catch (const char* error)
+  {
+    std::cerr << error << "\n";
+    return 1;
+  }
+  char array[] = { 0 };
 
-  int result = spiridonov::hasConsecutiveDuplicates(str);
+  size_t result = spiridonov::hasConsecutiveDuplicates(str);
 
-  std::cout << "Result: " << result << "\n";
+  try
+  {
+    if (result == 1)
+    {
+      std::cout << "Array has consecutive duplicates\n";
+    }
+    else
+    {
+      std::cout << "Array does not have consecutive duplicates\n";
+    }
+  }
+  catch (const char* error)
+  {
+    std::cerr << error << "\n";
+    delete[] str;
+    return 1;
+  }
 
+  std::cout << "Result: " << spiridonov::hasConsecutiveDuplicates(str) << "\n";
   return 0;
 }
