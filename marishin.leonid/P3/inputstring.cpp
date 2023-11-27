68a5682d805c0508bedee4dd2d9677293bf6234c
marlions <marishin.lv@edu.spbstu.ru>
2023-11-27 06:30:49 +0300

    input string cpp

---
+++ marishin.leonid/P3/inputstring.cpp
@@ -0,0 +1,55 @@
+#include "inputstring.hpp"
+
+char* marishin::inputString(std::istream& input)
+{
+  const size_t initialBufferSize = 10;
+  char* buffer = nullptr;
+  try
+  {
+    buffer = new char[initialBufferSize];
+  }
+  catch (const std::bad_alloc& ba)
+  {
+    delete[] buffer;
+    throw ba;
+  }
+  size_t bufferSize = initialBufferSize;
+  size_t currentIndex = 0;
+  std::cout << "Vvedite stroky: ";
+  char currentChar = 0;
+  while ((std::cin >> currentChar) && (currentChar != '\n'))
+  {
+    if (currentIndex == (bufferSize - 1))
+    {
+      bufferSize *= 2;
+      char* newBuffer = nullptr;
+      try
+      {
+        newBuffer = new char[bufferSize];
+      }
+      catch (const std::bad_alloc& ba)
+      {
+        delete[] buffer;
+        throw ba;
+      }
+      for (int i = 0; i < currentIndex; ++i)
+      {
+        newBuffer[i] = buffer[i];
+      }
+      delete[] buffer;
+      buffer = newBuffer;
+    }
+    buffer[currentIndex] = currentChar;
+    currentIndex++;
+  }
+  buffer[currentIndex] = '\0';
+  if (buffer[0] == '\0')
+  {
+    delete[] buffer;
+    throw std::logic_error("empty input!");
+  }
+  else
+  {
+    return buffer;
+  }
+}
