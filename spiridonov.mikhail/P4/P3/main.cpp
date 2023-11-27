bcaa3c1bc4ad79ddf37534e4d9d2e70f2000e81b
mordw1n <lolpovar0@gmail.com>
2023-11-27 06:55:41 +0300

    first commit function

--- spiridonov.mikhail/P4/P3/main.cpp
+++ spiridonov.mikhail/P4/P3/main.cpp
@@ -1,2 +1,33 @@
+#include <iostream>
+#include <cstring>
+#include <cctype>
+
+int hasConsecutiveDuplicates(const char* str)
+{
+  int length = strlen(str);
+
+  for (int i = 1; i < length; i++)
+  {
+    if (str[i] == str[i - 1])
+    {
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
 int main()
-{}
+{
+  const int max_size = 100;
+  char str[max_size];
+
+  std::cout << "Enter a string: ";
+  std::cin.getline(str, max_size);
+
+  int result = hasConsecutiveDuplicates(str);
+
+  std::cout << "Result: " << result << "\n";
+
+  return 0;
+}
