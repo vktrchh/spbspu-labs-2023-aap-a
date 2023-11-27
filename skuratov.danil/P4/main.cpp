6f9cd71d256274d73c1855dd7c0625975f0d51e1
Skuratov09 <skyratovdaniil28@gmail.com>
2023-11-27 22:57:40 +0300

    Second commit

--- skuratov.danil/P4/main.cpp
+++ skuratov.danil/P4/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <fstream>
+#include <cctype>
 
 int main(int argc, char* argv[])
 {
-
+  if (argc < 4)
+  {
+    std::cerr << "Not enough parameters" << '\n';
+  }
+  else if(argc > 4)
+  {
+    std::cerr << "Too many arguments" << '\n';
+  }
+  int taskNumber = std::atoi(argv[1]);
+  if (taskNumber == 1)
+  {
+  
+  }
+  else if (taskNumber == 2)
+  {
+  
+  }
+  else
+  {
+  	std::cerr << "The first parameter is out of range" << '\n';
+  }
+  char* inputFileName = argv[2];
+  std::ifstream in(inputFileName);
+  if (!in)
+  {
+  	std::cerr << "Invalid input file" << '\n';
+  }
+  return 0;
 }
