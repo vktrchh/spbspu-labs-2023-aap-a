5cb65d876e32784ebfe7787c2830b57ccede70c7
marlions <marishin.lv@edu.spbstu.ru>
2023-11-27 06:24:28 +0300

    modified string

---
+++ marishin.leonid/P3/modifiedstring.hpp
@@ -0,0 +1,10 @@
+#ifndef MODIFIED_STRING
+#define MODIFIED_STRING
+
+#include <new>
+
+namespace marishin
+{
+  char* modifieldstring(const char* stroka, char targetChar, char replacementChar);
+}
+#endif
