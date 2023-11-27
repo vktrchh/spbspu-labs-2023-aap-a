deb42d3a0969abb3808d4c3445500ba30741cfe2
Piyavva <piyavkin05@bk.ru>
2023-11-27 14:45:26 +0300

    Letters are filled into an array

--- piyavkin.anton/P3/main.cpp
+++ piyavkin.anton/P3/main.cpp
@@ -11,6 +11,19 @@ char * CpLine(char * old_line, char * new_line, size_t length)
   return new_line;
 }
 
+bool * Alphabet(bool * alphabet, char * line, size_t length)
+{
+  for (size_t i = 0; i < length; ++i)
+  {
+    int symbol = line[i]-'a';
+    if (symbol >= 0 && symbol <= 26)
+    {
+      alphabet[symbol] = true;
+    }
+  }
+  return alphabet;
+}
+
 int main()
 {
   size_t length = 0;
@@ -45,9 +58,11 @@ int main()
   }
   std::cin >> std::skipws;
   bool * alphabet = nullptr;
+  char * new_alphabet = nullptr;
   try
   {
     alphabet = new bool [26]{};
+    new_alphabet = new char [0]{};
   }
   catch (...)
   {
@@ -55,22 +70,36 @@ int main()
     delete [] new_line;
     return 1;
   }
-  for (size_t i = 0; i < length; ++i)
-  {
-    int symbol = new_line[i]-'a';
-    if (symbol >= 0 && symbol <= 26)
-    {
-      alphabet[symbol] = true;
-    }
-  }
+  alphabet = Alphabet(alphabet, new_line, length);
+  size_t size = 0;
+  size_t n = 0;
   for (int i = 0; i < 26; ++i)
   {
     if (alphabet[i] == false)
     {
       char letter = 'a' + i;
-      std::cout << letter;
+      char * old_alphabet = new_alphabet;
+      try
+      {
+        new_alphabet = new char [size + 1]{};
+      }
+      catch (...)
+      {
+        std::cerr << "Dynamic memory overflow";
+        delete [] old_alphabet;
+        return 1;
+      }
+      new_alphabet = CpLine(old_alphabet, new_alphabet, size);
+      new_alphabet[n] = letter;
+      ++size;
+      ++n;
     }
   }
   delete [] new_line;
   delete [] alphabet;
+  for (size_t i = 0; i < n; ++i)
+  {
+    std::cout << new_alphabet[i];
+  }
+  delete [] new_alphabet;
 }
