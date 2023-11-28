90ce5f62afed1e95cf17fc21b0283bd11e89feab
Piyavva <piyavkin05@bk.ru>
2023-11-28 16:22:53 +0300

    Removed code repetition and other minor errors

--- piyavkin.anton/P4/main.cpp
+++ piyavkin.anton/P4/main.cpp
@@ -13,61 +13,63 @@ int main(int argc, char * argv[])
     std::cerr << "Arguments entered incorrectly\n";
     return 1;
   }
-  char * endOfParsing = nullptr;
-  int num = std::strtoll(argv[1], &endOfParsing, 10);
-  if (*endOfParsing != '\0')
+  int num = 0;
+  try
   {
-    std::cerr << "The first parameter is incorrectly specified\n";
-    return 3;
-  }
-  else
-  {
-    try
-    {
-      if (num != 1 && num != 2)
-      {
-        std::cerr << "The first parameter is incorrectly specified\n";
-        return 3;
-      }
-    }
-    catch (...)
+    char * endOfParsing = nullptr;
+    num = std::strtoll(argv[1], std::addressof(endOfParsing), 10);
+    if (*endOfParsing != '\0')
     {
       std::cerr << "The first parameter is incorrectly specified\n";
       return 3;
     }
-    size_t rows = 0;
-    size_t cols = 0;
-    std::ifstream input(argv[2]);
-    input >> rows >> cols;
-    if (num == 1)
+    if (num != 1 && num != 2)
     {
-      int matrix[10000]{};
-      for (size_t i = 0; i < rows * cols; ++i)
-      {
-        input >> matrix[i];
-      }
-      if (!input)
-      {
-        std::cerr << "Can not read number\n";
-        return 2;
-      }
-      std::ofstream output(argv[3]);
-      output << (LowerTriangularMatrix(rows * cols, rows, cols, matrix) && NonZero(rows * cols, matrix)) << "\n";
-    }
-    if (num == 2)
-    {
-      int * matrix = new int [rows*cols];
-      size_t result = InputArray(input, matrix, rows * cols, rows * cols);
-      if (!input)
-      {
-        std::cerr << "Can not read\n";
-        delete [] matrix;
-        return 2;
-      }
-      std::ofstream output(argv[3]);
-      output << (LowerTriangularMatrix(result, rows, cols, matrix) && NonZero(result, matrix)) << "\n";
-      delete [] matrix;
+      std::cerr << "The first parameter is incorrectly specified\n";
+      return 3;
     }
   }
+  catch (...)
+  {
+    std::cerr << "The first parameter is incorrectly specified\n";
+    return 3;
+  }
+  size_t rows = 0;
+  size_t cols = 0;
+  std::ifstream input(argv[2]);
+  input >> rows >> cols;
+  int * matrix = nullptr;
+  int a[10000]{};
+  if (num == 1)
+  {
+    matrix = a;
+  }
+  if (num == 2)
+  {
+    try
+    {
+      matrix = new int [rows * cols]{};
+    }
+    catch (...)
+    {
+      std::cerr << "Dynamic memory overflow";
+      return 3;
+    }
+  }
+  size_t result = InputArray(input, matrix, rows * cols, rows * cols);
+  if (!input)
+  {
+    if (num == 2)
+    {
+      delete [] matrix;
+    }
+    std::cerr << "Can not read number\n";
+    return 2;
+  }
+  std::ofstream output(argv[3]);
+  output << (LowerTriangularMatrix(result, rows, cols, matrix) && NonZero(rows * cols, matrix)) << "\n";
+  if (num == 2)
+  {
+    delete [] matrix;
+  }
 }
-
