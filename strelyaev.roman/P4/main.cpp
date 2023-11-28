bfaebdebf97e269aa46c22a440b74bfbe2d9e7a1
Slivka <roman.strelyaeff@gmail.com>
2023-11-28 21:31:27 +0300

    first arg fix 1

--- strelyaev.roman/P4/main.cpp
+++ strelyaev.roman/P4/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <cstring>
 #include <cstddef>
 #include <exception>
 #include "matrix.h"
@@ -34,7 +33,9 @@ int main(int argc, char * argv[])
     return 1;
   }
 
-  if ((n < 1) || (n > 2) || (position < (std::strlen(argv[1]))))
+  int i = 0;
+  for (i; argv[1][i] != '\0'; i++);
+  if ((i != 1))
   {
     std::cerr << "1st argument must be 1-2 number" << "\n";
     return 1;
