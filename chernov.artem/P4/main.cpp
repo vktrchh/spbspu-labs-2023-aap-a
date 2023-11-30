d732007cb527e79456c116c69f60082210cb6e58
CA21lmFlame <chernov.ar@spbstu.ru>
2023-11-30 12:22:26 +0300

    input, output

--- chernov.artem/P4/main.cpp
+++ chernov.artem/P4/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
-#include <ifstream>
-#include <ofstream>
+#include <fstream>
 
 int main(int argc, char * argv[])
 {
+  using namespace chernov;
+  int num = 0;
   if (argc != 4)
   {
     std::cerr << "Error in command line arguments\n";
+    return 1;
+  }
+  if (num > 2)
+  {
+    std::cerr << "No such task\n";
     return 1;
   }
   size_t rows = 0;
