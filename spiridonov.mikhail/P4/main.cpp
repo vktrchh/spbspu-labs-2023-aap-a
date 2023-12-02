d5a079c8081fbfc00eae3394f7db06f716846066
mordw1n <lolpovar0@gmail.com>
2023-12-02 18:54:16 +0300

    fatal fixing main.cpp

--- spiridonov.mikhail/P4/main.cpp
+++ spiridonov.mikhail/P4/main.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <fstream>
 #include <cstddef>
+#include <cstdlib>
+#include <stdexcept>
 
 int main(int argc, char* argv[])
 {
@@ -14,14 +16,18 @@ int main(int argc, char* argv[])
     return 1;
   }
 
+  int num = 0;
+
   if ((std::atoi(argv[1]) == 1) || (std::atoi(argv[1]) == 2))
   {
     char * arg = argv[1];
     int length = 0;
+
     while (arg[length] != '\0')
     {
       length++;
     }
+
     if (length > 1)
     {
       std::cerr << "Invalid first argument" << "\n";
@@ -29,35 +35,32 @@ int main(int argc, char* argv[])
     }
   }
 
-  int num = 0;
-
   try
   {
     num = std::atoi(argv[1]);
   }
-
-  catch (const std::exception & e)
+  catch (const std::exception& e)
   {
-    std::cerr << "Error parsing first argument" << "\n";
+    std::cerr << "Error parsing first argument: " << e.what() << "\n";
     return 1;
   }
 
-  if ((num < 1) || (num > 2))
+  if (num < 1 || num > 2)
   {
     std::cerr << "First argument must be 1 or 2" << "\n";
     return 1;
   }
 
-  size_t rows = 0;
-  size_t cols = 0;
   std::ifstream input(argv[2]);
   std::ofstream output(argv[3]);
+  size_t rows = 0;
+  size_t cols = 0;
+
   input >> rows >> cols;
-  size_t min_sum = 0;
 
   if (!input)
   {
-    std::cout << "Failed to open input file: " << argv[2] << "\n";
+    std::cerr << "Failed to open input file: " << argv[2] << "\n";
     return 2;
   }
 
@@ -68,43 +71,38 @@ int main(int argc, char* argv[])
     return 0;
   }
 
-  int * matrix = nullptr;
+  int* matrix = nullptr;
   int interim_matrix[10000] = {};
 
   if (num == 1)
   {
-    try
-    {
-      matrix = interim_matrix;
-    }
-    catch (const std::logic_error & e)
-    {
-      std::cerr << "Failed to creating matrix: " << e.what() << "\n";
-      return 2;
-    }
+    matrix = interim_matrix;
   }
 
-  else if (num == 2)
+  if (num == 2)
   {
     matrix = new int[rows * cols];
-    try
-    {
-      spiridonov::readMatrix(input, matrix, rows, cols);
-    }
-    catch (const std::logic_error & e)
-    {
-      std::cerr << "Failed to allocate memory for matrix: " << e.what() << "\n";
-      delete[] matrix;
-      return 2;
-    }
-    min_sum = spiridonov::getMinimumSum(matrix, rows, cols);
-    delete[] matrix;
   }
 
-  else
+  try
   {
-    std::cout << "Invalid value for num: " << num << "\n";
-    return 1;
+    spiridonov::readMatrix(input, matrix, rows, cols);
+  }
+  catch (const std::logic_error& e)
+  {
+    std::cerr << "Failed to allocate memory for matrix: " << e.what() << "\n";
+    if (num == 2)
+    {
+      delete[] matrix;
+    }
+    return 2;
+  }
+
+  size_t min_sum = spiridonov::getMinimumSum(matrix, rows, cols);
+
+  if (num == 2)
+  {
+    delete[] matrix;
   }
 
   if (!output)
@@ -112,6 +110,7 @@ int main(int argc, char* argv[])
     std::cerr << "Failed to open output file: " << argv[3] << "\n";
     return 1;
   }
+
   output << min_sum << "\n";
   return 0;
 }
