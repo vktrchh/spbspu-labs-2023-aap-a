5cab3adb80447da3aa40537e7950f8632293f37b
dmitei <dima9990860349@icloud.com>
2023-12-02 14:51:25 +0300

    command line processing

--- shabalin.dmitrij/P4/main.cpp
+++ shabalin.dmitrij/P4/main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include <fstream> 
+#include <cstdlib> 
 
-int main() 
+int convert(char * str)
 {
   return 0;
+}
+
+int main(int argc, char * argv[]) 
+{
+  for (int i = 0; i < argc; i++)
+  {
+    std::cout << argv[i] << "\n";
+  }
+  if (argc != 4)
+  {
+    std::cerr << "Error in command line arguments\n";
+    return 1;
+  }
+  //argv[1]
+  int num = std::strtoll(argv[1], nullptr, 10);
+  std::cout << "Num is " << num << "\n";
+
+  //agrv[2]
+  int i = 0;
+  {
+    std::fstream input(argv[2]);
+    input >> i;
+    if (!input)
+    {
+      std::cerr << "Cannot read a number\n";
+      return 2;
+    }
+  }
+
+  //argv[3]
+  std::ofstream output(argv[3]);
+  output << i << "\n";
 }
 
