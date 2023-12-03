d529145bc0c36ef3b0b3753ee9bf6d745a416b74
jiggerman <liggerman@mail.ru>
2023-12-04 02:06:35 +0300

    Removed code repetition and unnecessary function

--- chistyakov.vladislav/P4/main.cpp
+++ chistyakov.vladislav/P4/main.cpp
@@ -4,12 +4,6 @@
 #include "countLocalMinimums.hpp"
 #include "inputArray.hpp"
 
-void writeAnswerInFile(int answer,char * outputFile)
-{
-  std::ofstream output(outputFile);
-  output << answer;
-}
-
 int main(int argc, char** argv)
 {
   using namespace chistyakov;
@@ -31,77 +25,57 @@ int main(int argc, char** argv)
     return 1;
   }
 
-  if (taskNumber == 1)
+  std::ifstream input(argv[2]);
+  size_t cols = 0;
+  size_t rows = 0;
+
+  input >> cols >> rows;
+  if (!input)
   {
-    {
-      std::ifstream input(argv[2]);
-      size_t cols = 0;
-      size_t rows = 0;
+      std::cerr << "Cannot parse number of column or rows\n";
+      return 2;
+  }
 
-      input >> cols >> rows;
-      if (!input)
-      {
-        std::cerr << "Cannot parse number of column or rows\n";
-        return 2;
-      }
+  int * array = nullptr;
 
-      int array[cols * rows];
-      int number = 0;
-      for (size_t i = 0; i < (cols * rows); ++i)
-      {
-        input >> number;
-        if (!input)
-        {
-          std::cerr << "Cannot parse number from file\n";
-          return 2;
-        }
-        array[i] = number;
-      }
-
-      size_t result = 0;
-      result = countLocalMinimums(array, rows, cols);
-      writeAnswerInFile(result, argv[3]);
-
-      return 0;
-    }
+  if (taskNumber == 1)
+  { 
+    int a[10000] = {};
+    array = a;
   }
   else if (taskNumber == 2)
   {
-    std::ifstream input(argv[2]);
-
-    size_t cols = 0;
-    size_t rows = 0;
-
-    input >> cols >> rows;
-
-    if (!input)
-    {
-      std::cerr << "Cannot parse number of column or rows\n";
-      return 2;
-    }
-
-    int * array = new int[rows * cols];
-    size_t numOfElements = 0;
-
-    numOfElements = inputArray(input, array, (rows * cols), (rows * cols));
-    if (numOfElements != (rows * cols))
-    {
-      std::cerr << "Cannot parse numbers from file\n";
-      delete [] array;
-      return 2;
-    }
-
-    size_t result = 0;
-    result = countLocalMinimums(array, rows, cols);
-
-    delete [] array;
-
-    writeAnswerInFile(result, argv[3]);
-    return 0;
+    array = new int[rows * cols];
   }
   else
   {
     std::cerr << "Values is not 1 or 2\n";
     return 1;
   }
+
+  size_t numOfElements = 0;
+
+  numOfElements = inputArray(input, array, (rows * cols), (rows * cols));
+  if (numOfElements != (rows * cols))
+  {
+    std::cerr << "Cannot parse numbers from file\n";
+    if (taskNumber == 2)
+    {
+      delete[] array;
+    }
+    return 2;
+  }
+
+  size_t result = 0;
+  result = countLocalMinimums(array, rows, cols);
+
+  if (taskNumber == 2)
+  {
+    delete[] array;
+  }
+
+  std::ofstream output(argv[3]);
+  output << result;
+
+  return 0;
 }
