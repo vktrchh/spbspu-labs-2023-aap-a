1da049926a9e37f50e703fa35ebef336a2155a8b
Skuratov09 <skyratovdaniil28@gmail.com>
2023-12-04 22:06:55 +0300

    Вроде разобрался

---
+++ skuratov.danil/P4/readArray.h
@@ -0,0 +1,9 @@
+#ifndef READ_ARRAY_H
+#define	READ_ARRAY_H
+
+#include <fstream>
+#include <cstddef>
+
+int* readArray(std::ifstream& in, int* array, const size_t& size);
+
+#endif
---
+++ skuratov.danil/P4/writeArray.h
@@ -0,0 +1,9 @@
+#ifndef WRITE_ARRAY_H
+#define	WRITE_ARRAY_H
+
+#include <fstream>
+#include <cstddef>
+
+void writeArray(std::ostream& out, int input, size_t size);
+
+#endif
