899bcf488a86b7485de9050eecf6d719bc35b6b0
Skuratov09 <skyratovdaniil28@gmail.com>
2023-12-04 22:45:37 +0300

    Полностью переписал все заголовочные файлы

--- skuratov.danil/P4/countSaddlePoints.h
+++ skuratov.danil/P4/countSaddlePoints.h
@@ -1,8 +1,7 @@
 #ifndef COUNT_SADDLE_POINTS_H
-#define	COUNT_SADDLE_POINTS_H
+#define COUNT_SADDLE_POINTS_H
 
 #include <cstddef>
 
 int countSaddlePoints(int* array, size_t rows, size_t cols);
-
 #endif
--- skuratov.danil/P4/writeArray.h
+++ skuratov.danil/P4/writeArray.h
@@ -1,9 +1,8 @@
 #ifndef WRITE_ARRAY_H
-#define	WRITE_ARRAY_H
+#define WRITE_ARRAY_H
 
 #include <fstream>
 #include <cstddef>
 
-void writeArray(std::ostream& out, int input, size_t size);
-
+void wtiteArray(std::ostream& out, int input, size_t size);
 #endif
