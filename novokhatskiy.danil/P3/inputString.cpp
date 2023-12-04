3e278bdade2d7e59293f505d145b81ffa1c64407
NovokhatskiyDanil <karuk.vova2005@mail.ru>
2023-12-04 12:27:42 +0300

    fix

--- novokhatskiy.danil/P3/inputString.cpp
+++ novokhatskiy.danil/P3/inputString.cpp
@@ -1,43 +1,15 @@
-#include "inputString.hpp"
+#include <iostream>
+#include <stdexcept>
+#include <cstddef>
 
-char* novokhatskiy::inputString(std::istream &input)
+void inArray(int& arr[][], size_t rows, size_t cols)
 {
-  size_t sizeString = 20;
-  size_t cycleIndex = 0;
-  char* string = nullptr;
-  try
-  {
-    string = new char[sizeString];
-  }
-  catch (const std::bad_alloc& err)
-  {
-    throw;
-  }
-  char symbol = 0;
-  input >> std::noskipws;
-  while ((input >> symbol) && (symbol != '\n')) 
-  {
-    string[cycleIndex] = symbol;
-    if (cycleIndex >= sizeString)
-    {
-      try
-      {
-        char* newString = new char[sizeString + 15];
-        for (size_t i = 0; i < sizeString; ++i)
-        {
-          newString[i] = string[i];
-        }
-        delete[] string;
-        string = newString;
-        sizeString += 15;
-      }
-      catch (...)
-      {
-        throw;
-      }
-    }
-    cycleIndex++;
-  }
-  input >> std::skipws;
-  return string;
+
+}
+
+int main()
+{     
+  size_t rows = 3;
+  size_t cols = 3;
+  int arr[rows][cosl]
 }
