3e0d20a561a0f521082222c8c5826c4973116fe4
Skuratov09 <skyratovdaniil28@gmail.com>
2023-12-06 01:59:38 +0300

    Fixed main.cpp again

--- skuratov.danil/P4/main.cpp
+++ skuratov.danil/P4/main.cpp
@@ -15,9 +15,14 @@ int main(int argc, char* argv[])
   const size_t MAX_SIZE = 10000;
   size_t rows = 0;
   size_t cols = 0;
-  if (argc != 4)
+  if (argc < 4)
   {
-    std::cerr << "Invalid parameters" << '\n';
+    std::cerr << "Not enough parameters" << '\n'; 
+    return 1;
+  }
+  else if (argc > 4)
+  {
+    std::cerr << "Too many arguments" << '\n';
     return 1;
   }
   const char* inputFileName = argv[2];
@@ -28,22 +33,25 @@ int main(int argc, char* argv[])
   try
   {
     readSize(in, rows, cols);
-  }
-  catch (const std::runtime_error& error)
-  {
-    std::cerr << error.what() << '\n';
-    return 2;
-  }
-  size_t sizeOfArray = rows * cols;
-  int* dynamicArray = new int[sizeOfArray];
-  int staticArray[MAX_SIZE] = { 0 };
-  if ((sizeOfArray < MAX_SIZE) && (rows != 0 || cols != 0))
-  {
+    size_t sizeOfArray = rows * cols;
+    if (sizeOfArray >= MAX_SIZE || (rows == 0 && cols == 0))
+    {
+      std::cerr << "Invalid array size" << '\n';
+      return 1;
+    }
+    int* dynamicArray = new int[sizeOfArray];
+    int staticArray[MAX_SIZE] = {};
     if (taskNumber == 1)
     {
       try
       {
         readArray(in, staticArray, MAX_SIZE);
+        if (rows * cols == 0)
+        {
+          std::cerr << "Empty array for task 1" << '\n';
+          delete[] dynamicArray;
+          return 1;
+        }
         int res = countSaddlePoints(staticArray, rows, cols);
         writeResult(out, res);
       }
@@ -51,15 +59,20 @@ int main(int argc, char* argv[])
       {
         std::cerr << error.what() << '\n';
         delete[] dynamicArray;
-        return 2;
+        return 1;
       }
-      delete[] dynamicArray;
     }
     else if (taskNumber == 2)
     {
       try
       {
         readArray(in, dynamicArray, sizeOfArray);
+        if (rows * cols == 0)
+        {
+          std::cerr << "Empty array for task 2" << '\n';
+          delete[] dynamicArray;
+          return 1;
+        }
         int res = findMinSumAlongSecondaryDiagonal(dynamicArray, rows, cols);
         writeResult(out, res);
       }
@@ -67,14 +80,21 @@ int main(int argc, char* argv[])
       {
         std::cerr << error.what() << '\n';
         delete[] dynamicArray;
-        return 2;
+        return 1;
       }
-      delete[] dynamicArray;
-    }
+    } 
     else
     {
+      std::cerr << "Invalid first parameter" << '\n';
       delete[] dynamicArray;
+      return 1;
     }
+    delete[] dynamicArray;
+  }
+  catch (const std::runtime_error& error)
+  {
+    std::cerr << error.what() << '\n'; 
+    return 1;
   }
   return 0;
 }
