72103f9ab94103a5533dac2328062638ce612b74
Skuratov09 <skyratovdaniil28@gmail.com>
2023-12-06 02:47:48 +0300

    Fixed

--- skuratov.danil/P4/main.cpp
+++ skuratov.danil/P4/main.cpp
@@ -15,21 +15,25 @@ int main(int argc, char* argv[])
   const size_t MAX_SIZE = 10000;
   size_t rows = 0;
   size_t cols = 0;
+
   if (argc < 4)
   {
-    std::cerr << "Not enough parameters" << '\n';
-    return 1;
+      std::cerr << "Not enough parameters" << '\n';
+      return 1;
   }
   else if (argc > 4)
   {
-    std::cerr << "Too many arguments" << '\n';
-    return 1;
+      std::cerr << "Too many arguments" << '\n';
+      return 1;
   }
+
   const char* inputFileName = argv[2];
   const char* outputFileName = argv[3];
   int taskNumber = std::atoi(argv[1]);
+
   std::ifstream in(inputFileName);
   std::ofstream out(outputFileName);
+
   try
   {
     readSize(in, rows, cols);
@@ -38,10 +42,15 @@ int main(int argc, char* argv[])
     {
       int* dynamicArray = new int[sizeOfArray];
       int staticArray[MAX_SIZE] = {};
+
       if (taskNumber == 1)
       {
         try
         {
+          if (rows * cols == 0)
+          {
+            throw std::runtime_error("Not enough data for task 1");
+          }
           readArray(in, staticArray, MAX_SIZE);
           int res = countSaddlePoints(staticArray, rows, cols);
           writeResult(out, res);
@@ -50,14 +59,17 @@ int main(int argc, char* argv[])
         {
           std::cerr << error.what() << '\n';
           delete[] dynamicArray;
-          return 0;
+          return 1;
         }
-        delete[] dynamicArray;
       }
       else if (taskNumber == 2)
       {
         try
         {
+          if (sizeOfArray == 0)
+          {
+            throw std::runtime_error("Not enough data for task 2");
+          }
           readArray(in, dynamicArray, sizeOfArray);
           int res = findMinSumAlongSecondaryDiagonal(dynamicArray, rows, cols);
           writeResult(out, res);
@@ -66,9 +78,8 @@ int main(int argc, char* argv[])
         {
           std::cerr << error.what() << '\n';
           delete[] dynamicArray;
-          return 0;
+          return 1;
         }
-        delete[] dynamicArray;
       }
       else
       {
@@ -76,6 +87,7 @@ int main(int argc, char* argv[])
         delete[] dynamicArray;
         return 1;
       }
+      delete[] dynamicArray;
     }
   }
   catch (const std::runtime_error& error)
