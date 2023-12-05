9854ffd907ea75b78ae272e4f65d460e44aa31f1
Skuratov09 <skyratovdaniil28@gmail.com>
2023-12-06 01:41:34 +0300

    Fixed main.cpp

--- skuratov.danil/P4/main.cpp
+++ skuratov.danil/P4/main.cpp
@@ -15,14 +15,9 @@ int main(int argc, char* argv[])
   const size_t MAX_SIZE = 10000;
   size_t rows = 0;
   size_t cols = 0;
-  if (argc < 4)
+  if (argc != 4)
   {
-    std::cerr << "Not enough parameters" << '\n';
-    return 1;
-  }
-  else if (argc > 4)
-  {
-    std::cerr << "Too many arguments" << '\n';
+    std::cerr << "Invalid parameters" << '\n';
     return 1;
   }
   const char* inputFileName = argv[2];
@@ -33,55 +28,53 @@ int main(int argc, char* argv[])
   try
   {
     readSize(in, rows, cols);
-    size_t sizeOfArray = rows * cols;
-    if ((sizeOfArray < MAX_SIZE) && (rows != 0 || cols != 0))
-    {
-      int* dynamicArray = new int[sizeOfArray];
-      int staticArray[MAX_SIZE] = {};
-      if (taskNumber == 1)
-      {
-        try
-        {
-          readArray(in, staticArray, MAX_SIZE);
-          int res = countSaddlePoints(staticArray, rows, cols);
-          writeResult(out, res);
-        }
-        catch (const std::runtime_error& error)
-        {
-          std::cerr << error.what() << '\n';
-          delete[] dynamicArray;
-          return 0;
-        }
-        delete[] dynamicArray;
-      }
-      else if (taskNumber == 2)
-      {
-        try
-        {
-        readArray(in, dynamicArray, sizeOfArray);
-        int res = findMinSumAlongSecondaryDiagonal(dynamicArray, rows, cols);
-        writeResult(out, res);
-        }
-        catch (const std::runtime_error& error)
-        {
-          std::cerr << error.what() << '\n';
-          delete[] dynamicArray;
-          return 0;
-        }
-       delete[] dynamicArray;
-      }
-      else
-      {
-        std::cerr << "Invalid first parameter" << '\n';
-        delete[] dynamicArray;
-        return 1;
-      }
-    }
   }
   catch (const std::runtime_error& error)
   {
     std::cerr << error.what() << '\n';
     return 2;
+  }
+  size_t sizeOfArray = rows * cols;
+  int* dynamicArray = new int[sizeOfArray];
+  int staticArray[MAX_SIZE] = { 0 };
+  if ((sizeOfArray < MAX_SIZE) && (rows != 0 || cols != 0))
+  {
+    if (taskNumber == 1)
+    {
+      try
+      {
+        readArray(in, staticArray, MAX_SIZE);
+        int res = countSaddlePoints(staticArray, rows, cols);
+        writeResult(out, res);
+      }
+      catch (const std::runtime_error& error)
+      {
+        std::cerr << error.what() << '\n';
+        delete[] dynamicArray;
+        return 2;
+      }
+      delete[] dynamicArray;
+    }
+    else if (taskNumber == 2)
+    {
+      try
+      {
+        readArray(in, dynamicArray, sizeOfArray);
+        int res = findMinSumAlongSecondaryDiagonal(dynamicArray, rows, cols);
+        writeResult(out, res);
+      }
+      catch (const std::runtime_error& error)
+      {
+        std::cerr << error.what() << '\n';
+        delete[] dynamicArray;
+        return 2;
+      }
+      delete[] dynamicArray;
+    }
+    else
+    {
+      delete[] dynamicArray;
+    }
   }
   return 0;
 }
