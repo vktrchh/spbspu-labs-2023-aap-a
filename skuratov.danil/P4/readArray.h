db3faec181f820d635802ea97422e27170129ecc
Skuratov09 <skyratovdaniil28@gmail.com>
2023-12-06 10:57:55 +0300

    Fixed 100% the last time

--- skuratov.danil/P4/readArray.h
+++ skuratov.danil/P4/readArray.h
@@ -1,10 +1,8 @@
 #ifndef READ_ARRAY_H
-#define	READ_ARRAY_H
+#define READ_ARRAY_H
 
 #include <fstream>
 #include <cstddef>
 
 int* readArray(std::ifstream& in, int array[], const size_t& rows, const size_t& cols);
-
 #endif
-
