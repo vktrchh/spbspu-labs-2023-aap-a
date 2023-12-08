ca3ebe56894723bdad7e781c5cfce62aee7a6b3a
dmitei <dima9990860349@icloud.com>
2023-12-08 06:29:25 +0300

    func added

---
+++ shabalin.dmitrij/P4/bld_smt_matrix.cpp
@@ -0,0 +1,6 @@
+#include <bld_smt_matrix.hpp>
+
+void shabalin::SmoothMatrix(int * matrix, double * smoothedMatrix, size_t rows, size_t cols)
+{
+  
+}
--- shabalin.dmitrij/P4/main.cpp
+++ shabalin.dmitrij/P4/main.cpp
@@ -3,48 +3,36 @@
 #include <cstdlib> 
 #include <string> 
 
-int convert(char * str)
-{
-  return 0;
-}
-
 int main(int argc, char * argv[]) 
 {
-  for (int i = 0; i < argc; i++)
-  {
-    std::cout << argv[i] << "\n";
-  }
   if (argc != 4)
   {
     std::cerr << "Error in command line arguments\n";
     return 1;
   }
   //argv[1]
-  int num = 0;
-  try 
+  char * task = nullptr;
+  int firstArgument = std::strtoll(argv[1], std::addressof(task), 10);
+  
+  if ((firstArgument > 2) || (firstArgument < 1))
   {
-    num = std::stoll(argv[1]);
-  }
-  catch (const std::invalid_argument &)
-  {
-    std::cerr << "Can not parse a value\n";
-    return 3;
+    std::cerr << "Number of task is out of range" << "\n";
+    return 2;
   }
   //agrv[2]
-  int i = 0;
+  std::fstream input(argv[2]);
+  if (!input)
   {
-    std::fstream input(argv[2]);
-    input >> i;
-    if (!input)
-    {
-      std::cerr << "Cannot read a number\n";
-      return 2;
-    }
+    std::cerr << "Cannot read a number\n";
+    return 2;
   }
-
   //argv[3]
   std::ofstream output(argv[3]);
-  output << i << "\n";
+  if (!output.is_open())
+  {
+    std::cerr << "Error.Output file is not open" << "\n";
+    return 2;
+  }
 }
 
 
