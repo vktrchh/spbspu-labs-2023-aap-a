d6d58928e297a89fbbf210b7cacdca6f100aa28d
vktrchh <viskas6969@gmail.com>
2023-12-10 22:26:24 +0300

    добавлен min_int

--- vojuck.victoria/P1/main.cpp
+++ vojuck.victoria/P1/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
+const int min_int = std::numlic_limits< int >::max();
 int main()
 {
   int num = 0;
   size_t lenght = 0;
-  int maxElement = 0;
+  int max_element = min_int;
   do
   {
     std::cin >> num;
