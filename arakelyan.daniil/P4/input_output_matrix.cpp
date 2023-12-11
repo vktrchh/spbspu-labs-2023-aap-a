af94ccb988e8a3cd7d828b35a01a89bf433e782f
osakasonyboy@gmail.com <sensh0god@gmail.com>
2023-12-11 22:01:43 +0300

    revert 1

--- arakelyan.daniil/P4/input_output_matrix.cpp
+++ arakelyan.daniil/P4/input_output_matrix.cpp
@@ -14,16 +14,12 @@ void arakelyan::inputMatrix(std::istream & input, int * matrix, size_t el)
 std::ostream & arakelyan::outputMatrix(std::ostream & output, const double * matrix, size_t rows, size_t cols)
 {
   output << rows << " " << cols;
-  std::ios_base::fmtflags defFlags = output.flags();
-  std::streamsize defPrecision = output.precision();
   for (size_t i = 0; i < rows; ++i)
   {
     for (size_t j = 0; j < cols; ++j)
     {
-      output << " " << std::fixed << std::setprecision(1) << matrix[i * cols + j];
+      output << " " << std::round(matrix[i * cols + j ]* 10) / 10;
     }
   }
-  output.flags(defFlags);
-  output.precision(defPrecision);
   return output;
 }
