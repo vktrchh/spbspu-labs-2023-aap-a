c35e2d2474abcaefca61840c538edcbb2a2601b2
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2023-12-11 11:05:46 +0300

    start work with files

--- vyzhanov.daniil/P4/main.cpp
+++ vyzhanov.daniil/P4/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include <fstream>
 
-int main(int argc, char * argv[]))
+int main(int argc, char * argv[])
 {
-  if (argc != 3)
+  if (argc != 4)
   {
     std::cerr << "Not enough arguments! \n";
+    return 1;
   }
+  int matrix = 0;
+  {
+    std::ifstream inputFile(argv[2]);
+    inputFile >> matrix;
+    if (!inputFile)
+    {
+      std::cerr << "Not a matrix\n";
+    }
+  }
+  std::ofstream outputFile(argv[3]);
+  outputFile << matrix;
 }
