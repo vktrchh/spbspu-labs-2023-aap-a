81719844c24d1428234ade0a2ce3d0f6b3ae884c
jiggerman <liggerman@mail.ru>
2023-12-15 03:18:09 +0300

    cout?..

--- chistyakov.vladislav/P3/sortingByQuantity.cpp
+++ chistyakov.vladislav/P3/sortingByQuantity.cpp
@@ -1,6 +1,6 @@
 #include "sortingByQuantity.hpp"
 #include <cstddef>
-
+#include <iostream>
 char* chistyakov::sortingByQuantity(char* chars, size_t* nums, size_t lenght)
 {
   for (size_t i = 1; i < lenght; ++i)
@@ -28,9 +28,9 @@ char* chistyakov::sortingByQuantity(char* chars, size_t* nums, size_t lenght)
         break;
     }
   }
-
+  std::cout << chars[0] << chars[1] << chars[2] << "\n";
   char arr[3] = {chars[0], chars[1], chars[2]};
   char* result = arr;
-
+  
   return result;
 }
