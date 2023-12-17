3c4ae38d86529b6894bd43aa35f2c947a7f0f292
SofiyLop <lopatina.sa@edu.spbstu.ru>
2023-12-17 04:32:08 +0300

    Add stoll

--- lopatina.sofia/P4/main.cpp
+++ lopatina.sofia/P4/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
+#include <string>
 
 int main(int argc, char ** argv)
 {
@@ -7,12 +9,34 @@ int main(int argc, char ** argv)
   {
     std::cout << argv[i] << '\n';
   }
-  if (argc != 4)
+  if (argc < 4)
   {
-    std::cerr << "Error in command line arguments\n";
+    std::cerr << "Not enough arguments\n";
     return 1;
   }
+  else if (argc > 4)
+  {
+    std::cerr << "Too many arguments\n";
+    return 1;
+  }
+
   //argv[1] - номер задания (1 - массив фикс размера; 2 - динамический)
+  int num = 0;
+  try
+  {
+    num = std::stoll(argv[1]);
+    std::cout << "Num is " << num << '\n';
+  }
+  catch (const std::out_of_range &)
+  {
+    std::cerr << "Value of first CLA is too large\n";
+    return 1;
+  }
+  catch (const std::invalid_argument &)
+  {
+    std::cerr << "Cannot parse a value\n";
+    return 1;
+  }
 
   //argv[2] - имя файла с массивом
   int i = 0;
