e5fe81b36a2b27e1717d7e78d3e4aa1ba5357696
SofiyLop <lopatina.sa@edu.spbstu.ru>
2023-12-17 03:08:19 +0300

    Read command line args

--- lopatina.sofia/P4/main.cpp
+++ lopatina.sofia/P4/main.cpp
@@ -1,2 +1,15 @@
-int main
-{}
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char ** argv)
+{
+  for (int i = 0; i < argc; ++i)
+  {
+    std::cout << argv[i] << '\n';
+  }
+  if (argc != 4)
+  {
+    std::cerr << "Error in command line arguments\n";
+    return 1;
+  }
+}
