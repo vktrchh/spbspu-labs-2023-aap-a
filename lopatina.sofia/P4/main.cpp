eedda1b71cef98ece1c8e3b27581a0dc7498c0e9
SofiyLop <lopatina.sa@edu.spbstu.ru>
2023-12-17 03:36:23 +0300

    Add fstream

--- lopatina.sofia/P4/main.cpp
+++ lopatina.sofia/P4/main.cpp
@@ -12,4 +12,22 @@ int main(int argc, char ** argv)
     std::cerr << "Error in command line arguments\n";
     return 1;
   }
+  //argv[1] - номер задания (1 - массив фикс размера; 2 - динамический)
+
+  //argv[2] - имя файла с массивом
+  int i = 0;
+  {
+    std::ifstream input(argv[2]);
+    input >> i;
+    if (!input)
+    {
+      std::cerr << "Cannot read a number\n";
+      return 2;
+    }
+  }
+  std::cout << i << '\n';
+
+  //argv[3] - имя файла для результата
+  std::ofstream output(argv[3]);
+  output << i << '\n';
 }
