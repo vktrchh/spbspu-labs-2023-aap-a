24d2a33fe8fb613fcec7fdca72217914c937d6a9
Piyavva <piyavkin05@bk.ru>
2023-12-18 20:19:56 +0300

    Shape

--- piyavkin.anton/T1/main.cpp
+++ piyavkin.anton/T1/main.cpp
@@ -6,6 +6,17 @@ struct point_t
 
 struct rectangle_t
 {
+  double width;
+  double heigth;
+  point_t pos;
+}
+
+class Shape
+{
+  virtual double getArea() = 0;
+  virtual rectangle_t getFrameRect() = 0;
+  virtual point_t move() = 0;
+  virtual void scale() = 0;
 }
 
 int main()
