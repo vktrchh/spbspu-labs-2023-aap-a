86fb8b8fb2e2afd2bd34f170003bd155f86bbb49
exxiiint <filipp.sivkov@yandex.ru>
2023-12-19 01:52:18 +0300

    fix CG

--- sivkov.filipp/P4/main.cpp
+++ sivkov.filipp/P4/main.cpp
@@ -7,84 +7,84 @@
 #include "matrix.hpp"
 #include "localMax.hpp"
 
-int main(int argc, char * argv[])
+int main(int argc, char* argv[])
 {
-  int count = 0;
-  if (argc == 1)
-  {
-    std::cout << "Not enough arguments\n";
-    return 1;
-  }
-  else if ( argc > 4)
-  {
-    std::cout << "Too many arguments\n";
-    return 1;
-  }
-  else if (argc != 4)
-  {
-    std::cout << "Error comand line\n";
-    return 1;
-  }
-  int numOfTask = 0;
-  try
-  {
-    numOfTask = std::stoi(argv[1]);
-  }
-  catch (const std::out_of_range&)
-  {
-    std::cerr << "Value of first CLA is too large\n";
-    return 1;
-  }
-  catch (const std::invalid_argument&)
-  {
-    std::cerr << "Cannot parse a value\n";
-    return 1;
-  }
-  if ((numOfTask != 1) && (numOfTask != 2))
-  {
-    std::cerr << "Error number of task";
-    return 1;
-  }
+    int count = 0;
+    if (argc == 1)
+    {
+        std::cout << "Not enough arguments\n";
+        return 1;
+    }
+    else if (argc > 4)
+    {
+        std::cout << "Too many arguments\n";
+        return 1;
+    }
+    else if (argc != 4)
+    {
+        std::cout << "Error comand line\n";
+        return 1;
+    }
+    int numOfTask = 0;
+    try
+    {
+        numOfTask = std::stoi(argv[1]);
+    }
+    catch (const std::out_of_range&)
+    {
+        std::cerr << "Value of first CLA is too large\n";
+        return 1;
+    }
+    catch (const std::invalid_argument&)
+    {
+        std::cerr << "Cannot parse a value\n";
+        return 1;
+    }
+    if ((numOfTask != 1) && (numOfTask != 2))
+    {
+        std::cerr << "Error number of task";
+        return 1;
+    }
 
-  size_t rows = 1, cols = 1;
-  std::ifstream input(argv[2]);
-  if (!input.is_open())
-  {
-    std::cerr << "Cannot open file!\n";
-    return 2;
-  }
-   if (!input)
-  {
-    std::cerr << "Cannot read numbers!\n";
-    return 2;
-  }
-  input >> rows >> cols;
-  if (rows == 0 && cols == 0)
-  {
+    size_t rows = 1, cols = 1;
+    std::ifstream input(argv[2]);
+    if (!input.is_open())
+    {
+        std::cerr << "Cannot open file!\n";
+        return 2;
+    }
+    if (!input)
+    {
+        std::cerr << "Cannot read numbers!\n";
+        return 2;
+    }
+    input >> rows >> cols;
+    if (rows == 0 && cols == 0)
+    {
+        return 0;
+    }
+    if (input.peek() == EOF)
+    {
+        std::cerr << "empty file!\n";
+        return 2;
+    }
+    if (numOfTask == 1)
+    {
+        int statMatrix[10000] = { 0 };
+        fillMatrix(statMatrix, argv[2], numOfTask);
+        count = localMaxCounter(statMatrix, rows, cols, count, numOfTask);
+    }
+
+    if (numOfTask == 2)
+    {
+        int* dynamicMatrix = new int[cols * rows];
+        fillMatrix(dynamicMatrix, argv[2], numOfTask);
+
+        count = localMaxCounter(dynamicMatrix, rows, cols, count, numOfTask);
+    }
+
+    std::ofstream output(argv[3]);
+    output << count << '\n';
+
     return 0;
-  }
-  if (input.peek() == EOF)
-  {
-    std::cerr << "empty file!\n";
-    return 2;
-  }
-  if (numOfTask == 1)
-  {
-    int statMatrix[10000] = {0};
-    fillMatrix(statMatrix, argv[2], numOfTask);
-    count = localMaxCounter(statMatrix, rows, cols, count, numOfTask);
-  }
-
-  if (numOfTask == 2)
-  {
-    int* dynamicMatrix = new int[cols * rows];
-    fillMatrix(dynamicMatrix, argv[2], numOfTask);
-
-    count = localMaxCounter(dynamicMatrix, rows, cols, count, numOfTask);
-  }
-
-  std::ofstream output(argv[3]);
-  output << count << '\n';
-
-  return 0;
 }
