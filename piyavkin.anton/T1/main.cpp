43ce2ccbd19fa71a7b6c9e35f4b195374d47e08a
Piyavva <piyavkin05@bk.ru>
2023-12-18 20:53:17 +0300

    area

--- piyavkin.anton/T1/main.cpp
+++ piyavkin.anton/T1/main.cpp
@@ -2,23 +2,46 @@ struct point_t
 {
   double x;
   double y;
-}
+};
 
 struct rectangle_t
 {
   double width;
   double heigth;
   point_t pos;
-}
+};
 
 class Shape
 {
-  virtual double getArea() = 0;
-  virtual rectangle_t getFrameRect() = 0;
-  virtual point_t move() = 0;
-  virtual void scale() = 0;
-}
+  public:
+    virtual double getArea() = 0;
+    virtual rectangle_t getFrameRect() = 0;
+    virtual point_t move() = 0;
+    virtual void scale() = 0;
+};
+
+class Rectangle: public Shape
+{
+  public:
+    Rectangle(point_t p1, point_t p2):
+      p1_(p1),
+      p2_(p2)
+    {}
+    virtual double getArea()
+    {
+      return std::abs((p1_.x - p2_.x) * (p1_.y - p2_.y));
+    }
+    virtual rectangle_t getFrameRect()
+    {
+      
+    }
+  private:
+    point_t p1_;
+    point_t p2_;
+};
 
 int main()
 {
+  Rectangle rectangle1;
+  std::cout << rectangle1.getArea(5, 5);
 }
