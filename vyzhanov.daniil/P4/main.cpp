ff3b23a950f949ffa3748dbf6be7febb888918e9
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2023-12-19 15:21:40 +0300

    fix main.cpp

--- vyzhanov.daniil/P4/main.cpp
+++ vyzhanov.daniil/P4/main.cpp
@@ -24,14 +24,35 @@ int main(int argc, char * argv[])
     std::cerr << "Cannot parse a value!\n";
     return 3;
   }
-  int matrix = 0;
+  size_t rows = 0;
+  size_t cols = 0;
+  std::ifstream inputFile(argv[2]);
+  inputFile >> rows >> cols;
+  int staticMatrix[10000] = {};
+  int *matrix = nullptr;
+  if (num == 1)
   {
-    std::ifstream inputFile(argv[2]);
-    inputFile >> matrix;
+      if (!inputFile)
+      {
+        std::cerr << "Not a matrix\n";
+      }
+      matrix = staticMatrix;
+  }
+  else if (num == 2)
+  {
+    matrix = new int[rows * cols];
     if (!inputFile)
     {
       std::cerr << "Not a matrix\n";
     }
+    for (int i = 0; i < rows* cols; i++ )
+    {
+      matrix[i] = 0;
+    }
+  }
+  else
+  {
+    std::cerr << "Number must be 1 or 2\n";
   }
   std::ofstream outputFile(argv[3]);
   outputFile << matrix;
