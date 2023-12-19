5822ec1395ca70caab19f4d1cd9630d4fee0ec04
Skuratov09 <skyratovdaniil28@gmail.com>
2023-12-19 18:38:36 +0300

    2nd commit

--- skuratov.danil/P3/main.cpp
+++ skuratov.danil/P3/main.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 #include <iomanip>
 
-int main ()
+int main()
 {
-  char* input = new char[20] {};
-
-  char c = 0;
+  int num = 10;
+  char* input = new char[num];
   size_t i = 0;
 
+  char c = 0;
   std::cin >> std::noskipws;
-  while ((std::cin >> c) && (i < 19))
+  while (std::cin >> c)
   {
-	input[i++] = c;
-	if (c == '\n')
-	{
-   	  input[i - 1] = 0;
-	  break;
-	}
+    if (c == '\n')
+    {
+      input[i] = '\0';
+      break;
+    }
+    input[i++] = c;
+
+    if (i >= num)
+    {
+      num *= 2;
+      char* symb = new char[num];
+      for (int g = 0; g < i; ++g)
+      {
+        symb[g] = input[g];
+      }
+      delete[] input;
+      input = symb;
+    }
   }
   std::cin >> std::skipws;
-
   std::cout << input << "\n";
   delete[] input;
+  return 0;
 }
