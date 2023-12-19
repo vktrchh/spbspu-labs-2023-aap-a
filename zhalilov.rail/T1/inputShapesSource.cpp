43a8cb45313cf9f5de3ade8d0c51688baac3bdeb
Rail (Susanoo) <zhalilov.rsh@edu.spbstu.ru>
2023-12-19 11:21:40 +0300

    Finished basic realization of inputing function

--- zhalilov.rail/T1/inputShapesSource.cpp
+++ zhalilov.rail/T1/inputShapesSource.cpp
@@ -1,23 +1,39 @@
 #include "inputShapesSource.hpp"
 
 #include <cstring>
+#include <string>
 #include <cctype>
 
 #include "Rectangle.hpp"
 #include "inputString.hpp"
 
-zhalilov::Shape *zhalilov::inputRectangle(char string[])
+zhalilov::Shape *zhalilov::inputRectangle(const char string[])
 {
-  return new Rectangle;
+  double coords[4] = {};
+  size_t delta = 0;
+  size_t rectangleNameLen = 9;
+  const char *newStr = string + rectangleNameLen;
+  for (size_t i = 0; i < 4; i++)
+  {
+    coords[i] = std::stod(newStr, &delta);
+    newStr = newStr + delta;
+  }
+  if (*newStr != '\0')
+  {
+    throw std::invalid_argument("Too many args in rect source");
+  }
+  point_t leftCorner = {coords[0], coords[1]};
+  point_t rightCorner = {coords[2], coords[3]};
+  return new Rectangle(leftCorner, rightCorner);
 }
 
-shapeInputFunc zhalilov::identifyShape(char string[])
+shapeInputFunc zhalilov::identifyShape(const char string[])
 {
   char *names[] = {"RECTANGLE"};
   shapeInputFunc functions[1];
   functions[0] = inputRectangle;
-  size_t namesLen = 1;
-  for (size_t i = 0; i < namesLen; i++)
+  size_t namesSize = 1;
+  for (size_t i = 0; i < namesSize; i++)
   {
     size_t nameLen = strlen(names[i]);
     if ((strncmp(names[i], string, nameLen) == 0)
@@ -29,7 +45,19 @@ shapeInputFunc zhalilov::identifyShape(char string[])
   return nullptr;
 }
 
-zhalilov::Shape *zhalilov::inputShapesSource(size_t &inputedShapes, size_t &processedShapes, double &ratio, std::istream &input)
+bool zhalilov::isScaleInputed(const char string[])
+{
+  char name[] = "SCALE";
+  size_t nameLen = 5;
+  if ((strncmp(name, string, nameLen) == 0)
+    && (isspace(string[nameLen])))
+  {
+    return true;
+  }
+  return false;
+}
+
+zhalilov::Shape **zhalilov::inputShapesSource(size_t &inputedShapes, size_t &processedShapes, double &ratio, std::istream &input)
 {
   inputedShapes = 0;
   processedShapes = 0;
@@ -59,6 +87,10 @@ zhalilov::Shape *zhalilov::inputShapesSource(size_t &inputedShapes, size_t &proc
       continue;
     }
 
+    if (isScaleInputed(string))
+    {
+      return shapes;
+    }
     shapeInputFunc inputFunc = identifyShape(string);
     if (!inputFunc)
     {
--- zhalilov.rail/T1/inputShapesSource.hpp
+++ zhalilov.rail/T1/inputShapesSource.hpp
@@ -6,13 +6,13 @@
 
 #include "Shape.hpp"
 
-using shapeInputFunc = zhalilov::Shape *(*)(char string[]);
-using shapeInputFuncArray = zhalilov::Shape *(*[])(char string[]);
+using shapeInputFunc = zhalilov::Shape *(*)(const char string[]);
 namespace zhalilov
 {
-  Shape *inputShapesSource(size_t &inputedShapes, size_t &processedShapes, double &ratio, std::istream &input);
-  shapeInputFunc identifyShape(char string[]);
-  Shape *inputRectangle(char string[]);
+  Shape **inputShapesSource(size_t &inputedShapes, size_t &processedShapes, double &ratio, std::istream &input);
+  shapeInputFunc identifyShape(const char string[]);
+  Shape *inputRectangle(const char string[]);
+  bool isScaleInputed(const char string[]);
 }
 
 #endif
