1914b401422d7807a7009b757ba09b6918a56a7d
polykisa <s.sanek2005@gmail.com>
2023-12-19 21:30:18 +0300

    more additions to main.cpp

--- skopchenko.alexander/P4/main.cpp
+++ skopchenko.alexander/P4/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char * argv[])
 
   if (argc != 4)
   {
-    std::cerr << "Invalid amount of arguments";
+    std::cerr << "Invalid amount of arguments\n";
     return 1;
   }
 
@@ -17,20 +17,20 @@ int main(int argc, char * argv[])
   }
   catch (const std::logic_error & e)
   {
-    std::cerr << "Invalid first argument";
+    std::cerr << "Invalid first argument\n";
     return 1;
   }
 
   if (arrType != 1 && arrType != 2)
   {
-    std::cerr << "First argument can only be '1' or '2'";
+    std::cerr << "First argument can only be '1' or '2'\n";
     return 1;
   }
 
   std::ifstream iFile(argv[2]);
   if (!iFile.is_open())
   {
-    std::cerr << "Couldn't open input file";
+    std::cerr << "Couldn't open input file\n";
     return 2;
   }
 
@@ -38,4 +38,19 @@ int main(int argc, char * argv[])
   size_t cols = 0;
 
   iFile >> rows >> cols;
+  if (!iFile)
+  {
+    std::cerr << "Couldn't read file\n";
+    return 2;
+  }
+  if (rows == 0 && cols == 0)
+  {
+    std::cout << "Matrix dimensions: 0;0\n";
+    return 0;
+  }
+  else if (rows * cols == 0 || cols != rows)
+  {
+    std::cerr << "Invalid Matrix dimensions\n";
+    return 2;
+  }
 }
