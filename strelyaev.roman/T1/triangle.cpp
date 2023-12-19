4c82578b9745fa7bec52fe9a956afda6d6bd6eee
Slivka <roman.strelyaeff@gmail.com>
2023-12-19 16:04:00 +0300

    triangle impl

--- strelyaev.roman/T1/triangle.cpp
+++ strelyaev.roman/T1/triangle.cpp
@@ -11,7 +11,7 @@ Triangle::Triangle(point_t p1, point_t p2, point_t p3):
 
 double Triangle::getDistance(const point_t p1, const point_t p2) const
 {
-    return std::sqrt(std::pow(p2.x_ - p1.x_, 2) + std::pow(p2.y_ - p1.y_, 2));
+  return std::sqrt(std::pow(p2.x_ - p1.x_, 2) + std::pow(p2.y_ - p1.y_, 2));
 }
 
 
@@ -30,14 +30,30 @@ rectangle_t Triangle::getFrameRect()
   double down = std::min(p1_.y_, std::min(p2_.y_, p3_.y_));
   double left = std::min(p1_.x_, std::min(p2_.x_, p3_.x_));
   double right = std::max(p1_.x_, std::max(p2_.x_, p3_.x_));
+
   double center_x = (right + left) / 2.0;
   double center_y = (up + down) / 2.0;
   point_t r_center = {center_x, center_y};
+
   rectangle_t r1 = {right - left, up - down, r_center};
   return r1;
 }
- void Triangle::move(point_t)
-   {}
+ void Triangle::move(point_t new_center)
+{
+  double offset_x = new_center.x_ - center_.x_;
+  double offset_y = new_center.y_ - center_.y_;
+
+  p1_.x_ += offset_x;
+  p1_.y_ += offset_y;
+
+  p2_.x_ += offset_x;
+  p2_.y_ += offset_y;
+
+  p3_.x_ += offset_x;
+  p3_.y_ += offset_y;
+
+  center_ = new_center;
+}
  void Triangle::move(double k)
 {
   center_.x_ += k;
@@ -49,5 +65,14 @@ rectangle_t Triangle::getFrameRect()
   p3_.x_ += k;
   p3_.y_ += k;
 }
- void Triangle::scale(double)
-   {}
+ void Triangle::scale(double k)
+{
+  p1_.x_ = center_.x_ + (p1_.x_ - center_.x_) * k;
+  p1_.y_ = center_.y_ + (p1_.y_ - center_.y_) * k;
+
+  p2_.x_ = center_.x_ + (p2_.x_ - center_.x_) * k;
+  p2_.y_ = center_.y_ + (p2_.y_ - center_.y_) * k;
+
+  p3_.x_ = center_.x_ + (p3_.x_ - center_.x_) * k;
+  p3_.y_ = center_.y_ + (p3_.y_ - center_.y_) * k;
+}
