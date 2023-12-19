538f11585f24cc032348f67e861e70a718bbce15
ZakozhurnikovaKristina <kristinazak150@gmail.com>
2023-12-19 19:22:41 +0300

    implementation of multiplication and division operators with an exception when dividing by zero

--- zakozhurnikova.kristina/T1/base-types.cpp
+++ zakozhurnikova.kristina/T1/base-types.cpp
@@ -22,3 +22,18 @@ point_t point_t::operator-(const point_t& p) const
   return point_t(x_ - p.x_, y_ - p.y_);
 }
 
+point_t point_t::operator*(double k) const
+{
+  return point_t(x_ * k, y_ * k);
+}
+
+point_t point_t::operator/(double k) const
+{
+  if (k == 0)
+  {
+    throw std::invalid_argument("Division by ZERO.");
+  }
+  return point_t(x_ / k, y_ / k);
+}
+
+
