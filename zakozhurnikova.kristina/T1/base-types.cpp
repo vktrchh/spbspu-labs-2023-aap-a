747127810a6277357169064a9a126b89279a40d9
ZakozhurnikovaKristina <kristinazak150@gmail.com>
2023-12-19 19:16:13 +0300

    implementation of assignment, subtraction and addition operators, just addition and subtraction operators, that is, creating a new point

--- zakozhurnikova.kristina/T1/base-types.cpp
+++ zakozhurnikova.kristina/T1/base-types.cpp
@@ -1,1 +1,24 @@
 #include "base-types.hpp"
+
+void point_t::operator+=(const point_t& p)
+{
+  x_ += p.x_;
+  y_ += p.y_;
+}
+
+void point_t::operator-=(const point_t& p)
+{
+  x_ -= p.x_;
+  y_ -= p.y_;
+}
+
+point_t point_t::operator+(const point_t& p) const
+{
+  return point_t(x_ + p.x_, y_ + p.y_);
+}
+
+point_t point_t::operator-(const point_t& p) const
+{
+  return point_t(x_ - p.x_, y_ - p.y_);
+}
+
