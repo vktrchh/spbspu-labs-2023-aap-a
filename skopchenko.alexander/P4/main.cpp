e648c11f2b33b939a5631145774774402fdb132a
polykisa <s.sanek2005@gmail.com>
2023-12-19 21:03:17 +0300

    prepared main.cpp for input

--- skopchenko.alexander/P4/main.cpp
+++ skopchenko.alexander/P4/main.cpp
@@ -1,5 +1,38 @@
-int main(){
+int main(int argc, char * argv[])
+{
 
+  if (argc != 4)
+  {
+    std::cerr << "Invalid amount of arguments";
+    return 1;
+  }
 
+  int arrType = 0;
+  try
+  {
+    arrType = std::stoi(argv[1]);
+  }
+  catch (const std::logic_error & e)
+  {
+    std::cerr << "Invalid first argument";
+    return 1;
+  }
 
+  if (arrType != 1 && arrType != 2)
+  {
+    std::cerr << "First argument can only be '1' or '2'";
+    return 1;
+  }
+
+  std::ifstream iFile(argv[2]);
+  if (!iFile.is_open())
+  {
+    std::cerr << "Couldn't open input file";
+    return 2;
+  }
+
+  size_t rows = 0;
+  size_t cols = 0;
+
+  iFile >> rows >> cols;
 }
