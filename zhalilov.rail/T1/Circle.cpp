19c916dce213324d02ac7f8584f6a91eabf0d9ee
Rail (Susanoo) <zhalilov.rsh@edu.spbstu.ru>
2023-12-19 20:24:54 +0300

    Added circle realization

---
+++ zhalilov.rail/T1/Circle.cpp
@@ -0,0 +1,44 @@
+#include "Circle.hpp"
+
+#include <stdexcept>
+
+zhalilov::Circle::Circle(const point_t &center, const double radius)
+{
+  if (radius <= 0.0)
+  {
+    throw std::invalid_argument("circle radius should be more than zero");
+  }
+  m_frameRect.pos = center;
+  m_frameRect.width = radius * 2;
+  m_frameRect.height = radius * 2;
+}
+
+double zhalilov::Circle::getArea() const
+{
+  return 3.14 * m_frameRect.width / 4.0;
+}
+
+zhalilov::rectangle_t zhalilov::Rectangle::getFrameRect() const
+{
+  m_frameRect.pos = point;
+}
+
+void zhalilov::Rectangle::move(const point_t &point)
+{
+  m_frameRect.pos = point;
+}
+
+void zhalilov::Rectangle::move(const double dx, const double dy)
+{
+  m_frameRect.pos = {m_frameRect.pos.x + dx, m_frameRect.pos.y + dy};
+}
+
+void zhalilov::Rectangle::scale(const double ratio)
+{
+  if (ratio <= 0.0)
+  {
+    throw std::invalid_argument("scaling ratio should be more than zero");
+  }
+  m_frameRect.width *= ratio;
+  m_frameRect.height *= ratio;
+}
---
+++ zhalilov.rail/T1/Circle.hpp
@@ -0,0 +1,22 @@
+#ifndef CIRCLE_HPP
+#define CIRCLE_HPP
+
+#include "Shape.hpp"
+
+namespace zhalilov
+{
+  class Circle : public Shape
+  {
+  public:
+    Circle(const point_t &center, const double radius);
+    virtual double getArea() const;
+    virtual rectangle_t getFrameRect() const;
+    virtual void move(const point_t &point);
+    virtual void move(const double dx, const double dy);
+    virtual void scale(const double ratio);
+  private:
+    rectangle_t m_frameRect;
+  }
+}
+
+#endif
