8fe26c44107cccb6da2166c980c700a8621ce57e
WarriorVortex <erohinvova01010111@gmail.com>
2023-12-19 21:19:40 +0300

    Changes according to CG

--- erohin.vladimir/P3/input_output_string.cpp
+++ erohin.vladimir/P3/input_output_string.cpp
@@ -4,36 +4,37 @@
 
 char* erohin::inputString(std::istream & input, char* str, const size_t iter_size)
 {
-    size_t size = iter_size;
-    char elem = 0;
-    size_t i = 0;
-    input >> std::noskipws;
-    while (input >> elem)
+  size_t size = iter_size;
+  char elem = 0;
+  size_t i = 0;
+  input >> std::noskipws;
+  while (input >> elem)
+  {
+    if (size == i)
     {
-        if (size == i)
-        {
-            size += iter_size;
-        }
-        str[i++] = elem;
-        if (elem == '\n')
-        {
-            str[i - 1] = '\0';
-            break;
-        }
-        if (!input)
-        {
-            throw std::logic_error("Wrong string input\n");
-        }
+      str = resize(str, size, iter_size);
+      size += iter_size;
     }
-    input >> std::skipws;
-    return str;
+    str[i++] = elem;
+    if (elem == '\n')
+    {
+      str[i - 1] = '\0';
+      break;
+    }
+    if (!input)
+    {
+      throw std::logic_error("Wrong string input\n");
+    }
+  }
+  input >> std::skipws;
+  return str;
 }
 
 void erohin::outputString(std::ostream& output, char* str)
 {
-    size_t i = 0;
-    while (str[i] != '\0')
-    {
-        output << str[i++];
-    }
+  size_t i = 0;
+  while (str[i] != '\0')
+  {
+    output << str[i++];
+  }
 }
