d17c7f2e2781cca9078073a23841e9d3a964cf21
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2023-12-20 07:22:15 +0300

    fix NumRowsWithoutRepeat.cpp

--- vyzhanov.daniil/P4/NumRowsWithoutRepeat.cpp
+++ vyzhanov.daniil/P4/NumRowsWithoutRepeat.cpp
@@ -3,28 +3,29 @@
 
 size_t vyzhanov::NumRowsWithoutRepeat(const int matrix[], size_t rows, size_t cols)
 {
-  size_t countNum = 0;
+  size_t countNum = 1;
   size_t countWithoutRep = 0;
-  for (size_t i = 0; i < rows * cols; i += rows)
+  for (size_t i = 0; i < rows * cols; i += cols)
   {
     for (size_t j = 1; j < cols - 1; j++)
     {
-      for (size_t k = cols - 1; k > 0; k -= j)
+      for (size_t k = cols - j; k > 0; k -= 1)
       {
-        if (matrix[i+j] != matrix[i+j+k])
+        if (matrix[i+j-1] != matrix[i+j+k-1])
         {
           countNum++;
+          if (countNum == cols)
+          {
+            countWithoutRep++;
+          }
         }
         else
         {
-          countNum = 0;
+          countNum = 1;
         }
       }
     }
-    if (countNum == cols)
-    {
-      countWithoutRep++;
-    }
+    countNum = 1;
   }
   return countWithoutRep;
 }
