171028d95e2a03aa06c799409324e5fc97f7cca2
fncg <stepanow.gr@edu.spbstu.ru>
2023-12-22 02:33:16 +0300

    command line error handling

--- stepanov.gena/P4/main.cpp
+++ stepanov.gena/P4/main.cpp
@@ -1,62 +1,36 @@
 #include <iostream>
 #include <fstream>
-#include <cstdlib>
-#include <string>
+#include <cstring>
 
-int main(int argc, char * argv[])
+int main(int argc, char* argv[])
 {
-  for (int i = 0; i < argc; ++i)
-  {
-    std::cout << argv[i] << "\n";
-  }
   if (argc != 4)
   {
-    std::cerr << "Error in command line arguments\n";
+    std::cerr << "Error in command line argument\n";
     return 1;
   }
-  // argv [1] - число, т.е. номер задания
 
-  int num = 0;
+  int typeMatrix = 0;
+  char* endOfParsing = nullptr;
+
   try
   {
-    num = std::stoll(argv[1]);
+    typeMatrix = std::strtoll(argv[1], std::addressof(endOfParsing), 10);
   }
-  catch (const std::out_of_range &)
+  catch (const std::out_of_range&)
   {
     std::cerr << "Value of first CLA is too large\n";
-    return 3;
+    return 1;
   }
-  catch (const std::invalid_argument &)
+  catch (const std::invalid_argument&)
   {
-    std::cerr << "Cannot parse a valute\n";
-    return 3;
+    std::cerr << "Cannot parse a value\n";
+    return 1;
   }
 
-  // argv [2] - имя файла с матрицей
-  int i = 0;
+  if (*endOfParsing != '\0')
   {
-    std::ifstream input(argv[2]);
-    input >> i;
-    if (!input)
-    {
-      std::cerr << "Cannot read a number\n";
-      return 2;
-    }
+    std::cerr << "Wrong first argument\n";
+    return 1;
   }
-
-  std::cout << i << "\n";
-
-  // argv [3] - имя файла для вывода результатов обработки
-  std::ofstream output(argv[3]);
-  output << i << "\n";
-
-  //num == 1
-  //int a[10000] = {};
-
-  //num == 2
-  //size_t rows = 0;
-  //size_t cols = 0;
-  // input >> rows >> cols;
-  //int * a = new int[rows * cols]
-  //a[i*cols + j
 }
