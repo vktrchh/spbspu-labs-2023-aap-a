0d4c9835443ffd80916ec34b650ff7cf7b046ccc
Skuratov09 <skyratovdaniil28@gmail.com>
2023-12-21 23:45:40 +0300

    8th commit

--- skuratov.danil/P3/removeDuplicate.cpp
+++ skuratov.danil/P3/removeDuplicate.cpp
@@ -2,20 +2,23 @@
 #include <iostream>
 #include <cstring>
 
-char* removeDuplicate(const char* str1, const char* str2) {
-    char result[100];  // предполагаем максимальный размер новой строки
-    strcpy(result, str1);  // копируем str1 в result - будем работать с копией
+char* removeDuplicate(const char* str1, const char* str2)
+{
+    char result[100];
+    strcpy(result, str1);
 
     int len1 = strlen(result);
     int len2 = strlen(str2);
 
-    for (int i = 0; i < len2; i++) {
+    for (int i = 0; i < len2; i++)
+    {
         char* pos = nullptr;
-        while ((pos = strchr(result, str2[i])) != nullptr) {
-            memmove(pos, pos + 1, strlen(pos));  // сдвигаем все символы после найденного влево на одну позицию
-            len1--;  // уменьшаем длину результирующей строки
+        while ((pos = strchr(result, str2[i])) != nullptr)
+        {
+            memmove(pos, pos + 1, strlen(pos));
+            len1--;
         }
     }
-    result[len1] = '\0';  // устанавливаем новый конец строки
+    result[len1] = '\0';
     return result;
 }
