0c61aec869d2967e82bffc8fa81aab833ed005d3
fncg <stepanow.gr@edu.spbstu.ru>
2023-12-22 10:17:23 +0300

    Добавил провреку rows and cols != 0

--- stepanov.gena/P4/count_local_min.cpp
+++ stepanov.gena/P4/count_local_min.cpp
@@ -3,22 +3,24 @@
 size_t stepanov::countLocalMin(int const matrix[], size_t rows, size_t cols)
 {
   size_t count = 0;
-
-  for (size_t i = 1; i < rows - 1; ++i)
+  if (rows != 0 && cols != 0)
   {
-    for (size_t j = 1; j < cols - 1; ++j)
+    for (size_t i = 1; i < rows - 1; ++i)
     {
-      size_t index = i * cols + j;
-      if (matrix[index] < matrix[index - cols] &&
-        matrix[index] < matrix[index + cols] &&
-        matrix[index] < matrix[index - 1] &&
-        matrix[index] < matrix[index + 1] &&
-        matrix[index] < matrix[index - cols - 1] &&
-        matrix[index] < matrix[index - cols + 1] &&
-        matrix[index] < matrix[index + cols - 1] &&
-        matrix[index] < matrix[index + cols + 1])
+      for (size_t j = 1; j < cols - 1; ++j)
       {
-        count++;
+        size_t index = i * cols + j;
+        if (matrix[index] < matrix[index - cols] &&
+          matrix[index] < matrix[index + cols] &&
+          matrix[index] < matrix[index - 1] &&
+          matrix[index] < matrix[index + 1] &&
+          matrix[index] < matrix[index - cols - 1] &&
+          matrix[index] < matrix[index - cols + 1] &&
+          matrix[index] < matrix[index + cols - 1] &&
+          matrix[index] < matrix[index + cols + 1])
+        {
+          count++;
+        }
       }
     }
   }
--- stepanov.gena/P4/most_long_sequence.cpp
+++ stepanov.gena/P4/most_long_sequence.cpp
@@ -1,33 +1,38 @@
 #include "most_long_sequence.hpp"
 
-size_t stepanov::mostLongSequence(const int matrix[], size_t rows, size_t cols)
+size_t stepanov::mostLongSequence(const int matrix[], size_t rows, size_t cols) 
 {
   size_t maxRow = -1;
   size_t maxLength = 0;
-
-  for (size_t i = 0; i < rows; ++i)
+  if (rows != 0 && cols != 0)
   {
-    size_t currentLength = 1;
-
-    for (size_t j = 1; j < cols; ++j)
+    for (size_t i = 0; i < rows; ++i)
     {
-      size_t index = i * cols + j;
-      if (matrix[index] == matrix[index - 1])
-      {
-        currentLength++;
-      }
-      else
-      {
-        currentLength = 1;
-      }
+      size_t currentLength = 1;
 
-      if (currentLength > maxLength)
+      for (size_t j = 1; j < cols; ++j)
       {
-        maxLength = currentLength;
-        maxRow = i;
+        size_t index = i * cols + j;
+        if (matrix[index] == matrix[index - 1])
+        {
+          currentLength++;
+        }
+        else
+        {
+          currentLength = 1;
+        }
+
+        if (currentLength > maxLength)
+        {
+          maxLength = currentLength;
+          maxRow = i + 1;
+        }
       }
     }
   }
-
+  else
+  {
+    maxRow = 0;
+  }
   return maxRow;
 }
