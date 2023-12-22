7167ac2cf01f595b287bee80746b57789036a75c
WarriorVortex <erohinvova01010111@gmail.com>
2023-12-22 19:30:32 +0300

    Added handling of null string

--- erohin.vladimir/P3/main.cpp
+++ erohin.vladimir/P3/main.cpp
@@ -15,20 +15,31 @@ int main()
   {
     first = inputString(std::cin);
   }
-  catch (const std::logic_error& e)
-  {
-    delete[] first;
-    std::cerr << e.what() << "\n";
-    return 2;
-  }
   catch (const std::bad_alloc& e)
   {
     delete[] first;
     std::cerr << "Cannot allocate memory: " << e.what() << "\n";
     return 1;
   }
-  std::cout << uniteString(first, second) << "\n";
+  catch (const std::logic_error& e)
+  {
+    delete[] first;
+    std::cerr << e.what() << "\n";
+    return 2;
+  }
+  char* united = uniteString(first, second);
+  if (united)
+  {
+    std::cout << united << "\n";
+  }
+  else
+  {
+    std::cerr << "United string is null\n";
+    delete[] first;
+    return 3;
+  }
   std::cout << replaceSymbol(first, old, change) << "\n";
+  delete[] united;
   delete[] first;
   return 0;
 }
