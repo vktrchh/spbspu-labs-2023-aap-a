bda14b09c1ebf7c13e5e45ecb52f8d5cba6d0372
NovokhatskiyDanil <karuk.vova2005@mail.ru>
2023-12-22 22:48:07 +0300

    fix everything: throw, CG,

--- novokhatskiy.danil/P3/inputString.cpp
+++ novokhatskiy.danil/P3/inputString.cpp
@@ -4,12 +4,20 @@ char* novokhatskiy::inputString(std::istream &input)
 {
   size_t sizeString = 20;
   size_t cycleIndex = 0;
-  char* string = new char[sizeString];
+  char* string = nullptr;
+  try
+  {
+    string = new char[sizeString];
+  }
+  catch (...)
+  {
+    return string;
+  }
   char symbol = 0;
   input >> std::noskipws;
   while ((input >> symbol) && (symbol != '\n'))
   {
-    if (cycleIndex + 1 >= sizeString)
+    if (cycleIndex + 1 == sizeString)
     {
       try
       {
--- novokhatskiy.danil/P3/main.cpp
+++ novokhatskiy.danil/P3/main.cpp
@@ -10,28 +10,25 @@ int main()
   try
   {
     buffer = inputString(std::cin);
+    if (!buffer) 
+    {
+      std::cerr << "Can't read the string\n";
+      return 1;
+    }
     if (buffer[0] == '\0')
     {
       std::cerr << "Wrong input\n";
       delete[] buffer;
       return 1;
-    }
-    if (!buffer)
-    {
-      std::cerr << "Can't read the string\n";
-      return 1;
-    }
-    else
-    {
-      buffer = deleteLatinLetters(buffer, buffer);
-      std::cout << buffer << "\n";
-      delete[] buffer;
-    }
-  }
-  catch (const std::bad_alloc &e)
-  {
+    }    
+    buffer = deleteLatinLetters(buffer, buffer);
+    std::cout << buffer << "\n";
     delete[] buffer;
-    throw e;
+  }
+  catch (...)
+  {
+    std::cerr << "Out of memory\n";
+    return 1; 
   }
   return 0;
 }
