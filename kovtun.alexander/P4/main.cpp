63660a7c81795edc9aaa84cf1da6441d48b329d5
puzz13 <puzz13one@gmail.com>
2023-12-24 22:39:19 +0300

    Verify task number

--- kovtun.alexander/P4/main.cpp
+++ kovtun.alexander/P4/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <exception>
 
 
 int main(int argc, char * argv[])
@@ -7,13 +9,31 @@ int main(int argc, char * argv[])
   {
     if (argc < 4)
     {
-      std::cerr << "Not enough arguments\n";
+      std::cerr << "not enough arguments\n";
     }
-    else if (argc > 4)
+    else
     {
-      std::cerr << "Too many arguments\n";
+      std::cerr << "too many arguments\n";
     }
 
     return 1;
   }
+
+  size_t num = 0;
+  try
+  {
+    num = std::stoll(argv[1]);
+    if (num != 1 && num != 2)
+    {
+      std::cerr << "the task number must be 1 or 2\n";
+      return 1;
+    }
+  }
+  catch (const std::exception & e)
+  {
+    std::cerr << "failed to parse the task number: " << e.what() << "\n";
+    return 1;
+  }
+
+  
 }
