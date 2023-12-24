057ff01e3be16609542e8189be09dcf80baca550
WarriorVortex <erohinvova01010111@gmail.com>
2023-12-24 18:17:10 +0300

    Rectangle class

---
+++ erohin.vladimir/T1/rectangle.hpp
@@ -0,0 +1,26 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+
+#include "base-types.hpp"
+#include "shape.hpp"
+
+namespace erohin
+{
+	class Rectangle: public Shape
+	{
+	public:
+		Rectangle();
+		Rectangle(point_t left_corner, point_t right_corner);
+		virtual ~Rectangle();
+		virtual double getArea() const;
+		virtual rectangle_t getFrameRect() const;
+		virtual void move(double dx, double dy);
+		virtual void move(point_t point);
+		virtual void scale(double ratio);
+	private:
+		rectangle_t frameRect_;
+	};
+}
+
+
+#endif
