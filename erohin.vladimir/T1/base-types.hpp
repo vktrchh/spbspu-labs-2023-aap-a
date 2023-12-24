71c0b14498334e477f829af2d0ef488674cf5ebd
WarriorVortex <erohinvova01010111@gmail.com>
2023-12-24 18:07:09 +0300

    Added point and rectangle types

---
+++ erohin.vladimir/T1/base-types.hpp
@@ -0,0 +1,20 @@
+#ifndef BASE_TYPES_HPP
+#define BASE_TYPES_HPP
+
+namespace erohin
+{
+	struct point_t
+	{
+		double x = 0.0;
+		double y = 0.0;
+	};
+
+	struct rectangle_t
+	{
+		double width = 0.0;
+		double height = 0.0;
+		point_t pos{0.0, 0.0};
+	};
+}
+
+#endif
