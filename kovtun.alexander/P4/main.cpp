3bfe0c8c201f09f7a68a00a4e7ea0d2ed1a4e69a
puzz13 <puzz13one@gmail.com>
2023-12-24 21:09:02 +0300

    Count command line parameters

--- kovtun.alexander/P4/main.cpp
+++ kovtun.alexander/P4/main.cpp
@@ -1,4 +1,19 @@
-int main()
-{
+#include <iostream>
 
+
+int main(int argc, char * argv[])
+{
+  if (argc != 4)
+  {
+    if (argc < 4)
+    {
+      std::cerr << "Not enough arguments\n";
+    }
+    else if (argc > 4)
+    {
+      std::cerr << "Too many arguments\n";
+    }
+
+    return 1;
+  }
 }
