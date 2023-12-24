93543f8302dc270d161911cc5f6d257ece43f236
WarriorVortex <erohinvova01010111@gmail.com>
2023-12-24 18:13:25 +0300

    Added function to work with geometry

---
+++ erohin.vladimir/T1/geom_func.hpp
@@ -0,0 +1,12 @@
+#ifndef GEOM_FUNC_HPP
+#define GEOM_FUNC_HPP
+
+#include "shape.hpp"
+
+namespace erohin
+{
+	void isoScale(Shape& shape, point_t pos, double ratio);
+	double getDistance(point_t from, point_t to);
+}
+
+#endif
