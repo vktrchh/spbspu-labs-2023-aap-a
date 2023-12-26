fba91cdb9e2a2523b3ea557b37234c181b6a2c8f
Z0ltaan <gregnov69@gmail.com>
2023-12-27 00:14:41 +0300

    added basic base..._hpp shape.hpp

---
+++ isaychev.vladislav/T1/base-types.hpp
@@ -0,0 +1,12 @@
+struct point_t
+{
+  double x_;
+  double y_;
+};
+
+struct rectangle_t
+{
+  double width;
+  double height;
+  point_t pos;
+};
---
+++ isaychev.vladislav/T1/shape.hpp
@@ -0,0 +1,7 @@
+class Shape
+{
+  virtual double getArea() const = 0;
+  virtual getFrameRect() const = 0;
+  virtual move() const = 0;
+  virtual scale() const = 0;
+};
