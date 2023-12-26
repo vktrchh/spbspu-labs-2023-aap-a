49a3380b6490f2044d0e02ee8054d11e5f23c3cd
polykisa <s.sanek2005@gmail.com>
2023-12-27 01:51:51 +0300

    major change in topclock.cpp

--- skopchenko.alexander/P4/topclock.cpp
+++ skopchenko.alexander/P4/topclock.cpp
@@ -1,24 +1,41 @@
 #include "topclock.h"
 
-void skopchenko::topClock(int * matrix , size_t dimension , size_t rows)
+void spiral(int * matrix , int cols , int rows)
 {
-  size_t currentCell = 0;
-  size_t currentRow = 0;
-  size_t currentCol = 0;
-  size_t counter = 1;
-  while (currentCol < dimension)
-  {
-    matrix[currentCell] -= counter;
-    counter++;
-    currentCell++;
-    currentCol++;
-  }
-  currentCell += 2;
-  while (currentRow < dimension)
-  {
-    matrix[currentCell] -= counter;
-    currentCell += rows;
-    counter++;
-    currentRow++;
+  int top = 0, bottom = rows - 1, left = 0, right = cols - 1;
+  int direction = 0; // 0: right, 1: down, 2: left, 3: up
+  int value = 1;
+
+  while (top <= bottom && left <= right) {
+    if (direction == 0) {
+      for (int i = left; i <= right; ++i) {
+        matrix[top * cols + i] -= value;
+        value++;
+      }
+      top++;
+      direction = 1;
+    } else if (direction == 1) {
+      for (int i = top; i <= bottom; ++i) {
+        matrix[i * cols + right] -= value;
+        value++;
+      }
+      right--;
+      direction = 2;
+    } else if (direction == 2) {
+      for (int i = right; i >= left; --i) {
+        matrix[bottom * cols + i] -= value;
+        value++;
+      }
+      bottom--;
+      direction = 3;
+    } else if (direction == 3) {
+      for (int i = bottom; i >= top; --i) {
+        matrix[i * cols + left] -= value;
+        value++;
+      }
+      left++;
+      direction = 0;
+    }
   }
 }
+
