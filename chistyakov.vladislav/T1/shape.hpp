eef0298d477f01a58c6be274bb7cf740a2e7e802
jiggerman <liggerman@mail.ru>
2023-12-26 20:59:58 +0300

    Add class Shape

---
+++ chistyakov.vladislav/T1/shape.hpp
@@ -0,0 +1,19 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+
+#include "base-types.hpp"
+
+namespace chistyakov
+{
+  class Shape
+  {
+    virtual ~Shape() = default;
+    virtual double getArea() const;
+    virtual rectangle_t getFrameRect() const;
+    virtual void move(const point_t & point);
+    virtual void move(const double mx, const double my);
+    virtual void scale(const double ratio);)
+  }
+}
+
+#endif
