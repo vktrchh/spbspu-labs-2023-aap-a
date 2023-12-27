653d5ccdb4aa50a45cc6aae2a999c2c02acbb1fb
exxiiint <filipp.sivkov@yandex.ru>
2023-12-27 14:24:43 +0300

    edit removing spaces

--- sivkov.filipp/P3/removing_spaces.cpp
+++ sivkov.filipp/P3/removing_spaces.cpp
@@ -1,32 +1,39 @@
 #include "removing_spaces.hpp"
 #include <iostream>
-void sivkov::deleteSpace(char* string,size_t size,size_t k,char* arrayWithOutSpace)
+char* deleteSpace(char* string)
 {
+  size_t k = 0;
+  for (size_t i = 0; string[i] != '\0'; i++)
+  {
+    k++;
+  }
+  char* arrayWithOutSpace = new char[k+1];
   int isSpace = 0;
   int indexArr = 0;
   for (int j = 0; j < k; j++)
   {
-      if (std::isspace(string[j]))
+    if (std::isspace(string[j]))
+    {
+      if (isSpace == 0)
       {
-          if (isSpace == 0)
-          {
-              arrayWithOutSpace[indexArr++] = ' ';
-              isSpace = 1;
-          }
+        arrayWithOutSpace[indexArr++] = ' ';
+        isSpace = 1;
       }
-      else
-      {
-          arrayWithOutSpace[indexArr++] = string[j];
-          isSpace = 0;
-      }
+    }
+    else
+    {
+      arrayWithOutSpace[indexArr++] = string[j];
+      isSpace = 0;
+    }
   }
   if (indexArr > 0 && arrayWithOutSpace[0] == ' ')
   {
     for (int j = 0; j < indexArr - 1; j++)
     {
-        arrayWithOutSpace[j] = arrayWithOutSpace[j + 1];
+      arrayWithOutSpace[j] = arrayWithOutSpace[j + 1];
     }
     indexArr--;
   }
   arrayWithOutSpace[indexArr] = '\0';
+  return arrayWithOutSpace;
 }
--- sivkov.filipp/P3/removing_spaces.hpp
+++ sivkov.filipp/P3/removing_spaces.hpp
@@ -1,8 +1,5 @@
 #ifndef REMOVING_SPACES_HPP
 #define REMOVING_SPACES_HPP
 #include <cstddef>
-namespace sivkov
-{
-  void deleteSpace(char* string, size_t size, size_t k, char* arrayWithOutSpace);
-}
+char* deleteSpace(char* string);
 #endif
