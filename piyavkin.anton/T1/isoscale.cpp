66ed5eec602d953b6a92992ac7279b698eead1c1
Piyavva <piyavkin05@bk.ru>
2023-12-28 00:40:40 +0300

    Translation unit

---
+++ piyavkin.anton/T1/isoscale.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "isoscale.hpp"
+
+void isoScale(Shape * shape, point_t center, double k)
+{
+  if (k < 0)
+  {
+    throw std::logic_error("Сoefficient less than 0");
+  }
+  point_t pos = shape->getFrameRect().pos;
+  shape->move(center);
+  shape->scale(k);
+  shape->move(k * (pos.x - shape->getFrameRect().pos.x), k * (pos.y - shape->getFrameRect().pos.y));
+}
---
+++ piyavkin.anton/T1/isoscale.hpp
@@ -0,0 +1,6 @@
+#ifndef ISOSCALE_HPP
+#define ISOSCALE_HPP
+#include "base-types.hpp"
+#include "shape.hpp"
+void isoScale(Shape * shape, point_t center, double k);
+#endif
