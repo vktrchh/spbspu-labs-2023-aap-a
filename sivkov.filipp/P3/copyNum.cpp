9797003ce9919741adcfb473bdbdec221545c684
exxiiint <filipp.sivkov@yandex.ru>
2023-12-27 14:20:52 +0300

    edit Copy num

--- sivkov.filipp/P3/copyNum.cpp
+++ sivkov.filipp/P3/copyNum.cpp
@@ -2,8 +2,26 @@
 #include <string>
 #include "copyNum.hpp"
 
-char* sivkov::copyNum(char* string1, char* string2, char* finalString, size_t k1, size_t k2, size_t k3)
+char* copyNum(char* string1, char* string2)
 {
+  size_t k1 = 0;
+  size_t k2 = 0;
+  size_t k3 = 0;
+  for (size_t i = 0; string1[i] != '\0'; i++)
+  {
+    k1++;
+  }
+  for (size_t i = 0; string2[i] != '\0'; i++)
+  {
+    k2++;
+  }
+
+  char* finalString = new char[k1 + k2 + 1];
+  if (!finalString)
+  {
+    string1 = nullptr;
+    return string1;
+  }
   for (size_t j = 0; j < k1; j++)
   {
     finalString[k3++] = string1[j];
--- sivkov.filipp/P3/copyNum.hpp
+++ sivkov.filipp/P3/copyNum.hpp
@@ -1,8 +1,5 @@
 #ifndef COPYNUM_HPP
 #define COPYNUM_HPP
 #include <cstddef>
-namespace sivkov
-{
-  char* copyNum(char* string1, char* string2, char* finalString, size_t k1, size_t k2, size_t k3);
-}
+char* copyNum(char* string1, char* string2);
 #endif
