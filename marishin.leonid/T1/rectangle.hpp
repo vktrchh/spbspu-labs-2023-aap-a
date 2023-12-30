1b696e2771fe1966b6595de7af67efcafe0c4240
marlions <marishin.lv@edu.spbstu.ru>
2023-12-30 14:10:35 +0300

    rect

--- marishin.leonid/T1/rectangle.hpp
+++ marishin.leonid/T1/rectangle.hpp
@@ -3,14 +3,19 @@
 
 #include "shape.hpp"
 
-class Rectangle
+class Rectangle : public Shape
 {
 public:
-  virtual double getArea(double a, double b) const;
-  virtual rectangle_t getFrameRect() const;
-  virtual void move(const point_t* newPos);
-  virtual void move(double dx, double dy);
-  virtual void scale(double factor);
+  Rectangle(const point_t* lowerLeftCorner, const point_t* topRightCorner);
+  virtual ~Rectangle() = default;
+  virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
+  virtual void move(const point_t* newPos) = 0;
+  virtual void move(double dx, double dy) = 0;
+  virtual void scale(double factor) = 0;
+private:
+  double width;
+  double height;
 };
 
 #endif
