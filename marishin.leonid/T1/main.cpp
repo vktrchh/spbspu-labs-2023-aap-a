02c9608ae17017634a32d28376ea66eb06526c2b
marlions <marishin.lv@edu.spbstu.ru>
2023-12-30 14:01:13 +0300

    main delete

--- marishin.leonid/T1/main.cpp
+++ marishin.leonid/T1/main.cpp
@@ -1,41 +1,15 @@
 #include <iostream>
-#include <sstream>
-
-const int MAX_ARRAYS = 10;
-const int MAX_ELEMENTS = 10;
 
 int main()
 {
-  std::string arrayOfArrays[MAX_ARRAYS][MAX_ELEMENTS];
-  int currentArray = 0;
-  while (true)
+  double x1 = 0;
+  double y1 = 0;
+  double x2 = 0;
+  double y2 = 0;
+  std::string str = "RECTANGLE";
+  std::cin >> x1 >> y1 >> x2 >> y2;
+  if (str == "RECTANGLE")
   {
-    std::string input;
-    std::getline(std::cin, input);
-    if (input == "0")
-    {
-      break;
-    }
-    std::istringstream stream(input);
-    int currentElement = 0;
-    std::string element;
-    while (stream >> element && currentElement < MAX_ELEMENTS)
-    {
-      arrayOfArrays[currentArray][currentElement++] = element;
-    }
-    currentArray++;
-    if (currentArray >= MAX_ARRAYS)
-    {
-      std::cerr << "Maximum number of arrays exceeded." << std::endl;
-      return 1;
-    }
-  }
-  for (int i = 0; i < currentArray; ++i)
-  {
-    for (int j = 0; j < MAX_ELEMENTS && !arrayOfArrays[i][j].empty(); ++j)
-    {
-      std::cout << arrayOfArrays[i][j] << " ";
-    }
-    std::cout << std::endl;
+    ///...
   }
 }
