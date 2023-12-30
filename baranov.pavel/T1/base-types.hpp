12b57baf33e35cda89f896be03346a8f48dea845
pashko2303 <baranov-p.b@yandex.ru>
2023-12-30 10:24:15 +0300

    Namespaces

--- baranov.pavel/T1/base-types.hpp
+++ baranov.pavel/T1/base-types.hpp
@@ -1,18 +1,23 @@
 #ifndef BASE_TYPES_HPP
 #define BASE_TYPES_HPP
 
-struct point_t
+namespace baranov
 {
-  double x;
-  double y;
-};
 
-struct rectangle_t
-{
-  double width;
-  double height;
-  point_t pos;
-};
+  struct point_t
+  {
+    double x;
+    double y;
+  };
+
+  struct rectangle_t
+  {
+    double width;
+    double height;
+    point_t pos;
+  };
+
+}
 
 #endif
 
--- baranov.pavel/T1/shape.hpp
+++ baranov.pavel/T1/shape.hpp
@@ -2,16 +2,21 @@
 #define SHAPE_HPP
 #include "base-types.hpp"
 
-class Shape
+namespace baranov
 {
-  public:
-    virtual ~Shape = default;
-    virtual double getArea() const = 0;
-    virtual rectangle_t getFrameRect() const = 0;
-    virtual void move(const point_t & destPos) = 0;
-    virtual void move(double dx, double dy) = 0;
-    virtual void scale(double ratio) = 0;
-};
+
+  class Shape
+  {
+    public:
+      virtual ~Shape = default;
+      virtual double getArea() const = 0;
+      virtual rectangle_t getFrameRect() const = 0;
+      virtual void move(const point_t & destPos) = 0;
+      virtual void move(double dx, double dy) = 0;
+      virtual void scale(double ratio) = 0;
+  };
+
+}
 
 #endif
 
