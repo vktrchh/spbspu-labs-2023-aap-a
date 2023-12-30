bd59df96db85a1ede05c5dcb0e183a9b03a1f49a
marlions <marishin.lv@edu.spbstu.ru>
2023-12-30 14:03:48 +0300

    shape

--- marishin.leonid/T1/shape.hpp
+++ marishin.leonid/T1/shape.hpp
@@ -6,11 +6,12 @@
 class Shape
 {
 public:
-  virtual double getArea(double a, double b) const;
-  virtual rectangle_t getFrameRect() const;
-  virtual void move(const point_t* newPos);
-  virtual void move(double dx, double dy);
-  virtual void scale(double factor);
+  virtual ~Shape() = default;
+  virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
+  virtual void move(const point_t* newPos) = 0;
+  virtual void move(double dx, double dy) = 0;
+  virtual void scale(double factor) = 0;
 };
 
 #endif
