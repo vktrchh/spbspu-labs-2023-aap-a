25bc7970eb1c582813db603eebe77bff70342c8e
pashko2303 <baranov-p.b@yandex.ru>
2023-12-30 10:18:45 +0300

    Set default destructor for class Shape

--- baranov.pavel/T1/shape.hpp
+++ baranov.pavel/T1/shape.hpp
@@ -5,6 +5,7 @@
 class Shape
 {
   public:
+    virtual ~Shape = default;
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
     virtual void move(const point_t & destPos) = 0;
