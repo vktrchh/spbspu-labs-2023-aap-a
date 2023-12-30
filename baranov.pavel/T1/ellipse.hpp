4411b2290ce05511f20195c170c3027587de4659
pashko2303 <baranov-p.b@yandex.ru>
2023-12-30 20:57:50 +0300

    Ellipse class decloration

---
+++ baranov.pavel/T1/ellipse.hpp
@@ -0,0 +1,24 @@
+#ifndef ELLIPSE_HPP
+#define ELLIPSE_HPP
+#include "shape.hpp"
+
+namespace baranov
+{
+  class Ellipse : public Shape
+  {
+    public:
+      Ellipse(const point_t & center, double xRadius, double yRadius);
+      virtual double getArea() const;
+      virtual rectangle_t getFrameRect() const;
+      virtual void move(const point_t & point);
+      virtual void move(double dx, double dy);
+      virtual void scale(double ratio);
+    private:
+      point_t center_;
+      double xRadius_;
+      double yRadius_;
+  };
+}
+
+#endif
+
