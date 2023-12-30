8b62e765499f45a15ff9542f6e56a5e5d570e2bd
pashko2303 <baranov-p.b@yandex.ru>
2023-12-30 18:00:12 +0300

    Realisation of moving methods for rectangle

--- baranov.pavel/T1/rectangle.cpp
+++ baranov.pavel/T1/rectangle.cpp
@@ -11,3 +11,30 @@ baranov::Rectangle::Rectangle(const point_t & ldCorner, const point_t & ruCorner
   }
 }
 
+double baranov::Rectangle::getArea() const
+{
+  return (ruCorner_.x - ldCorner_.x) * (ruCorner_.y - ldCorner_.y);
+}
+
+baranov::rectangle_t baranov::Rectangle::getFrameRect() const
+{
+  double height = ruCorner_.y - ldCorner_.y;
+  double width = ruCorner_.x - ldCorner_.x;
+  point_t pos = { ldCorner_.x +  width / 2, ldCorner_.y + height / 2 };
+  return { width, height, pos };
+}
+
+void baranov::Rectangle::move(const point_t & destPos)
+{
+  rectangle_t frameRect = getFrameRect();
+  move(destPos.x - frameRect.pos.x, destPos.y - frameRect.pos.y);
+}
+
+void baranov::Rectangle::move(const double dx, const double dy)
+{
+  ruCorner_.x += dx;
+  ruCorner_.y += dy;
+  ldCorner_.x += dx;
+  ldCorner_.y += dy;
+}
+
