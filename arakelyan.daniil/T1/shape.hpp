58943ea5357ce188368d94982bd40782ed8def11
osakasonyboy@gmail.com <sensh0god@gmail.com>
2023-12-30 17:01:27 +0300

    added base-types.hpp and shape.hpp

---
+++ arakelyan.daniil/T1/shape.hpp
@@ -0,0 +1,15 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+#include "base-types.hpp"
+
+struct Shape
+{
+  virtual double getArea() const = 0;
+  virtual point_t getFrameRect() = 0;
+  virtual void move(point_t) = 0;
+  virtual void move(double) = 0;
+  virtual void move(double, double) = 0;
+  virtual void scale(double) = 0;
+};
+
+#endif
