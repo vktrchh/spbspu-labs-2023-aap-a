06aae5d63bddbb4729e2f329c970d86045faf077
polykisa <s.sanek2005@gmail.com>
2023-12-31 06:38:23 +0300

    back on track

---
+++ skopchenko.alexander/P4/smoothed.h
@@ -0,0 +1,13 @@
+#ifndef SMOOTHED_H
+#define SMOOTHED_H
+
+#include <cstddef>
+
+namespace skopchenko
+{
+
+double * smoothMatrix(int * matrix, double * smoothMatrix , size_t rows , size_t cols);
+
+}
+
+#endif
