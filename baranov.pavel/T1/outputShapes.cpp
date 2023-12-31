15c558fdc26d7970e1dfda6e888574ba14cdba33
pashko2303 <baranov-p.b@yandex.ru>
2023-12-31 14:56:13 +0300

    Output shapes area and frame rectangles centers

--- baranov.pavel/T1/outputShapes.cpp
+++ baranov.pavel/T1/outputShapes.cpp
@@ -12,5 +12,11 @@ double baranov::getSumArea(const Shape ** shapes, const size_t count)
 
 void baranov::outputShapes(std::ostream & output, const Shape ** shapes, const size_t count)
 {
-  output << getSumArea(shapes, count);
+  output << getSumArea(shapes, count) << ' ';
+  for (size_t i = 0; i < count; ++i)
+  {
+    point_t pos = shapes[i]->getFrameRect().pos;
+    output << pos.x << ' ' << pos.y  << ' ';
+  }
+  output << '\n';
 }
