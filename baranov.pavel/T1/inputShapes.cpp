2883fc4a0ecf944c81b9972957077cca3342e0a3
pashko2303 <baranov-p.b@yandex.ru>
2023-12-31 10:32:39 +0300

    Different shapes parsing

--- baranov.pavel/T1/inputShapes.cpp
+++ baranov.pavel/T1/inputShapes.cpp
@@ -1,5 +1,11 @@
 #include "inputShapes.hpp"
+#include "inputString.hpp"
 #include "shape.hpp"
+#include "rectangle.hpp"
+#include "ring.hpp"
+#include "ellipse.hpp"
+#include <string>
+#include <stdexcept>
 
 baranov::Shape ** baranov::expandShapes(baranov::Shape ** shapes, const size_t size, const size_t newSize)
 {
@@ -11,3 +17,57 @@ baranov::Shape ** baranov::expandShapes(baranov::Shape ** shapes, const size_t s
   return result;
 }
 
+baranov::Shape * baranov::parseRectangle(const char * string)
+{
+  double rectParameters[4]{};
+  size_t pos = 0;
+  string += 9;
+  for (size_t i = 0; i < 4; ++i)
+  {
+    rectParameters[i] = std::stod(string, & pos);
+    string += pos;
+  }
+  if (*string != '\0')
+  {
+    throw std::invalid_argument("Invalid rectangle parameters");
+  }
+  baranov::point_t ldCorner = { rectParameters[0], rectParameters[1] };
+  baranov::point_t ruCorner = { rectParameters[2], rectParameters[3] };
+  return new baranov::Rectangle(ldCorner, ruCorner);
+}
+
+baranov::Shape * baranov::parseRing(const char * string)
+{
+  double ringParameters[4]{};
+  size_t pos = 0;
+  string += 4;
+  for (size_t i = 0; i < 4; ++i)
+  {
+    ringParameters[i] = std::stod(string, & pos);
+    string += pos;
+  }
+  if (*string != '\0')
+  {
+    throw std::invalid_argument("Invalid ring parameters");
+  }
+  baranov::point_t center = { ringParameters[0], ringParameters[1] };
+  return new baranov::Ring(center, ringParameters[2], ringParameters[3]);
+}
+
+baranov::Shape * baranov::parseEllipse(const char * string)
+{
+  double ellipseParameters[4]{};
+  size_t pos = 0;
+  string += 7;
+  for (size_t i = 0; i < 4; ++i)
+  {
+    ellipseParameters[i] = std::stod(string, & pos);
+    string += pos;
+  }
+  if (*string != '\0')
+  {
+    throw std::invalid_argument("Invalid ellipse parameters");
+  }
+  baranov::point_t center = { ellipseParameters[0], ellipseParameters[1] };
+  return new baranov::Ellipse(center, ellipseParameters[2], ellipseParameters[3]);
+}
--- baranov.pavel/T1/inputShapes.hpp
+++ baranov.pavel/T1/inputShapes.hpp
@@ -6,6 +6,9 @@
 namespace baranov
 {
   baranov::Shape ** expandShapes(baranov::Shape ** shapes, size_t size, size_t newSize);
+  baranov::Shape * parseRectangle(const char * string);
+  baranov::Shape * parseRing(const char * string);
+  baranov::Shape * parseEllipse(const char * string);
   baranov::Shape ** inputShapes(std::istream & input, size_t & size);
 }
 
