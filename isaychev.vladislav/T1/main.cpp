6aff94068a1532611dcb68b1f94501478baac472
Z0ltaan <gregnov69@gmail.com>
2024-01-01 20:03:22 +0300

    added workpice(in main.cpp) for createFigure function

--- isaychev.vladislav/T1/main.cpp
+++ isaychev.vladislav/T1/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "shape.hpp"
 //#include "rectangle.hpp"
 //#include "base-types.hpp"
 #include "inputString.hpp"
@@ -7,7 +8,7 @@ int checkString(const char * string, const char * strForCheck)
 {
   size_t i = 0;
   int checkFlag = 1;
-  while (strForCheck[i] != '\0')
+  while (strForCheck[i] != '\0')// || string[i] != '\0')
   {
     if (strForCheck[i] != string[i])
     {
@@ -18,13 +19,60 @@ int checkString(const char * string, const char * strForCheck)
   return checkFlag;
 }
 
+size_t determineShape(const char * str)
+{
+  constexpr size_t numOfFigures = 3;
+  const char * figureNames[numOfFigures] = {"RECTANGLE", "CIRCLE", "REGULAR"};
+  size_t numOfCurrFigure = 1;
+  for (size_t i = 0; i < numOfFigures; i++)
+  {
+    if (checkString(str, figureNames[i]) == 1)
+    {
+      return numOfCurrFigure;
+    }
+    numOfCurrFigure++;
+  }
+  return 0;
+}
+
+//  size_t countWSpaces(const char * str) {}
+
+  //double * parseFigureDesc
+
+/*Shape * createFigure(const char * str)
+{
+  numOfCurrFigure = determineShape(str);
+  Shape * currFigure = nullptr;
+  if (numOfCurrFigure == 0)
+  {
+    std::cout << "null\n";
+    return currFigure;
+  }
+  else if (numOfCurrFigure == 1)
+  {
+//    currFigure = createRectangle();
+    std::cout << "rec\n";
+    return currFigure;
+  }
+  else if (numOfCurrFigure == 2)
+  {
+    currFigure = createCircle();
+    return currFigure;
+  }
+  else if (numOfCurrFigure == 3)
+  {
+    currFigure = createRegular();
+    return currFigure;
+  }
+}*/
+
 int main()
 {
   using namespace isaychev;
   const char * scaleStr = "SCALE ";
   char * currDesc = nullptr;
-  size_t capacity = 10;
-  size_t length = 0;
+  size_t capacity = 10, length = 0, i = 0;
+  Shape * Figures[1000] = {};
   while (std::cin)
   {
     try
@@ -41,5 +89,8 @@ int main()
     {
       break;
     }
+    std::cout << determineShape(currDesc) << "\n";
+    delete [] currDesc;
   }
+  delete [] currDesc;
 }
