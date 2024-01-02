69d57b8952038d1a5481b9cd644f4dc196c9369a
User1235321 <pavreb1235321@gmail.com>
2024-01-02 19:10:24 +0300

    Working rectangle

--- rebdev.pavel/T1/rectangle.cpp
+++ rebdev.pavel/T1/rectangle.cpp
@@ -5,41 +5,77 @@ rebdev::Rectangle::Rectangle():
   upRightAngle_(point_t{0.0, 0.0})
 {};
 rebdev::Rectangle::Rectangle(const point_t & lowLeftAngle, const point_t & upRightAngle):
-  lowLeftAngle_(lowLeftAngle),
-  upRightAngle_(upRightAngle)
-{};
+  lowLeftAngle_(point_t{0.0, 0.0}),
+  upRightAngle_(point_t{0.0, 0.0})
+{
+  if ((upRightAngle.x_ < lowLeftAngle.x_) || (upRightAngle.y_ < lowLeftAngle.y_))
+  {
+    lowLeftAngle_ = upRightAngle;
+    upRightAngle_ = lowLeftAngle;
+  }
+  else
+  {
+    lowLeftAngle_ = lowLeftAngle;
+    upRightAngle_ = upRightAngle;
+  }
+};
 rebdev::Rectangle::Rectangle(const double xLLA, const double yLLA, const double xURA, const double yURA):
-  lowLeftAngle_(point_t{xLLA, yLLA}),
-  upRightAngle_(point_t{xURA, yURA})
-{};
+  lowLeftAngle_(point_t{0.0, 0.0}),
+  upRightAngle_(point_t{0.0, 0.0})
+{
+  if ((xURA < xLLA) || (yURA < yLLA))
+  {
+    lowLeftAngle_ = point_t{xURA, yURA};
+    upRightAngle_ = point_t{xLLA, yLLA};
+  }
+  else
+  {
+    lowLeftAngle_ = point_t{xLLA, yLLA};
+    upRightAngle_ = point_t{xURA, yURA};
+  }
+};
 
 
 double rebdev::Rectangle::getArea() const
 {
-  //some realization
-  return  0.0;
+  return  ((upRightAngle_.x_ - lowLeftAngle_.x_) * (upRightAngle_.y_ - lowLeftAngle_.y_));
 };
 
 rebdev::rectangle_t rebdev::Rectangle::getFrameRect()
 {
-  //some realization
-  return rectangle_t{0.0, 0.0, 0.0};
+  double width = upRightAngle_.x_ - lowLeftAngle_.x_;
+  double height = upRightAngle_.y_ - lowLeftAngle_.y_;
+  point_t pos = {(upRightAngle_.x_ + lowLeftAngle_.x_)/2, (upRightAngle_.y_ + lowLeftAngle_.y_)/2};
+  return rectangle_t{width, height, pos};
 };
 
-void rebdev::Rectangle::move(const rectangle_t &)
+void rebdev::Rectangle::move(const point_t point)
 {
-  //some realization
+  rectangle_t frameRectangle = getFrameRect();
+
+  double yChange = frameRectangle.pos_.y_ - point.y_;
+  double xChange = frameRectangle.pos_.x_ - point.x_;
+
+  upRightAngle_.y_ += yChange;
+  upRightAngle_.x_ += xChange;
+  lowLeftAngle_.y_ += yChange;
+  lowLeftAngle_.x_ += xChange;
 };
 
 void rebdev::Rectangle::move(const double x, const double y)
 {
   lowLeftAngle_.x_ += x;
   lowLeftAngle_.y_ += y;
-  //some realization
+  upRightAngle_.x_ += x;
+  upRightAngle_.y_ += y;
 };
 
 void rebdev::Rectangle::scale(const double k)
 {
-  lowLeftAngle_.x_ += k;
-  //somerealization
+  rectangle_t frameRectangle = getFrameRect();
+
+  upRightAngle_.x_ = (upRightAngle_.x_ - frameRectangle.pos_.x_) * k + frameRectangle.pos_.x_;
+  upRightAngle_.y_ = (upRightAngle_.y_ - frameRectangle.pos_.y_) * k + frameRectangle.pos_.y_;
+  lowLeftAngle_.x_ = (lowLeftAngle_.x_ - frameRectangle.pos_.x_) * k + frameRectangle.pos_.x_;
+  lowLeftAngle_.y_ = (lowLeftAngle_.y_ - frameRectangle.pos_.y_) * k + frameRectangle.pos_.y_;
 };
--- rebdev.pavel/T1/rectangle.hpp
+++ rebdev.pavel/T1/rectangle.hpp
@@ -10,11 +10,11 @@ namespace rebdev
       Rectangle();
       Rectangle(const point_t & lowLeftAngle, const point_t & upRightAngle);
       Rectangle(const double xLLA, const double yLLA, const double xURA, const double yURA);
-      
+
 
       double getArea() const;
       rectangle_t getFrameRect();
-      void move(const rectangle_t &);
+      void move(const point_t point);
       void move(const double x, const double y);
       void scale(const double k);
     private:
