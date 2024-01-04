102d6104ef529646f9f345a33fdb48eb43a09f77
IastWish <antony_lebedevv@mail.ru>
2024-01-04 18:56:50 +0300

    Arg[1] processing

--- lebedev.anton/P4/main.cpp
+++ lebedev.anton/P4/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main(int argc, char * argv[])
 {
@@ -7,4 +9,26 @@ int main(int argc, char * argv[])
     std::cerr << "Error: Four command line arguments are needed\n";
     return 1;
   }
+
+  int num = 0;
+  try
+  {
+    num = std::stoll(argv[1]);
+  }
+  catch (const std::out_of_range &)
+  {
+    std::cerr << "Error: First parameter must be 1 or 2\n";
+    return 1;
+  }
+  catch (const std::invalid_argument &)
+  {
+    std::cerr << "Error: First parameter must be a number\n";
+    return 1;
+  }
+  if (num != 1 && num != 2)
+  {
+    std::cerr << "Error: First parameter must be 1 or 2\n";
+    return 1;
+  }
+  return 0;
 }
