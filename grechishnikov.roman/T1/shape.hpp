2fe207ac8b940ccc421c7746af2f5aa0b9d08692
Steuger7 <steuger7@mail.ru>
2024-01-05 00:17:33 +0300

    base-types and shape declaration

---
+++ grechishnikov.roman/T1/shape.hpp
@@ -0,0 +1,20 @@
+#ifndef SHAPE_HPP
+#define SHAPE_HPP
+
+#include "base-types.hpp"
+
+namespace grechishnikov
+{
+  class Shape
+  {
+  public:
+    virtual ~Shape() = default;
+    virtual double getArea() const = 0;
+    virtual rectangle_t gitFrameRect() const = 0;
+    virtual void move(const point_t& pos) = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual void scale(double rate) = 0;
+  };
+}
+
+#endif
