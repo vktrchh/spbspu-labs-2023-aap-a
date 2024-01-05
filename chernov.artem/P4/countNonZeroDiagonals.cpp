ee8e2ff431dfbec9d48f6175aec99676825c6659
CA21lmFlame <chernov.ar@spbstu.ru>
2024-01-05 23:05:02 +0300

    used an unused variable

--- chernov.artem/P4/countNonZeroDiagonals.cpp
+++ chernov.artem/P4/countNonZeroDiagonals.cpp
@@ -4,12 +4,12 @@
 
 size_t chernov::countNonZeroDiagonals(int* matrix, size_t cols, size_t rows)
 {
-  size_t counter = (cols - 1) * 2;
-  for (size_t i = 1; i < cols; i++)
+  size_t counter = (rows - 1) * 2;
+  for (size_t i = 1; i < rows; i++)
   {
-    for (size_t j = 0; j < cols - i; j++)
+    for (size_t j = 0; j < rows - i; j++)
     {
-      if (matrix[cols * j + j + i] == 0)
+      if (matrix[rows * j + j + i] == 0)
       {
         counter -= 1;
         break;
