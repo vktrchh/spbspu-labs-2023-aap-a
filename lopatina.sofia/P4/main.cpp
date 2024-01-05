fd2bacb08f8009446e9c44bfbad4372a3d73c20e
SofiyLop <lopatina.sa@edu.spbstu.ru>
2024-01-06 00:59:05 +0300

    Working with matrix

--- lopatina.sofia/P4/main.cpp
+++ lopatina.sofia/P4/main.cpp
@@ -3,11 +3,11 @@
 #include <cstdlib>
 #include <string>
 
-size_t inputArray(std::istream & in, int * start, size_t amount, size_t toread)
+size_t inputArray(std::istream & in, int * matrix, size_t amount, size_t toread)
 {
   for (size_t i = 0; i < std::min(toread, amount); ++i)
   {
-    if (!(in >> start[i]))
+    if (!(in >> matrix[i]))
     {
       return i;
     }
@@ -45,7 +45,7 @@ int main(int argc, char ** argv)
   }
   catch (const std::out_of_range &)
   {
-    std::cerr << "First parameter is too large\n";
+    std::cerr << "First parameter is out of range\n";
     return 1;
   }
   catch (const std::invalid_argument &)
@@ -61,15 +61,14 @@ int main(int argc, char ** argv)
 
   //argv[2] - имя файла с массивом
   size_t rows = 0, cols = 0;
-  //{
-    std::ifstream input(argv[2]);
-    input >> rows >> cols;;
-    if (!input)
-    {
-      std::cerr << "Cannot read rows or cols\n";
-      return 2;
-    }
-  //}
+
+  std::ifstream input(argv[2]);
+  input >> rows >> cols;;
+  if (!input)
+  {
+    std::cerr << "Cannot read rows or cols\n";
+    return 2;
+  }
   std::cout << "Rows and cols are: " <<  rows << ' ' << cols << '\n';
 
   //------------------------
@@ -78,9 +77,50 @@ int main(int argc, char ** argv)
   {
     int matrix[10000] = {};
     size_t result =  inputArray(input, matrix, rows * cols, rows * cols);
-    std::cout << result << '\n';
+    //---for test---
+    std::cout << "Result inputArray: " <<  result << '\n';
+    std::cout << "Matrix: ";
+    for (int i = 0; i < rows * cols; ++i)
+    {
+      std::cout << matrix[i] << ' ';
+    }
+    std::cout << '\n';
+    //--------------
+    if (result != rows * cols)
+    {
+      std::cerr << "Invalid input array\n";
+      return 2;
+    }
+    //работа с матрицей (нижняя треугольная матрица => квадратная матрица)
+    if (rows != cols || rows == 0 && cols == 0)
+    {
+      std::cout << "false\n";
+      return 0;
+    }
+    for (int i = 0; i < rows; ++i)
+    {
+      for (int j = 0; j < cols; ++j)
+      {
+        if (i < j)
+        {
+          if (matrix[i * rows + j] != 0)
+          {
+            std::cout << "false\n";
+            return 0;
+          }
+        }
+      }
+    }
+    std::cout << "true\n";
+    return 0;
+  //------------------------------------
   }
+
+
+
+
   // динамический массив ; free store; num = 2
+  //if (num == 2)
   //------------------------
 
 
