8f3cf48345f304387bc011c7c08fce55cf2c5e48
IastWish <antony_lebedevv@mail.ru>
2024-01-06 16:10:04 +0300

    Inputting array function

---
+++ lebedev.anton/P4/input_array.hpp
@@ -0,0 +1,9 @@
+#ifndef INPUT_ARRAY-HPP
+#define INPUT_ARRAY_HPP
+
+#include <iostream>
+namespace lebedev
+{
+  size_t inputArray(std::istream & input, int matrix[], size_t rows, size_t cols);
+}
+#endif
