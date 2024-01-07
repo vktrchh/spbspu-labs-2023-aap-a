f8d5b9d8cfd2d1ab8f5448f0e338da7b7493858e
marlions <marishin.lv@edu.spbstu.ru>
2024-01-07 22:08:38 +0300

    scale

--- marishin.leonid/T1/scale.cpp
+++ marishin.leonid/T1/scale.cpp
@@ -1,18 +1,15 @@
 #include "scale.hpp"
 
-namespace marishin
+void marishin::scale(Shape* shape, point_t& center, double scaleCoefficient)
 {
-  void scale(Shape* shape, point_t& center, double scaleCoefficient)
+  if (scaleCoefficient < 0)
   {
-    if (scaleCoefficient < 0)
-    {
-      throw std::invalid_argument("Invalid scale coefficient. Must be non-negative.");
-    }
-
-    point_t initialPosition = shape->getFrameRect().pos;
-    shape->move(center);
-    shape->scale(scaleCoefficient);
-    shape->move(scaleCoefficient * (initialPosition.x - shape->getFrameRect().pos.x),
-    scaleCoefficient * (initialPosition.y - shape->getFrameRect().pos.y));
+    throw std::invalid_argument("Invalid scale coefficient. Must be non-negative.");
   }
+
+  point_t initialPosition = shape->getFrameRect().pos;
+  shape->move(center);
+  shape->scale(scaleCoefficient);
+  shape->move(scaleCoefficient * (initialPosition.x - shape->getFrameRect().pos.x),
+  scaleCoefficient * (initialPosition.y - shape->getFrameRect().pos.y));
 }
