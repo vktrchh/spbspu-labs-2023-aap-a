a37e2cb17c49975582505ac72b8e14bbcf0807c6
marlions <marishin.lv@edu.spbstu.ru>
2024-01-07 20:11:10 +0300

    include

--- marishin.leonid/T1/inputshape.hpp
+++ marishin.leonid/T1/inputshape.hpp
@@ -1,4 +1,13 @@
 #ifndef INPUTSHAPE_HPP
 #define INPUTSHAPE_HPP
 
+#include "shape.hpp"
+#include "rectangle.hpp"
+#include "triangle.hpp"
+#include "ring.hpp"
+#include <string>
+#include <iostream>
+
+
+
 #endif
