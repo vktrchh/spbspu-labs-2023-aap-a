8a7ff988f9d22842fb683236c0bf764cde5e2880
marlions <marishin.lv@edu.spbstu.ru>
2024-01-07 21:11:51 +0300

    Exist

--- marishin.leonid/T1/calculatetriangle.cpp
+++ marishin.leonid/T1/calculatetriangle.cpp
@@ -4,3 +4,13 @@ double calculateDistance(const point_t& p1, const point_t& p2)
 {
   return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
 }
+
+bool isTriangleExist(const point_t& p1, const point_t& p2, const point_t& p3)
+{
+  double a = calculateDistance(p1, p2);
+  double b = calculateDistance(p2, p3);
+  double c = calculateDistance(p3, p1);
+
+  return (a + b > c) && (b + c > a) && (c + a > b);
+}
+
