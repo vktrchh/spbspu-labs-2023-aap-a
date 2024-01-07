07b5f274f96e43c0229bd6d9cf19ca15fcaa9655
marlions <marishin.lv@edu.spbstu.ru>
2024-01-07 22:12:12 +0300

    triangle

--- marishin.leonid/T1/triangle.hpp
+++ marishin.leonid/T1/triangle.hpp
@@ -4,20 +4,23 @@
 #include "shape.hpp"
 #include "calculatetriangle.hpp"
 
-class Triangle : public Shape
+namespace marishin
 {
-public:
-  Triangle(const point_t firstPoint, const point_t secondPoint, const point_t thirdPoint);
-  virtual ~Triangle();
-  virtual double getArea();
-  virtual rectangle_t getFrameRect();
-  virtual void move(const point_t newPos);
-  virtual void move(const double dx, const double dy);
-  virtual void scale(const double factor);
-private:
-  point_t firstPoint_;
-  point_t secondPoint_;
-  point_t thirdPoint_;
+  class Triangle : public Shape
+  {
+  public:
+    Triangle(const point_t firstPoint, const point_t secondPoint, const point_t thirdPoint);
+    virtual ~Triangle();
+    virtual double getArea();
+    virtual rectangle_t getFrameRect();
+    virtual void move(const point_t newPos);
+    virtual void move(const double dx, const double dy);
+    virtual void scale(const double factor);
+  private:
+    point_t firstPoint_;
+    point_t secondPoint_;
+    point_t thirdPoint_;
+  }
 }
 
 #endif
