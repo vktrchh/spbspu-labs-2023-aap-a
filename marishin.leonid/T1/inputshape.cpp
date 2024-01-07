3f3b9422f933328413158e283b6876ebd94342d8
marlions <marishin.lv@edu.spbstu.ru>
2024-01-07 17:07:37 +0300

    num_shapes

--- marishin.leonid/T1/inputshape.cpp
+++ marishin.leonid/T1/inputshape.cpp
@@ -2,5 +2,9 @@
 
 marishin::Shape** marishin::inputShape(std::istream& in, size_t& shapeCount)
 {
+  const size_t NUM_SHAPES = 3;
+  const std::string shapeNames[NUM_SHAPES] = { "RECTANGLE", "TRIANGLE", "RING" };
+  const size_t shapeParametrsCount[NUM_SHAPES] = { 4, 6, 4};
+
 
 }
