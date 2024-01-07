b775951b9b7e6ff673813439d0befde8c7585979
jiggerman <liggerman@mail.ru>
2024-01-07 19:33:57 +0300

    Add hpp file for SQUARE

---
+++ chistyakov.vladislav/T1/square.hpp
@@ -0,0 +1,24 @@
+#ifndef SQUARE_HPP
+#define SQUARE_HPP
+
+#include "shape.hpp"
+
+namespace chistyakov
+{
+  class Square : public Shape
+  {
+  public:
+    Square(const point_t & leftCorner, const double side);
+    virtual ~Square();
+    virtual double getArea() const;
+    virtual rectangle_t getFrameRect() const;
+    virtual void move(const point_t & point);
+    virtual void move(const double mx, const double my);
+    virtual void scale(const double ratio);
+  private:
+    point_t leftCorner_;
+    double side_;
+  }
+}
+
+#endif
