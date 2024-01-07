81f361d3b877998cbf0415b924db2c08e53fc32e
marlions <marishin.lv@edu.spbstu.ru>
2024-01-07 22:18:52 +0300

    fix

--- marishin.leonid/T1/calculatetriangle.cpp
+++ marishin.leonid/T1/calculatetriangle.cpp
@@ -1,11 +1,11 @@
 #include "calculatetriangle.hpp"
 
-double calculateDistance(const point_t& p1, const point_t& p2)
+double marishin::calculateDistance(const point_t& p1, const point_t& p2)
 {
   return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
 }
 
-bool isTriangleExist(const point_t& p1, const point_t& p2, const point_t& p3)
+bool marishin::isTriangleExist(const point_t& p1, const point_t& p2, const point_t& p3)
 {
   double a = calculateDistance(p1, p2);
   double b = calculateDistance(p2, p3);
@@ -14,7 +14,7 @@ bool isTriangleExist(const point_t& p1, const point_t& p2, const point_t& p3)
   return (a + b > c) && (b + c > a) && (c + a > b);
 }
 
-double calculateTriangleArea(const point_t& p1, const point_t& p2, const point_t& p3)
+double marishin::calculateTriangleArea(const point_t& p1, const point_t& p2, const point_t& p3)
 {
   return 0.5 * abs((p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y)));
 }
