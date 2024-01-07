d44a0e1028b8a17367fae280438ca2ccedca4e8e
marlions <marishin.lv@edu.spbstu.ru>
2024-01-07 22:00:12 +0300

    cg

--- marishin.leonid/T1/ring.cpp
+++ marishin.leonid/T1/ring.cpp
@@ -1,6 +1,6 @@
 #include "ring.hpp"
 
-Ring::Ring(point_t center, const double radius1, const double radius2):
+marishin::Ring::Ring(point_t center, const double radius1, const double radius2):
   radius1_(radius1),
   radius2_(radius2),
   center_(center)
@@ -11,33 +11,33 @@ Ring::Ring(point_t center, const double radius1, const double radius2):
   }
 }
 
-Ring::~Ring()
+marishin::Ring::~Ring()
 {
 }
 
-double Ring::getArea()
+double marishin::Ring::getArea()
 {
   return ((M_PI * pow(radius2_, 2)) - (M_PI * pow(radius1_, 2)));
 }
 
-rectangle_t Ring::getFrameRect()
+rectangle_t marishin::Ring::getFrameRect()
 {
   point_t pos = center_;
   return { pos, radius2_ * 2, radius2_ * 2 };
 }
 
-void Ring::move(const point_t newPos)
+void marishin::Ring::move(const point_t newPos)
 {
   center_ = newPos;
 }
 
-void Ring::move(const double dx, const double dy)
+void marishin::Ring::move(const double dx, const double dy)
 {
   center_.x += dx;
   center_.y += dy;
 }
 
-void Ring::scale(const double factor)
+void marishin::Ring::scale(const double factor)
 {
   radius1_ *= factor;
   radius2_ *= factor;
