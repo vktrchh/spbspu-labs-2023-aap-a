c6c7ef49f868cc3bfbf104fbe807aac19ea34832
marlions <marishin.lv@edu.spbstu.ru>
2024-01-07 21:28:34 +0300

    class Ring

--- marishin.leonid/T1/ring.hpp
+++ marishin.leonid/T1/ring.hpp
@@ -6,4 +6,20 @@
 #include <cmath>
 #define _USE_MATH_DEFINES
 
+class Ring : public Shape
+{
+public:
+  Ring(point_t center, const double radius1, const double radius2);
+  virtual ~Ring();
+  virtual double getArea();
+  virtual rectangle_t getFrameRect();
+  virtual void move(const point_t newPos);
+  virtual void move(const double dx, const double dy);
+  virtual void scale(const double factor);
+private:
+  point_t center_;
+  double radius1_;
+  double radius2_;
+};
+
 #endif
