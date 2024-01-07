e517d812e2693a83f9cd92d624c18485ac260025
marlions <marishin.lv@edu.spbstu.ru>
2024-01-07 20:35:30 +0300

    out

--- marishin.leonid/T1/displayoutput.cpp
+++ marishin.leonid/T1/displayoutput.cpp
@@ -4,6 +4,15 @@ namespace marishin
 {
   void outputShapesInfo(std::ostream& out, size_t shapeCount, Shape** shapes)
   {
+    double totalArea = 0;
+
+    for (size_t i = 0; i < shapeCount; ++i)
+    {
+      totalArea += shapes[i]->getArea();
+    }
+
+    out << "Total Area: " << totalArea << " ";
+
 
   }
 }
