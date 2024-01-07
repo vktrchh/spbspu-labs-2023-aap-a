270845ef01b726c858f01872048b1f8d81b84ddd
marlions <marishin.lv@edu.spbstu.ru>
2024-01-07 22:30:20 +0300

    fix 2

--- marishin.leonid/T1/triangle.cpp
+++ marishin.leonid/T1/triangle.cpp
@@ -1,7 +1,7 @@
 #include "triangle.hpp"
 #include <stdexcept>
 
-Triangle::Triangle(const point_t firstPoint, const point_t secondPoint, const point_t thirdPoint):
+marishin::Triangle::Triangle(const point_t firstPoint, const point_t secondPoint, const point_t thirdPoint):
   firstPoint_(firstPoint),
   secondPoint_(secondPoint),
   thirdPoint_(thirdPoint)
@@ -12,17 +12,17 @@ Triangle::Triangle(const point_t firstPoint, const point_t secondPoint, const po
   }
 }
 
-Triangle::~Triangle()
+marishin::Triangle::~Triangle()
 {
 }
 
-double Triangle::getArea()
+double marishin::Triangle::getArea()
 {
-  double area = calculateTriangleArea(firstPoint_, secondPoint_, thirdPoint_);
+  double area = marishin::calculateTriangleArea(firstPoint_, secondPoint_, thirdPoint_);
   return area;
 }
 
-rectangle_t Triangle::getFrameRect()
+rectangle_t marishin::Triangle::getFrameRect()
 {
   point_t pos = { (std::max(firstPoint_.x, (std::max(secondPoint_.x, thirdPoint_.x))) - std::min(firstPoint_.x, (std::min(secondPoint_.x, thirdPoint_.x)))) / 2, (std::max(firstPoint_.y, (std::max(secondPoint_.y, thirdPoint_.y))) - std::min(firstPoint_.y, (std::min(secondPoint_.y, thirdPoint_.y)))) / 2 };
   double height = (std::max(firstPoint_.y, (std::max(secondPoint_.y, thirdPoint_.y))) - std::min(firstPoint_.y, (std::min(secondPoint_.y, thirdPoint_.y))));
@@ -30,13 +30,13 @@ rectangle_t Triangle::getFrameRect()
   return { pos, height, width };
 }
 
-void Triangle::move(const point_t newPos)
+void marishin::Triangle::move(const point_t newPos)
 {
   point_t pos = { (firstPoint_.x + secondPoint_.x + thirdPoint_.x) / 3.0, (firstPoint_.y + secondPoint_.y + thirdPoint_.y) / 3.0 };
-  move(newPos.x - pos.x, newPos.y - pos.y);
+  marishin::move(newPos.x - pos.x, newPos.y - pos.y);
 }
 
-void Triangle::move(const double dx, const double dy)
+void marishin::Triangle::move(const double dx, const double dy)
 {
   point_t pos = { (firstPoint_.x + secondPoint_.x + thirdPoint_.x) / 3.0, (firstPoint_.y + secondPoint_.y + thirdPoint_.y) / 3.0 };
 
@@ -53,7 +53,7 @@ void Triangle::move(const double dx, const double dy)
   thirdPoint_.y += dy;
 }
 
-void Triangle::scale(const double factor)
+void marishin::Triangle::scale(const double factor)
 {
   point_t pos = { (firstPoint_.x + secondPoint_.x + thirdPoint_.x) / 3.0, (firstPoint_.y + secondPoint_.y + thirdPoint_.y) / 3.0 };
 
