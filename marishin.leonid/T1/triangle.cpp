6a8f3f66369c816b86cc90b73847f64818f55af6
marlions <marishin.lv@edu.spbstu.ru>
2024-01-07 21:53:24 +0300

    class

--- marishin.leonid/T1/triangle.cpp
+++ marishin.leonid/T1/triangle.cpp
@@ -1,4 +1,68 @@
 #include "triangle.hpp"
 #include <stdexcept>
 
+Triangle::Triangle(const point_t firstPoint, const point_t secondPoint, const point_t thirdPoint):
+  firstPoint_(firstPoint),
+  secondPoint_(secondPoint),
+  thirdPoint_(thirdPoint)
+{
+  if (!(isTriangleExist(firstPoint_, secondPoint_, thirdPoint_)))
+  {
+    throw std::logic_error("Irregular triangle");
+  }
+}
 
+Triangle::~Triangle()
+{
+}
+
+double Triangle::getArea()
+{
+  double area = calculateTriangleArea(firstPoint_, secondPoint_, thirdPoint_);
+  return area;
+}
+
+rectangle_t Triangle::getFrameRect()
+{
+  point_t pos = { (std::max(firstPoint_.x, (std::max(secondPoint_.x, thirdPoint_.x))) - std::min(firstPoint_.x, (std::min(secondPoint_.x, thirdPoint_.x)))) / 2, (std::max(firstPoint_.y, (std::max(secondPoint_.y, thirdPoint_.y))) - std::min(firstPoint_.y, (std::min(secondPoint_.y, thirdPoint_.y)))) / 2 };
+  double height = (std::max(firstPoint_.y, (std::max(secondPoint_.y, thirdPoint_.y))) - std::min(firstPoint_.y, (std::min(secondPoint_.y, thirdPoint_.y))));
+  double width = (std::max(firstPoint_.x, (std::max(secondPoint_.x, thirdPoint_.x))) - std::min(firstPoint_.x, (std::min(secondPoint_.x, thirdPoint_.x))));
+  return { pos, height, width };
+}
+
+void Triangle::move(const point_t newPos)
+{
+  point_t pos = { (firstPoint_.x + secondPoint_.x + thirdPoint_.x) / 3.0, (firstPoint_.y + secondPoint_.y + thirdPoint_.y) / 3.0 };
+  move(newPos.x - pos.x, newPos.y - pos.y);
+}
+
+void Triangle::move(const double dx, const double dy)
+{
+  point_t pos = { (firstPoint_.x + secondPoint_.x + thirdPoint_.x) / 3.0, (firstPoint_.y + secondPoint_.y + thirdPoint_.y) / 3.0 };
+
+  pos.x += dx;
+  pos.y += dy;
+
+  firstPoint_.x += dx;
+  firstPoint_.y += dy;
+
+  secondPoint_.x += dx;
+  secondPoint_.y += dy;
+
+  thirdPoint_.x += dx;
+  thirdPoint_.y += dy;
+}
+
+void Triangle::scale(const double factor)
+{
+  point_t pos = { (firstPoint_.x + secondPoint_.x + thirdPoint_.x) / 3.0, (firstPoint_.y + secondPoint_.y + thirdPoint_.y) / 3.0 };
+
+  firstPoint_.x = pos.x + (firstPoint_.x - pos.x) * factor;
+  firstPoint_.y = pos.y + (firstPoint_.y - pos.y) * factor;
+
+  secondPoint_.x = pos.x + (secondPoint_.x - pos.x) * factor;
+  secondPoint_.y = pos.y + (secondPoint_.y - pos.y) * factor;
+
+  thirdPoint_.x = pos.x + (thirdPoint_.x - pos.x) * factor;
+  thirdPoint_.y = pos.y + (thirdPoint_.y - pos.y) * factor;
+}
