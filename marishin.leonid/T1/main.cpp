8d1b9c151423535add429971c6e557ab9ec5acf0
marlions <marishin.lv@edu.spbstu.ru>
2024-01-07 16:24:28 +0300

    myShapes

--- marishin.leonid/T1/main.cpp
+++ marishin.leonid/T1/main.cpp
@@ -4,14 +4,6 @@
 
 int main()
 {
-  double x1 = 0;
-  double y1 = 0;
-  double x2 = 0;
-  double y2 = 0;
-  std::string str = "RECTANGLE";
-  std::cin >> x1 >> y1 >> x2 >> y2;
-  if (str == "RECTANGLE")
-  {
-    ///...
-  }
+  Shape** myShapes = nullptr;
+  size_t numberOfShapes = 0;
 }
