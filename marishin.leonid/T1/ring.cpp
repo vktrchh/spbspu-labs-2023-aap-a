1f02a6618a17836c6f019e3bd00625901b8cfc33
marlions <marishin.lv@edu.spbstu.ru>
2024-01-07 21:38:14 +0300

    Ring::Ring

--- marishin.leonid/T1/ring.cpp
+++ marishin.leonid/T1/ring.cpp
@@ -1,4 +1,13 @@
 #include "ring.hpp"
 #include <math.h>
 
-
+Ring::Ring(point_t center, const double radius1, const double radius2):
+  radius1_(radius1),
+  radius2_(radius2),
+  center_(center)
+{
+  if (radius1_ <= 0 || radius2_ <= 0 || radius1_ >= radius2_)
+  {
+    throw std::logic_error("Invalid ring");
+  }
+}
