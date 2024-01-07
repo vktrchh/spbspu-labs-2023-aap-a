be2dfed3a6828ef163e547b7aa8a702dfd0ade31
marlions <marishin.lv@edu.spbstu.ru>
2024-01-07 22:06:28 +0300

    output

--- marishin.leonid/T1/displayoutput.cpp
+++ marishin.leonid/T1/displayoutput.cpp
@@ -1,35 +1,32 @@
 #include "displayoutput.hpp"
 
-namespace marishin
+void marishin::outputShapesInfo(std::ostream& out, size_t shapeCount, Shape** shapes)
 {
-  void outputShapesInfo(std::ostream& out, size_t shapeCount, Shape** shapes)
+  double totalArea = 0;
+
+  for (size_t i = 0; i < shapeCount; ++i)
   {
-    double totalArea = 0;
-
-    for (size_t i = 0; i < shapeCount; ++i)
-    {
-      totalArea += shapes[i]->getArea();
-    }
-
-    out << "Total Area: " << totalArea << " ";
-
-    for (size_t i = 0; i < shapeCount; ++i)
-    {
-      const auto& frameRect = shapes[i]->getFrameRect();
-      double width = frameRect.width;
-      double height = frameRect.height;
-      point_t point = frameRect.pos;
-
-      point_t lowerLeftCorner = { point.x - width / 2.0, point.y - height / 2.0 };
-      point_t topRightCorner = { point.x + width / 2.0, point.y + height / 2.0 };
-
-      out << std::fixed << std::setprecision(1) << std::round(10 * lowerLeftCorner.x) / 10 << " "
-          << std::round(10 * lowerLeftCorner.y) / 10 << " " << std::round(10 * topRightCorner.x) / 10 << " "
-          << std::round(10 * topRightCorner.y) / 10;
-    }
-
-
-    out << "\n";
-
+    totalArea += shapes[i]->getArea();
   }
+
+  out << "Total Area: " << totalArea << " ";
+
+  for (size_t i = 0; i < shapeCount; ++i)
+  {
+    const auto& frameRect = shapes[i]->getFrameRect();
+    double width = frameRect.width;
+    double height = frameRect.height;
+    point_t point = frameRect.pos;
+
+    point_t lowerLeftCorner = { point.x - width / 2.0, point.y - height / 2.0 };
+    point_t topRightCorner = { point.x + width / 2.0, point.y + height / 2.0 };
+
+    out << std::fixed << std::setprecision(1) << std::round(10 * lowerLeftCorner.x) / 10 << " "
+        << std::round(10 * lowerLeftCorner.y) / 10 << " " << std::round(10 * topRightCorner.x) / 10 << " "
+        << std::round(10 * topRightCorner.y) / 10;
+  }
+
+
+  out << "\n";
+
 }
