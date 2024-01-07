026799411dfa480d2f0693229f14d2c56d9a65c7
marlions <marishin.lv@edu.spbstu.ru>
2024-01-07 20:51:03 +0300

    namespace

--- marishin.leonid/T1/rectangle.hpp
+++ marishin.leonid/T1/rectangle.hpp
@@ -3,19 +3,22 @@
 
 #include "shape.hpp"
 
-class Rectangle : public Shape
+namespace marishin
 {
-public:
-  Rectangle(const point_t lowerLeftCorner, const point_t topRightCorner);
-  virtual ~Rectangle();
-  virtual double getArea();
-  virtual rectangle_t getFrameRect();
-  virtual void move(const point_t newPos);
-  virtual void move(const double dx, const double dy);
-  virtual void scale(const double factor);
-private:
-  point_t lowerLeftCorner_;
-  point_t topRightCorner_;
-};
+  class Rectangle : public Shape
+  {
+  public:
+    Rectangle(const point_t lowerLeftCorner, const point_t topRightCorner);
+    virtual ~Rectangle();
+    virtual double getArea();
+    virtual rectangle_t getFrameRect();
+    virtual void move(const point_t newPos);
+    virtual void move(const double dx, const double dy);
+    virtual void scale(const double factor);
+  private:
+    point_t lowerLeftCorner_;
+    point_t topRightCorner_;
+  };
+}
 
 #endif
