d92788a63c46c041ec6ccd864e497b0a24154896
marlions <marishin.lv@edu.spbstu.ru>
2024-01-07 20:12:14 +0300

    hpp

--- marishin.leonid/T1/inputshape.hpp
+++ marishin.leonid/T1/inputshape.hpp
@@ -8,6 +8,9 @@
 #include <string>
 #include <iostream>
 
-
+Shape** inputShape(std::istream& in, size_t& shapeCount);
+void cleanupShapes(Shape** shapes, size_t count);
+Shape* createShape(const std::string& name, double* parameters);
+void skipLine(std::istream& in);
 
 #endif
