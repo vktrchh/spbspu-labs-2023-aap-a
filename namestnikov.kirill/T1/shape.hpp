6503f12363ab8a0356cc63da2f03b4e2fea84d3b
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-01-08 20:46:28 +0300

    shape.hpp change

--- namestnikov.kirill/T1/shape.hpp
+++ namestnikov.kirill/T1/shape.hpp
@@ -11,8 +11,8 @@ namespace namestnikov
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
     virtual void move(const point_t & p) = 0;
-    virtual void move(double dx, double dy) = 0;
-    virtual void scale(double coefficient) = 0;
+    virtual void move(const double dx, const double dy) = 0;
+    virtual void scale(const double coefficient) = 0;
     virtual ~Shape(){};
   };
 }
