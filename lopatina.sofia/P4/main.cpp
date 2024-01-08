402465315f64a3ad0df5d8f12e7b24bd51b03527
SofiyLop <lopatina.sa@edu.spbstu.ru>
2024-01-08 21:02:38 +0300

    Translation units

--- lopatina.sofia/P4/main.cpp
+++ lopatina.sofia/P4/main.cpp
@@ -2,92 +2,13 @@
 #include <fstream>
 #include <cstdlib>
 #include <string>
+#include "input_array.hpp"
+#include "is_lowertri_matrix.hpp"
+#include "count_diag_nzero.hpp"
 
-//input_arrey.cpp
-size_t inputArray(std::istream & in, int * matrix, size_t amount, size_t toread)
-{
-  for (size_t i = 0; i < std::min(toread, amount); ++i)
-  {
-    if (!(in >> matrix[i]))
-    {
-      return i;
-    }
-  }
-  return std::min(toread, amount);
-}
-
-//is_lower_tri_matrix.cpp
-int isLowerTriMatrix(int * matrix, size_t rows, size_t cols)
-{
-  if (rows != cols || rows == 0 || rows == 1)
-  {
-    return 0;
-  }
-  for (size_t i = 0; i < rows; ++i)
-  {
-    for (size_t j = 0; j < cols; ++j)
-    {
-      if (i < j && matrix[i * rows + j] != 0)
-      {
-        return 0;
-      }
-    }
-  }
-  return 1;
-}
-
-//num_diag_without_zero.cpp
-size_t countDiagWithoutZero(int * matrix, size_t rows, size_t cols)
-{
-  if (rows != cols || rows == 0)
-  {
-    return 0;
-  }
-  size_t counter = 0;
-  for (size_t i = 0; i < rows; ++i)
-  {
-    if (i == 0)
-    {
-      for (size_t j = 0; j < cols; ++j)
-      {
-        if (i != j)
-        {
-          counter += 1;
-          size_t n1 = i, n2 = j;
-          for (size_t k = 0; k < cols - j; ++k)
-          {
-            if (matrix[n1 * rows + n2] == 0)
-            {
-              counter -= 1;
-              break;
-            }
-            n1 += 1;
-            n2 += 1;
-          }
-        }
-      }
-    }
-    else if (i > 0)
-    {
-      counter += 1;
-      size_t n1 = i;
-      for (size_t k = 0; k < rows - i; ++k)
-      {
-        if (matrix[n1 * rows + k] == 0)
-        {
-          counter -= 1;
-          break;
-        }
-        n1 += 1;
-      }
-    }
-  }
-  return counter;
-}
-
-//main.cpp
 int main(int argc, char ** argv)
 {
+  using namespace lopatina;
   if (argc < 4)
   {
     std::cerr << "Not enough arguments\n";
@@ -99,7 +20,6 @@ int main(int argc, char ** argv)
     return 1;
   }
 
-  //argv[1] - номер задания (1 - массив фикс размера; 2 - динамический)
   int num = 0;
   try
   {
@@ -121,9 +41,7 @@ int main(int argc, char ** argv)
     return 1;
   }
 
-  //argv[2] - имя файла с массивом
   size_t rows = 0, cols = 0;
-
   std::ifstream input(argv[2]);
   input >> rows >> cols;
   if (!input)
@@ -132,19 +50,17 @@ int main(int argc, char ** argv)
     return 2;
   }
 
-  //----------Работа с массивом--------------
   int res1 = 0;
   size_t res2 = 0;
   if (num == 1)
   {
     int matrix[10000] = {};
-    size_t result =  inputArray(input, matrix, rows * cols, rows * cols);
+    size_t result = inputArray(input, matrix, rows * cols, rows * cols);
     if (result != rows * cols)
     {
       std::cerr << "Invalid input array\n";
       return 2;
     }
-
     res1 = isLowerTriMatrix(matrix, rows, cols);
     res2 = countDiagWithoutZero(matrix, rows, cols);
   }
@@ -152,20 +68,18 @@ int main(int argc, char ** argv)
   if (num == 2)
   {
     int * matrix = new int[rows * cols];
-    size_t result =  inputArray(input, matrix, rows * cols, rows * cols);
+    size_t result = inputArray(input, matrix, rows * cols, rows * cols);
     if (result != rows * cols)
     {
       std::cerr << "Invalid input array\n";
       delete[] matrix;
       return 2;
     }
-
     res1 = isLowerTriMatrix(matrix, rows, cols);
     res2 = countDiagWithoutZero(matrix, rows, cols);
     delete[] matrix;
   }
 
-  //argv[3] - имя файла для результата
   std::ofstream output(argv[3]);
   if (res1 == 0)
   {
