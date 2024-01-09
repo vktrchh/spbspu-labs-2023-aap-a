f596665c47a3cae5bc432e71e5edb4399cc88376
NovokhatskiyDanil <karuk.vova2005@mail.ru>
2024-01-09 23:54:13 +0300

    space is found another try

--- novokhatskiy.danil/T1/rectangle.cpp
+++ novokhatskiy.danil/T1/rectangle.cpp
@@ -1,7 +1,8 @@
 #include "rectangle.hpp"
 #include <stdexcept>
 
-novokhatskiy::Rectangle::Rectangle(const point_t &leftAngle, const point_t &rightAngle)
+novokhatskiy::Rectangle::Rectangle(const point_t &leftAngle, 
+const point_t &rightAngle)
 {
   if ((leftAngle.y >= leftAngle.x) || (rightAngle.y >= rightAngle.x))
   {
@@ -16,12 +17,14 @@ double novokhatskiy::Rectangle::getArea() const
 }
 rectangle_t novokhatskiy::Rectangle::getFrameRect() const
 {
-  return {(rightAngle_.x - leftAngle_.x), (rightAngle_.y - leftAngle_.y), {((rightAngle_.x - leftAngle_.x) / 2), ((rightAngle_.y - leftAngle_.y) / 2)}};
+  return {(rightAngle_.x - leftAngle_.x), (rightAngle_.y - leftAngle_.y), 
+  {((rightAngle_.x - leftAngle_.x) / 2), 
+  ((rightAngle_.y - leftAngle_.y) / 2)}};
 }
 void novokhatskiy::Rectangle::move(const point_t &p)
 {
   point_t center = {((rightAngle_.x - leftAngle_.x) / 2),
-                    ((rightAngle_.y - leftAngle_.y) / 2)};
+  ((rightAngle_.y - leftAngle_.y) / 2)};
   point_t difference = {p.x - center.x, p.y - center.y};
   leftAngle_.x += difference.x;
   leftAngle_.y += difference.y;
@@ -42,7 +45,7 @@ void novokhatskiy::Rectangle::scale(double ratio)
     throw std::invalid_argument("The ratio can't be negative or zero\n");
   }
   point_t center = {((rightAngle_.x - leftAngle_.x) / 2),
-                    ((rightAngle_.y - leftAngle_.y) / 2)};
+  ((rightAngle_.y - leftAngle_.y) / 2)};
   leftAngle_.x += (leftAngle_.x - center.x) * (ratio - 1);
   leftAngle_.y += (leftAngle_.y - center.y) * (ratio - 1);
   rightAngle_.x += (rightAngle_.x - center.x) * (ratio - 1);
