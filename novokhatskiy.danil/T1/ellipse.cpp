dd59a22e6726392e44b2bfc34c2ce1357ed323b9
DanilNovokhatskiy <karuk.vova2005@mail.ru>
2024-01-11 19:05:04 +0300

    fix

--- novokhatskiy.danil/T1/ellipse.cpp
+++ novokhatskiy.danil/T1/ellipse.cpp
@@ -5,41 +5,41 @@
 
 novokhatskiy::Ellipse::Ellipse(const point_t& center, double vert, double horizon)
 {
-	if ((vert < 0.0) && (horizon < 0.0))
-	{
-		throw std::invalid_argument("Ellipse radiuses are wrong\n");
-	}
-	center_ = center;
-	vert_ = vert;
-	horizon_ = horizon;
+  if ((vert < 0.0) && (horizon < 0.0))
+  {
+    throw std::invalid_argument("Ellipse radiuses are wrong\n");
+  }
+  center_ = center;
+  vert_ = vert;
+  horizon_ = horizon;
 }
 
 double novokhatskiy::Ellipse::getArea() const
 {
-	return 2.0 * M_PI * vert_ * horizon_;
+  return 2.0 * M_PI * vert_ * horizon_;
 }
 
 novokhatskiy::rectangle_t novokhatskiy::Ellipse::getFrameRect() const
 {
-	return {2 * horizon_, 2 * vert_, center_};
+  return {2 * horizon_, 2 * vert_, center_};
 }
 
 void novokhatskiy::Ellipse::move(const point_t& point)
 {
-	center_ = point;
+  center_ = point;
 }
 
 void novokhatskiy::Ellipse::move(double x, double y)
 {
-	center_ = {center_.x + x, center_.y + y };
+  center_ = {center_.x + x, center_.y + y };
 }
 
 void novokhatskiy::Ellipse::scale(double ratio)
 {
-	if (ratio < 0.0)
-	{
-		throw std::invalid_argument("The ratio can't be negative\n");
-	}
-	vert_ *= ratio;
-	horizon_ *= ratio;
+  if (ratio < 0.0)
+  {
+    throw std::invalid_argument("The ratio can't be negative\n");
+  }
+  vert_ *= ratio;
+  horizon_ *= ratio;
 }
--- novokhatskiy.danil/T1/outputShapes.cpp
+++ novokhatskiy.danil/T1/outputShapes.cpp
@@ -4,27 +4,27 @@
 void novokhatskiy::outputShapes(std::ostream& output, size_t shapeCounter, Shape** shapes)
 {
   double area = 0.0;
-	for (size_t i = 0; i < shapeCounter; i++)
-	{
-		if (shapes[i])
-		{
-			area += shapes[i]->getArea();
-		}
-	}
-	output << std::round(10.0 * area) / 10.0 << " ";
-	for (size_t i = 0; i < shapeCounter; ++i)
-	{
-		if (shapes[i])
-		{
-			double width = shapes[i]->getFrameRect().width;
-			double height = shapes[i]->getFrameRect().height;
-			point_t point = shapes[i]->getFrameRect().pos;
-			point_t leftCorner = {point.x - width / 2.0, point.y - height / 2.0};
-			point_t rightCorner = {point.x + width / 2.0, point.y + height / 2.0};
-			output << std::round(10.0 * leftCorner.x) / 10.0 << " " << std::round(10.0 * leftCorner.y) / 10.0
-			<< " " << std::round(10.0 * rightCorner.x) / 10.0 << " "
-			<< std::round(10.0 * rightCorner.y) / 10.0;
-		}
-	}
-	output << "\n";
+  for (size_t i = 0; i < shapeCounter; i++)
+  {
+    if (shapes[i])
+    {
+      area += shapes[i]->getArea();
+    }
+  }
+  output << std::round(10.0 * area) / 10.0 << " ";
+  for (size_t i = 0; i < shapeCounter; ++i)
+  {
+    if (shapes[i])
+    {
+      double width = shapes[i]->getFrameRect().width;
+      double height = shapes[i]->getFrameRect().height;
+      point_t point = shapes[i]->getFrameRect().pos;
+      point_t leftCorner = {point.x - width / 2.0, point.y - height / 2.0};
+      point_t rightCorner = {point.x + width / 2.0, point.y + height / 2.0};
+      output  << std::round(10.0 * leftCorner.x) / 10.0 << " "
+      << std::round(10.0 * leftCorner.y) / 10.0 << " " << std::round(10.0 * rightCorner.x) / 10.0
+      << " " << std::round(10.0 * rightCorner.y) / 10.0;
+    }
+  }
+  output << "\n";
 }
