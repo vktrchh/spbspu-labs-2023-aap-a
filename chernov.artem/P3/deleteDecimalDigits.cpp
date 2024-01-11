538a2b284135a88deb25cb832ee52555aea07b19
CA21lmFlame <chernov.ar@spbstu.ru>
2024-01-11 19:12:21 +0300

    implemented one option

--- chernov.artem/P3/deleteDecimalDigits.cpp
+++ chernov.artem/P3/deleteDecimalDigits.cpp
@@ -4,5 +4,15 @@
 
 char* chernov::removeDecimalDigits(char* input, char* stringWithDecimalDigitsRemoved, size_t sizeOfInput)
 {
+  size_t j = 0;
+  for (size_t i = 0; i < sizeOfInput; i++)
+  {
+    if (!isdigit(input[i]))
+    {
+      stringWithDecimalDigitsRemoved[j] = input[i];
+      j++;
+    }
+  }
+
   return stringWithDecimalDigitsRemoved;
 }
