7e7f8833441157390cd1d4b601a2bb6b58b1c2c7
CA21lmFlame <chernov.ar@spbstu.ru>
2024-01-11 19:25:42 +0300

    added function for removing vowels

---
+++ chernov.artem/P3/deleteVowels.cpp
@@ -0,0 +1,5 @@
+#include "removeVowels.hpp"
+#include <cstddef>
+
+char* chernov::removeVowels(char* input, char* stringWithVowelsRemoved, size_t sizeOfInput)
+{}
---
+++ chernov.artem/P3/deleteVowels.hpp
@@ -0,0 +1,10 @@
+#ifndef REMOVE_VOWELS_HPP
+#define	REMOVE_VOWELS_HPP
+#include <cstddef>
+
+namespace chernov
+{
+  char* removeVowels(char* input, char* stringWithVowelsRemoved, size_t sizeOfInput);
+}
+
+#endif
