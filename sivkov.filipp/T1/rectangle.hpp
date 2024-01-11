4b6f9b6fe60e2ca3f9218c8f921655695fa289e8
exxiiint <filipp.sivkov@yandex.ru>
2024-01-11 22:23:44 +0300

    add rectangle.hpp

---
+++ sivkov.filipp/T1/rectangle.hpp
@@ -0,0 +1,22 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+
+#include "shape.hpp"
+
+class Rectangle : public Shape
+{
+public:
+  Rectangle(const point_t &leftTop, const point_t &rightBottom);
+  virtual ~Rectangle();
+  virtual double getArea() const override;
+  virtual rectangle_t getFrameRect() const override;
+  virtual void move(const point_t &newPos) override;
+  virtual void move(double dx, double dy) override;
+  virtual void scale(double factor) override;
+
+private:
+  double leftTop_;
+  double rightBottom_;
+};
+
+#endif
--- sivkov.filipp/T1/shape.hpp
+++ sivkov.filipp/T1/shape.hpp
@@ -6,12 +6,12 @@
 class Shape
 {
 public:
-    virtual ~Shape() = default;
-    virtual double getArea() const = 0;
-    virtual rectangle_t getFrameRect() const = 0;
-    virtual void move(const point_t &newPos) = 0;
-    virtual void move(double dx, double dy) = 0;
-    virtual void scale(double factor) = 0;
+  virtual ~Shape() = default;
+  virtual double getArea() const = 0;
+  virtual rectangle_t getFrameRect() const = 0;
+  virtual void move(const point_t &newPos) = 0;
+  virtual void move(double dx, double dy) = 0;
+  virtual void scale(double factor) = 0;
 };
 
 #endif
