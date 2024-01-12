3c668dba5ed7320655332cb49557e489a048a62c
magggottt <gladyshevmatvey@gmail.com>
2024-01-12 16:22:15 +0700

    dafa

--- gladyshev.matvey/P3/allocatememory.cpp
+++ gladyshev.matvey/P3/allocatememory.cpp
@@ -2,11 +2,10 @@
 
 char * gladyshev::newMemory(const char * basicar, size_t len)
 {
-  char * newarr = new char[len + 10];
+  char * newarr = new char[len + 10]{};
   for (size_t i = 0; i < len; ++i)
   {
     newarr[i] = basicar[i];
   }
-  newarr[len] = '\0';
   return newarr;
 }
