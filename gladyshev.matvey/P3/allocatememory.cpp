0118c3e1e890cffb33e72718cbcb9e2e6cf03138
magggottt <gladyshevmatvey@gmail.com>
2024-01-12 15:31:06 +0700

    fix

--- gladyshev.matvey/P3/allocatememory.cpp
+++ gladyshev.matvey/P3/allocatememory.cpp
@@ -2,8 +2,7 @@
 
 char * gladyshev::newMemory(const char * basicar, size_t len)
 {
-  char * newarr = nullptr;
-  newarr = new char[len + 10];
+  char * newarr = new char[len + 10];
   for (size_t i = 0; i < len; ++i)
   {
     newarr[i] = basicar[i];
