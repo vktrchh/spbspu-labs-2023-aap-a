542a8be1a4fde5025d88f0b62c649b70c398adda
jiggerman <liggerman@mail.ru>
2024-01-13 14:14:25 +0300

    Rename mergin, add namespace for some foo and fix smh

--- chistyakov.vladislav/T1/complexquad.cpp
+++ chistyakov.vladislav/T1/complexquad.cpp
@@ -1,21 +1,21 @@
-#include "copmlexquad.hpp"
+#include "complexquad.hpp"
 
 chistyakov::Complexquad::Complexquad(const point_t & pnt1, const point_t & pnt2, const point_t & pnt3, const point_t & pnt4):
-  point_t pnt1_(pnt1),
-  point_t pnt2_(pnt2),
-  point_t pnt3_(pnt3),
-  point_t pnt4_(pnt4)
+  pnt1_(pnt1),
+  pnt2_(pnt2),
+  pnt3_(pnt3),
+  pnt4_(pnt4)
 {
-  point_t points[4] = { pnt1, pnt2, pnt3, pnt4 };
+  chistyakov::point_t points[4] = { pnt1, pnt2, pnt3, pnt4 };
   for (int i = 0; i < 4; ++i)
   {
-    point_t nowPoint = points[i];
+    chistyakov::point_t nowPoint = points[i];
 
     for (int j = i; j < 4; ++j)
     {
       if (nowPoint.x == points[j].x && nowPoint.y == points[j].y)
       {
-        throw invalid_argument("Invalid argument for points in complexquad");
+        throw std::invalid_argument("Invalid argument for points in complexquad");
       }
     }
   }
@@ -27,7 +27,7 @@ chistyakov::Complexquad::Complexquad(const point_t & pnt1, const point_t & pnt2,
     double sn = (pnt3.x - pnt4.x) + (pnt3.y - pnt4.y) * q;
     if (!sn)
     {
-      throw invalid_argument("The lines don't intersect");
+      throw std::invalid_argument("The lines don't intersect");
     }
     double fn = (pnt3.x - pnt1.x) + (pnt3.y - pnt1.y) * q;
     n = fn / sn;
@@ -36,7 +36,7 @@ chistyakov::Complexquad::Complexquad(const point_t & pnt1, const point_t & pnt2,
   {
     if (!(pnt3.y - pnt4.y))
     {
-      throw invalid_argument("The lines don't intersect");
+      throw std::invalid_argument("The lines don't intersect");
     }
     n = (pnt3.y - pnt1.y) / (pnt3.y - pnt4.y);
   }
@@ -51,16 +51,16 @@ chistyakov::Complexquad::~Complexquad()
 
 double chistyakov::Complexquad::getArea() const
 {
-  double area1 = (pnt1_.x(pnt4_.y - cross_.y) + pnt4_.x(cross_.y - pnt1_.y) + cross_.x(pnt1_.y - pnt4_.y)) / 2.0;
-  double area2 = (pnt2_.x(pnt3_.y - cross_.y) + pnt3_.x(cross_.y - pnt2_.y) + cross_.x(pnt2_.y - pnt3_.y)) / 2.0;
+  double area1 = (pnt1_.x * (pnt4_.y - cross_.y) + pnt4_.x * (cross_.y - pnt1_.y) + cross_.x * (pnt1_.y - pnt4_.y)) / 2.0;
+  double area2 = (pnt2_.x * (pnt3_.y - cross_.y) + pnt3_.x * (cross_.y - pnt2_.y) + cross_.x * (pnt2_.y - pnt3_.y)) / 2.0;
   return area1 + area2;
 }
 
-rectangle_t chistyakov::Complexquad::getFrameRect() const
+chistyakov::rectangle_t chistyakov::Complexquad::getFrameRect() const
 {
-  point_t points[3] = { pnt2_, pnt3_, pnt4_ };
-  point_t leftCorner = {pnt1_.x, pnt1_.y};
-  point_t rightCorner = {pnt1_.x, pnt1_.y};
+  chistyakov::point_t points[3] = { pnt2_, pnt3_, pnt4_ };
+  chistyakov::point_t leftCorner = {pnt1_.x, pnt1_.y};
+  chistyakov::point_t rightCorner = {pnt1_.x, pnt1_.y};
 
   for (int i = 0; i < 3; ++i)
   {
@@ -84,13 +84,13 @@ rectangle_t chistyakov::Complexquad::getFrameRect() const
 
   double width = rightCorner.x - leftCorner.x;
   double height = rightCorner.y - leftCorner.y;
-  point_t pos = { width/2.0 + leftCorner.x, height/2.0 + leftCorner.y};
+  chistyakov::point_t pos = { width/2.0 + leftCorner.x, height/2.0 + leftCorner.y};
   return {width, height, pos};
 }
 
 void chistyakov::Complexquad::move(const point_t & point)
 {
-  point_t points[4] = { pnt1_, pnt2_, pnt3_, pnt4_ };
+  chistyakov::point_t points[4] = { pnt1_, pnt2_, pnt3_, pnt4_ };
   for (int i = 0; i < 4; ++i)
   {
     points[i].x += point.x;
@@ -102,7 +102,7 @@ void chistyakov::Complexquad::move(const point_t & point)
 
 void chistyakov::Complexquad::move(const double mx, const double my)
 {
-  point_t points[4] = { pnt1_, pnt2_, pnt3_, pnt4_ };
+  chistyakov::point_t points[4] = { pnt1_, pnt2_, pnt3_, pnt4_ };
   for (int i = 0; i < 4; ++i)
   {
     points[i].x += mx;
@@ -112,9 +112,9 @@ void chistyakov::Complexquad::move(const double mx, const double my)
   cross_.y += my;
 }
 
-void scale(const double ratio)
+void chistyakov::Complexquad::scale(const double ratio)
 {
-  point_t points[4] = { pnt1_, pnt2_, pnt3_, pnt4_ };
+  chistyakov::point_t points[4] = { pnt1_, pnt2_, pnt3_, pnt4_ };
   for (int i = 0; i < 4; ++i)
   {
     points[i].x = cross_.x + (points[i].x - cross_.x) * ratio;
