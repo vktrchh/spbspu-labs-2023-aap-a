b1a7ff5930eee67e66ffb35475ba6ea5a49fa239
User1235321 <pavreb1235321@gmail.com>
2024-01-13 09:55:02 +0300

    Correct isoscale

--- rebdev.pavel/T1/isoScale.cpp
+++ rebdev.pavel/T1/isoScale.cpp
@@ -1,13 +1,11 @@
 #include "isoScale.hpp"
+#include <iostream>
 
 void rebdev::isoScale(Shape ** shapes, const int & numOfShape, const point_t & scalePoint, const double & k)
 {
   for (int i = 0; i < numOfShape; ++i)
   {
-    point_t center = shapes[i]->getFrameRect().pos_;
     shapes[i]->move(scalePoint);
     shapes[i]->scale(k);
-    point_t newDistance = {(scalePoint.x_ - center.x_) * k, (scalePoint.y_ - center.y_) * k};
-    shapes[i]->move(newDistance.x_, newDistance.y_);
   }
 };
