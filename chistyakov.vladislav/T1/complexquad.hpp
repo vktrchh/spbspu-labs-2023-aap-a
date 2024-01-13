066f0f879dfb29e0fbcb7ea6709078d8a636ba0b
jiggerman <liggerman@mail.ru>
2024-01-13 14:13:07 +0300

    rename margin

--- chistyakov.vladislav/T1/complexquad.hpp
+++ chistyakov.vladislav/T1/complexquad.hpp
@@ -1,14 +1,16 @@
 #ifndef COMPLEXQUAD_HPP
 #define COMPLEXQUAD_HPP
 
+#include <stdexcept>
 #include "shape.hpp"
+#include "base-types.hpp"
 
 namespace chistyakov
 {
   class Complexquad : public Shape
   {
   public:
-    Complexquad(const point_t & first, const point_t & second, const point_t & third, const point_t & fourth);
+    Complexquad(const point_t & pnt1, const point_t & pnt2, const point_t & pnt3, const point_t & pnt4);
     virtual ~Complexquad();
     virtual double getArea() const;
     virtual rectangle_t getFrameRect() const;
@@ -16,12 +18,12 @@ namespace chistyakov
     virtual void move(const double mx, const double my);
     virtual void scale(const double ratio);
   private:
-    point_t firstPoint_;
-    point_t secondPoint_;
-    point_t thirdPoint_;
-    point_t fourthPoint_;
+    point_t pnt1_;
+    point_t pnt2_;
+    point_t pnt3_;
+    point_t pnt4_;
     point_t cross_;
-  }
+  };
 }
 
 #endif
