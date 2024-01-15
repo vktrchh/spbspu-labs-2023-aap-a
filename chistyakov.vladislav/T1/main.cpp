e4f1b61de9559f86f81d945fca4cb7ddd6333e95
jiggerman <liggerman@mail.ru>
2024-01-15 16:47:59 +0300

    Realization

--- chistyakov.vladislav/T1/main.cpp
+++ chistyakov.vladislav/T1/main.cpp
@@ -1,4 +1,52 @@
 #include <iostream>
+#include "rectangle.hpp"
+#include "complexquad.hpp"
+#include "shape.hpp"
+#include "inputShapes.hpp"
+#include "isotropScale.hpp"
 
 int main()
-{};
+{
+  using namespace chistyakov;
+  double scaleInfo[3]{};
+  Shape * shapes[1000]{};
+
+  for (size_t i = 0; i < 1000; ++i)
+  {
+    shapes[i] = nullptr;
+  }
+
+  try
+  {
+    inputShapes(std::cin, shapes, scaleInfo);
+  }
+  catch (const std::invalid_argument & e)
+  {
+    std::cerr << "Achtung " << e.what() << "\n";
+    freeArray(shapes, 1000);
+    return 1;
+  }
+  catch (const std::logic_error & e)
+  {
+    std::cout << "Error: " << e.what() << "\n";
+    freeArray(shapes, 1000);
+    return 2;
+  }
+
+  for (size_t i = 0; i < 1000; ++i)
+  {
+    if (shapes[i] == nullptr)
+    {
+      break;
+    }
+    std::cout << shapes[i]->getArea() << " ";
+    shapes[i]->printCords();
+    isotropScale(shapes[i], {scaleInfo[0], scaleInfo[1]}, scaleInfo[2]);
+    std::cout << shapes[i]->getArea() << " ";
+    shapes[i]->printCords();
+  }
+
+  freeArray(shapes, 1000);
+
+  return 0;
+};
