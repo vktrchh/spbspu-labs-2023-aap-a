dd53d97eabb27d457fd14505ce18714ca14f9349
Steuger7 <steuger7@mail.ru>
2024-01-15 20:05:02 +0300

    Test v1

--- grechishnikov.roman/P3/countUnique.cpp
+++ grechishnikov.roman/P3/countUnique.cpp
@@ -4,13 +4,13 @@
 size_t grechishnikov::countUnique(const char* str)
 {
   size_t result = 0;
-  for (size_t i = 0; str[i] != '\0'; i++)
+  for (size_t i = 0; str[i] != '\0'; ++i)
   {
     if (std::isalpha(str[i]))
     {
       char del = std::tolower(str[i]);
       int check = 1;
-      for (size_t g = 0; g < i; g++)
+      for (size_t g = 0; g < i; ++g)
       {
         if (del == std::tolower(str[g]))
         {
--- grechishnikov.roman/P3/inputStr.cpp
+++ grechishnikov.roman/P3/inputStr.cpp
@@ -47,6 +47,10 @@ char* grechishnikov::inputStr(std::istream& in)
 char* reallocate(const char* str, size_t lenF, size_t lenS)
 {
   char* newStr = new char [lenS];
+  for (size_t i = 0; i < lenS; ++i)
+  {
+    newStr[i] = 0;
+  }
   size_t min = (lenF < lenS) ? lenF : lenS;
   for (size_t i = 0; i < min; ++i)
   {
