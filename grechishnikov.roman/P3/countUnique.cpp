ec313618ae0fbcb9974f1286811b63ecab090ce7
Steuger7 <steuger7@mail.ru>
2024-01-15 19:27:11 +0300

    int -> size_t

--- grechishnikov.roman/P3/countUnique.cpp
+++ grechishnikov.roman/P3/countUnique.cpp
@@ -10,7 +10,7 @@ size_t grechishnikov::countUnique(const char* str)
     {
       char del = std::tolower(str[i]);
       int check = 1;
-      for (int g = 0; g < i; g++)
+      for (size_t g = 0; g < i; g++)
       {
         if (del == std::tolower(str[g]))
         {
