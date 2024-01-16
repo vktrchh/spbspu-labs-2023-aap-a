681c02550b57cbccddd2a30c62ae0956aa0a7301
Slivka <roman.strelyaeff@gmail.com>
2024-01-16 22:41:39 +0300

    format fix

--- strelyaev.roman/T1/inputShapes.cpp
+++ strelyaev.roman/T1/inputShapes.cpp
@@ -3,12 +3,6 @@
 #include <stdexcept>
 #include <string>
 
-
-
-
-
-#include <iostream>
-
 strelyaev::Shape * strelyaev::inputRectangle(const char string[])
 {
   const char * coords_string = string + 10;
