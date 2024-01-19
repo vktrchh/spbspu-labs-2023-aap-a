a72bb358687be0add0723880e56aa6519be70ee6
kirillnamestnikov <kir.namestnikov@gmail.com>
2024-01-19 16:43:49 +0300

    header files changed

--- namestnikov.kirill/P3/input_string.hpp
+++ namestnikov.kirill/P3/input_string.hpp
@@ -6,7 +6,6 @@
 namespace namestnikov
 {
   char * inputString(std::istream & in, size_t & strIndex);
-  char * getLongerString(char * string, size_t oldSize, size_t newSize);
 }
 
 #endif
--- namestnikov.kirill/P3/unique_sym_array.hpp
+++ namestnikov.kirill/P3/unique_sym_array.hpp
@@ -4,8 +4,6 @@
 
 namespace namestnikov
 {
-  void fillUniqueSymArray(const char * str1, const char * str2, char * result, size_t & resultIndex);
-  bool isSymbolInString(const char * string, char sym);
   char * getUniqueSymArray(const char * str1, const char * str2, size_t firstSize, size_t secondSize);
 }
 
