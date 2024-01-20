5bdd835ee43557eb501c6c31377d1080185fa3c8
marlions <marishin.lv@edu.spbstu.ru>
2024-01-21 02:34:55 +0300

    modifystring

--- marishin.leonid/P3/modifystring.hpp
+++ marishin.leonid/P3/modifystring.hpp
@@ -1,10 +1,8 @@
 #ifndef MODIFY_STRING
 #define MODIFY_STRING
 
-#include <iostream>
-
 namespace marishin
 {
-  char* modifyString(const char* stroka, const char targetChar, const char replacementChar);
+  char* modifyString(char* string, char firstSymbol, char secondSymbol);
 }
 #endif
