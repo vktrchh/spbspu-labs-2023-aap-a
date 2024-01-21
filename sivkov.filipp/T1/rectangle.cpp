d29fcf40e1801ae51b936dd14744dceeadb51afd
exxiiint <filipp.sivkov@yandex.ru>
2024-01-21 23:39:35 +0300

    fix rectangle.cpp

--- sivkov.filipp/T1/rectangle.cpp
+++ sivkov.filipp/T1/rectangle.cpp
@@ -1,10 +1,11 @@
 #include "rectangle.hpp"
+#include <stdexcept>
 
-Rectangle::Rectangle(const point_t &leftTop, const point_t &rightBottom):
+Rectangle::Rectangle(const point_t& leftTop, const point_t& rightBottom) :
   leftTop_(leftTop),
   rightBottom_(rightBottom)
 {
-  if (leftTop_.x >= rightBottom_.x || leftTop_.y >= rightBottom_.y)
+  if (leftTop_.x >= rightBottom_.x || leftTop_.y <= rightBottom_.y)
   {
     throw std::invalid_argument("Invalid rectangle coordinates");
   }
@@ -12,26 +13,34 @@ Rectangle::Rectangle(const point_t &leftTop, const point_t &rightBottom):
 
 double Rectangle::getArea() const
 {
-  double width = rightBottom_.x - leftTop_.x;
-  double height = rightBottom_.y - leftTop_.y;
-  return width * height;
+  double area = getFrameRect().width * getFrameRect().height;
+  return area;
 }
 
 rectangle_t Rectangle::getFrameRect() const
 {
   double width = rightBottom_.x - leftTop_.x;
+  if (width < 0)
+  {
+    width = width * -1;
+  }
   double height = rightBottom_.y - leftTop_.y;
+  if (height < 0)
+  {
+    height = height * -1;
+  }
   point_t center = { (leftTop_.x + rightBottom_.x) / 2, (leftTop_.y + rightBottom_.y) / 2 };
-  return {center, width, height};
+  return { center, width, height };
 }
 
-void Rectangle::move(const point_t &newPos)
+void Rectangle::move(const point_t& newPos)
 {
-  double center = getFrameRect().pos;
-  double dx = point.x - center.x
-  double dy = point.y - center.y
-  move(dx,dy)
+  point_t center = { (leftTop_.x + rightBottom_.x) / 2, (leftTop_.y + rightBottom_.y) / 2 };
+  double dx = newPos.x - center.x;
+  double dy = newPos.y - center.y;
+  move(dx, dy);
 }
+
 
 void Rectangle::move(double dx, double dy)
 {
@@ -41,15 +50,15 @@ void Rectangle::move(double dx, double dy)
   rightBottom_.y += dy;
 }
 
+
 void Rectangle::scale(double factor)
 {
   if (factor > 0)
   {
-    point_t center = getFrameRect().pos;
+    point_t pos = getFrameRect().pos;
     double newWidth = (rightBottom_.x - leftTop_.x) * factor;
     double newHeight = (rightBottom_.y - leftTop_.y) * factor;
-    leftTop_ = {center.x - newWidth / 2, center.y - newHeight / 2};
-    rightBottom_ = {center.x + newWidth / 2, center.y + newHeight / 2};
+    leftTop_ = { pos.x - newWidth / 2, pos.y - newHeight / 2 };
+    rightBottom_ = { pos.x + newWidth / 2, pos.y + newHeight / 2 };
   }
 }
-
