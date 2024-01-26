9058703e7bf96a5e1da914eefd7f49b0a7e60a23
mordw1n <lolpovar0@gmail.com>
2024-01-26 15:04:51 +0300

    fix length

--- spiridonov.mikhail/P4/P3/consecutiveDup.cpp
+++ spiridonov.mikhail/P4/P3/consecutiveDup.cpp
@@ -1,5 +1,4 @@
 #include "consecutiveDup.hpp"
-#include <cstring>
 
 int spiridonov::hasConsecutiveDuplicates(const char * str)
 {
@@ -8,14 +7,7 @@ int spiridonov::hasConsecutiveDuplicates(const char * str)
     return 0;
   }
 
-  int length = strlen(str);
-
-  if (length < 2)
-  {
-    return 0;
-  }
-
-  for (int i = 1; i < length; ++i)
+  for (int i = 1; str[i] != '\0'; ++i)
   {
     if (str[i] == str[i - 1])
     {
