48b9571f4bce52db7509e6a5d60c6e5f0c0e35ab
Z0ltaan <gregnov69@gmail.com>
2024-01-26 23:28:27 +0300

    still tryin'

---
+++ isaychev.vladislav/T1/inputString.cpp
@@ -0,0 +1,43 @@
+#include "inputString.hpp"
+#include <iostream>
+#include <stdexcept>
+#include "stringManipulations.hpp"
+
+char * isaychev::inputString(std::istream & input)
+{
+  size_t capacity = 10, length = 0;
+  char * str = nullptr;
+  try
+  {
+    str = new char[capacity]{};
+    char curr_char = 0;
+    input >> std::noskipws;
+    while ((input >> curr_char) && (length < capacity))
+    {
+//      if (std::cin.eof())
+      if (length == capacity - 1)
+      {
+        capacity += 10;
+        char * bigger_str = nullptr;
+        bigger_str = new char[capacity]{};
+        copyString(str, bigger_str, length);
+        delete [] str;
+        str = bigger_str;
+      }
+      str[length] = curr_char;
+      if (curr_char == '\n')
+      {
+        str[length] = '\0';
+        break;
+      }
+      length++;
+    }
+  }
+  catch (const std::bad_alloc &)
+  {
+    delete [] str;
+    throw;
+  }
+  input >> std::skipws;
+  return str;
+}
