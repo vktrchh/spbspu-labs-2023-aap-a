70d5f0998af2b16fefbcec57f3cee134f6d82d21
mordw1n <lolpovar0@gmail.com>
2024-01-26 14:28:56 +0300

    fix name func manageRes

--- spiridonov.mikhail/P4/P3/manageResources.cpp
+++ spiridonov.mikhail/P4/P3/manageResources.cpp
@@ -1,7 +1,6 @@
-#include "manageRes.hpp"
-#include <iostream>
+#include "manageResources.hpp"
 
-char * spiridonov::manageRes(const char* currentArray, const size_t strOld, size_t strNew)
+char * spiridonov::manageResources(const char* currentArray, const size_t strOld, size_t strNew)
 {
   char* result = new char[strNew];
   for (size_t i = 0; i < strOld; ++i)
---
+++ spiridonov.mikhail/P4/P3/manageResources.hpp
@@ -0,0 +1,10 @@
+#ifndef MANAGE_RESOURCES_HPP
+#define MANAGE_RESOURCES_HPP
+#include <iostream>
+
+namespace spiridonov
+{
+  char* manageResources(const char* currentArray, const size_t strOld, size_t strNew);
+}
+
+#endif
