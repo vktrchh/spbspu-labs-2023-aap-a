c544d2ddbc9417b870ceaf2e1d9f075a16fa803e
Z0ltaan <gregnov69@gmail.com>
2024-01-26 23:21:35 +0300

    placed removed files back

---
+++ isaychev.vladislav/T1/creatingFigures.hpp
@@ -0,0 +1,13 @@
+#ifndef CREATINGFIGURES_HPP
+#define CREATINGFIGURES_HPP
+
+#include "rectangle.hpp"
+#include "circle.hpp"
+#include "regular.hpp"
+
+namespace isaychev
+{
+  Shape * createFigure(char * str);
+}
+
+#endif
---
+++ isaychev.vladislav/T1/inputString.hpp
@@ -0,0 +1,11 @@
+#ifndef INPUTSTRING_HPP
+#define INPUTSTRING_HPP
+
+#include <iosfwd>
+
+namespace isaychev
+{
+  char * inputString(std::istream & input);
+}
+
+#endif
