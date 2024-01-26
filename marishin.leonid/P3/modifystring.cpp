87aff7e9a0340c656bf1f9ed0b86c4692970d628
marlions <marishin.lv@edu.spbstu.ru>
2024-01-26 18:21:17 +0300

    correction modifystring

--- marishin.leonid/P3/modifystring.cpp
+++ marishin.leonid/P3/modifystring.cpp
@@ -2,14 +2,14 @@
 
 char* marishin::modifyString(char* string, char firstSymbol, char secondSymbol)
 {
-  size_t count = 0;
-  while (string[count] != '\0')
+  char* current = string;
+  while (*current != '\0')
   {
-    if (string[count] == firstSymbol)
+    if (*current == firstSymbol)
     {
-      string[count] = secondSymbol;
+      *current = secondSymbol;
     }
-    ++count;
+    ++current;
   }
   return string;
 }
