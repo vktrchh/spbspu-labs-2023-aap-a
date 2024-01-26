acf4b9def54bc82f79f6a42ab06ebf768d70c795
Z0ltaan <gregnov69@gmail.com>
2024-01-26 23:45:53 +0300

    fix because of checks

--- isaychev.vladislav/T1/shape.hpp
+++ isaychev.vladislav/T1/shape.hpp
@@ -8,6 +8,7 @@ namespace isaychev
   class Shape
   {
    public:
+    virtual ~Shape() = default;
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
     virtual void move(const point_t & newPos) = 0;
