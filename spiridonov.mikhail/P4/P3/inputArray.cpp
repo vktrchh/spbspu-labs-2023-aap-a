212033d4cfca9b66345e805335efbe24082bd42e
mordw1n <lolpovar0@gmail.com>
2024-01-26 11:59:36 +0300

    fix inputArray.cpp

--- spiridonov.mikhail/P4/P3/inputArray.cpp
+++ spiridonov.mikhail/P4/P3/inputArray.cpp
@@ -1,43 +1,47 @@
 #include <iostream>
 #include "inputArray.hpp"
 
-char * spiridonov::inputArray(std::istream& input, size_t sizeOfStr)
+char * spiridonov::inputArray(std::istream& input, size_t& sizeOfStr)
 {
+  const size_t chunkSize = 10;
   char * currentArray = new char[sizeOfStr];
   char character = 0;
   size_t i = 0;
   input >> std::noskipws;
-  while ((input >> character) && (character != '\n'))
-  {
-    currentArray[i] = character;
-    i++;
 
-    if (i >= sizeOfStr)
+  try
+  {
+    while ((input >> character) && (character != '\n'))
     {
-      try
+      currentArray[i] = character;
+      i++;
+
+      if (i >= sizeOfStr)
       {
-        char* newArray = new char[sizeOfStr + 10];
-        for (size_t j = 0; j < i; ++j)
+        char* newArray = new char[sizeOfStr + chunkSize];
+        for (size_t j = 0; j < sizeOfStr; ++j)
         {
           newArray[j] = currentArray[j];
         }
         delete[] currentArray;
         currentArray = newArray;
-        sizeOfStr += 10;
-      }
-      catch (...)
-      {
-        delete[] currentArray;
-        std::cerr << "Failed allocate memory" << "\n";
-        return nullptr;
+        sizeOfStr += chunkSize;
       }
     }
   }
+  catch (const std::bad_alloc&)
+  {
+    delete[] currentArray;
+    throw;
+  }
+
   char* finalArray = new char[i];
   for (size_t j = 0; j < i; ++j)
   {
     finalArray[j] = currentArray[j];
   }
+
   delete[] currentArray;
+  sizeOfStr = i;
   return finalArray;
 }
