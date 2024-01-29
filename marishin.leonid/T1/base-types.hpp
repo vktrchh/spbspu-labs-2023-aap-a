ccd0f4e6e4b157ed555051344143b4e524d81c58
marlions <marishin.lv@edu.spbstu.ru>
2024-01-29 13:19:32 +0300

    base-types

--- marishin.leonid/T1/base-types.hpp
+++ marishin.leonid/T1/base-types.hpp
@@ -1,15 +1,18 @@
 #ifndef BASE_TYPES_HPP
 #define BASE_TYPES_HPP
 
-struct point_t
+namespace marishin
 {
-  double x;
-  double y;
-};
-struct rectangle_t
-{
-  point_t pos;
-  double width;
-  double height;
-};
+  struct point_t
+  {
+    double x;
+    double y;
+  };
+  struct rectangle_t
+  {
+    point_t pos;
+    double width;
+    double height;
+  };
+}
 #endif
