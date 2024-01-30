c6cbd18cf177d23f49dc446d61057a55bec99d89
marlions <marishin.lv@edu.spbstu.ru>
2024-01-30 21:06:48 +0300

    ring

--- marishin.leonid/T1/ring.cpp
+++ marishin.leonid/T1/ring.cpp
@@ -5,7 +5,7 @@ marishin::Ring::Ring(const point_t &center, double radius1, double radius2):
   radius1_(radius1),
   radius2_(radius2)
 {
-  if (radius1_ <= 0.0 || radius2_ <= 0.0 || radius1_ >= radius2_)
+  if (radius1 <= 0.0 || radius2 <= 0.0 || radius1 >= radius2)
   {
     throw std::invalid_argument("Invalid ring");
   }
