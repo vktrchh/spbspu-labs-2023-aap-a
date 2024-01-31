944a9802ad7a7a951273f0fcfcb01f8ca18797a2
mordw1n <lolpovar0@gmail.com>
2024-01-31 12:18:54 +0300

    fix inputArray

--- spiridonov.mikhail/P3/inputArray.cpp
+++ spiridonov.mikhail/P3/inputArray.cpp
@@ -1,16 +1,18 @@
 #include "inputArray.hpp"
 #include <iostream>
 
-char * spiridonov::inputArray(std::istream& input, size_t& sizeOfStr)
+char* spiridonov::inputArray(std::istream& input, size_t& sizeOfStr)
 {
   const size_t chunkSize = 10;
-  char * currentArray = new char[sizeOfStr]();
-  char character = 0;
-  size_t i = 0;
-  input >> std::noskipws;
-
+  char* currentArray = nullptr;
   try
   {
+    currentArray = new char[sizeOfStr]();
+    char character = 0;
+    size_t i = 0;
+
+    input >> std::noskipws;
+
     while ((input >> character) && (character != '\n'))
     {
       currentArray[i] = character;
@@ -19,21 +21,22 @@ char * spiridonov::inputArray(std::istream& input, size_t& sizeOfStr)
       if (i >= sizeOfStr)
       {
         char* newArray = new char[sizeOfStr + chunkSize]();
-        for (size_t j = 0; j < sizeOfStr; ++j)
-        {
-          newArray[j] = currentArray[j];
-        }
+        std::copy(currentArray, currentArray + sizeOfStr, newArray);
         delete[] currentArray;
         currentArray = newArray;
         sizeOfStr += chunkSize;
       }
     }
     sizeOfStr = i;
+
+    char* finalArray = new char[sizeOfStr + 1]();
+    std::copy(currentArray, currentArray + sizeOfStr, finalArray);
+    delete[] currentArray;
+    return finalArray;
   }
   catch (const std::bad_alloc&)
   {
     delete[] currentArray;
     throw;
   }
-  return currentArray;
 }
