a78692ad594de6fc2abaadac42eb29370fc41049
exxiiint <filipp.sivkov@yandex.ru>
2024-01-31 15:24:56 +0300

    [SPC-RMV]

--- sivkov.filipp/P3/main.cpp
+++ sivkov.filipp/P3/main.cpp
@@ -7,10 +7,10 @@ int main()
 {
   size_t size = 20;
   char* string1 = nullptr;
-  char* string2 = nullptr;
+//  char* string2 = nullptr;
   char* inputString1 = nullptr;
-  char* inputString2 = nullptr;
-  char* inputString3 = nullptr;
+//  char* inputString2 = nullptr;
+//  char* inputString3 = nullptr;
 
   try
   {
@@ -27,39 +27,13 @@ int main()
     std::cerr << "Memory allocation failed: " << e.what() << "\n";
     return 1;
   }
-  catch (...)
-  {
-    std::cerr << "line error\n";
-    if (inputString1 != nullptr)
-    {
-      delete[] inputString1;
-    }
-    if (string1 != nullptr)
-    {
-      delete[] string1;
-    }
-    if (inputString2 != nullptr)
-    {
-      delete[] inputString2;
-    }
-    if (string2 != nullptr)
-    {
-      delete[] string2;
-    }
-    if (inputString3 != nullptr)
-    {
-      delete[] inputString3;
-    }
-    return 1;
-  }
-
   std::cout << string1 << '\n';
 //  std::cout << string2 << '\n';
 
   delete[] inputString1;
   delete[] string1;
-  delete[] inputString2;
-  delete[] string2;
-  delete[] inputString3;
+//  delete[] inputString2;
+//  delete[] string2;
+//  delete[] inputString3;
   return 0;
 }
