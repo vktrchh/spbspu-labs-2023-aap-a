632b8d72a812ccdaafc62aa7e873681503f0e96b
exxiiint <filipp.sivkov@yandex.ru>
2024-01-31 16:27:52 +0300

    [SPC-RMV]

--- sivkov.filipp/P3/main.cpp
+++ sivkov.filipp/P3/main.cpp
@@ -9,18 +9,20 @@ int main()
 {
   char* string1 = nullptr;
   char* string2 = nullptr;
+/*
   char* inputString1 = nullptr;
   char* inputString2 = nullptr;
   char* inputString3 = nullptr;
-
+*/
   try
   {
     inputString1 = addString(std::cin);
     string1 = deleteSpace(inputString1);
+/*
     inputString2 = addString(std::cin);
     inputString3 = addString(std::cin);
     string2 = copyNum(inputString2, inputString3);
-
+*/
   }
   catch (const std::bad_alloc& e)
   {
@@ -38,6 +40,7 @@ int main()
     {
       delete[] string1;
     }
+/*
     if (inputString2 != nullptr)
     {
       delete[] inputString2;
@@ -50,6 +53,7 @@ int main()
     {
       delete[] inputString3;
     }
+*/
     return 1;
   }
   std::cout << string1 << '\n';
@@ -57,8 +61,8 @@ int main()
 
   delete[] inputString1;
   delete[] string1;
-  delete[] inputString2;
-  delete[] string2;
-  delete[] inputString3;
+//  delete[] inputString2;
+//  delete[] string2;
+//  delete[] inputString3;
   return 0;
 }
