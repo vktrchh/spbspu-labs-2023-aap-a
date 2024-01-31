d05e2757f3c863e3c19f46e0cbec727dfd537d9d
exxiiint <filipp.sivkov@yandex.ru>
2024-01-31 17:32:40 +0300

    [SPC-RMV]

--- sivkov.filipp/P3/main.cpp
+++ sivkov.filipp/P3/main.cpp
@@ -8,18 +8,18 @@
 int main()
 {
   char* string1 = nullptr;
-  char* string2 = nullptr;
+//  char* string2 = nullptr;
 
   char* inputString1 = nullptr;
 
-  char* inputString2 = nullptr;
-  char  inputString3[13] = {"h3ll0 w0r1d!"};
+//  char* inputString2 = nullptr;
+//  char  inputString3[13] = {"h3ll0 w0r1d!"};
   try
   {
     inputString1 = addString(std::cin);
     string1 = deleteSpace(inputString1);
-    inputString2 = addString(std::cin);
-    string2 = copyNum(inputString2, inputString3);
+  //  inputString2 = addString(std::cin);
+  //  string2 = copyNum(inputString2, inputString3);
   }
   catch (const std::bad_alloc& e)
   {
@@ -29,7 +29,7 @@ int main()
   catch (...)
   {
     std::cerr << "line error\n";
-  if (inputString1 != nullptr)
+    if (inputString1 != nullptr)
     {
       delete[] inputString1;
     }
@@ -37,6 +37,7 @@ int main()
     {
       delete[] string1;
     }
+/*
     if (inputString2 != nullptr)
     {
       delete[] inputString2;
@@ -45,15 +46,15 @@ int main()
     {
       delete[] string2;
     }
-
+*/
     return 1;
   }
   std::cout << string1 << '\n';
-  std::cout << string2 << '\n';
+//  std::cout << string2 << '\n';
 
   delete[] inputString1;
   delete[] string1;
-  delete[] inputString2;
-  delete[] string2;
+//  delete[] inputString2;
+//  delete[] string2;
   return 0;
 }
