b6734c4705ad04d96eecd0ddbcd9684a737a1932
Filipp Sivkov <126404330+exxiiint@users.noreply.github.com>
2024-02-02 14:16:58 +0300

    Update copyNum.hpp

--- sivkov.filipp/P3/copyNum.hpp
+++ sivkov.filipp/P3/copyNum.hpp
@@ -1,6 +1,8 @@
 #ifndef COPYNUM_HPP
 #define COPYNUM_HPP
 
-char* copyNum(char* string1, const char* string2);
-
+namespace sivkov
+{
+  char* copyNum(char* string1, const char* string2);
+}
 #endif
