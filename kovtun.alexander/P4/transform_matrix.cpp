944ddb5f1c82b0b406c0760c5adb53a8de984404
puzz13 <puzz13one@gmail.com>
2024-02-05 22:22:12 +0300

    CG fix

--- kovtun.alexander/P4/transform_matrix.cpp
+++ kovtun.alexander/P4/transform_matrix.cpp
@@ -11,34 +11,33 @@ void kovtun::transformMatrix(int * matrix, size_t rows, size_t cols)
 
   while (top <= bottom && left <= right)
   {
-      for (size_t i = top; i <= bottom; i++)
-      {
-        matrix[i * cols + left] += add;
-        add++;
-      }
-      left++;
+    for (size_t i = top; i <= bottom; i++)
+    {
+      matrix[i * cols + left] += add;
+      add++;
+    }
+    left++;
 
-      for (size_t i = left; i <= right; i++)
-      {
-        matrix[bottom * cols + i] += add;
-        add++;
-      }
-      bottom--;
+    for (size_t i = left; i <= right; i++)
+    {
+      matrix[bottom * cols + i] += add;
+      add++;
+    }
+    bottom--;
 
-      // size_t will never be <0 meh
-      for (size_t i = bottom; i + 1 >= top + 1; i--)
-      {
-        matrix[i * cols + right] += add;
-        add++;
-      }
-      right--;
+    for (size_t i = bottom; i + 1 >= top + 1; i--)
+    {
+      matrix[i * cols + right] += add;
+      add++;
+    }
+    right--;
 
-      for (size_t i = right; i >= left; i--)
-      {
-        matrix[top * cols + i] += add;
-        add++;
-      }
-      top++;
-   }
+    for (size_t i = right; i >= left; i--)
+    {
+      matrix[top * cols + i] += add;
+      add++;
+    }
+    top++;
+  }
 
 }
