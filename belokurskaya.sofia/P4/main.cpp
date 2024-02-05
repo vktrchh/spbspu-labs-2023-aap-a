86b8681108c0f1bfb1172da18f4ab7d577627a5f
sofabelokurskaya@yandex.ru <sofabelokurskaya@yandex.ru>
2024-02-05 19:37:42 +0300

    added files

--- belokurskaya.sofia/P4/main.cpp
+++ belokurskaya.sofia/P4/main.cpp
@@ -1,60 +1,9 @@
+#include "process_matrix.hpp"
+
 #include <iostream>
 #include <fstream>
-
-void freeMatrix(int ** m, size_t rows)
-{
-  for (size_t i = 0; i < rows; ++i)
-  {
-    delete [] m[i];
-  }
-  delete [] m;
-}
-
-int ** createMatrix(size_t rows, size_t cols)
-{
-  int ** rowsptrs = new int *[rows];
-  for (size_t i = 0; i < rows; ++i)
-    {
-      rowsptrs[i] = new int[cols];
-    }
-    return rowsptrs;
-}
-
-size_t findMaxRow(int * matrix, size_t rows, size_t cols)
-{
-  size_t maxRow = 0;
-  size_t max = 0;
-
-  for (size_t i = 0; i < rows; ++i)
-  {
-    size_t localCount = 1;
-    for (size_t j = 0; j < cols - 1; ++j)
-    {
-      if (matrix[i * cols + j] == matrix[i * cols + j + 1])
-      {
-        ++localCount;
-        if (localCount > max)
-        {
-          max = localCount;
-          maxRow = i + 1;
-        }
-      }
-      else
-      {
-        localCount = 1;
-      }
-    }
-  }
-  return maxRow;
-}
-
-void processMatrix(int ** matrix, size_t rows, size_t cols, const char * number)
-{
-  size_t maxRow = findMaxRow(& matrix[0][0], rows, cols);
-
-  std::ofstream output(number);
-  output << maxRow;
-}
+#include <cstdlib>
+#include <stdexcept>
 
 int main(int argc, char * argv[])
 {
@@ -64,10 +13,17 @@ int main(int argc, char * argv[])
     return 1;
   }
 
-  int num = 0;
+  int task_number = 0;
+
   try
   {
-    num = std::atoi(argv[1]);
+    size_t next_char_index = 0;
+    task_number = std::stoi(argv[1], std::addressof (next_char_index));
+
+    if (argv[1][next_char_index] != '\0')
+    {
+      throw std::invalid_argument("Invalid characters in the first command line argument");
+    }
   }
   catch (const std::out_of_range &)
   {
@@ -77,60 +33,78 @@ int main(int argc, char * argv[])
   catch (const std::invalid_argument &)
   {
     std::cerr << "The first argument is not a number\n";
-    return  1;
+    return 1;
   }
 
-  if (num != 1 && num != 2)
+  if (task_number != 1 && task_number != 2)
   {
     std::cerr << "Number of the task is wrong\n";
     return 1;
   }
 
-  size_t rows = 0, cols = 0;
-  std::ifstream input(argv[2]);
-  input >> rows >> cols;
+  std::ifstream input_file(argv[2]);
 
-  if (!input)
+  if (!input_file)
   {
     std::cerr << "Cannot read a number\n";
     return 2;
   }
 
-  if (num == 1)
+  size_t rows = 0, cols = 0;
+
+  if (!(input_file >> rows >> cols))
   {
-    if (rows * cols > 10000)
-    {
-      std::cerr << "Size of the matrix exceeds the limit for a static array\n";
-      return 1;
-    }
-
-    int * matrix = new int[rows * cols];
-
-    for (size_t i = 0; i < rows; ++i)
-    {
-      for (size_t j = 0; j < cols; ++j)
-      {
-        input >> matrix[i * cols + j];
-      }
-    }
-    processMatrix(& matrix, rows, cols, argv[3]);
-    delete [] matrix;
+    std::cerr << "Error reading rows and cols from file\n";
+    return 2;
   }
 
-  else if (num == 2)
-  {
-    int *  matrix = new int[rows * cols];
+  std::ofstream output_file(argv[3]);
 
-    for (size_t i = 0; i < rows; ++i)
-    {
-      for (size_t j = 0; j < cols; ++j)
-      {
-        input >> matrix[i * rows + j];
-      }
-    }
-    processMatrix(& matrix, rows, cols, argv[3]);
-    delete [] matrix;
+  if (cols == 0 || rows == 0)
+  {
+    output_file << "0\n";
+    return 0;
   }
-  input.close();
+
+  int max_matrix_size[10000] = {0};
+  int * matrix = nullptr;
+
+  if (task_number == 1)
+  {
+    matrix = max_matrix_size;
+  }
+  else
+  {
+    matrix = new int[rows * cols]{0};
+  }
+
+  try
+  {
+    belokurskaya::readMatrix(input_file, matrix, rows, cols);
+  }
+  catch (const std::exception & e)
+  {
+    std::cerr << "Error" << e.what() << "\n";
+    if (task_number == 2)
+    {
+      delete[] matrix;
+    }
+    return 2;
+  }
+
+  int result = belokurskaya::findMaxSequence(matrix, rows, cols);
+
+  if (task_number == 2)
+  {
+    delete[] matrix;
+  }
+
+  if (!output_file)
+  {
+    std::cerr << "Error opening output file\n";
+    return 2;
+  }
+
+  output_file << result + 1 << "\n";
   return 0;
 }
