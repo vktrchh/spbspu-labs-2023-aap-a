94aab233c04c035a1a53a3e294e7cbc622b59487
User1235321 <pavreb1235321@gmail.com>
2024-02-05 13:56:37 +0300

    Add normal isoscale

--- rebdev.pavel/T1/isoScale.cpp
+++ rebdev.pavel/T1/isoScale.cpp
@@ -5,7 +5,9 @@ void rebdev::isoScale(Shape ** shapes, const int & numOfShape, const point_t & s
 {
   for (int i = 0; i < numOfShape; ++i)
   {
+    point_t centerPoint = shapes[i]->getFrameRect().pos;
     shapes[i]->move(scalePoint);
     shapes[i]->scale(k);
+    shapes[i]->move((centerPoint.x - scalePoint.x) * k, (centerPoint.y - scalePoint.y) * k);
   }
 };
