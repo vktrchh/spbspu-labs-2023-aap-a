4280516436dc4ea25bc93f0a7f11685dcb5d78d1
sofabelokurskaya@yandex.ru <sofabelokurskaya@yandex.ru>
2024-02-06 03:49:09 +0300

    try to fix valgrind error

--- belokurskaya.sofia/P3/main.cpp
+++ belokurskaya.sofia/P3/main.cpp
@@ -7,7 +7,11 @@
 int main()
 {
   using namespace belokurskaya;
-  char* input_str = nullptr;
+
+  const int initial_input_size = 100;
+  const int new_memory = 10;
+
+  char* input_str = new char[initial_input_size + 1];
   char* not_input_chars = nullptr;
   char* original_input_str = nullptr;
 
@@ -15,18 +19,14 @@ int main()
   {
     input_str = inputString(std::cin);
     not_input_chars = new char[26]{};
-
     transformString(input_str, not_input_chars);
 
     std::cout << not_input_chars << "\n";
 
-    size_t input_str_length = strlen(input_str);
-    original_input_str = resizeStringBuffer(input_str, input_str_length, input_str_length + 1);
+    original_input_str = resizeStringBuffer(input_str, strlen(input_str), strlen(input_str) + new_memory + 1);
 
     toLowerCase(original_input_str);
-
     std::cout << original_input_str << "\n";
-
     delete[] input_str;
     delete[] not_input_chars;
     delete[] original_input_str;
@@ -34,6 +34,9 @@ int main()
   catch (const std::bad_alloc& e)
   {
     std::cerr << "Error: " << e.what() << "\n";
+    delete[] input_str;
+    delete[] not_input_chars;
+    delete[] original_input_str;
     return 1;
   }
   catch (const std::logic_error& e)
