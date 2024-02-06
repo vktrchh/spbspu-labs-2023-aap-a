eb9f8dce8cfeeec738a086f9378408694dbf5aef
kastiela <kristinazak150@gmail.com>
2024-02-06 22:02:57 +0300

    fix complexquad constructors

--- zakozhurnikova.kristina/T1/complexquad.hpp
+++ zakozhurnikova.kristina/T1/complexquad.hpp
@@ -8,6 +8,21 @@ namespace zakozhurnikova
   {
   public:
     Complexquad() = delete;
+    Complexquad(const point_t& p1, const point_t& p2, const point_t& p3, const point_t& p4) :
+      p1_(p1),
+      p2_(p2),
+      p3_(p3),
+      p4_(p4)
+    {}
+    Complexquad(const Complexquad& comp) :
+      p1_(comp.p1_),
+      p2_(comp.p2_),
+      p3_(comp.p3_),
+      p4_(comp.p4_)
+    {}
+    Complexquad(Complexquad&& comp) = default;
+    ~Complexquad()
+    {}
     double getArea() const;
     rectangle_t getFrameRect() const;
     void move(const point_t& p);
