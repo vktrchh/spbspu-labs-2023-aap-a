7675176b5152cfdfd913d39ffdaa8577a9dd6fad
Skuratov09 <skyratovdaniil28@gmail.com>
2024-02-07 17:18:22 +0300

    ...

--- skuratov.danil/P3/removeDuplicate.cpp
+++ skuratov.danil/P3/removeDuplicate.cpp
@@ -4,27 +4,27 @@
 
 namespace skuratov
 {
-    char* removeDuplicate(char* result, const char* lineOfProgram, const char* lineOfInput, size_t len1, size_t len2)
+  char* removeDuplicate(char* result, const char* lineOfProgram, const char* lineOfInput, size_t len1, size_t len2)
+  {
+    int index = 0;
+    int found = 0;
+    for (size_t i = 0; i < len1; i++)
     {
-        int index = 0;
-        int found = 0;
-        for (size_t i = 0; i < len1; i++)
+      found = 0;
+      for (size_t j = 0; j < len2; j++)
+      {
+        if (lineOfInput[i] == lineOfProgram[j])
         {
-            found = 0;
-            for (size_t j = 0; j < len2; j++)
-            {
-                if (lineOfInput[i] == lineOfProgram[j])
-                {
-                    found = 1;
-                    break;
-                }
-            }
-            if (!found)
-            {
-                result[index++] = lineOfInput[i];
-            }
+          found = 1;
+          break;
         }
-        result[index] = '\0';
-        return result;
+      }
+      if (!found)
+      {
+        result[index++] = lineOfInput[i];
+      }
     }
+    result[index] = '\0';
+    return result;
+  }
 }
