15a5a0a1f2fc7f6606c1fd013f96639702c7d39a
SofiyLop <lopatina.sa@edu.spbstu.ru>
2024-02-08 02:05:17 +0300

    Some tries to check the diamond

--- lopatina.sofia/T1/main.cpp
+++ lopatina.sofia/T1/main.cpp
@@ -174,9 +174,86 @@ private:
   point_t pos_;
 };
 
+/*
+class Diamond: public Shape
+{
+public:
+  Diamond(point_t point1, point_t point2, point_t point3)
+  {
+    height_rect_ = b.y_ - a.y_;
+    width_rect_ = b.x_ - a.x_;
+    pos_rect_.x_ = (a.x_ + b.x_) / 2.0;
+    pos_rect_.y_ = (a.y_ + b.y_) / 2.0;
+  }
+  virtual double getArea()
+  {
+    return height_rect_ * width_rect_;
+  }
+  virtual rectangle_t getFrameRect()
+  {
+    rectangle_t newRect{ height_rect_, width_rect_, pos_rect_ };
+    return newRect;
+  }
+  virtual void move(point_t s)
+  {
+    pos_rect_.x_ = s.x_;
+    pos_rect_.y_ = s.y_;
+  }
+  virtual void move(double x, double y)
+  {
+    pos_rect_.x_ += x;
+    pos_rect_.y_ += y;
+  }
+  virtual rectangle_t scale(point_t s, double k)
+  {
+    point_t point{ pos_rect_.x_, pos_rect_.y_ };
+    move(s);
+    height_rect_ = height_rect_ * k;
+    width_rect_ = width_rect_ * k;
+    std::cout << "IN RECT: HEIGHT: " << height_rect_ << "   WIDTH: " << width_rect_ << '\n';
+    point_t new_pos{ pos_rect_.x_ - k * (pos_rect_.x_ - point.x_), pos_rect_.y_ - k * (pos_rect_.y_ - point.y_) };
+    move(new_pos);
+    rectangle_t newRect{ height_rect_, width_rect_, pos_rect_ };
+    return newRect;
+  }
+private:
+  double height_diam_;
+  double width_diam_;
+  point_t pos_diam_;
+};
+*/
+
+bool isCentralPoint(double point1_x, double point1_y, double point2_x, double point3_y)
+{
+  if ((point1_x == point2_x) && (point1_y == point3_y))
+  {
+    return true;
+  }
+  return false;
+}
+
+void distributePoints(point_t cent_point, point_t side_point_x, point_t side_point_y, point_t first_p, point_t second_p)
+{
+  if (first_p.x_ == cent_point.x_)
+  {
+    side_point_x.x_ = first_p.x_;
+    side_point_x.y_ = first_p.y_;
+    side_point_y.x_ = second_p.x_;
+    side_point_y.y_ = second_p.y_;
+  }
+  else
+  {
+    side_point_y.x_ = first_p.x_;
+    side_point_y.y_ = first_p.y_;
+    side_point_x.x_ = second_p.x_;
+    side_point_x.y_ = second_p.y_;
+  }
+  std::cout << "YES First point: " << first_p.x_ << ' ' << first_p.y_ << " Second point: " << second_p.x_ << ' ' << second_p.y_ << '\n';
+  std::cout << "SIDE x: " << side_point_x.x_ << ' ' << side_point_x.y_ << '\n';
+}
+
 int main()
 {
-  std::string array[1000] = {};
 //-----------
   std::cout << "TEST:\n";
   Shape *first = new Rectangle({ 1.0, 1.0 }, { 3.0, 4.0 });
@@ -233,6 +310,7 @@ int main()
   std::cout << (scale3_x - (w4 / 2.0)) << ' ' << (scale3_y - (h4 / 2.0)) << '\n';
   std::cout << (scale3_x + (w4 / 2.0)) << ' ' << (scale3_y + (h4 / 2.0)) << '\n';
 
+/*
   std::cout << "\nTEST_NUMBERS:\n";
   std::cout << "3.46 -> " << round(3.46 * 10) / 10 << '\n';
   std::cout << "3.44 -> " << round(3.44 * 10) / 10 << '\n';
@@ -241,46 +319,90 @@ int main()
   double f = 3.0;
   std::cout << "3.0 -> " << std::fixed << std::setprecision(1) << f << '\n';
   std::cout << "------\n";
+*/
 
 //-----------
+  double area_before = 0;
+  double area_after = 0;
+  rectangle_t array[1000] = {};
   size_t counter = 0;
+
   while (counter != 1000)
   {
-    std::string filler = "";
-    std::cin >> filler;
-    //array[counter] = filler;
-    std::cout << "Test: filler: " << filler << "  array: " << array[counter] << "  counter: " << counter << '\n';
-    if (filler == "SCALE")
+    std::string name = "";
+    std::cin >> name;
+    std::cout << "Test: name: " << name << "  array: - " << "  counter: " << counter << '\n';
+    if (name == "SCALE")
     {
       break;
     }
-    if (filler == "RECTANGLE")
+    if (name == "RECTANGLE")
     {
       double low_left_x = 0, low_left_y = 0, up_right_x = 0, up_right_y = 0;
       std::cin >> low_left_x >> low_left_y >> up_right_x >> up_right_y;
       std::cout << "Test rect: " << low_left_x << ' ' << low_left_y << ' ' << up_right_x << ' ' << up_right_y;
       Rectangle *first = new Rectangle({ low_left_x, low_left_y }, { up_right_x, up_right_y });
     }
-    if (filler == "SQUARE")
+    if (name == "SQUARE")
     {
       double low_left_x = 0, low_left_y = 0, side = 0;
       std::cin >> low_left_x >> low_left_y >> side;
       std::cout << "Test square: " << low_left_x << ' ' << low_left_y << ' ' << side;
     }
-    if (filler == "TRIANGLE")
+    if (name == "TRIANGLE")
     {
       double point1_x = 0, point1_y = 0, point2_x = 0, point2_y = 0, point3_x = 0, point3_y = 0;
       std::cin >> point1_x >> point1_y >> point2_x >> point2_y >> point3_x >> point3_y;
       std::cout << "Test tri: " <<  point1_x << ' ' << point1_y << ' ' << point2_x << ' ' << point2_y << ' ' << point3_x << ' ' << point3_y;
     }
-    if (filler == "DIAMOND")
+    if (name == "DIAMOND")
     {
       double point1_x = 0, point1_y = 0, point2_x = 0, point2_y = 0, point3_x = 0, point3_y = 0;
       std::cin >> point1_x >> point1_y >> point2_x >> point2_y >> point3_x >> point3_y;
       std::cout << "Test diamond: " <<  point1_x << ' ' << point1_y << ' ' << point2_x << ' ' << point2_y << ' ' << point3_x << ' ' << point3_y;
+
+      if (((point1_x != point2_x) || (point1_y != point2_y)) && ((point2_x != point3_x) || (point2_y != point3_y)) && ((point3_x != point1_x) || (point3_y != point1_y)))
+      {
+        std::cout << "No same points\n";
+        point_t point1 = {point1_x, point1_y};
+        point_t point2 = {point2_x, point2_y};
+        point_t point3 = {point3_x, point3_y};
+        point_t central_point = {0.0, 0.0};
+        point_t side_point_x = {0.0, 0.0};
+        point_t side_point_y = {0.0, 0.0};
+        if (isCentralPoint(point1_x, point1_y, point2_x, point3_y) || isCentralPoint(point1_x, point1_y, point3_x, point2_y))
+        {
+          central_point.x_ = point1_x;
+          central_point.y_ = point1_y;
+          distributePoints(central_point, side_point_x, side_point_y, point2, point3);
+        }
+        else if (isCentralPoint(point2_x, point2_y, point1_x, point3_y) || isCentralPoint(point2_x, point2_y, point3_x, point1_y))
+        {
+          central_point.x_ = point2_x;
+          central_point.y_ = point2_y;
+          distributePoints(central_point, side_point_x, side_point_y, point1, point3);
+        }
+        else if (isCentralPoint(point3_x, point3_y, point1_x, point2_y) || isCentralPoint(point3_x, point3_y, point2_x, point1_y))
+        {
+          central_point.x_ = point3_x;
+          central_point.y_ = point3_y;
+          distributePoints(central_point, side_point_x, side_point_y, point1, point2);
+          std::cout << "TEST DIST POINTS: CENTRAL: " << central_point.x_ << ' ' << central_point.y_ << " SIDE X: " << side_point_x.x_ << ' ' << side_point_x.y_ << " SIDE Y: " << side_point_y.x_ << ' ' << side_point_y.y_ << '\n';
+           std::cout << "SIDE x: " << side_point_x.x_ << ' ' << side_point_x.y_ << '\n';
+       }
+        else
+        {
+          std::cout << "Invalid parameters\n";
+        }
+      }
+      else
+      {
+        std::cout << "Same points\n";
+      }
+
     }
+
     counter += 1;
   }
-  //std::cout << array[counter] << '\n';
   std::cout << counter << '\n';
 }
