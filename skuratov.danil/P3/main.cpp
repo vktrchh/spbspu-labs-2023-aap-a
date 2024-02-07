be9298fcb8f51b7dee5f7c5682e6fb521286020d
Skuratov09 <skyratovdaniil28@gmail.com>
2024-02-07 16:46:27 +0300

    i dont know what cg wants

--- skuratov.danil/P3/main.cpp
+++ skuratov.danil/P3/main.cpp
@@ -8,35 +8,35 @@
 
 int main()
 {
-  using namespace skuratov;
-  size_t size = 20;
-  const char* lineOfProgram = "abc";
-  try
-  {
-    char* inputLine = new char[size] {};
-    inputLine = readString(inputLine, size);
-    if (inputLine[0] == '\0')
+    using namespace skuratov;
+    size_t size = 20;
+    const char* lineOfProgram = "abc";
+    try
     {
-      delete[] inputLine;
-      throw std::invalid_argument("Empty input");
+        char* inputLine = new char[size] {};
+        inputLine = readString(inputLine, size);
+        if (inputLine[0] == '\0')
+        {
+            delete[] inputLine;
+            throw std::invalid_argument("Empty input");
+        }
+        size_t inputLineLength = 0;
+        while (inputLine[inputLineLength] != '\0')
+        {
+            inputLineLength++;
+        }
+        char* mergedStrings = new char[size * 2] {};
+        char* stringWithRemovedDuplicates = new char[size] {};
+        std::cout << mergeTwoLines(mergedStrings, inputLine, lineOfProgram) << '\n';
+        std::cout << removeDuplicate(stringWithRemovedDuplicates, lineOfProgram, inputLine, 3, inputLineLength);
+        delete[] inputLine;
+        delete[] mergedStrings;
+        delete[] stringWithRemovedDuplicates;
     }
-    size_t inputLineLength = 0;
-    while (inputLine[inputLineLength] != '\0')
+    catch (const std::exception& e)
     {
-      inputLineLength++;
+        std::cerr << e.what() << '\n';
+        return 1;
     }
-    char* mergedStrings = new char[size * 2] {};
-    char* stringWithRemovedDuplicates = new char[size] {};
-    std::cout << mergeTwoLines(mergedStrings, inputLine, lineOfProgram) << '\n';
-    std::cout << removeDuplicate(stringWithRemovedDuplicates, lineOfProgram, inputLine, 3, inputLineLength);
-    delete[] inputLine;
-    delete[] mergedStrings;
-    delete[] stringWithRemovedDuplicates;
-  }
-  catch (const std::exception& e)
-  {
-    std::cerr << e.what() << '\n';
-    return 1;
-  }
-  return 0;
+    return 0;
 }
--- skuratov.danil/P3/mergeTwoLines.cpp
+++ skuratov.danil/P3/mergeTwoLines.cpp
@@ -5,15 +5,15 @@
 
 char* skuratov::mergeTwoLines(char* result, const char* inputLine, const char* lineOfProgram)
 {
-  int i = 0;
-  while (*inputLine != '\0')
-  {
-    result[i++] = *inputLine++;
-  }
-  while (*lineOfProgram != '\0')
-  {
-    result[i++] = *lineOfProgram++;
-  }
-  result[i] = '\0';
-  return result;
+    int i = 0;
+    while (*inputLine != '\0')
+    {
+        result[i++] = *inputLine++;
+    }
+    while (*lineOfProgram != '\0')
+    {
+        result[i++] = *lineOfProgram++;
+    }
+    result[i] = '\0';
+    return result;
 }
