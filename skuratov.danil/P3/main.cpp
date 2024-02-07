54bb9ed125e190a353155e616f4dbc91d5df162a
Skuratov09 <skyratovdaniil28@gmail.com>
2024-02-07 16:57:56 +0300

    I hope I understand

--- skuratov.danil/P3/main.cpp
+++ skuratov.danil/P3/main.cpp
@@ -8,35 +8,35 @@
 
 int main()
 {
-    using namespace skuratov;
-    size_t size = 20;
-    const char* lineOfProgram = "abc";
-    try
+  using namespace skuratov;
+  size_t size = 20;
+  const char* lineOfProgram = "abc";
+  try
+  {
+    char* inputLine = new char[size] {};
+    inputLine = readString(inputLine, size);
+    if (inputLine[0] == '\0')
     {
-        char* inputLine = new char[size] {};
-        inputLine = readString(inputLine, size);
-        if (inputLine[0] == '\0')
-        {
-            delete[] inputLine;
-            throw std::invalid_argument("Empty input");
-        }
-        size_t inputLineLength = 0;
-        while (inputLine[inputLineLength] != '\0')
-        {
-            inputLineLength++;
-        }
-        char* mergedStrings = new char[size * 2] {};
-        char* stringWithRemovedDuplicates = new char[size] {};
-        std::cout << mergeTwoLines(mergedStrings, inputLine, lineOfProgram) << '\n';
-        std::cout << removeDuplicate(stringWithRemovedDuplicates, lineOfProgram, inputLine, 3, inputLineLength);
-        delete[] inputLine;
-        delete[] mergedStrings;
-        delete[] stringWithRemovedDuplicates;
+      delete[] inputLine;
+      throw std::invalid_argument("Empty input");
     }
-    catch (const std::exception& e)
+    size_t inputLineLength = 0;
+    while (inputLine[inputLineLength] != '\0')
     {
-        std::cerr << e.what() << '\n';
-        return 1;
+      inputLineLength++;
     }
-    return 0;
+    char* mergedStrings = new char[size * 2] {};
+    char* stringWithRemovedDuplicates = new char[size] {};
+    std::cout << mergeTwoLines(mergedStrings, inputLine, lineOfProgram) << '\n';
+    std::cout << removeDuplicate(stringWithRemovedDuplicates, lineOfProgram, inputLine, 3, inputLineLength);
+    delete[] inputLine;
+    delete[] mergedStrings;
+    delete[] stringWithRemovedDuplicates;
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+    return 1;
+  }
+  return 0;
 }
