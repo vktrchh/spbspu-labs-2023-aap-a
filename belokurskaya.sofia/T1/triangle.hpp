f0cedf51e70560aef56bfec0ce71e7f55d69d0e7
sofabelokurskaya@yandex.ru <sofabelokurskaya@yandex.ru>
2024-02-07 07:51:15 +0300

    added rectangle.hpp

---
+++ belokurskaya.sofia/T1/triangle.hpp
@@ -0,0 +1,28 @@
+#ifndef TRIANGLE_HPP
+#define TRIANGLE_HPP
+
+#include "shape.hpp"
+
+namespace belokurskaya
+{
+  class Triangle : public Shape
+  {
+    public:
+      Triangle(const point_t & vertex1, const point_t & vertex2, const point_t & vertex3);
+
+      double getArea() const override;
+      rectangle_t getFrameRect() const override;
+      void move(const point_t& new_pos) override;
+      void move(double dx, double dy) override;
+      void scale(double factor) override;
+
+    private:
+      point_t vertex1_;
+      point_t vertex2_;
+      point_t vertex3_;
+
+      point_t calculateCentroid() const;
+  };
+}
+
+#endif
