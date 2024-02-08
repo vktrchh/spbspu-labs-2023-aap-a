07f79b9ef2d0db2e12f8b2f3c7ee58903c23ae2c
deadshumz <shumzz@bk.ru>
2024-02-08 17:13:34 +0300

    trailing spaces

--- petuhov.kirill/P4/islowertriangular.cpp
+++ petuhov.kirill/P4/islowertriangular.cpp
@@ -3,7 +3,7 @@
 bool petuhov::isLowerTriangular(const int* matrix, size_t rows, size_t cols)
 {
   bool isLowerTriangularMatrix = true;
-  
+
   for (size_t i = 0; i < rows; ++i)
   {
     for (size_t j = i + 1; j < cols; ++j)
@@ -19,6 +19,6 @@ bool petuhov::isLowerTriangular(const int* matrix, size_t rows, size_t cols)
       break;
     }
   }
-  
+
   return isLowerTriangularMatrix;
 }
