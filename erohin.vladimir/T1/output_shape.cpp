230e9810a9171bfb66f0d29d0400e0ccec6938ba
WarriorVortex <erohinvova01010111@gmail.com>
2024-02-09 02:39:19 +0300

    Removed unnecessary try-catch

--- erohin.vladimir/T1/output_shape.cpp
+++ erohin.vladimir/T1/output_shape.cpp
@@ -6,18 +6,9 @@
 void erohin::outputShape(std::ostream& output, const Shape* const* shape, size_t size)
 {
   double sumArea = 0.0;
-  double area = 0.0;
   for (size_t i = 0; i < size; ++i)
   {
-    try
-    {
-      area = shape[i] ? shape[i]->getArea() : 0.0;
-    }
-    catch (...)
-    {
-      area = 0.0;
-    }
-    sumArea += area;
+    sumArea += shape[i] ? shape[i]->getArea() : 0.0;
   }
   output << sumArea;
   rectangle_t frameRect = {0.0, 0.0, {0.0, 0.0}};
