0fd67127c5b5c33e67810b46c373215830e78ed4
osakasonyboy@gmail.com <sensh0god@gmail.com>
2024-02-08 22:17:27 +0300

    added namespace in base-types.hpp

--- arakelyan.daniil/T1/base-types.hpp
+++ arakelyan.daniil/T1/base-types.hpp
@@ -1,16 +1,19 @@
 #ifndef BASE_TYPES_HPP
 #define BASE_TYPES_HPP
 
-struct point_t
+namespace arakelyan
 {
-  double x;
-  double y;
-};
+  struct point_t
+  {
+    double x;
+    double y;
+  };
 
-struct rectangle_t
-{
-  double width;
-  double height;
-  point_t pos;
-};
+  struct rectangle_t
+  {
+    double width;
+    double height;
+    point_t pos;
+  };
+}
 #endif
