ea7af94d0a9fcf994790bf31949d82de6d094613
deadshumz <shumzz@bk.ru>
2024-02-08 19:02:47 +0300

    fixes

--- petuhov.kirill/P4/createMatrix.cpp
+++ petuhov.kirill/P4/createMatrix.cpp
@@ -3,6 +3,11 @@
 
 int * petuhov::createMatrix(size_t rows, size_t cols)
 {
+  if (rows == 0 || cols == 0)
+  {
+    throw std::invalid_argument("Invalid matrix dimensions: rows or columns is zero");
+  }
+
   int *matrix = new int[rows * cols];
   if (!matrix)
   {
