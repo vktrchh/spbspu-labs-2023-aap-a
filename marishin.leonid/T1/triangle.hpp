c4e1673219df630bf58248faa7f6308c2b3ab3ce
marlions <marishin.lv@edu.spbstu.ru>
2024-02-09 23:07:07 +0300

    cg

--- marishin.leonid/T1/triangle.hpp
+++ marishin.leonid/T1/triangle.hpp
@@ -11,7 +11,7 @@ namespace marishin
     Triangle(point_t firstPoint, point_t secondPoint, point_t thirdPoint);
     virtual double getArea() const;
     virtual rectangle_t getFrameRect() const;
-    virtual void move(const point_t newPos);
+    virtual void move(point_t newPos);
     virtual void move(double dx, double dy);
     virtual void scale(double factor);
   private:
