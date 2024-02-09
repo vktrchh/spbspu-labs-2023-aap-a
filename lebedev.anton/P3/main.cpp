193efffe1738fa752bfa8eb00cbac7d66fe0c82d
IastWish <antony_lebedevv@mail.ru>
2024-02-09 22:47:29 +0300

    String input function

--- lebedev.anton/P3/main.cpp
+++ lebedev.anton/P3/main.cpp
@@ -1,2 +1,26 @@
+#include <iostream>
+
+#include "input_string.hpp"
+
 int main()
-{}
+{
+  using namespace lebedev;
+  char * string = nullptr;
+  try
+  {
+    string = inputString(std::cin);
+  }
+  catch (...)
+  {
+    std::cerr << "Error in memory allocation\n";
+    return 1;
+  }
+  if (string[0] == '\0')
+  {
+    std::cerr << "Error: Empty input\n";
+    delete[] string;
+    return 1;
+  }
+
+  //std::cout << string;
+}
