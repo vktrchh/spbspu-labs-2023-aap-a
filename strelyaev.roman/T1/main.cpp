6efd4cbf317cb3e32d22f0ceec86ef1c8cb30c54
Slivka <roman.strelyaeff@gmail.com>
2024-02-09 20:03:07 +0300

    fix

--- strelyaev.roman/T1/main.cpp
+++ strelyaev.roman/T1/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <cstring>
-//#include <getString.hpp>
+#include <getString.hpp>
 #include "base-types.hpp"
 #include "shape.hpp"
 #include "rectangle.hpp"
