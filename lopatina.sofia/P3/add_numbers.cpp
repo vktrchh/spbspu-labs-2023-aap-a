89e304d43260bffdd550ee31e97eb54c7cef8fad
SofiyLop <lopatina.sa@edu.spbstu.ru>
2024-02-11 14:42:10 +0300

    No comments

--- lopatina.sofia/P3/add_numbers.cpp
+++ lopatina.sofia/P3/add_numbers.cpp
@@ -16,13 +16,6 @@ size_t lopatina::countNumbers(const char * str)
 
 void lopatina::addNumbers(char * first_str, const char * second_str, size_t size1, size_t size2, char * result)
 {
-  /*size_t num_dig = countNumbers(second_str);
-  if (num_dig == 0)
-  {
-    return first_str;
-  }
-  char * result = new char[size1 + num_dig];
-*/
   size_t last = 0;
   for (size_t i = 0; i < size1; ++i)
   {
@@ -42,5 +35,4 @@ void lopatina::addNumbers(char * first_str, const char * second_str, size_t size
     }
   }
   result[last + 1] = '\0';
-  //return result;
 }
