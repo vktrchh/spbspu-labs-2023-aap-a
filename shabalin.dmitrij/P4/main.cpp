c4edf95a869e94ecea8f8f458805a9adb3d51c5a
dmitei <dima9990860349@icloud.com>
2024-02-12 00:51:25 +0300

    CG FIXXXXXX2

--- shabalin.dmitrij/P4/main.cpp
+++ shabalin.dmitrij/P4/main.cpp
@@ -58,15 +58,15 @@ double* makeUpSmoothMatrix(const int* matrix, double* smooth, size_t rows, size_
     {
         double sum = 0.0;
         size_t count = 0;
-        for (int j = -1; j <= 1; ++j)
+        for (size_t j = -1; j <= 1; ++j)
         {
-            for (int d = -1; d <= 1; ++d)
+            for (size_t d = -1; d <= 1; ++d)
             {
                 if (!((j == 0) && (d == 0)))
                 {
                     size_t currentRow = (i / cols) + j;
                     size_t currentColumn = (i % cols) + d;
-                    if ((currentRow < rows) && (currentColumn < cols) && (currentRow >= 0 && currentColumn >=0))
+                    if ((currentRow < rows) && (currentColumn < cols) && (currentRow > 0) && (currentColumn >0))
                     {
                         size_t newPosition = currentRow * i + currentColumn;
                         sum += matrix[newPosition];
