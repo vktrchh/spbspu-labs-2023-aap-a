e5c2ccf33804eec1005a5ce3c81b57880859a8c3
sofabelokurskaya@yandex.ru <sofabelokurskaya@yandex.ru>
2024-02-12 18:58:29 +0300

    try to fix negative scale factor

--- belokurskaya.sofia/T1/iso_scale.cpp
+++ belokurskaya.sofia/T1/iso_scale.cpp
@@ -1,7 +1,12 @@
 #include "iso_scale.hpp"
+#include "stdexcept"
 
 void belokurskaya::isoScale(Shape * shapes, const point_t & pos, double ratio)
 {
+  if (ratio <= 0.0)
+  {
+    throw std::invalid_argument("Invalid scaling factor");
+  }
   point_t init_pos = shapes->getFrameRect().pos;
   shapes->move(pos);
   point_t new_pos = shapes->getFrameRect().pos;
