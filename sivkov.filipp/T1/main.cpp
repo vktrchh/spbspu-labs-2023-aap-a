89e7d776cb44923f439e49cc53481b969092ee7a
exxiiint <filipp.sivkov@yandex.ru>
2024-02-12 23:11:16 +0300

    git add main.cpp and scale

--- sivkov.filipp/T1/main.cpp
+++ sivkov.filipp/T1/main.cpp
@@ -1,3 +1,37 @@
+#include "concave.hpp"
+#include "complexquad.hpp"
+#include "inputShape.hpp"
+#include "base-types.hpp"
+#include "rectangle.hpp"
+#include "shape.hpp"
+#include "scale.hpp"
+#include "othersFun.hpp"
+#include "output.hpp"
+#include <iostream>
+
 int main()
 {
+  Shape** figures = nullptr;
+  size_t count = 0;
+  try
+  {
+
+    figures = inputShape(std::cin, count);
+    double k = 0;
+    point_t center = { 0, 0 };
+    std::cin >> center.x >> center.y >> k;
+    std::cout << std::fixed;
+    output(std::cout, figures, count);
+    for (size_t i = 0; i < count; ++i)
+    {
+      scale(figures[i], center, k);
+    }
+    output(std::cout, figures, count);
+
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Exception caught: " << e.what() << std::endl;
+  }
+  return 0;
 }
---
+++ sivkov.filipp/T1/scale.hpp
@@ -0,0 +1,8 @@
+#ifndef SCALE_HPP
+#define SCALE_HPP
+#include "base-types.hpp"
+#include "shape.hpp"
+
+void scale(Shape* shape, point_t center, double k);
+
+#endif
