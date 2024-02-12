ae28893e0fbd6c0f23e1dad93f562124687843c6
exxiiint <filipp.sivkov@yandex.ru>
2024-02-12 16:35:24 +0300

    fix copyNum

--- sivkov.filipp/P3/copyNum.cpp
+++ sivkov.filipp/P3/copyNum.cpp
@@ -1,25 +1,24 @@
 #include "copyNum.hpp"
 #include <cctype>
-#include <cstddef>
 
 void sivkov::copyNum(char* string1, const char* string2)
 {
-  size_t length1 = 0;
-  while (string1[length1] != '\0')
-  {
-    ++length1;
-  }
-  size_t index1 = length1;
-  size_t index2 = 0;
-  while (string2[index2] != '\0')
-  {
-    if (std::isdigit(string2[index2]))
-    {
-      string1[index1] = string2[index2];
-      ++index1;
-    }
-    ++index2;
-  }
-  string1[index1] = '\0';
-}
+  char* ptr1 = string1;
+  const char* ptr2 = string2;
 
+  while (*ptr1)
+  {
+    ++ptr1;
+  }
+
+  while (*ptr2)
+  {
+    if (std::isdigit(*ptr2))
+    {
+      *ptr1 = *ptr2;
+      ++ptr1;
+    }
+    ++ptr2;
+  }
+  *ptr1 = '\0';
+}
