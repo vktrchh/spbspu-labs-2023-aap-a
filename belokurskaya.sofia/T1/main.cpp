f94068abcffb5268dbc857acb241e8d01b3f1b90
sofabelokurskaya@yandex.ru <sofabelokurskaya@yandex.ru>
2024-02-12 08:36:25 +0300

    output fixing

--- belokurskaya.sofia/T1/main.cpp
+++ belokurskaya.sofia/T1/main.cpp
@@ -12,6 +12,8 @@ int main()
   const int max_shapes = 1000;
   belokurskaya::Shape* shapes[max_shapes];
   int shape_count = 0;
+  double total_area_before_scaling = 0.0;
+  double total_area_after_scaling = 0.0;
 
   std::string input;
   while (std::getline(std::cin, input))
@@ -29,12 +31,16 @@ int main()
       double center_x = (lower_left_x + upper_right_x) / 2.0;
       double center_y = (lower_left_y + upper_right_y) / 2.0;
       shapes[shape_count++] = new belokurskaya::Rectangle({ center_x, center_y }, width, height);
+      total_area_before_scaling += shapes[shape_count]->getArea();
+      shape_count++;
     }
     else if (command == "TRIANGLE" && shape_count < max_shapes)
     {
       double vertex1_x, vertex1_y, vertex2_x, vertex2_y, vertex3_x, vertex3_y;
       inputStream >> vertex1_x >> vertex1_y >> vertex2_x >> vertex2_y >> vertex3_x >> vertex3_y;
       shapes[shape_count++] = new belokurskaya::Triangle({vertex1_x, vertex1_y}, {vertex2_x, vertex2_y}, {vertex3_x, vertex3_y});
+      total_area_before_scaling += shapes[shape_count]->getArea();
+      shape_count++;
     }
     else if (command == "CONCAVE" && shape_count < max_shapes)
     {
@@ -42,18 +48,22 @@ int main()
       inputStream >> vertex1_x >> vertex1_y >> vertex2_x >> vertex2_y >> vertex3_x >> vertex3_y >> vertex4_x >> vertex4_y;
       shapes[shape_count++] = new belokurskaya::Concave({vertex1_x, vertex1_y},
       {vertex2_x, vertex2_y}, {vertex3_x, vertex3_y}, {vertex4_x, vertex4_y});
+      total_area_before_scaling += shapes[shape_count]->getArea();
+      shape_count++;
     }
     else if (command == "SCALE")
     {
       double scale_point_x, scale_point_y, scale_factor;
       inputStream >> scale_point_x >> scale_point_y >> scale_factor;
+
+      std::cout << total_area_before_scaling << " ";
+
       for (int i = 0; i < shape_count; ++i)
       {
         std::cout << std::fixed;
         std::cout.precision(1);
 
         belokurskaya::rectangle_t frameRect = shapes[i]->getFrameRect();
-        std::cout << shapes[i]->getArea();
         std::cout << frameRect.pos.x - frameRect.width / 2.0 << " " << frameRect.pos.y - frameRect.height / 2.0 << " ";
         std::cout << frameRect.pos.x + frameRect.width / 2.0 << " " << frameRect.pos.y + frameRect.height / 2.0 << "\n";
       }
@@ -68,12 +78,27 @@ int main()
           shapes[i]->move({ scale_point_x, scale_point_y});
           shapes[i]->scale(scale_factor);
           shapes[i]->move({ -dx, -dy });
+          total_area_after_scaling += shapes[i]->getArea();
         }
         catch (const std::invalid_argument& e)
         {
           std::cerr << "Error: " << e.what() << "\n";
         }
       }
+      std::cout << total_area_after_scaling << " ";
+
+      for (int i = 0; i < shape_count; ++i)
+      {
+        std::cout << std::fixed;
+        std::cout.precision(1);
+        belokurskaya::rectangle_t frameRect = shapes[i]->getFrameRect();
+        double scale_lower_left_x = frameRect.pos.x - frameRect.width / 2.0;
+        double scale_lower_left_y = frameRect.pos.y - frameRect.height / 2.0;
+        double scale_upper_right_x = frameRect.pos.x + frameRect.width / 2.0;
+        double scale_upper_right_y = frameRect.pos.y + frameRect.height / 2.0;
+        std::cout << scale_lower_left_x << " " << scale_lower_left_y << " " << scale_upper_right_x << " " << scale_upper_right_y << "\n";
+      }
+      std::cout << "\n";
       break;
     }
     else
@@ -85,16 +110,6 @@ int main()
 
   for (int i = 0; i < shape_count; ++i)
   {
-    std::cout << std::fixed;
-    std::cout.precision(1);
-    std::cout << shapes[i]->getArea() << " ";
-    belokurskaya::rectangle_t frameRect = shapes[i]->getFrameRect();
-    double scale_lower_left_x = frameRect.pos.x - frameRect.width / 2.0;
-    double scale_lower_left_y = frameRect.pos.y - frameRect.height / 2.0;
-    double scale_upper_right_x = frameRect.pos.x + frameRect.width / 2.0;
-    double scale_upper_right_y = frameRect.pos.y + frameRect.height / 2.0;
-    std::cout << scale_lower_left_x << " " << scale_lower_left_y << " " << scale_upper_right_x << " " << scale_upper_right_y << "\n";
-
     delete shapes[i];
   }
   return 0;
