866650a014ec8ef6e17c2fb573d5f629eecc4046
magggottt <gladyshevmatvey@gmail.com>
2024-02-12 23:25:19 +0700

    gix

--- gladyshev.matvey/T1/definedata.hpp
+++ gladyshev.matvey/T1/definedata.hpp
@@ -13,7 +13,7 @@ namespace gladyshev
   void inputPoints(std::istream& in, double * arrcords, size_t counter);
   void inputScale(std::istream& in, point_t& pos, double& factor);
   void freeMemory(Shape ** shapes, size_t amount);
-  Shape * identifyShape(std::string inputName, std::istream& in);
+  Shape * identifyShape(std::string& inputName, std::istream& in);
   void isoScale(Shape * shapes, point_t pos, double factor);
 }
 
