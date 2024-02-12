11c4015c87774257124ba2a1fe1ff35168d21d77
sofabelokurskaya@yandex.ru <sofabelokurskaya@yandex.ru>
2024-02-12 03:18:28 +0300

    calculate concave area

--- concave.cpp
+++ concave.cpp
@@ -21,3 +21,15 @@ vertex1_(vertex1), vertex2_(vertex2), vertex3_(vertex3), vertex4_(vertex4)
     throw std::invalid_argument("The first three vertices do not form a triangle");
   }
 }
+
+double belokurskaya::Concave::getArea() const
+{
+  double triangle1_area = calculateTriangleArea(vertex1_, vertex2_, vertex4_);
+  double triangle2_area = calculateTriangleArea(vertex2_, vertex3_, vertex4_);
+  return triangle1_area + triangle2_area;
+}
+
+double belokurskaya::Concave::calculateTriangleArea(const point_t & p1, const point_t & p2, const point_t & p3) const
+{
+  return std::abs((p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y)) / 2.0);
+}
--- concave.hpp
+++ concave.hpp
@@ -9,7 +9,10 @@ namespace belokurskaya
   {
     public:
       Concave(const point_t & vertex1, const point_t & vertex2, const point_t & vertex3, const point_t & vertex4);
+      double getArea() const override;
 
-};
-
+    private:
+      double calculateTriangleArea(const point_t & p1, const point_t & p2, const point_t & p3) const;
+  };
+}
 #endif
