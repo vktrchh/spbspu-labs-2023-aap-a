dab25aee76c5c6037d7291d0db7ed0e83237ac6a
puzz13 <puzz13one@gmail.com>
2024-02-13 19:53:11 +0300

    First steps w/ input
    help pls

--- kovtun.alexander/P3/main.cpp
+++ kovtun.alexander/P3/main.cpp
@@ -1,4 +1,22 @@
+#include <iostream>
+
 int main()
 {
+  size_t size = 5;
+  char c = '\n';
+  char * input = new char[size]();
 
+  for (size_t counter = 0; counter < size; counter++)
+  {
+    std::cin >> c;
+    input[counter] = c;
+  }
+
+  for (int i = 0; i < size; i++)
+  {
+    std::cout << input[i];
+  }
+  delete [] input;
+
+  return 0;
 }
