786fc69792db51b36f38d0e408bb70f984e37c02
fncg <cld@dkstp>
2024-02-14 01:18:47 +0300

    Добавлена обработка последовательности символов

--- stepanov.gena/P3/main.cpp
+++ stepanov.gena/P3/main.cpp
@@ -1,3 +1,23 @@
+#include <iostream>
+#include <iomanip>
+
 int main()
 {
+  char buffer[20] = {};
+  char symbol = 0;
+  size_t i = 0;
+
+  std::cin >> std::noskipws;
+  while ((std::cin >> symbol) && (i < 19))
+  {
+    buffer[i++] = symbol;
+    if (symbol == '\n')
+    {
+      buffer[i-1] = 0;
+      break;
+    }
+  }
+  std::cin >> std::skipws;
+
+  std::cout << buffer << "\n";
 }
