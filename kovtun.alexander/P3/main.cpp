88ea26dc0885764de5816664e35f26bf919b8f64
puzz13 <puzz13one@gmail.com>
2024-02-13 23:30:56 +0300

    Add input array expanding on need basis

--- kovtun.alexander/P3/main.cpp
+++ kovtun.alexander/P3/main.cpp
@@ -3,13 +3,13 @@
 
 int main()
 {
-  size_t stringSize = 5;
+  size_t stringSize = 50;
   size_t charIndex = 0;
   char c = '\n';
   char * input = new char[stringSize]();
 
   std::cin >> std::noskipws;
-  while (std::cin >> c  && charIndex < stringSize)
+  while (std::cin >> c)
   {
     if (c == '\n')
     {
@@ -19,10 +19,24 @@ int main()
 
     input[charIndex] = c;
     charIndex++;
+
+    if (charIndex == stringSize)
+    {
+      // possible wrapping around
+      stringSize *= 2;
+      char * copy = new char[stringSize]();
+      // TODO: handle bad allocation
+      for (int i = 0; i < charIndex; i++)
+      {
+        copy[i] = input[i];
+      }
+      delete [] input;
+      input = copy;
+    }
   }
   std::cin >> std::skipws;
 
-  std::cout << input;
+  std::cout << input << '\n';
   delete [] input;
 
   return 0;
