b7153766c1441ee279325b7fc7725901edd3a263
v-zaycev <70699650+v-zaycev@users.noreply.github.com>
2024-02-14 02:52:09 +0300

    CG fix

--- zaitsev.vladimir/T1/composite_shape.hpp
+++ zaitsev.vladimir/T1/composite_shape.hpp
@@ -5,27 +5,27 @@
 
 namespace zaitsev
 {
-	class CompositeShape
-	{
-	public:
-		CompositeShape();
-		~CompositeShape();
-		CompositeShape(const CompositeShape& other);
-		CompositeShape& operator=(const CompositeShape& other);
-		CompositeShape(CompositeShape&& other);
-		CompositeShape& operator=(CompositeShape&& other);
-		void push_back(Shape* shape);
-		void pop_back();
-		Shape& at(size_t pos);
-		const Shape& at(size_t pos) const;
-		Shape& operator[](size_t pos);
-		const Shape& operator[](size_t pos) const;
-		bool empty() const;
-		size_t size() const;
-	private:
-		size_t size_;
-		size_t capacity_;
-		Shape** shapes_;
-	};
+  class CompositeShape
+  {
+  public:
+    CompositeShape();
+    ~CompositeShape();
+    CompositeShape(const CompositeShape& other);
+    CompositeShape& operator=(const CompositeShape& other);
+    CompositeShape(CompositeShape&& other);
+    CompositeShape& operator=(CompositeShape&& other);
+    void push_back(Shape* shape);
+    void pop_back();
+    Shape& at(size_t pos);
+    const Shape& at(size_t pos) const;
+    Shape& operator[](size_t pos);
+    const Shape& operator[](size_t pos) const;
+    bool empty() const;
+    size_t size() const;
+  private:
+    size_t size_;
+    size_t capacity_;
+    Shape** shapes_;
+  };
 }
 #endif COMPOSITE_SHAPE_HPP
