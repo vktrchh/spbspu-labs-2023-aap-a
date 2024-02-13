89581ffb007a42df6ffed603c5c15dfbb5b031eb
SofiyLop <lopatina.sa@edu.spbstu.ru>
2024-02-14 00:16:36 +0300

    How about that valgrind?

--- lopatina.sofia/P3/expand_array.cpp
+++ lopatina.sofia/P3/expand_array.cpp
@@ -4,7 +4,7 @@ char * lopatina::expandArray(char * arr, size_t size)
 {
   size_t new_size = size + 1;
   char * new_arr = new char[new_size];
-  for (size_t i = 0; i < size; ++i)
+  for (size_t i = 0; i < size + 1; ++i)
   {
     new_arr[i] = arr[i];
   }
