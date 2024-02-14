542e970624978e49c5b89d7d4fca35e82c514038
puzz13 <puzz13one@gmail.com>
2024-02-14 20:31:03 +0300

    Add UNI-TWO solution

---
+++ kovtun.alexander/P3/strings_merge.cpp
@@ -0,0 +1,37 @@
+#include "strings_merge.h"
+
+char * kovtun::mergeStrings(const char * first, const char * second)
+{
+  size_t size = getStringLength(first) + getStringLength(second) + 1;
+  char * result = nullptr;
+  try
+  {
+    result = new char[size]();
+  }
+  catch (const std::bad_alloc & e)
+  {
+    std::cerr << "failed to allocate new memory for string\n";
+    throw e;
+  }
+
+  size_t index = 0;
+  while (*first != '\0' || *second != '\0')
+  {
+    if (*first != '\0')
+    {
+      result[index] = *first;
+      first++;
+      index++;
+    }
+
+    if (*second != '\0')
+    {
+      result[index] = *second;
+      second++;
+      index++;
+    }
+  }
+  result[size] = '\0';
+
+  return result;
+}
---
+++ kovtun.alexander/P3/strings_merge.h
@@ -0,0 +1,13 @@
+#ifndef STRINGS_MERGE_H
+#define STRINGS_MERGE_H
+
+#include "read_string.h"
+
+#include <iostream>
+
+namespace kovtun
+{
+  char * mergeStrings(const char * first, const char * second);
+}
+
+#endif
