9aac4d76d416085b17bb7a2dfe20c5ada99a44fb
puzz13 <puzz13one@gmail.com>
2024-02-14 20:59:39 +0300

    Add UPP-LOW part in main

--- kovtun.alexander/P3/main.cpp
+++ kovtun.alexander/P3/main.cpp
@@ -1,21 +1,35 @@
 #include "read_string.h"
+#include "convert_to_lowercase.h"
 
 #include <iostream>
 
 int main()
 {
-  char * string = nullptr;
+  char * firstString = nullptr;
+  char * loweredString = nullptr;
   try
   {
-    string = kovtun::readString(std::cin);
+    firstString = kovtun::readString(std::cin);
+    size_t firstStringLength = kovtun::getStringLength(firstString);
+
+    loweredString = new char[firstStringLength + 1]();
+    loweredString[firstStringLength] = '\0';
+    kovtun::convertToLowercase(loweredString, firstString);
   }
   catch (const std::bad_alloc & e)
   {
     std::cerr << e.what() << "\n";
     return 1;
   }
+  catch (...)
+  {
+    std::cerr << "";
+    return 1;
+  }
 
-  std::cout << string;
+  std::cout << loweredString << "\n";
+
+  delete [] loweredString;
 
   return 0;
 }
