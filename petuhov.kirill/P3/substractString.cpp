98f77975f44461fdf50df2598dae59c8eb3dbe1d
deadshumz <shumzz@bk.ru>
2024-02-14 15:17:40 +0300

    substractString

--- petuhov.kirill/P3/substractString.cpp
+++ petuhov.kirill/P3/substractString.cpp
@@ -2,5 +2,30 @@
 
 char * petuhov::substractString(const char * firstString, size_t firstStringSize, const char * secondString)
 {
-  
+  if (firstStringSize <= 0)
+  {
+    throw std::logic_error("First string size is under or equal 0");
+  }
+  char * result = new char[firstStringSize];
+  size_t resultIndex = 0;
+
+  for (int i = 0; firstString[i] != '\0'; ++i)
+  {
+    bool charFound = false;
+
+    for (int j = 0; secondString[j] != '\0'; ++j)
+    {
+      if (firstString[i] == secondString[j])
+      {
+        charFound = true;
+      }
+    }
+
+    if (!charFound)
+    {
+      result[resultIndex++] = firstString[i];
+    }
+  }
+  result[resultIndex] = '\0';
+  return result;
 }
