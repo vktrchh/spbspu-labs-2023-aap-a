057266862bf49a4bd5f1079aa7ffa2f5f7de177c
marlions <marishin.lv@edu.spbstu.ru>
2024-02-14 23:56:24 +0300

    simplified the inputshape and added changes

--- marishin.leonid/T1/inputshape.cpp
+++ marishin.leonid/T1/inputshape.cpp
@@ -1,59 +1,119 @@
 #include "inputshape.hpp"
 
+void marishin::readRectangle(std::istream& in, Shape** currentShapes, size_t& shapeCount)
+{
+  const size_t num = 4;
+  double option[num] = {};
+  for (size_t j = 0; j < num; j++)
+  {
+    in >> option[j];
+  }
+
+  if (!in)
+  {
+    cleanupShapes(currentShapes, shapeCount);
+    throw std::invalid_argument("Invalid rectangle parameters");
+  }
+  currentShapes[shapeCount] = new Rectangle({ option[0], option[1] }, { option[2], option[3] });
+}
+
+void marishin::readTriangle(std::istream& in, Shape** currentShapes, size_t& shapeCount)
+{
+  const size_t num = 6;
+  double option[num] = {};
+  for (size_t j = 0; j < num; j++)
+  {
+    in >> option[j];
+  }
+
+  if (!in)
+  {
+    cleanupShapes(currentShapes, shapeCount);
+    throw std::invalid_argument("Invalid triangle parameters");
+  }
+  currentShapes[shapeCount] = new Triangle({ option[0], option[1] }, { option[2], option[3] }, { option[4], option[5] });
+}
+
+void marishin::readRing(std::istream& in, Shape** currentShapes, size_t& shapeCount)
+{
+  const size_t num = 4;
+  double option[num] = {};
+  for (size_t j = 0; j < num; j++)
+  {
+    in >> option[j];
+  }
+
+  if (!in)
+  {
+    cleanupShapes(currentShapes, shapeCount);
+    throw std::invalid_argument("Invalid ring parameters");
+  }
+  currentShapes[shapeCount] = new Ring({ option[0], option[1] }, option[2], option[3]);
+}
+
+marishin::Shape* marishin::readShape(std::istream& in, const std::string& currentName, Shape** currentShapes, size_t& shapeCount)
+{
+  try
+  {
+    if (currentName == "RECTANGLE")
+    {
+      readRectangle(in, currentShapes, shapeCount);
+    }
+    else if (currentName == "TRIANGLE")
+    {
+      readTriangle(in, currentShapes, shapeCount);
+    }
+    else if (currentName == "RING")
+    {
+      readRing(in, currentShapes, shapeCount);
+    }
+    else
+    {
+      throw std::invalid_argument("Unknown shape type");
+    }
+  }
+  catch (const std::bad_alloc& e)
+  {
+    cleanupShapes(currentShapes, shapeCount);
+    throw;
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << '\n';
+  }
+}
+
 marishin::Shape** marishin::inputShape(std::istream& in, size_t& shapeCount)
 {
-  size_t numShapes = 3;
+  const size_t numShapes = 3;
   std::string shapeNames[numShapes] = { "RECTANGLE", "TRIANGLE", "RING" };
-  size_t shapeParametersCount[numShapes] = { 4, 6, 4 };
-  std::string currentName = "";
   marishin::Shape** currentShapes = nullptr;
   marishin::Shape** oldShapes = nullptr;
   char symbol = 0;
+  std::string currentName = "";
+
   while (in >> currentName)
   {
     for (size_t i = 0; i < numShapes; ++i)
     {
       if (currentName == shapeNames[i])
       {
-        double option[shapeParametersCount[i]]{};
-
-        for (size_t j = 0; j < shapeParametersCount[i]; j++)
-        {
-          in >> option[j];
-        }
-
-        if (!in)
-        {
-          cleanupShapes(currentShapes, shapeCount);
-          throw std::invalid_argument("Invalid arguments");
-        }
-
-        oldShapes = currentShapes;
-        currentShapes = new Shape * [shapeCount + 1];
-
-        if (oldShapes)
-        {
-          for (size_t k = 0; k < shapeCount; k++)
-          {
-            currentShapes[k] = oldShapes[k];
-          }
-        }
-        delete[] oldShapes;
-
         try
         {
-          if (currentName == "RECTANGLE")
+          marishin::Shape* newShape = readShape(in, currentName, currentShapes, shapeCount);
+
+          oldShapes = currentShapes;
+          currentShapes = new marishin::Shape * [shapeCount + 1];
+          if (oldShapes)
           {
-            currentShapes[shapeCount] = new Rectangle({ option[0], option[1] }, { option[2], option[3] });
+            for (size_t k = 0; k < shapeCount; k++)
+            {
+              currentShapes[k] = oldShapes[k];
+            }
           }
-          else if (currentName == "TRIANGLE")
-          {
-            currentShapes[shapeCount] = new Triangle({ option[0], option[1] }, { option[2], option[3] }, { option[4], option[5] });
-          }
-          else if (currentName == "RING")
-          {
-            currentShapes[shapeCount] = new Ring({ option[0], option[1] }, option[2], option[3]);
-          }
+          delete[] oldShapes;
+
+          currentShapes[shapeCount] = newShape;
           ++shapeCount;
         }
         catch (const std::bad_alloc& e)
@@ -68,14 +128,17 @@ marishin::Shape** marishin::inputShape(std::istream& in, size_t& shapeCount)
       }
     }
 
-    if (currentName == "")
-    {
-      std::cerr << "Incorrect input";
-    }
     if (currentName == "SCALE")
     {
       break;
     }
+
+    if (!in)
+    {
+      cleanupShapes(currentShapes, shapeCount);
+      throw std::logic_error("It is not shape");
+    }
+
     in >> std::noskipws;
     while (symbol != '\n')
     {
@@ -85,6 +148,7 @@ marishin::Shape** marishin::inputShape(std::istream& in, size_t& shapeCount)
   }
   return currentShapes;
 }
+
 
 void marishin::cleanupShapes(Shape** shapes, size_t count)
 {
--- marishin.leonid/T1/inputshape.hpp
+++ marishin.leonid/T1/inputshape.hpp
@@ -5,12 +5,15 @@
 #include "triangle.hpp"
 #include "ring.hpp"
 #include <iostream>
-#include <vector>
 
 namespace marishin
 {
+  void readRectangle(std::istream& in, Shape** currentShapes, size_t& shapeCount);
+  void readTriangle(std::istream& in, Shape** currentShapes, size_t& shapeCount);
+  void readRing(std::istream& in, Shape** currentShapes, size_t& shapeCount);
+  Shape* readShape(std::istream& in, const std::string& currentName, Shape** currentShapes, size_t& shapeCount);
   Shape** inputShape(std::istream& in, size_t& shapeCount);
-  void cleanupShapes(Shape** shapes, size_t count);
+  void cleanupShapes(Shape** shapes, size_t& count);
 }
 
 #endif
