e9f8a1c07e55e470e74c8601129ad5a130b2d0a0
exxiiint <filipp.sivkov@yandex.ru>
2024-02-14 16:23:38 +0300

    edit main.cpp

--- sivkov.filipp/T1/main.cpp
+++ sivkov.filipp/T1/main.cpp
@@ -21,17 +21,32 @@ int main()
     point_t center = { 0, 0 };
     std::cin >> center.x >> center.y >> k;
     std::cout << std::fixed;
+    std::cout.precision(1);
     output(std::cout, figures, count);
+    std::cout << '\n';
     for (size_t i = 0; i < count; ++i)
     {
       scale(figures[i], center, k);
     }
     output(std::cout, figures, count);
-
+    std::cout << '\n';
+    for (size_t i = 0; i < count; ++i)
+    {
+      delete figures[i];
+    }
+    delete[] figures;
   }
   catch (const std::exception& e)
   {
     std::cerr << "Exception caught: " << e.what() << std::endl;
+    if (figures != nullptr)
+    {
+      for (size_t i = 0; i < count; ++i)
+      {
+        delete figures[i];
+      }
+      delete[] figures;
+    }
   }
   return 0;
 }
