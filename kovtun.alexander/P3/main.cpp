a639b2407c8c4057301ddb39bf95a4cc63735000
puzz13 <puzz13one@gmail.com>
2024-02-14 21:16:34 +0300

    Add UNI-TWO part in main

--- kovtun.alexander/P3/main.cpp
+++ kovtun.alexander/P3/main.cpp
@@ -1,12 +1,15 @@
 #include "read_string.h"
 #include "convert_to_lowercase.h"
+#include "strings_merge.h"
 
 #include <iostream>
 
 int main()
 {
   char * firstString = nullptr;
+  char * secondString = nullptr;
   char * loweredString = nullptr;
+  char * mergedString = nullptr;
   try
   {
     firstString = kovtun::readString(std::cin);
@@ -15,6 +18,9 @@ int main()
     loweredString = new char[firstStringLength + 1]();
     loweredString[firstStringLength] = '\0';
     kovtun::convertToLowercase(loweredString, firstString);
+
+    secondString = kovtun::readString(std::cin);
+    mergedString = kovtun::mergeStrings(firstString, secondString);
   }
   catch (const std::bad_alloc & e)
   {
@@ -28,8 +34,12 @@ int main()
   }
 
   std::cout << loweredString << "\n";
+  std::cout << mergedString << "\n";
 
+  delete [] firstString;
+  delete [] secondString;
   delete [] loweredString;
+  delete [] mergedString;
 
   return 0;
 }
