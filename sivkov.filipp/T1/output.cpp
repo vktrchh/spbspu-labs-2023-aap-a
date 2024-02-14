3e1559d67ead0a7b98476debddb1467ddb7aef7b
exxiiint <filipp.sivkov@yandex.ru>
2024-02-14 15:36:32 +0300

    fix

--- sivkov.filipp/T1/output.cpp
+++ sivkov.filipp/T1/output.cpp
@@ -1,15 +1,15 @@
 #include "output.hpp"
 #include "othersFun.hpp"
 
-void output(std::ostream& out, Shape** shapes, size_t ñount)
+void output(std::ostream& out, Shape** shapes, size_t count)
 {
   double sumOfArea = 0;
-  for (size_t i = 0; i < ñount; ++i)
+  for (size_t i = 0; i < count; ++i)
   {
     sumOfArea = shapes[i]->getArea() + sumOfArea;
   }
   out << sumOfArea;
-  for (size_t i = 0; i < ñount; ++i)
+  for (size_t i = 0; i < count; ++i)
   {
     outputFrameRectInfo(out, shapes[i]);
   }
