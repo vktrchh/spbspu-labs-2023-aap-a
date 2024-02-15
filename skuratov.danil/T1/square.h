203b370a3132741671e5622a9b7c47d05f3916e1
Skuratov09 <skyratovdaniil28@gmail.com>
2024-02-16 00:32:10 +0300

    Inherited from class Shape

--- skuratov.danil/T1/square.h
+++ skuratov.danil/T1/square.h
@@ -3,6 +3,19 @@
 
 #include "shape.h"
 
-
+namespace skuratov
+{
+  class Square: public Shape
+  {
+  public:
+    Square();
+    virtual double getArea() const;
+    virtual rectangle_t getFrameRect() const;
+    virtual void move(const point_t& A);
+    virtual void move(double dx, double dy);
+    virtual void scale(double scalingFactor);
+    virtual ~Square() = default;
+  };
+}
 
 #endif
