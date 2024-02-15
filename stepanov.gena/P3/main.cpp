643f41111a52b033b4a1c46751a4e81ba2b8f99f
fncg <cld@dkstp>
2024-02-15 04:11:35 +0300

    FIX Проблемы с выводом

--- stepanov.gena/P3/main.cpp
+++ stepanov.gena/P3/main.cpp
@@ -6,8 +6,22 @@
 int main()
 {
   using namespace stepanov;
+  char* newString = nullptr;
   size_t sizeString = 20;
   char* resultString = inputArray(std::cin, sizeString);
+  try
+  {
+    newString = new char[sizeString] {};
+    for (size_t i = 0; resultString[i] != '\0'; ++i)
+    {
+      newString[i] = resultString[i];
+    }
+  }
+  catch (const std::bad_alloc& e)
+  {
+    std::cerr << "Not enough memory:" << e.what() << '\n';
+    return 1;
+  }
   if (resultString[0] == '\0')
   {
     std::cerr << "Empty string" << '\n';
@@ -15,11 +29,12 @@ int main()
     return 1;
   }
   std::cout << "String: " << resultString << '\n';
-  char* resultRemoveVowels = removeVowels(resultString);
+  char* resultRemoveVowels = removeVowels(newString);
   std::cout << "[RMV-VOW]: " << resultRemoveVowels << '\n';
   char* resultUpToLow = replaceStringToLow(resultString);
   std::cout << "[UPP-LOW]: " << resultUpToLow << '\n';
   delete[] resultString;
+  delete[] newString;
 }
 
 
