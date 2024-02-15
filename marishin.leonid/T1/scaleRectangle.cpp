2aa9baf79441a528219c2f4810ab3f6a6cdd6cc7
marlions <marishin.lv@edu.spbstu.ru>
2024-02-15 21:34:13 +0300

    namespace

--- marishin.leonid/T1/scaleRectangle.cpp
+++ marishin.leonid/T1/scaleRectangle.cpp
@@ -1,13 +1,13 @@
 #include "scaleRectangle.hpp"
 
-marishin::point_t marishin::mixPoint(marishin::point_t corner, double dx, double dy)
+marishin::point_t marishin::mixPoint(point_t corner, double dx, double dy)
 {
   corner.x += dx;
   corner.y += dy;
   return corner;
 }
 
-marishin::point_t marishin::scalePoint(marishin::point_t corner, marishin::point_t pos, double factor)
+marishin::point_t marishin::scalePoint(point_t corner, point_t pos, double factor)
 {
   corner.x = corner.x - (pos.x - corner.x) * (factor - 1);
   corner.y = corner.y - (pos.y - corner.y) * (factor - 1);
