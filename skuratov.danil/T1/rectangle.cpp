a9eb655281fb0fd93af88a810334cb322f2d7f81
Skuratov09 <skyratovdaniil28@gmail.com>
2024-02-15 22:53:22 +0300

    Add getArea and geFrameRect for Rectangle

--- skuratov.danil/T1/rectangle.cpp
+++ skuratov.danil/T1/rectangle.cpp
@@ -1,7 +1,25 @@
 #include "rectangle.h"
 #include <stdexcept>
 
-Skuratov::Rectangle::Rectangle(const point_t &lowerLeftCorner, const point_t &upperRightCorner):
+Skuratov::Rectangle::Rectangle(const point_t & lowerLeftCorner, const point_t & upperRightCorner):
   lowerLeftCorner_(lowerLeftCorner),
   upperRightCorner_(upperRightCorner)
-{}
+{
+  if (lowerLeftCorner_.x >= upperRightCorner_.x || lowerLeftCorner_.y >= upperRightCorner_.x)
+  {
+    throw std::invalid_argument("Error: incorrect rectangle parametres");
+  }
+}
+
+double skuratov::Rectangle::getArea() const
+{
+  return (upperRightCorner_.x - lowerLeftCorner_.x) * (upperRightCorner_.y - lowerLeftCorner_.y);
+}
+
+skuratov::rectangle_t skuratov::Rectangle::getFrameRect() const
+{
+  double width = upperRightCorner_.x - lowerLeftCorner_.x;
+  double height = upperRightCorner_.y - lowerLeftCorner_.y;
+  point_t B = { (lowerLeftCorner_.x + width) / 2.0, (lowerLeftCorner_.y + height) / 2.0 };
+  return { width, height, B };
+}
--- skuratov.danil/T1/rectangle.h
+++ skuratov.danil/T1/rectangle.h
@@ -5,7 +5,7 @@
 
 namespace skuratov
 {
-  class Rectangle : public Shape
+  class Rectangle: public Shape
   {
   public:
     Rectangle(const point_t &lowerLeftCorner, const point_t &upperRightCorner);
@@ -14,6 +14,7 @@ namespace skuratov
     virtual void move(const point_t& A);
     virtual void move(double dx, double dy);
     virtual void scale(double scalingFactor);
+    virtual ~Rectangle() = default;
 
   private:
     point_t lowerLeftCorner_;
