dbf17955feee7f8cf37a7e8fdacc36981841a657
Skuratov09 <skyratovdaniil28@gmail.com>
2024-02-16 23:41:58 +0300

    Add getArea and getFrameRect for square

--- skuratov.danil/T1/square.cpp
+++ skuratov.danil/T1/square.cpp
@@ -5,4 +5,20 @@
 skuratov::Square::Square(const point_t& bottomLeftCorner, double sideLength):
   bottomLeftCorner_(bottomLeftCorner),
   sideLength_(sideLength)
-{}
+{
+  if (sideLength <= 0)
+  {
+    throw std::invalid_argument("Error: incorrect square parameters");
+  }
+}
+
+double skuratov::Square::getArea() const
+{
+  return sideLength_ * sideLength_;
+}
+
+skuratov::rectangle_t skuratov::Square::getFrameRect() const
+{
+  point_t core = { bottomLeftCorner_.x + (sideLength_ / 2), bottomLeftCorner_.y + (sideLength_ / 2) };
+  return { sideLength_, sideLength_, core };
+}
