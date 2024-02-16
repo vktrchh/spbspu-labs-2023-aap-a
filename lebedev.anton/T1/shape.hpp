26d97af01458c19d2c3735fd02e5766ab6ac4c62
IastWish <antony_lebedevv@mail.ru>
2024-02-17 00:19:43 +0300

    add class Rectangle

--- lebedev.anton/T1/shape.hpp
+++ lebedev.anton/T1/shape.hpp
@@ -8,12 +8,12 @@ namespace lebedev
   class Shape
   {
   public:
+    virtual ~Shape() = default;
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
-    virtual void move(const point_t & t) = 0;
+    virtual void move(const point_t & p) = 0;
     virtual void move(double dx, double dy) = 0;
     virtual void scale(double ratio) = 0;
-    virtual ~Shape() = default;
   };
 }
 
