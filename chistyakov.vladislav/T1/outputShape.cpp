e2ad86f10de351a4b23a3e9eab752892bb83bf9c
jiggerman <liggerman@mail.ru>
2024-02-18 21:35:59 +0300

    Refresh foo

--- chistyakov.vladislav/T1/outputShape.cpp
+++ chistyakov.vladislav/T1/outputShape.cpp
@@ -1,20 +1,20 @@
 #include "outputShape.hpp"
 
-std::ostream & chistyakov::outputShapes(std::ostream & output, const Shape * const shapes, size_t size)
+void chistyakov::outputShapes(std::ostream & output, const Shape * const * shapes, size_t size)
 {
-  if (size == 0)
-  {
-    throw std::invalid_argument("The list of shapes is empty");
-  }
   output << std::fixed;
 
   double fullArea = 0.0;
   for (size_t i = 0; i < size; ++i)
   {
-    fullArea += shapes[i]->getArea();
+    if (shapes[i] == nullptr)
+    {
+      break;
+    }
+    fullArea += shapes[i].getArea();
   }
 
-  output << fullArea;
+  output << fullArea << "\n";
 
   point_t downLeft = { 0.0, 0.0 };
   point_t topRight = { 0.0, 0.0 };
@@ -22,6 +22,10 @@ std::ostream & chistyakov::outputShapes(std::ostream & output, const Shape * con
 
   for (size_t i = 0; i < size; ++i)
   {
+    if (shapes[i] == nullptr)
+    {
+      break;
+    }
     rectangle = shapes[i]->getFrameRect();
     downLeft = {rectangle.pos.x - rectangle.width / 2.0, rectangle.pos.y - rectangle.height / 2.0};
     topRight = {rectangle.pos.x + rectangle.width / 2.0, rectangle.pos.y + rectangle.height / 2.0};
