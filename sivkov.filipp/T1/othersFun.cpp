7702457fb9365e8e66b820b523a48af964f517b9
exxiiint <filipp.sivkov@yandex.ru>
2024-02-18 22:10:02 +0300

    rly last fix ))0)

--- sivkov.filipp/T1/othersFun.cpp
+++ sivkov.filipp/T1/othersFun.cpp
@@ -9,6 +9,7 @@ namespace sivkov
 {
   void outputFrameRectInfo(std::ostream& out, Shape* shape)
   {
+    out << " ";
     out << shape->getFrameRect().pos.x - shape->getFrameRect().width / 2 << " ";
     out << shape->getFrameRect().pos.y - shape->getFrameRect().height / 2 << " ";
     out << shape->getFrameRect().pos.x + shape->getFrameRect().width / 2 << " ";
