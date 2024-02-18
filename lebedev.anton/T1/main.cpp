36df609237923e23b7d40185ff72da99c7a8c2da
IastWish <antony_lebedevv@mail.ru>
2024-02-18 20:55:26 +0300

    Add input and output functions

--- lebedev.anton/T1/main.cpp
+++ lebedev.anton/T1/main.cpp
@@ -1,4 +1,36 @@
 #include <iostream>
+#include <iomanip>
+#include "input_output.hpp"
+#include "rectangle.hpp"
+#include "parallelogram.hpp"
+#include "diamond.hpp"
+#include "complexquad.hpp"
 
 int main()
-{}
+{
+  using namespace lebedev;
+  Shape ** shapes = nullptr;
+  size_t shapes_count = 0;
+  point_t point = {0, 0};
+  double ratio = 0.0;
+  try
+  {
+    shapes = inputShapes(std::cin, shapes_count);
+    std::cout << std::setprecision(1) << std::fixed;
+    outputShapes(std::cout, shapes, shapes_count);
+    std::cout << '\n';
+  }
+  catch (const std::bad_alloc &)
+  {
+    std::cerr << "Error with memory allocation\n";
+    return 1;
+  }
+  catch (const std::exception & e)
+  {
+    std::cerr << e.what() << '\n';
+    clearShapes(shapes, shapes_count);
+    return 1;
+  }
+  clearShapes(shapes, shapes_count);
+  return 0;
+}
