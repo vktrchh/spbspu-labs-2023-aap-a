d0cd59a6fcf416dfc40d49df4f467b6fe3b02ebe
IastWish <antony_lebedevv@mail.ru>
2024-02-18 21:35:26 +0300

    Add isotrophic scaling

--- lebedev.anton/T1/main.cpp
+++ lebedev.anton/T1/main.cpp
@@ -5,16 +5,34 @@
 #include "parallelogram.hpp"
 #include "diamond.hpp"
 #include "complexquad.hpp"
+#include "iso_scale.hpp"
 
 int main()
 {
   using namespace lebedev;
   Shape ** shapes = nullptr;
-  size_t shapes_count = 0;;
+  size_t shapes_count = 0;
+  point_t point = {0, 0};
+  double ratio = 0.0;
   try
   {
     shapes = inputShapes(std::cin, shapes_count);
     std::cout << std::setprecision(1) << std::fixed;
+    outputShapes(std::cout, shapes, shapes_count);
+    std::cout << '\n';
+    std::cin >> point.x >> point.y >> ratio;
+    if (!std::cin)
+    {
+      throw std::invalid_argument("Error: wrong input in scale");
+    }
+    if (ratio <= 0)
+    {
+      throw std::invalid_argument("Error: scaling ratio must be greater than zero");
+    }
+    for (size_t i = 0; i < shapes_count; i++)
+    {
+      isoScale(shapes[i], point, ratio);
+    }
     outputShapes(std::cout, shapes, shapes_count);
     std::cout << '\n';
   }
