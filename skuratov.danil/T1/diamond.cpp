7aa02f58d31df1d748eb7c0b7616d196150f7f7e
Skuratov09 <skyratovdaniil28@gmail.com>
2024-02-18 21:49:26 +0300

    Added check and tninking about getArea

--- skuratov.danil/T1/diamond.cpp
+++ skuratov.danil/T1/diamond.cpp
@@ -2,8 +2,19 @@
 
 #include <stdexcept>
 
-skuratov::Diamond::Diamond(const point_t& corner1, const point_t& corner2, const point_t& corner3):
+skuratov::Diamond::Diamond(const point_t& corner1, const point_t& corner2, const point_t& corner3) :
   corner1_(corner1),
   corner2_(corner2),
   corner3_(corner3)
+{
+  if (!((corner1_.x == corner2_.x && corner2_.y == corner3_.y)
+    || (corner1_.x == corner3_.x && corner1_.y == corner2_.y)
+    || (corner1_.y == corner2_.y && corner2_.x == corner3_.x)))
+  {
+    throw std::invalid_argument("Error: diagonals of the diamond must be parallel to the coordinate axes");
+  }
+}
+
+double skuratov::Diamond::getArea() const
 {}
+
