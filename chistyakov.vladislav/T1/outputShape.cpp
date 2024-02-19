3ed2e9ecd0cb2bf5c23aba7628d61f29f30f7ea6
jiggerman <liggerman@mail.ru>
2024-02-19 11:55:44 +0300

    Add check if list empty

--- chistyakov.vladislav/T1/outputShape.cpp
+++ chistyakov.vladislav/T1/outputShape.cpp
@@ -2,6 +2,11 @@
 
 void chistyakov::outputShape(std::ostream & output, const Shape * const * shapes, size_t size)
 {
+  if (size == 0)
+  {
+    throw std::logic_error("The list of shapes is empty!");
+  }
+
   output << std::fixed;
   output.precision(1);
 
@@ -23,10 +28,6 @@ void chistyakov::outputShape(std::ostream & output, const Shape * const * shapes
 
   for (size_t i = 0; i < size; ++i)
   {
-    if (shapes[i] == nullptr)
-    {
-      break;
-    }
     rectangle = shapes[i]->getFrameRect();
     downLeft = {rectangle.pos.x - rectangle.width / 2.0, rectangle.pos.y - rectangle.height / 2.0};
     topRight = {rectangle.pos.x + rectangle.width / 2.0, rectangle.pos.y + rectangle.height / 2.0};
