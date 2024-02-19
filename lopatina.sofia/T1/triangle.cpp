bdcdc5844e40d3874ff7b73a1750ef09a4008fb3
SofiyLop <lopatina.sa@edu.spbstu.ru>
2024-02-19 17:10:04 +0300

    Fix triangle

--- lopatina.sofia/T1/triangle.cpp
+++ lopatina.sofia/T1/triangle.cpp
@@ -12,14 +12,6 @@ lopatina::Triangle::Triangle(point_t point1, point_t point2, point_t point3)
   p3_.y_ = point3.y_;
   pos_tri_.x_ = (point1.x_ + point2.x_ + point3.x_) / 3.0;
   pos_tri_.y_ = (point1.y_ + point2.y_ + point3.y_) / 3.0;
-  double max_x = std::max({point1.x_, point2.x_, point3.x_});
-  double min_x = std::min({point1.x_, point2.x_, point3.x_});
-  double max_y = std::max({point1.y_, point2.y_, point3.y_});
-  double min_y = std::min({point1.y_, point2.y_, point3.y_});
-  height_tri_ = max_y - min_y;
-  width_tri_ = max_x - min_x;
-  pos_.x_ = min_x + (width_tri_ / 2.0);
-  pos_.y_ = min_y + (height_tri_ / 2.0);
 }
 
 double lopatina::Triangle::getArea()
@@ -29,6 +21,13 @@ double lopatina::Triangle::getArea()
 
 lopatina::rectangle_t lopatina::Triangle::getFrameRect()
 {
+  double max_x = std::max({p1_.x_, p2_.x_, p3_.x_});
+  double min_x = std::min({p1_.x_, p2_.x_, p3_.x_});
+  double max_y = std::max({p1_.y_, p2_.y_, p3_.y_});
+  double min_y = std::min({p1_.y_, p2_.y_, p3_.y_});
+  double height_tri_ = max_y - min_y;
+  double width_tri_ = max_x - min_x;
+  point_t pos_ = {min_x + (width_tri_ / 2.0), min_y + (height_tri_ / 2.0)};
   rectangle_t newRect{height_tri_, width_tri_, pos_};
   return newRect;
 }
@@ -44,8 +43,6 @@ void lopatina::Triangle::move(double dx, double dy)
 {
   pos_tri_.x_ += dx;
   pos_tri_.y_ += dy;
-  pos_.x_ += dx;
-  pos_.y_ += dy;
   p1_.x_ += dx;
   p1_.y_ += dy;
   p2_.x_ += dx;
@@ -58,8 +55,6 @@ void lopatina::Triangle::scale(point_t new_pos, double k)
 {
   point_t old_pos{pos_tri_.x_, pos_tri_.y_};
   move(new_pos);
-  height_tri_ = height_tri_ * k;
-  width_tri_ = width_tri_ * k;
   p1_.x_ = pos_tri_.x_ + (p1_.x_ - pos_tri_.x_) * k;
   p1_.y_ = pos_tri_.y_ + (p1_.y_ - pos_tri_.y_) * k;
   p2_.x_ = pos_tri_.x_ + (p2_.x_ - pos_tri_.x_) * k;
