7f70cb6033e1132cabbaab88e5766273089d69c7
jiggerman <liggerman@mail.ru>
2024-02-19 12:09:48 +0300

    Add check if ratio <= 0

--- chistyakov.vladislav/T1/isotropScale.cpp
+++ chistyakov.vladislav/T1/isotropScale.cpp
@@ -2,6 +2,11 @@
 
 void chistyakov::isotropScale(Shape ** shapes, const point_t & point, const double ratio, const size_t size)
 {
+  if (ratio <= 0)
+  {
+    throw std::invalid_argument("Scale coefficient should be a positive real number.");
+  }
+
   for (size_t i = 0; i < size; ++i)
   {
     point_t startPoint = shapes[i]->getFrameRect().pos;
