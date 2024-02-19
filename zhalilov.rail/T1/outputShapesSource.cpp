0312ae6c471fbc69275d8385abc7222661ba1681
Rail <ksovfox@mail.ru>
2024-02-19 11:05:33 +0300

    Removed shapes checking

--- zhalilov.rail/T1/outputShapesSource.cpp
+++ zhalilov.rail/T1/outputShapesSource.cpp
@@ -17,17 +17,14 @@ std::ostream &zhalilov::outputShapesSource(const Shape * const *shapes, size_t s
   output << square;
   for (size_t i = 0; i < size; i++)
   {
-    if (shapes[i])
-    {
-      double width = shapes[i]->getFrameRect().width;
-      double height = shapes[i]->getFrameRect().height;
-      double posX = shapes[i]->getFrameRect().pos.x;
-      double posY = shapes[i]->getFrameRect().pos.y;
-      point_t leftCorner = { posX - width / 2.0, posY - height / 2.0 };
-      point_t rightCorner = { posX + width / 2.0, posY + height / 2.0 };
-      output << " " << leftCorner.x << " " << leftCorner.y;
-      output << " " << rightCorner.x << " " << rightCorner.y;
-    }
+    double width = shapes[i]->getFrameRect().width;
+    double height = shapes[i]->getFrameRect().height;
+    double posX = shapes[i]->getFrameRect().pos.x;
+    double posY = shapes[i]->getFrameRect().pos.y;
+    point_t leftCorner = { posX - width / 2.0, posY - height / 2.0 };
+    point_t rightCorner = { posX + width / 2.0, posY + height / 2.0 };
+    output << " " << leftCorner.x << " " << leftCorner.y;
+    output << " " << rightCorner.x << " " << rightCorner.y;
   }
   return output;
 }
