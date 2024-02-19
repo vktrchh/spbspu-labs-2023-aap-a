ecf4c5b5fbb104b3b032688d877c6378f21dcab9
Skuratov09 <skyratovdaniil28@gmail.com>
2024-02-20 02:12:20 +0300

    That's it for now, I'll finish it tomorrow

--- skuratov.danil/T1/main.cpp
+++ skuratov.danil/T1/main.cpp
@@ -23,15 +23,30 @@ int main()
     std::string type;
     iss >> type;
     if (type == "DIAMOND")
-    {}
+    {
+      double x1, y1, x2, y2, x3, y3;
+      iss >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+      std::unique_ptr<Diamond> diamond(new Diamond(point_t{ x1, y1 }, point_t{ x2, y2 }, point_t{ x3, y3 }));
+      shapes.push_back(std::move(diamond));
+    }
     else if (type == "RECTANGLE")
-    {}
+    {
+      double x, y, width, height;
+      iss >> x >> y >> width >> height;
+      std::unique_ptr<Rectangle> rectangle(new Rectangle(point_t{ x, y }, point_t{ x + width, y + height }));
+      shapes.push_back(std::move(rectangle));
+    }
     else if (type == "SQUARE")
-    {}
+    {
+      double x, y, sideLength;
+      iss >> x >> y >> sideLength;
+      std::unique_ptr<Square> square(new Square(point_t{ x, y }, sideLength));
+      shapes.push_back(std::move(square));
+    }
   }
   for (const auto& shape: shapes)
   {
-    std::cout << "Area: " << shape->getArea() << std::endl;
+    std::cout << "Area: " << shape->getArea() << '\n';
     rectangle_t frameRect = shape->getFrameRect();
     std::cout << "Frame Rectangle: width =" << frameRect.width << ", height =" << frameRect.height << ", pos = ("
       << frameRect.pos.x << ", " << frameRect.pos.y << ")" << '\n';
