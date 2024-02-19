1760a2da46a7d4087691bdb40a8a8440543a077b
Skuratov09 <skyratovdaniil28@gmail.com>
2024-02-20 00:08:21 +0300

    Add getArea for diamond and other

--- skuratov.danil/T1/diamond.cpp
+++ skuratov.danil/T1/diamond.cpp
@@ -1,6 +1,7 @@
 #include "diamond.h"
 
 #include <stdexcept>
+#include <cmath>
 
 skuratov::Diamond::Diamond(const point_t& corner1, const point_t& corner2, const point_t& corner3) :
   corner1_(corner1),
@@ -16,5 +17,25 @@ skuratov::Diamond::Diamond(const point_t& corner1, const point_t& corner2, const
 }
 
 double skuratov::Diamond::getArea() const
+{
+  double diagonal1 = std::sqrt(std::pow(corner1_.x - corner3_.x, 2) + std::pow(corner1_.y - corner3_.y, 2));
+  double diagonal2 = std::sqrt(std::pow(corner1_.x - corner2_.x, 2) + std::pow(corner1_.y - corner2_.y, 2));
+  return (diagonal1 * diagonal2) / 2;
+}
+
+skuratov::rectangle_t skuratov::Diamond::getFrameRect() const
 {}
 
+void skuratov::Diamond::move(const point_t& A)
+{}
+
+void skuratov::Diamond::move(double dx, double dy)
+{}
+
+void skuratov::Diamond::scale(double scalingFactor)
+{
+  if (scalingFactor <= 0)
+  {
+    throw std::invalid_argument("Error: scale factor of diamond should be a positive");
+  }
+}
