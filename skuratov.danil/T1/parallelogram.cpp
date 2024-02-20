9be0db8ae47b2a48dbd5d6820fa04fbcab522a20
Skuratov09 <skyratovdaniil28@gmail.com>
2024-02-20 13:53:02 +0300

    Add getArea and getFrameRect for parallelogram

--- skuratov.danil/T1/parallelogram.cpp
+++ skuratov.danil/T1/parallelogram.cpp
@@ -1,6 +1,22 @@
 #include "parallelogram.h"
 
 #include <stdexcept>
+#include <cmath>
+#include <algorithm>
+
+skuratov::point_t findCenOfParal(const skuratov::point_t& point1, const skuratov::point_t& point2, const skuratov::point_t& point3)
+{
+  skuratov::point_t mid1 = { (point1.x + point2.x) / 2, (point1.y + point2.y) / 2 };
+  skuratov::point_t mid2 = { (point2.x + point3.x) / 2, (point2.y + point3.y) / 2 };
+
+  double slope1 = (point1.y - point3.y) / (point1.x - point3.x);
+  double slope2 = (point2.y - point3.y) / (point2.x - point3.x);
+
+  skuratov::point_t center = {};
+  center.x = (slope1 * mid1.x - slope2 * mid2.x + mid2.y - mid1.y) / (slope1 - slope2);
+  center.y = slope1 * (center.x - mid1.x) + mid1.y;
+  return center;
+}
 
 skuratov::Parallelogram::Parallelogram(const point_t& point1, const point_t& point2, const point_t& point3):
   point1_(point1),
@@ -16,10 +32,20 @@ skuratov::Parallelogram::Parallelogram(const point_t& point1, const point_t& poi
 }
 
 double skuratov::Parallelogram::getArea() const
-{}
+{
+  double side1 = std::sqrt(std::pow(point2_.x - point1_.x, 2) + std::pow(point2_.y - point1_.y, 2));
+  double side2 = std::sqrt(std::pow(point3_.x - point1_.x, 2) + std::pow(point3_.y - point1_.y, 2));
+  return side1 * side2;
+}
 
 skuratov::rectangle_t skuratov::Parallelogram::getFrameRect() const
-{}
+{
+  double width = std::abs(point2_.x - point3_.x) + std::abs(point1_.x - point2_.x);
+  double height = (point1_.y == point2_.y) * std::abs(point2_.y - point3_.y) + (point1_.y != point2_.y) * std::abs(point1_.y - point2_.y);
+  point_t center = findCenOfParal(point1_, point2_, point3_);
+  return { width, height, center };
+
+}
 
 void skuratov::Parallelogram::move(const point_t& A)
 {}
