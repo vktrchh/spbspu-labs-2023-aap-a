2e43f2c1f492797b538260c71b4a64c0d3c989b3
seep-code <seepox7@gmail.com>
2024-02-20 23:28:25 +0300

    long line

--- ishmuratov.eric/T1/input.cpp
+++ ishmuratov.eric/T1/input.cpp
@@ -45,26 +45,26 @@ ishmuratov::Shape * ishmuratov::initShape(const std::string & shapeName, std::is
   const char * shapeNames[4] = {"RECTANGLE", "TRIANGLE", "DIAMOND", "CONCAVE"};
   for (size_t i = 0; i < 4; ++i)
   {
-    double pointArray[8]{};
+    double pA[8]{};
     if (shapeName == shapeNames[0])
     {
-      inputPoint(input, pointArray, 4);
-      return new Rectangle({ pointArray[0], pointArray[1] }, { pointArray[2], pointArray[3] });
+      inputPoint(input, pA, 4);
+      return new Rectangle({ pA[0], pA[1] }, { pA[2], pA[3] });
     }
     else if (shapeName == shapeNames[1])
     {
-      inputPoint(input, pointArray, 6);
-      return new Triangle({ pointArray[0], pointArray[1] }, { pointArray[2], pointArray[3] }, { pointArray[4], pointArray[5] });
+      inputPoint(input, pA, 6);
+      return new Triangle({ pA[0], pA[1] }, { pA[2], pA[3] }, { pA[4], pA[5] });
     }
     else if (shapeName == shapeNames[2])
     {
-      inputPoint(input, pointArray, 6);
-      return new Diamond({ pointArray[0], pointArray[1] }, { pointArray[2], pointArray[3] }, { pointArray[4], pointArray[5] });
+      inputPoint(input, pA, 6);
+      return new Diamond({ pA[0], pA[1] }, { pA[2], pA[3] }, { pA[4], pA[5] });
     }
     else if (shapeName == shapeNames[3])
     {
-      inputPoint(input, pointArray, 8);
-      return new Concave({ pointArray[0], pointArray[1] }, { pointArray[2], pointArray[3] }, { pointArray[4], pointArray[5] }, { pointArray[6], pointArray[7] });
+      inputPoint(input, pA, 8);
+      return new Concave({ pA[0], pA[1] }, { pA[2], pA[3] }, { pA[4], pA[5] }, { pA[6], pA[7] });
     }
     else
     {
