08f5061c6dce78d9c8504caaa369c350631c3982
Z0ltaan <gregnov69@gmail.com>
2024-02-21 00:19:20 +0300

    outpur results fix

--- isaychev.vladislav/T1/outputResults.cpp
+++ isaychev.vladislav/T1/outputResults.cpp
@@ -2,18 +2,16 @@
 #include <cmath>
 #include <iostream>
 #include <iomanip>
-#include "regular.hpp"
 
 void isaychev::outputResults(std::ostream & out, const CompositeShape & cShape)
 {
-  out << std::fixed;
-  out << std::setprecision(1) << cShape.getArea();
+  out << cShape.getArea();
   for (size_t j = 0; j < cShape.size(); j++)
   {
     rectangle_t rect = cShape[j].getFrameRect();
-    out << std::setprecision(1) << " " << std::round((rect.pos.x - (rect.width / 2)) * 10) / 10;
-    out << std::setprecision(1) << " " << std::round((rect.pos.y - (rect.height / 2)) * 10) / 10;
-    out << std::setprecision(1) << " " << std::round((rect.pos.x + (rect.width / 2)) * 10) / 10;
-    out << std::setprecision(1) << " " << std::round((rect.pos.y + (rect.height / 2)) * 10) / 10;
+    out << " " << std::round((rect.pos.x - (rect.width / 2)) * 10) / 10;
+    out << " " << std::round((rect.pos.y - (rect.height / 2)) * 10) / 10;
+    out << " " << std::round((rect.pos.x + (rect.width / 2)) * 10) / 10;
+    out << " " << std::round((rect.pos.y + (rect.height / 2)) * 10) / 10;
   }
 }
