9a79efe954601d773b70bead936ff92733702946
Skuratov09 <skyratovdaniil28@gmail.com>
2024-02-20 13:29:56 +0300

    Doing parallelogram

--- skuratov.danil/T1/parallelogram.cpp
+++ skuratov.danil/T1/parallelogram.cpp
@@ -7,5 +7,30 @@ skuratov::Parallelogram::Parallelogram(const point_t& point1, const point_t& poi
   point2_(point2),
   point3_(point3)
 {
+  if ((point1_.x != point2_.x || point1_.y != point2_.y)
+    || (point1_.x != point3_.x || point1_.y != point3_.y)
+    || (point2_.x != point3_.x || point2_.y != point3_.y))
+  {
+    throw std::invalid_argument("All sides of the parallelogram must be parallel to the coordinate axes!");
+  }
+}
 
+double skuratov::Parallelogram::getArea() const
+{}
+
+skuratov::rectangle_t skuratov::Parallelogram::getFrameRect() const
+{}
+
+void skuratov::Parallelogram::move(const point_t& A)
+{}
+
+void skuratov::Parallelogram::move(double dx, double dy)
+{}
+
+void skuratov::Parallelogram::scale(double scalingFactor)
+{
+  if (scalingFactor <= 0)
+  {
+    throw std::invalid_argument("Error: scale factor of parallelogram should be a positive");
+  }
 }
