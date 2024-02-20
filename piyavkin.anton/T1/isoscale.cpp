b4170caee8e18a848dd64d1052e9c3bd8fae6286
Piyavva <piyavkin05@bk.ru>
2024-02-20 21:23:32 +0300

    Fixed CG

--- piyavkin.anton/T1/isoscale.cpp
+++ piyavkin.anton/T1/isoscale.cpp
@@ -1,18 +1,16 @@
 #include "isoscale.hpp"
 #include <stdexcept>
 
-namespace piyavkin
+void piyavkin::isoScale(Shape* shape, const point_t& center, const double k)
 {
-  void isoScale(Shape* shape, const point_t& center, const double k)
+  if (k < 0)
   {
-    if (k < 0)
-    {
-      throw std::invalid_argument("Сoefficient less than 0");
-    }
-    point_t pos = shape->getFrameRect().pos;
-    shape->move(center);
-    point_t newPos = shape->getFrameRect().pos;
-    shape->scale(k);
-    shape->move(k * (pos.x - newPos.x), k * (pos.y - newPos.y));
+    throw std::invalid_argument("Сoefficient less than 0");
   }
+  point_t pos = shape->getFrameRect().pos;
+  shape->move(center);
+  point_t newPos = shape->getFrameRect().pos;
+  shape->scale(k);
+  shape->move(k * (pos.x - newPos.x), k * (pos.y - newPos.y));
 }
+
