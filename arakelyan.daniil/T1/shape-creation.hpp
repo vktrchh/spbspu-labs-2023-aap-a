68f14a94a8783cc1943cc5d7bc522b92f6da3e4c
osakasonyboy@gmail.com <sensh0god@gmail.com>
2024-02-20 21:34:48 +0300

    forgot add this file

--- arakelyan.daniil/T1/shape-creation.hpp
+++ arakelyan.daniil/T1/shape-creation.hpp
@@ -7,10 +7,10 @@
 
 namespace arakelyan
 {
-  void defineAndCreateShape(Shape **myShapes, const size_t shapesCount, const char * sting);
+  void defineAndCreateShape(Shape **myShapes, size_t shapesCount, const char * sting);
   Shape * createPar(const char * string);
   Shape * createRect(const char * string);
   Shape * createDiam(const char * string);
-  double * extractDataForShape(const char * string, double coordStorage[], const size_t pointsCount, const size_t wordLen);
+  double * extractDataForShape(const char * string, double coordStorage[], size_t pointsCount, size_t wordLen);
 }
 #endif
