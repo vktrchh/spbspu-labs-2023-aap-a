6d4b82a433516044928f309007bca3c46e1f8f52
dmitei <dima9990860349@icloud.com>
2024-02-21 17:12:02 +0300

    leak fixing

--- shabalin.dmitrij/P4/main.cpp
+++ shabalin.dmitrij/P4/main.cpp
@@ -101,6 +101,11 @@ int main(int argc, char *argv[])
   catch (const std::exception &e)
   {
     std::cerr << e.what() << '\n';
+    if (task == 2)
+    {
+      delete[] matrix;
+      delete[] smoothedMatrix;
+    }
     return 1;
   }
 
