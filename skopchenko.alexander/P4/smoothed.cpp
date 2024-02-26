8ad9fbe14d5e751db8dd1d93fd64d1ac6545da8e
polykisa <s.sanek2005@gmail.com>
2024-02-26 10:18:01 +0300

    chipi chipi chapa chapa

---
+++ skopchenko.alexander/P4/smoothed.cpp
@@ -0,0 +1,58 @@
+#include "smoothed.h"
+
+void skopchenko::smoothedMatrix(int *matrix, double *smoothMatrix, size_t rows, size_t cols) {
+  for (size_t i = 0; i < rows; ++i)
+  {
+    for (size_t j = 0; j < cols; ++j)
+    {
+      size_t count = 0;
+      double sum = 0;
+      if (rows == 1 && cols == 1)
+      {
+        return;
+      }
+      if (i > 0)
+      {
+        sum += matrix[(i - 1) * cols + j];
+        count++;
+      }
+      if (i < rows - 1)
+      {
+        sum += matrix[(i + 1) * cols + j];
+        count++;
+      }
+      if (j > 0)
+      {
+        sum += matrix[i * cols + (j - 1)];
+        count++;
+      }
+      if (j < cols - 1)
+      {
+        sum += matrix[i * cols + (j + 1)];
+        count++;
+      }
+      if (i > 0 && j > 0)
+      {
+        sum += matrix[(i - 1) * cols + (j - 1)];
+        count++;
+      }
+      if (i > 0 && j < cols - 1)
+      {
+        sum += matrix[(i - 1) * cols + (j + 1)];
+        count++;
+      }
+      if (i < rows - 1 && j > 0)
+      {
+        sum += matrix[(i + 1) * cols + (j - 1)];
+        count++;
+      }
+      if (i < rows - 1 && j < cols - 1)
+      {
+        sum += matrix[(i + 1) * cols + (j + 1)];
+        count++;
+      }
+      smoothMatrix[i * cols + j] = sum / count;
+    }
+  }
+}
+
