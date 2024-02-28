223c448d01d90c6dd6a35bc970bc5e29377048f9
Gorgor777 <sergei-agarkov07@mail.ru>
2024-02-28 11:29:48 +0300

    make input

--- agarkov.sergey/P4/main.cpp
+++ agarkov.sergey/P4/main.cpp
@@ -1,4 +1,16 @@
-int main()
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char* argv[])
 {
+  if (argc != 4)
+  {
+    std::cerr << "Incorrect arg\n";
+    return 1;
+  }
+
+  std::ifstream input_file(argv[2]);
+  std::ofstream output_file(argv[3]);
+
   return 0;
 }
