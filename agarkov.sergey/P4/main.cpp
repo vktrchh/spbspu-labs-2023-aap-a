a8678d6bac8d5fdd9846bbcd2e6c9817b64bcb41
Gorgor777 <sergei-agarkov07@mail.ru>
2024-02-28 12:01:11 +0300

    ckech file

--- agarkov.sergey/P4/main.cpp
+++ agarkov.sergey/P4/main.cpp
@@ -11,6 +11,15 @@ int main(int argc, char* argv[])
 
   std::ifstream input_file(argv[2]);
   std::ofstream output_file(argv[3]);
-
+  if (!input_file.is_open())
+  {
+    std::cerr << "Error input file\n";
+    return 1;
+  }
+  if (!output_file.is_open())
+  {
+    std::cerr << "Error output file\n";
+    return 1;
+  }
   return 0;
 }
