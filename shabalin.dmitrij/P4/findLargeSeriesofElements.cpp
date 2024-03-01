7a93535ad6ecdce07503e324a9624bb3b4bf0e69
dmitei <dima9990860349@icloud.com>
2024-03-01 15:57:30 +0300

    null matrix UB fix

--- shabalin.dmitrij/P4/findLargeSeriesofElements.cpp
+++ shabalin.dmitrij/P4/findLargeSeriesofElements.cpp
@@ -3,24 +3,26 @@
 
 size_t shabalin::findLargeSeriesOfEqualElements(const int *matrix, size_t rows, size_t cols)
 {
+  if (rows == 0 && cols == 0)
+  {
+    return 0;
+  }
+
   size_t maximumNumberSeries = 0;
   size_t numberSeries = 0;
   size_t rowNumber = 0;
 
-  for (size_t row = 0; row < rows; ++row)
-  {
-    for (size_t col = 0; col < cols - 1; ++col)
+  for (size_t i = 0; i < rows * cols - 1; ++i)
     {
-      if (matrix[row * cols + col] == matrix[row * cols + col + 1])
+      if (matrix[i] == matrix[i + 1])
       {
         numberSeries += 1;
       }
       if (numberSeries > maximumNumberSeries)
       {
         maximumNumberSeries = numberSeries;
-        rowNumber = row + 1;
+        rowNumber = (i / cols) + 1;
       }
     }
-  }
   return rowNumber;
 }
