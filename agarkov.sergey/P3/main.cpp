e322829d4c2f046a86233f43d405b5d1ad80877c
Gorgor777 <sergei-agarkov07@mail.ru>
2024-03-02 22:44:16 +0300

    try make str input

--- agarkov.sergey/P3/main.cpp
+++ agarkov.sergey/P3/main.cpp
@@ -1,4 +1,17 @@
+#include <iostream>
+
 int main()
 {
+  char a[100];
+  int count = 0;
+  while (std::cin >> a[count])
+  {
+    count++;
+  }
+  a[count+1] = '\0';
+  for (int i = 0; a[i] != '\0'; ++i)
+  {
+    std::cout << a[i];
+  }
   return 0;
 }
