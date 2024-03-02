5ab8d26fec8c590c6852892c0401fd2ad55545b8
Gorgor777 <sergei-agarkov07@mail.ru>
2024-03-02 22:18:46 +0300

    fix header guard, use std::min

--- agarkov.sergey/P4/transformMatrixAPeriph.cpp
+++ agarkov.sergey/P4/transformMatrixAPeriph.cpp
@@ -1,21 +1,5 @@
 #include "transformMatrixAPeriph.hpp"
-
-int getMinRange(int a, int b, int c, int d)
-{
-  if (b < a)
-  {
-    a = b;
-  }
-  if (c < a)
-  {
-    a = c;
-  }
-  if (d < a)
-  {
-    a = d;
-  }
-  return a;
-}
+#include <algorithm>
 
 void agarkov::transformMatrixAPeriph(int* array, size_t rows, size_t cols)
 {
@@ -23,7 +7,7 @@ void agarkov::transformMatrixAPeriph(int* array, size_t rows, size_t cols)
   {
     for (size_t j = 0; j < cols; j++)
     {
-      array[i * cols + j] += getMinRange(i + 1, j + 1, rows - i, cols - j);
+      array[i * cols + j] += std::min({i + 1, j + 1, rows - i, cols - j});
     }
   }
 }
