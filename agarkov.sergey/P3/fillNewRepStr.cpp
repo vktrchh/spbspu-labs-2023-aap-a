4aed9b0c54c0fe7f9ee81a5917cdde0bcf675f10
Gorgor777 <sergei-agarkov07@mail.ru>
2024-03-05 00:02:05 +0300

    test repstr

--- agarkov.sergey/P3/fillNewRepStr.cpp
+++ agarkov.sergey/P3/fillNewRepStr.cpp
@@ -1,3 +1,17 @@
 #include "fillNewRepStr.hpp"
 
-void fillNewRepStr(const char* source, char* dest);
+void fillNewRepStr(const char* source, char* dest)
+{
+  for (auto i = source; *i != '\0'; i++)
+  {
+    for (auto j = i; *j != '\0'; ++j)
+    {
+      if (*i == *j)
+      {
+        *dest = *i;
+        dest++;
+      }
+    }
+  }
+  *(dest++) = '\0';
+}
--- agarkov.sergey/P3/main.cpp
+++ agarkov.sergey/P3/main.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include "inputCStr.hpp"
+#include "fillNewRepStr.hpp"
 
 int main()
 {
   char* c_str = nullptr;
   c_str = inputCStr(std::cin);
-  std::cout << c_str <<"\n";
-  return 0;
+  std::cout << c_str << "\n";
+  size_t size = 0;
+  while (c_str[size] != '\0')
+  {
+    size++;
+  }
+
+  char* rep_str = new char[size/2 + 1];
+  fillNewRepStr(c_str, rep_str);
+  std::cout << rep_str << "\n";
 }
