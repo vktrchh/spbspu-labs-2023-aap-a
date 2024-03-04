d93c1709d3d2dbcc85eaa2d52d994eae934511c9
Gorgor777 <sergei-agarkov07@mail.ru>
2024-03-04 23:06:52 +0300

    finish str input

--- agarkov.sergey/P3/main.cpp
+++ agarkov.sergey/P3/main.cpp
@@ -4,12 +4,35 @@ int main()
 {
   size_t capacity = 10;
   char* a = new char[capacity];
-  int count = 0;
-  while (std::cin >> a[count])
+  size_t size = 0;
+
+  std::cin >> std::noskipws;
+  do
   {
-    count++;
+    if (size == capacity)
+    {
+      try
+      {
+        char* new_str = new char[capacity + 20];
+        for (auto i =a, j = new_str; i != a + size; ++i, ++j)
+        {
+          *j = *i;
+        }
+        delete[] a;
+        a = new_str;
+        capacity += 20;
+      }
+      catch (const std::bad_alloc& e)
+      {
+        delete[] a;
+        throw e;
+      }
+    }
+    std::cin >>a[size];
   }
-  a[count+1] = '\0';
+  while (std::cin && a[size++] != '\n');
+
+  a[size - 1] = '\0';
   for (int i = 0; a[i] != '\0'; ++i)
   {
     std::cout << a[i];
