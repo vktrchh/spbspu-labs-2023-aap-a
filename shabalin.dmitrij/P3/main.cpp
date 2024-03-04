2987faadab01042439050d97cb667f775d6ed3a0
dmitei <dima9990860349@icloud.com>
2024-03-04 12:15:15 +0300

    fix my mistake

--- shabalin.dmitrij/P3/main.cpp
+++ shabalin.dmitrij/P3/main.cpp
@@ -78,9 +78,9 @@ char *makeLowerCaseString(char *str)
   }
   for (size_t i = 0; i < str[i] != '\0'; ++i)
   {
-    if(isupper(str[i]))
+    if(islower(str[i]))
       {
-        str[i] = tolower(str[i]);
+        str[i] = toupper(str[i]);
       }
   }
   return str;
