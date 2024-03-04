259aca12f64f4ffb016918922afeacf36f7c8cdc
Gorgor777 <sergei-agarkov07@mail.ru>
2024-03-04 23:23:18 +0300

    make func and test

---
+++ agarkov.sergey/P3/inputCStr.hpp
@@ -0,0 +1,8 @@
+#ifndef INPUTCSTR_HPP
+#define INPUTCSTR_HPP
+
+#include <istream>
+
+char* inputCStr(std::istream& in);
+
+#endif
--- agarkov.sergey/P3/main.cpp
+++ agarkov.sergey/P3/main.cpp
@@ -1,41 +1,10 @@
 #include <iostream>
+#include "inputCStr.hpp"
 
 int main()
 {
-  size_t capacity = 10;
-  char* a = new char[capacity];
-  size_t size = 0;
-
-  std::cin >> std::noskipws;
-  do
-  {
-    if (size == capacity)
-    {
-      try
-      {
-        char* new_str = new char[capacity + 20];
-        for (auto i =a, j = new_str; i != a + size; ++i, ++j)
-        {
-          *j = *i;
-        }
-        delete[] a;
-        a = new_str;
-        capacity += 20;
-      }
-      catch (const std::bad_alloc& e)
-      {
-        delete[] a;
-        throw e;
-      }
-    }
-    std::cin >>a[size];
-  }
-  while (std::cin && a[size++] != '\n');
-
-  a[size - 1] = '\0';
-  for (int i = 0; a[i] != '\0'; ++i)
-  {
-    std::cout << a[i];
-  }
+  char* c_str = nullptr;
+  c_str = inputCStr(std::cin);
+  std::cout << c_str <<"\n";
   return 0;
 }
