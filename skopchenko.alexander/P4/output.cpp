32db478aae3b120f30aefdbe44cb981490053866
polykisa <s.sanek2005@gmail.com>
2024-03-06 23:39:15 +0300

    precision fix

--- skopchenko.alexander/P4/output.cpp
+++ skopchenko.alexander/P4/output.cpp
@@ -1,7 +1,8 @@
 #include "output.h"
 #include <fstream>
 
-void skopchenko::intOutput(std::ostream &oFile, int *matrix, size_t rows, size_t cols) {
+void skopchenko::intOutput(std::ostream &oFile, int *matrix, size_t rows, size_t cols)
+{
   if (!(oFile << rows << ' ' << cols))
   {
     throw std::logic_error("Error during output:/");
@@ -16,12 +17,13 @@ void skopchenko::intOutput(std::ostream &oFile, int *matrix, size_t rows, size_t
   }
 }
 
-void skopchenko::doubleOutput(std::ostream &oFile, const double *matrix, size_t rows, size_t cols) {
+void skopchenko::doubleOutput(std::ostream &oFile, const double *matrix, size_t rows, size_t cols)
+{
   if (!(oFile << rows << ' ' << cols))
   {
     throw std::logic_error("Error during output:/");
   }
-  oFile << std::fixed;
+  oFile << std::fixed << std::setprecision(1);
   for (size_t i = 0; i < rows * cols; ++i)
   {
     if (!(oFile << ' ' << matrix[i]))
