49327b42432608e76ff186b778efecd3b593612d
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2024-03-07 00:25:58 +0300

    getArea() for Triangle

--- vyzhanov.daniil/T1/triangle.cpp
+++ vyzhanov.daniil/T1/triangle.cpp
@@ -24,3 +24,26 @@ double vyzhanov::Triangle::getArea() const
 {
   return vyzhanov::Triangle::findAreaOfTri(points_[0], points_[1], points_[2]);
 }
+double findAreaOfTri(const vyzhanov::point_t& a, const vyzhanov::point_t& b, const vyzhanov::point_t& c);
+
+double findAreaOfTri(const vyzhanov::point_t& a, const vyzhanov::point_t& b, const vyzhanov::point_t& c)
+{
+  double sq = ((b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y)) / 2;
+  sq = std::abs(sq);
+  return sq;
+}
+
+vyzhanov::Triangle::Triangle(const point_t& a, const point_t& b, const point_t& c):
+  points_{ a, b, c },
+  size_(3)
+{
+  if (findAreaOfTri(a, b, c) == 0)
+  {
+    throw std::invalid_argument("Invalid points");
+  }
+}
+
+double vyzhanov::Triangle::getArea() const
+{
+  return findAreaOfTri(points_[0], points_[1], points_[2]);
+}
--- vyzhanov.daniil/T1/triangle.hpp
+++ vyzhanov.daniil/T1/triangle.hpp
@@ -11,11 +11,11 @@ namespace vyzhanov
   {
   public:
     Triangle(const point_t& a, const point_t& b, const point_t& c);
-    double findAreaOfTri(const point_t& a, const point_t& b, const point_t& c);
     virtual double getArea() const;
     virtual rectangle_t getFrameRect() const;
     virtual void move(const point_t& pos);
     virtual void move(double dx, double dy);
+    virtual void scale(const double ratio);
   private:
     point_t points_[3];
     size_t size_;
