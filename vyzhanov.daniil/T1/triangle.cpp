3c36ed742afd1900b25690f7e19f25d12bfb3075
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2024-03-07 00:27:31 +0300

    getFrameRect() fot Triangle

--- vyzhanov.daniil/T1/triangle.cpp
+++ vyzhanov.daniil/T1/triangle.cpp
@@ -3,27 +3,6 @@
 #include <cmath>
 #include <algorithm>
 
-double vyzhanov::Triangle::findAreaOfTri(const vyzhanov::point_t& a, const vyzhanov::point_t& b, const vyzhanov::point_t& c)
-{
-  double sq = ((b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y)) / 2;
-  sq = std::abs(sq);
-  return sq;
-}
-
-vyzhanov::Triangle::Triangle(const point_t& a, const point_t& b, const point_t& c):
-  points_{ 1, 2, 2 },
-  size_(3)
-{
-  if (findAreaOfTri(a, b, c) == 0)
-  {
-    throw std::invalid_argument("Invalid points");
-  }
-}
-
-double vyzhanov::Triangle::getArea() const
-{
-  return vyzhanov::Triangle::findAreaOfTri(points_[0], points_[1], points_[2]);
-}
 double findAreaOfTri(const vyzhanov::point_t& a, const vyzhanov::point_t& b, const vyzhanov::point_t& c);
 
 double findAreaOfTri(const vyzhanov::point_t& a, const vyzhanov::point_t& b, const vyzhanov::point_t& c)
@@ -46,4 +25,12 @@ vyzhanov::Triangle::Triangle(const point_t& a, const point_t& b, const point_t& 
 double vyzhanov::Triangle::getArea() const
 {
   return findAreaOfTri(points_[0], points_[1], points_[2]);
+}
+
+vyzhanov::rectangle_t vyzhanov::Triangle::getFrameRect() const
+{
+  double height = (std::max(std::max(points_[0].y, points_[1].y), points_[2].y) - std::min(std::min(points_[0].y, points_[1].y), points_[2].y));
+  double width = (std::max(std::max(points_[0].x, points_[1].x), points_[2].x) - std::min(std::min(points_[0].x, points_[1].x), points_[2].x));
+  point_t pos = { std::min(std::min(points_[0].x, points_[1].x), points_[2].x) + width / 2, std::min(std::min(points_[0].y, points_[1].y), points_[2].y) + height / 2 };
+  return { width, height, pos };
 }
