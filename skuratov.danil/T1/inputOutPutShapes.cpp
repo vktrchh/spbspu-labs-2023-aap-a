21dc9d7c5d1a8afaef5dde55c6e3bdc0b010821a
Skuratov09 <skyratovdaniil28@gmail.com>
2024-03-06 21:40:00 +0300

    Fixed the last mistakes

--- skuratov.danil/T1/inputOutPutShapes.cpp
+++ skuratov.danil/T1/inputOutPutShapes.cpp
@@ -31,7 +31,7 @@ void skuratov::inputRectangle(std::istream& input, Shape** shapes, size_t& count
   shapes[count] = new Rectangle({ args[0], args[1] }, { args[2], args[3] });
 }
 
-void skuratov::outputFrameRec(std::ostream& out, Shape* shape)
+void skuratov::outputFrameRec(std::ostream& out, const Shape* shape)
 {
   out << " ";
   out << shape->getFrameRect().pos.x - shape->getFrameRect().width / 2 << " ";
@@ -43,8 +43,7 @@ void skuratov::outputFrameRec(std::ostream& out, Shape* shape)
 void skuratov::inputSquare(std::istream& input, Shape** shapes, size_t& count)
 {
   double args[3] = {};
-  size_t i = 0;
-  while (i < 3)
+  for (size_t i = 0; i < 3; ++i)
   {
     input >> args[i];
     if (!input)
@@ -59,15 +58,10 @@ void skuratov::inputSquare(std::istream& input, Shape** shapes, size_t& count)
 void skuratov::inputDiamond(std::istream& input, Shape** shapes, size_t& count)
 {
   double args[6] = {};
-  size_t i = 0;
-  while (i < 6)
+  input >> args[0] >> args[1] >> args[2] >> args[3] >> args[4] >> args[5];
+  if (!input)
   {
-    input >> args[i];
-    if (!input)
-    {
-      throw std::invalid_argument("Invalid arguments for the Diamond");
-    }
-    ++i;
+    throw std::invalid_argument("Invalid arguments for the Diamond");
   }
   shapes[count] = new Diamond({ args[0], args[1] }, { args[2], args[3] }, { args[4], args[5] });
 }
@@ -76,14 +70,16 @@ void skuratov::inputParallelogram(std::istream& input, Shape** shapes, size_t& c
 {
   double args[6] = {};
   size_t i = 0;
-  while (i < 6)
+  do
   {
     input >> args[i];
-    if (!input)
-    {
-      throw std::invalid_argument("Invalid arguments for the Parallelogram");
-    }
     ++i;
+  }
+  while (i < 6 && input);
+
+  if (!input)
+  {
+    throw std::invalid_argument("Invalid arguments for the Parallelogram");
   }
   shapes[count] = new Parallelogram({ args[0], args[1] }, { args[2], args[3] }, { args[4], args[5] });
 }
@@ -92,21 +88,21 @@ skuratov::Shape** skuratov::inputShapes(std::istream& input, size_t& count)
 {
   std::string shape = "";
   Shape** newShape = nullptr;
-  Shape** oldShape = nullptr;
   while (input >> shape && shape != "SCALE")
   {
-    oldShape = newShape;
-    newShape = new Shape * [count + 1] {};
-    if (oldShape)
-    {
-      for (size_t i = 0; i < count; ++i)
-      {
-        newShape[i] = oldShape[i];
-      }
-    }
-    delete[] oldShape;
     try
     {
+      Shape** oldShape = newShape;
+      newShape = new Shape*[count + 1] {};
+      if (oldShape)
+      {
+        for (size_t i = 0; i < count; ++i)
+        {
+          newShape[i] = oldShape[i];
+        }
+      }
+      delete[] oldShape;
+
       if (shape == "RECTANGLE")
       {
         skuratov::inputRectangle(input, newShape, count);
@@ -152,7 +148,7 @@ skuratov::Shape** skuratov::inputShapes(std::istream& input, size_t& count)
   return newShape;
 }
 
-void skuratov::outputShapes(std::ostream& out, Shape* const* shapes, size_t count)
+void skuratov::outputShapes(std::ostream& out, const Shape* const* shapes, size_t count)
 {
   double area = 0;
   for (size_t i = 0; i < count; ++i)
