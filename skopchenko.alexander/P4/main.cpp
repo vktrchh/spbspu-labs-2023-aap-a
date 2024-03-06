d89479b6c4a9d9028f13e9ac96afee66f92cdf8e
polykisa <s.sanek2005@gmail.com>
2024-03-06 23:59:57 +0300

    main fix

--- skopchenko.alexander/P4/main.cpp
+++ skopchenko.alexander/P4/main.cpp
@@ -1,4 +1,4 @@
-#include "smoothed.h"
+kk#include "smoothed.h"
 #include "topclock.h"
 #include "input.h"
 #include "output.h"
@@ -7,140 +7,151 @@
 
 int main(int argc, char * argv[])
 {
-    if (argc != 4)
-    {
-        std::cerr << "Invalid amount of arguments\n";
-        return 1;
-    }
+  if (argc != 4)
+  {
+    std::cerr << "Invalid amount of arguments\n";
+    return 1;
+  }
 
-    int arrType = 0;
+  int arrType = 0;
+  try
+  {
+    arrType = std::stoi(argv[1]);
+  }
+  catch (const std::logic_error & e)
+  {
+    std::cerr << "Invalid first argument\n";
+    return 1;
+  }
+
+  if (arrType != 1 && arrType != 2)
+  {
+    std::cerr << "First argument can only be '1' or '2'\n";
+    return 1;
+  }
+
+  std::ifstream iFile(argv[2]);
+  if (!iFile.is_open())
+  {
+    std::cerr << "Couldn't open input file\n";
+    return 2;
+  }
+
+  size_t rows = 0;
+  size_t cols = 0;
+
+  iFile >> rows >> cols;
+  if (!iFile)
+  {
+    std::cerr << "Couldn't read file\n";
+    return 2;
+  }
+  if (rows * cols == 0 || cols != rows)
+  {
+    std::cerr << "Invalid Matrix dimensions\n";
+    return 2;
+  }
+  else
+  {
+    int *matrix = nullptr;
+    double *smooth = nullptr;
+
+    if (arrType == 1)
+    {
+      int fixedArray[10000] = {};
+      matrix = fixedArray;
+      double smoothArray[10000] = {};
+      smooth = smoothArray;
+    } else
+    {
+      try
+      {
+        matrix = new int[cols * rows];
+        smooth = new double[cols * rows];
+      }
+      catch (const std::bad_alloc &e)
+      {
+        std::cerr << "Error while allocating dynamic array\n";
+        delete[] matrix;
+        delete[] smooth;
+        return 2;
+      }
+    }
     try
     {
-        arrType = std::stoi(argv[1]);
+      skopchenko::input(iFile, matrix, rows, cols);
     }
-    catch (const std::logic_error & e)
+    catch (std::runtime_error &e)
     {
-        std::cerr << "Invalid first argument\n";
-        return 1;
-    }
-
-    if (arrType != 1 && arrType != 2)
-    {
-        std::cerr << "First argument can only be '1' or '2'\n";
-        return 1;
+      std::cerr << "Couldn't input matrix\n";
+      if (arrType == 2)
+      {
+        delete[] matrix;
+      }
+      return 2;
     }
 
-    std::ifstream iFile(argv[2]);
-    if (!iFile.is_open())
+    try
     {
-        std::cerr << "Couldn't open input file\n";
+      std::ofstream oFile(argv[3]);
+      if (!oFile.is_open())
+      {
+        std::cerr << "Couldn't open output file\n";
         return 2;
-    }
-
-    size_t rows = 0;
-    size_t cols = 0;
-
-    iFile >> rows >> cols;
-    if (!iFile)
-    {
-        std::cerr << "Couldn't read file\n";
+      }
+      size_t counter = 1;
+      int top = 0;
+      int left = 0;
+      skopchenko::smoothedMatrix(matrix, smooth, rows, cols);
+      try
+      {
+        skopchenko::doubleOutput(oFile, smooth, rows, cols);
+        oFile << "\n";
+      }
+      catch (std::runtime_error &e)
+      {
+        std::cerr << "Couldn't output matrix\n";
+        if (arrType == 2)
+        {
+          delete[] matrix;
+          delete[] smooth;
+        }
         return 2;
-    }
-    if (rows * cols == 0 || cols != rows)
-    {
-        std::cerr << "Invalid Matrix dimensions\n";
+      }
+      skopchenko::topClock(matrix, rows , cols , counter , top , rows - 1 , left , cols - 1);
+      try
+      {
+        skopchenko::intOutput(oFile, matrix, rows, cols);
+        oFile << "\n";
+      }
+      catch (std::runtime_error &e)
+      {
+        std::cerr << "Couldn't output matrix\n";
+        if (arrType == 2)
+        {
+          delete[] matrix;
+          delete[] smooth;
+        }
         return 2;
+      }
     }
-    else
+    catch (std::logic_error &e)
     {
-        int* matrix;
-        double* smooth;
-
-        if (arrType == 1) {
-            int fixedMatrix[cols * rows];
-            double fixedSmooth[cols * rows];
-            matrix = fixedMatrix;
-            smooth = fixedSmooth;
-        } else {
-            matrix = new int[cols * rows];
-            smooth = new double[cols * rows];
-        }
-
-        try
-        {
-            skopchenko::input(iFile, matrix, rows, cols);
-        }
-        catch (std::runtime_error &e)
-        {
-            std::cerr << "Couldn't input matrix\n";
-            if (arrType == 2) {
-                delete[] matrix;
-                delete[] smooth;
-            }
-            return 2;
-        }
-
-        try
-        {
-            std::ofstream oFile(argv[3]);
-            if (!oFile.is_open())
-            {
-                std::cerr << "Couldn't open output file\n";
-                if (arrType == 2) {
-                    delete[] matrix;
-                    delete[] smooth;
-                }
-                return 2;
-            }
-            size_t counter = 1;
-            int top = 0;
-            int left = 0;
-            skopchenko::smoothedMatrix(matrix, smooth, rows, cols);
-            try
-            {
-                skopchenko::doubleOutput(oFile, smooth, rows, cols);
-                oFile << "\n";
-            }
-            catch (std::runtime_error &e)
-            {
-                std::cerr << "Couldn't output matrix\n";
-                if (arrType == 2) {
-                    delete[] matrix;
-                    delete[] smooth;
-                }
-                return 2;
-            }
-            skopchenko::topClock(matrix, rows , cols , counter , top , rows - 1 , left , cols - 1);
-            try
-            {
-                skopchenko::intOutput(oFile, matrix, rows, cols);
-                oFile << "\n";
-            }
-            catch (std::runtime_error &e)
-            {
-                std::cerr << "Couldn't output matrix\n";
-                if (arrType == 2) {
-                    delete[] matrix;
-                    delete[] smooth;
-                }
-                return 2;
-            }
-        }
-        catch (std::logic_error &e)
-        {
-            std::cerr << "Error while executing one of functions";
-            if (arrType == 2) {
-                delete[] matrix;
-                delete[] smooth;
-            }
-            return 1;
-        }
-
-        if (arrType == 2) {
-            delete[] matrix;
-            delete[] smooth;
-        }
+      std::cerr << "Error while executing one of functions";
+      if (arrType == 2)
+      {
+        delete[] matrix;
+        delete[] smooth;
+      }
+      return 1;
     }
 
-    return 0;
+    if (arrType == 2)
+    {
+
+      delete[] matrix;
+      delete[] smooth;
+
+    }
+  }
 }
