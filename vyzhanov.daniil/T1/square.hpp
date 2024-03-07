c4872da859a0adc2a2a89409013aefbcfffc5ca3
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2024-03-07 15:41:57 +0300

    create square.hpp

---
+++ vyzhanov.daniil/T1/square.hpp
@@ -0,0 +1,23 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+#include "base-types.hpp"
+#include "shape.hpp"
+namespace vyzhanov
+{
+    class Square : public Shape
+    {
+    public:
+    public:
+        Square(const point_t& leftCorner, double length);
+        virtual ~Square() = default;
+        virtual double getArea() const;
+        virtual rectangle_t getFrameRect() const;
+        virtual void move(const point_t& point);
+        virtual void move(double dx, double dy);
+        virtual void scale(double ratio);
+    private:
+        point_t downLeftCorner_;
+        double length_;
+    };
+}
+#endif
