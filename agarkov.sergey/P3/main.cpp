df4a56236cc354ee38c01f72df0cb519d26123e6
Gorgor777 <sergei-agarkov07@mail.ru>
2024-03-07 12:07:20 +0300

    exception save

--- agarkov.sergey/P3/main.cpp
+++ agarkov.sergey/P3/main.cpp
@@ -6,14 +6,21 @@
 int main()
 {
   char* c_str = nullptr;
-  c_str = inputCStr(std::cin);
-  std::cout << c_str << "\n";
+  try
+  {
+    c_str = inputCString(std::cin);
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
   size_t size = 0;
   while (c_str[size] != '\0')
   {
     size++;
   }
-
+  std::cout << c_string << "\n";
   char* rep_str = new char[size/2 + 1];
   fillNewRepStr(c_str, rep_str);
   std::cout << rep_str << "\n";
