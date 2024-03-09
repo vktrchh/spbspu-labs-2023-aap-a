f4a59c47a1373ba67badd4cc351cda905935dd28
vktrchh <viskas6969@gmail.com>
2024-03-09 22:55:04 +0300

    read and fill file

--- vojuck.victoria/P4/main.cpp
+++ vojuck.victoria/P4/main.cpp
@@ -1,2 +1,27 @@
-int main()
-{}
+#include <iostream>
+#include <ifstream>
+#include <ofsream>
+#include <cstdlib>
+
+int main(int argc, char ** argv)
+{
+  if (argc != 4)
+  {
+    std::cerr << "why not 3?(";
+    return 1;
+  }
+  int i  = 0;
+  {
+    std::ifstrean input(argv[2]);
+    input >> i;
+    if(!iput)
+    {
+      std::cerr << "cannot read";
+      return 2;
+    }
+  }
+  std::ofstream output(argv[3]);
+  output << i << "n\";
+}
+
+
