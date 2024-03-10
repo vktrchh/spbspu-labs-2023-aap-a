44ce043847b24dd0f63596c7ca5e656a64cf9ebb
vktrchh <viskas6969@gmail.com>
2024-03-10 12:02:22 +0300

    add input array function

--- vojuck.victoria/P4/main.cpp
+++ vojuck.victoria/P4/main.cpp
@@ -3,6 +3,11 @@
 #include <ofsream>
 #include <cstdlib>
 
+
+size_t inputArray(std::istream & in, int * array, size_t size, size_t toread);
+
+
+
 int main(int argc, char ** argv)
 {
   if (argc != 4)
@@ -22,6 +27,22 @@ int main(int argc, char ** argv)
   }
   std::ofstream output(argv[3]);
   output << i << "n\";
+
+}
+
+
+
+size_t inputArray(std::istream & in, int * array, size_t size, size_t toread)
+{
+  for (size_t i = 0; i < std::min(size, toread); ++i)
+  {
+    in >> array[i];
+    if (!in)
+    {
+      return i;
+    }
+  }
+  return std::min(size, toread);
 }
 
 
