f52e7deb41f8b97fcf2846e81a8c9d3bdb81c697
vktrchh <viskas6969@gmail.com>
2024-03-10 18:38:33 +0300

    translation unit

---
+++ vojuck.victoria/P4/findMaxSum.hpp
@@ -0,0 +1,6 @@
+#ifndef FINDMAXSUM_HPP
+#define FINDMAXSYM_HPP
+
+int findMaxSum(int* array, int rows, int cols);
+
+#endif
---
+++ vojuck.victoria/P4/isUpperTriangular.hpp
@@ -0,0 +1,6 @@
+#ifndef ISUPPERTRIANGULAR_HPP
+#define ISUPPERTRIANGULAR_HPP
+
+bool isUpperTriangular(int* matrix, int rows, int cols);
+
+#endif
