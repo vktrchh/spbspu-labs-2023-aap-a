8a561a1807f577425291b6ba0410f73bb3b93d88
vktrchh <viskas6969@gmail.com>
2024-03-10 12:32:09 +0300

    add fanction var1

--- vojuck.victoria/P4/main.cpp
+++ vojuck.victoria/P4/main.cpp
@@ -5,7 +5,7 @@
 
 
 size_t inputArray(std::istream & in, int * array, size_t size, size_t toread);
-
+int findMaxSum(int* array, int rows, int cols);
 
 
 int main(int argc, char ** argv)
@@ -43,6 +43,43 @@ size_t inputArray(std::istream & in, int * array, size_t size, size_t toread)
     }
   }
   return std::min(size, toread);
+}
+
+
+int findMaxSum(int* array, int rows, int cols)
+{
+  int maxSum = 0;
+  for (int i = 0; i < rows; i++)
+  {
+    int sum = 0;
+    for (int j = i; j < rows * cols; j += (cols + 1))
+    {
+      if (j % cols <= i)
+      {
+        sum += array[j];
+      }
+    }
+      if (sum > maxSum)
+      {
+        maxSum = sum;
+      }
+  }
+  for (int i = cols; i < rows * cols; i += cols)
+  {
+    int sum = 0;
+    for (int j = i; j < rows * cols; j += (cols - 1))
+    {
+      if (j % cols >= i % cols)
+      {
+        sum += array[j];
+      }
+    }
+      if (sum > maxSum)
+      {
+        maxSum = sum;
+      }
+  }
+  return maxSum;
 }
 
 
