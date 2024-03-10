b135d25587b448ea72153b4f918bbfb14f9dc26c
vktrchh <viskas6969@gmail.com>
2024-03-10 19:06:19 +0300

    i hope last

--- vojuck.victoria/P4/isUpperTriangular.cpp
+++ vojuck.victoria/P4/isUpperTriangular.cpp
@@ -4,7 +4,7 @@ bool isUpperTriangular(int* matrix, size_t rows, size_t cols)
 {
   for (size_t i = 0; i < rows; i++)
   {
-    for (sixe_t j = 0; j < cols; j++)
+    for (size_t j = 0; j < cols; j++)
     {
       if (i > j && matrix[i * cols + j] != 0)
       {
