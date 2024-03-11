377e8da73f3aadd2d88ab192a22692ac8b7c9d4f
vktrchh <viskas6969@gmail.com>
2024-03-12 01:16:12 +0300

    i hate git(

--- vojuck.victoria/P4/findMaxSum.cpp
+++ vojuck.victoria/P4/findMaxSum.cpp
@@ -4,14 +4,13 @@
 
 int findMaxSum(int* array, size_t rows, size_t cols)
 {
-  int n = 5;
   int maxSum = 0;
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < rows; i++) {
         int sum1 = 0;
         int sum2 = 0;
     for (int j = 0; j < n - i; j++)
     {
-      sum1 += array[j + rows*j + i];
+      sum1 += array[j + cols*j + i];
       sum2 += array[j + i + cols*j];
     }
     maxSum = std::max(maxSum, std::max(sum1, sum2));
