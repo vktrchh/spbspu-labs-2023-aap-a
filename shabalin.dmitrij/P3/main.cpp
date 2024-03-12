a4153878b134574bcdd6370579a38ae5b9b6cbb7
dmitei <dima9990860349@icloud.com>
2024-03-12 12:44:45 +0300

    main change

--- shabalin.dmitrij/P3/main.cpp
+++ shabalin.dmitrij/P3/main.cpp
@@ -1,6 +1,5 @@
-#include "duplicateNumbers.h"
-#include "makeLowerCaseString.h"
-#include "inputOfString.h"
+/*
+
 
 int main()
 {
@@ -29,5 +28,37 @@ int main()
   makeLowerCaseString(finalyString, result);
   std::cout << result;
 
+  return 0;
+}
+*/
+
+#include <iostream>
+#include "duplicateNumbers.h"
+#include "makeLowerCaseString.h"
+#include "inputOfString.h"
+
+int main()
+{
+  using namespace shabalin;
+  char *finalString = nullptr;
+  try
+  {
+    finalString = inputOfString(std::cin);
+  }
+  catch (const std::bad_alloc& e)
+  {
+    std::cerr << "Error: " << e.what() << "\n";
+    return 1;
+  }
+  if (finalString[0] == '\0')
+  {
+    delete[] finalString;
+    std::cerr << "Error: Empty string!" << "\n";
+    return 1;
+  }
+  std::cout << duplicateNumbers(finalString) << "\n";
+  char result[] = "123";
+  std::cout << makeLowerCaseString(finalString, result) << "\n";
+  delete[] finalString;
   return 0;
 }
