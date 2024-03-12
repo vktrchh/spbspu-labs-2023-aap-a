d4ba3ec312aafbb1a08a255ea5deafc4cd08c378
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2024-03-12 13:12:38 +0300

    fix precision

--- vyzhanov.daniil/T1/output.cpp
+++ vyzhanov.daniil/T1/output.cpp
@@ -2,13 +2,15 @@
 
 void vyzhanov::outputShapes(std::ostream& output, const Shape* const* shapes, const size_t count)
 {
-  double result = 0;
-  for (size_t i = 0; i < count; ++i)
+  double result = 0.0;
+  for (size_t i = 0.0; i < count; ++i)
   {
     result += shapes[i]->getArea();
   }
+  output << std::fixed;
+  output.precision(1);
   output << result;
-  for (size_t i = 0; i < count; ++i)
+  for (size_t i = 0.0; i < count; ++i)
   {
     rectangle_t frameRect = shapes[i]->getFrameRect();
     point_t pos = frameRect.pos;
