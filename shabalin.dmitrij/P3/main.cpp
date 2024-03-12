f74cc5f604c8b19dcf50c11076160d31c9c95583
dmitei <dima9990860349@icloud.com>
2024-03-12 12:20:07 +0300

    delete unused

--- shabalin.dmitrij/P3/main.cpp
+++ shabalin.dmitrij/P3/main.cpp
@@ -7,23 +7,19 @@ int main()
   using namespace shabalin;
   char *finalyString = nullptr;
   size_t stringSize = 50;
-  char *temporaryString = nullptr;
   try
   {
     finalyString = inputOfString(std::cin, stringSize);
-    char *temporaryString = new char[stringSize];
   }
-  catch (std::exception & e)
+  catch (std::bad_alloc & e)
   {
     delete[] finalyString;
-    delete[] temporaryString;
     std::cerr << "Error: " << e.what() << "\n";
     return 1;
   }
 
   if (finalyString[0] == '\0')
   {
-    delete[] temporaryString;
     delete[] finalyString;
     std::cerr << "Error" << "\n";
     return 1;
@@ -37,6 +33,5 @@ int main()
   std::cout << result;
 
   delete[] finalyString;
-  delete[] temporaryString;
   return 0;
 }
