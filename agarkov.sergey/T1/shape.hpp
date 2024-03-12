1d0033c755d07518fafaf65e20e2751c4358c6d9
Gorgor777 <sergei-agarkov07@mail.ru>
2024-03-12 12:22:58 +0300

    shape.hpp

---
+++ agarkov.sergey/T1/shape.hpp
@@ -0,0 +1,20 @@
+ #ifndef SHAPE_HPP
+#define SHAPE_HPP
+#include "baseTypes.hpp"
+
+namespace agarkov
+{
+  class Shape
+  {
+  public:
+    virtual double getArea() const = 0;
+    virtual rectangle_t getFrameRectangle() const = 0;
+    virtual void move(double dx, double dy) = 0;
+    virtual void move(point_t position) = 0;
+    virtual void scale(double k) = 0;
+
+    virtual ~Shape() = default;
+  };
+}
+
+#endif
