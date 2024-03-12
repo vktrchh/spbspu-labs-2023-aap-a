47a7236e76f338faaed52c8de4698c675b682f29
dmitei <dima9990860349@icloud.com>
2024-03-12 13:37:07 +0300

    main changes

--- shabalin.dmitrij/P3/main.cpp
+++ shabalin.dmitrij/P3/main.cpp
@@ -19,7 +19,6 @@ int main()
     std::cout << duplicateNumbers(finalString) << "\n";
     char result[] = "123";
     std::cout << makeLowerCaseString(finalString, result) << "\n";
-    return 0;
   }
   catch (const std::bad_alloc &e)
   {
@@ -27,4 +26,6 @@ int main()
     std::cerr << "Error: " << e.what() << "\n";
     return 1;
   }
+  delete[] finalString;
+  return 0;
 }
