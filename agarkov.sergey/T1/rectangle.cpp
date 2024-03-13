fd63ad39c1d833b9f7546e94272f4295f62c6b7c
Gorgor777 <sergei-agarkov07@mail.ru>
2024-03-13 18:18:33 +0300

    start parallelogram

--- agarkov.sergey/T1/rectangle.cpp
+++ agarkov.sergey/T1/rectangle.cpp
@@ -11,11 +11,6 @@ agarkov::Rectangle::Rectangle(agarkov::point_t left_bottom, agarkov::point_t rig
   }
 }
 
-bool agarkov::Rectangle::isCorrectRectangle() const
-{
-  return ((left_bottom_.x < right_top_.x) && (left_bottom_.y < right_top_.y));
-}
-
 double agarkov::Rectangle::getArea() const
 {
   return getFrameRectangle().height_ * getFrameRectangle().width_;
@@ -24,49 +19,39 @@ double agarkov::Rectangle::getArea() const
 agarkov::rectangle_t agarkov::Rectangle::getFrameRectangle() const
 {
   point_t centre_frame_rect = getLineCenter(left_bottom_, right_top_);
-  double height_frame_rect = right_top_.y - left_bottom_.y;
-  double width_frame_rect = right_top_.x - left_bottom_.x;
+  double height_frame_rect = right_top_.y_ - left_bottom_.y_;
+  double width_frame_rect = right_top_.x_ - left_bottom_.x_;
   return {centre_frame_rect, width_frame_rect, height_frame_rect};
 }
 
-void agarkov::Rectangle::move(double dx, double dy)
+void agarkov::Rectangle::move(const double dx, const double dy)
 {
   left_bottom_.move(dx, dy);
   right_top_.move(dx, dy);
 }
 
-void agarkov::Rectangle::move(agarkov::point_t position)
+void agarkov::Rectangle::move(const agarkov::point_t position)
 {
   point_t centre = getLineCenter(left_bottom_, right_top_);
-  double dx = position.x - centre.x;
-  double dy = position.y - centre.y;
+  double dx = position.x_ - centre.x_;
+  double dy = position.y_ - centre.y_;
   move(dx, dy);
+}
 
-<<<<<<< HEAD
-=======
-void agarkov::Rectangle::scale(double k)
+void agarkov::Rectangle::scale(const double k)
 {
   if (k <= 0)
   {
     throw std::invalid_argument("Incorrect scale coefficient");
   }
   point_t center = getFrameRectangle().pos_;
-<<<<<<< HEAD
   left_bottom_.x_ = center.x_ + (left_bottom_.x_ - center.x_) * k;
   left_bottom_.y_ = center.y_ + (left_bottom_.y_ - center.y_) * k;
   right_top_.x_ = center.x_ + (right_top_.x_ - center.x_) * k;
   right_top_.y_ = center.y_ + (right_top_.y_ - center.y_) * k;
->>>>>>> f841b99b (move dx dy)
-=======
-  left_bottom_.unsaveScale(center, k);
-  right_top_.unsaveScale(center, k);
->>>>>>> 8ddeabb8 (include unsaveScale to old scale)
 }
-<<<<<<< HEAD
-=======
 
 bool agarkov::Rectangle::isCorrectRectangle() const
 {
   return ((left_bottom_.x_ < right_top_.x_) && (left_bottom_.y_ < right_top_.y_));
 }
->>>>>>> e94b406b (start parallelogram)
