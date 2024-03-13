e53f465d4805b43be6de364af6f13ca6398e7fa9
jiggerman <liggerman@mail.ru>
2024-03-13 20:00:50 +0300

    ~ -> default

--- chistyakov.vladislav/T1/square.cpp
+++ chistyakov.vladislav/T1/square.cpp
@@ -11,9 +11,7 @@ chistyakov::Square::Square(const point_t & leftDownCorner, const double side):
   }
 }
 
-chistyakov::Square::~Square()
-{
-}
+chistyakov::Square::~Square() = = default;
 
 double chistyakov::Square::getArea() const
 {
