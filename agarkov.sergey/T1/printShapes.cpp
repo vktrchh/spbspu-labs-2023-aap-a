be0613f05527b1cbb3f4638836b1712a91001ebb
Gorgor777 <sergei-agarkov07@mail.ru>
2024-03-13 23:15:32 +0300

    print shapes

--- agarkov.sergey/T1/printShapes.cpp
+++ agarkov.sergey/T1/printShapes.cpp
@@ -1,1 +1,27 @@
- 
+#include "printShapes.hpp"
+#include <iomanip>
+#include <ostream>
+#include <cstddef>
+#include "shape.hpp"
+
+void agarkov::printData(std::ostream& out, const agarkov::Shape* const* array, const size_t size)
+{
+  out << std::fixed << std::setprecision(1);
+  double sum = 0.0;
+  for (size_t i = 0; i < size; i++)
+  {
+    sum += array[i]->getArea();
+  }
+  out << sum;
+
+  for (size_t i = 0; i < size; i++)
+  {
+    out << " ";
+    agarkov::rectangle_t frame_rectangle = array[i]->getFrameRectangle();
+    double x1 = frame_rectangle.pos_.x_ - frame_rectangle.width_ / 2;
+    double y1 = frame_rectangle.pos_.y_ - frame_rectangle.height_ / 2;
+    double x2 = frame_rectangle.pos_.x_ + frame_rectangle.width_ / 2;
+    double y2 = frame_rectangle.pos_.y_ + frame_rectangle.height_ / 2;
+    out << x1 << " " << y1 << " " << x2 << " " << y2;
+  }
+} 
--- agarkov.sergey/T1/printShapes.hpp
+++ agarkov.sergey/T1/printShapes.hpp
@@ -1,6 +1,13 @@
 #ifndef PRINTSHAPES_HPP
 #define PRINTSHAPES_HPP
 
+#include <ostream>
+#include <cstddef>
+#include "shape.hpp"
 
+namespace agarkov
+{
+  void printData(std::ostream& out, const Shape* const* array, size_t size);
+}
 
 #endif
