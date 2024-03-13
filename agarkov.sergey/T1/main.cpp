c2b0a0f4bef00297b354a80d9a2a77cbc5886e1b
Gorgor777 <sergei-agarkov07@mail.ru>
2024-03-13 23:21:03 +0300

    start main

--- agarkov.sergey/T1/main.cpp
+++ agarkov.sergey/T1/main.cpp
@@ -8,22 +8,10 @@
 
 int main()
 {
-  agarkov::Square sq = agarkov::Square({1, 1}, 2);
-  std::cout << sq.left_bottom_.x_ << " " << sq.left_bottom_.y_ << " " << sq.getArea() << "\n";
-  sq.scale(3);
-  std::cout << sq.left_bottom_.x_ << " " << sq.left_bottom_.y_<< " " << sq.getArea() << "\n";
-  agarkov::Rectangle rc = agarkov::Rectangle({1, 1}, {2, 2});
-  std::cout << rc.left_bottom_.x_ << " " << rc.left_bottom_.y_ << " " << rc.getArea() << "\n";
-  rc.scale(3);
-  std::cout << rc.left_bottom_.x_ << " " << rc.left_bottom_.y_<< " " << rc.getArea() << "\n";
-  agarkov::Parallelogram pr = agarkov::Parallelogram({1, 1}, {2, 1}, {2, 2});
-  std::cout << pr.point1_.x_ << " " << pr.point1_.y_ << " " << pr.getArea() << "\n";
-  pr.scale(3);
-  std::cout << pr.point1_.x_ << " " << pr.point1_.y_<< " " << pr.getArea() << "\n";
-  agarkov::Diamond dm = agarkov::Diamond({0, 0.5}, {1, 0}, {0, 0});
-  std::cout << dm.point1_.x_ << " " << dm.point1_.y_ << " " << dm.getArea() << "\n";
-  dm.scale(3);
-  std::cout << dm.point1_.x_ << " " << dm.point1_.y_<< " " << dm.getArea() << "\n";
-  isoScale(&sq, {1,1}, 2);
-  std::cout << sq.left_bottom_.x_ << " " << sq.left_bottom_.y_<< " " << sq.getArea() << "\n";
+  std::string shape_name = "";
+  double scale_coefficient = 0.0;
+  agarkov::point_t scale_point{0.0, 0.0};
+  size_t capacity = 10;
+  size_t size = 0;
+  agarkov::Shape** shapes = new agarkov::Shape*[capacity];
 }
