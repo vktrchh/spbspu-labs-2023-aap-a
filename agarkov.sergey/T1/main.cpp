1d4ee57ce5a5d73dc12028403f3e239dd4419810
Gorgor777 <sergei-agarkov07@mail.ru>
2024-03-13 23:26:28 +0300

    expand input array

--- agarkov.sergey/T1/main.cpp
+++ agarkov.sergey/T1/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include <string>
 
+#include "shape.hpp"
 #include "square.hpp"
 #include "rectangle.hpp"
 #include "parallelogram.hpp"
 #include "diamond.hpp"
 #include "isoScale.hpp"
+#include "printShapes.hpp"
+#include "workWithArray.hpp"
 
 int main()
 {
@@ -12,6 +16,23 @@ int main()
   double scale_coefficient = 0.0;
   agarkov::point_t scale_point{0.0, 0.0};
   size_t capacity = 10;
+  size_t buf = 10;
   size_t size = 0;
   agarkov::Shape** shapes = new agarkov::Shape*[capacity];
+  do
+  {
+    agarkov::Shape* shape = nullptr;
+    try
+    {
+      if (size + 1 == capacity)
+      {
+        agarkov::Shape** expanded_shapes = agarkov::expandArray(shapes, capacity, buf);
+        capacity += buf;
+        delete[] shapes;
+        shapes = expanded_shapes;
+      }
+    std::cin >> shape_name;
+  }
+
+  while (std::cin)
 }
