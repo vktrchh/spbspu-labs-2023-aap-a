fc2c56f575c8fe9bf3807bf1f6f03f68c8f101ed
jiggerman <liggerman@mail.ru>
2024-03-13 20:03:49 +0300

    CG 10 shape

--- chistyakov.vladislav/T1/shape.hpp
+++ chistyakov.vladislav/T1/shape.hpp
@@ -12,9 +12,9 @@ namespace chistyakov
     virtual ~Shape() = default;
     virtual double getArea() const = 0;
     virtual rectangle_t getFrameRect() const = 0;
-    virtual void move(const point_t & point) = 0;
-    virtual void move(const double mx, const double my) = 0;
-    virtual void scale(const double ratio) = 0;
+    virtual void move(point_t & point) = 0;
+    virtual void move(double mx, double my) = 0;
+    virtual void scale(double ratio) = 0;
   };
 }
 
