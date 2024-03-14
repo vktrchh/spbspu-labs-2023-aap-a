e977a8ba46d84b102aa6737943ae03f1ae61fa71
DaniilPrstrz <Vyzanovdaniil@gmail.com>
2024-03-14 10:06:24 +0300

    fix regular without triangle

--- vyzhanov.daniil/T1/regular.cpp
+++ vyzhanov.daniil/T1/regular.cpp
@@ -4,15 +4,16 @@
 #include <limits>
 
 vyzhanov::Regular::Regular(const point_t& p1, const point_t& p2, const point_t& p3):
-  mainTri_(Triangle(p1, p2, p3)),
-  points_{p1, p2, p3}
+  a_(p1),
+  b_(p2),
+  c_(p3)
 {
-  double bigRad = std::pow((p1.x - p2.x), 2) + std::pow((p1.y - p2.y), 2);
-  double smallRad = std::pow((p1.x - p3.x), 2) + std::pow((p1.y - p3.y), 2);
-  double foundation = std::pow((p2.x - p3.x), 2) + std::pow((p2.y - p3.y), 2);
-  bool isReg = !(smallRad + foundation == bigRad);
-  isReg = isReg && !(foundation + bigRad == smallRad);
-  isReg = isReg || (std::max(std::sqrt(bigRad), std::sqrt(foundation)) > std::sqrt(smallRad) * 2);
+  double firstLine = std::pow(p1.x - p2.x, 2) + std::pow(p1.y - p2.y, 2);
+  double secondLine = std::pow(p1.x - p3.x, 2) + std::pow(p1.y - p3.y, 2);
+  double foundation = std::pow(p2.x - p3.x, 2) + std::pow(p2.y - p3.y, 2);
+  bool isReg = !(firstLine + foundation == secondLine);
+  isReg = isReg && !(foundation + secondLine == firstLine);
+  isReg = isReg || (std::max(std::sqrt(firstLine), std::sqrt(secondLine)) > std::sqrt(foundation) * 2);
   if (isReg)
   {
     throw std::invalid_argument("Error: invalid regular agrugments");
@@ -21,36 +22,41 @@ vyzhanov::Regular::Regular(const point_t& p1, const point_t& p2, const point_t& 
 
 double vyzhanov::Regular::getArea() const
 {
-  double bigRad = std::pow((points_[0].x - points_[1].x), 2) + std::pow((points_[0].y - points_[1].y), 2);
-  double smallRad = std::pow((points_[0].x - points_[2].x), 2) + std::pow((points_[0].y - points_[2].y), 2);
-  double PI = 3.14159265358979323846;
-  size_t sideCount = std::round(-2.0 * PI / (std::asin(std::min(smallRad, bigRad) / std::max(smallRad, bigRad)) * 2.0 - PI));
-  return mainTri_.getArea() * sideCount * 2;
+  double firstLine = std::sqrt(std::pow(a_.x - b_.x, 2) + std::pow(a_.y - b_.y, 2));
+  double secondLine = std::sqrt(std::pow(a_.x - c_.x, 2) + std::pow(a_.y - c_.y, 2));
+  const double PI = 3.14159265358979323846;
+  double smallRad = std::min(firstLine, secondLine);
+  double bigRad = std::max(firstLine, secondLine);
+  int sideCount = std::round(-2.0 * PI / (std::asin(smallRad / bigRad) * 2.0 - PI));
+  double len = 2.0 * bigRad * std::sin(PI / sideCount);
+  return 0.5 * sideCount * len * smallRad;
 }
 
 vyzhanov::rectangle_t vyzhanov::Regular::getFrameRect() const
 {
-  double bigRad = std::pow((points_[0].x - points_[1].x), 2) + std::pow((points_[0].y - points_[1].y), 2);
-  double smallRad = std::pow((points_[0].x - points_[2].x), 2) + std::pow((points_[0].y - points_[2].y), 2);
+  double firstLine = std::sqrt(std::pow(a_.x - b_.x, 2) + std::pow(a_.y - b_.y, 2));
+  double secondLine = std::sqrt(std::pow(a_.x - c_.x, 2) + std::pow(a_.y - c_.y, 2));
   double PI = 3.14159265358979323846;
-  size_t sideCount = std::round(-2.0 * PI / (std::asin(std::min(smallRad, bigRad) / std::max(smallRad, bigRad)) * 2.0 - PI));
+  double smallRad = std::min(firstLine, secondLine);
+  double bigRad = std::max(firstLine, secondLine);
+  size_t sideCount = std::round(-2.0 * PI / (std::asin(smallRad / bigRad) * 2.0 - PI));
   double len = 2.0 * bigRad * std::sin(PI / sideCount);
   double maxX = std::numeric_limits< double >::lowest();
   double maxY = maxX;
   double minX = std::numeric_limits< double >::max();
   double minY = minX;
-  double replace = 0;
-  if (bigRad < smallRad)
-  {
-      replace = bigRad;
-      bigRad = smallRad;
-      smallRad = replace;
-      replace = 0;
-  }
-  point_t point = points_[1];
 
-  point.x = point.x - points_[0].x;
-  point.y = point.y - points_[0].y;
+  point_t point;
+  if (bigRad == firstLine)
+  {
+      point = b_;
+  }
+  else
+  {
+      point = c_;
+  }
+  point.x = point.x - a_.x;
+  point.y = point.y - a_.y;
   double angle = 2.0 * asin(len / 2.0 / bigRad);
   for (size_t i = 0; i != sideCount; ++i)
   {
@@ -63,25 +69,30 @@ vyzhanov::rectangle_t vyzhanov::Regular::getFrameRect() const
     maxY = std::max(maxY, point.y);
     minY = std::min(minY, point.y);
   }
-  return { maxX - minX, maxY - minY, points_[0]};
+  return { maxX - minX, maxY - minY, a_};
 }
 
 void vyzhanov::Regular::move(const point_t& point)
 {
-  double dx = point.x - points_[1].x;
-  double dy = point.y - points_[1].y;
+  double dx = point.x - a_.x;
+  double dy = point.y - a_.y;
   move(dx, dy);
 }
 
 void vyzhanov::Regular::move(double dx, double dy)
 {
-    mainTri_.move(dx, dy);
+  a_.x += dx;
+  a_.y += dy;
+  b_.x += dx;
+  b_.y += dy;
+  c_.x += dx;
+  c_.y += dy;
 }
 
 void vyzhanov::Regular::scale(double ratio)
 {
-    points_[1].x = points_[0].x + (points_[1].x - points_[0].x) * ratio;
-    points_[1].y = points_[0].y + (points_[1].y - points_[0].y) * ratio;
-    points_[2].x = points_[0].x + (points_[2].x - points_[0].x) * ratio;
-    points_[2].y = points_[0].y + (points_[2].y - points_[0].y) * ratio;
+  b_.x = a_.x + (b_.x - a_.x) * ratio;
+  b_.y = a_.y + (b_.y - a_.y) * ratio;
+  c_.x = a_.x + (c_.x - a_.x) * ratio;
+  c_.y = a_.y + (c_.y - a_.y) * ratio;
 }
--- vyzhanov.daniil/T1/regular.hpp
+++ vyzhanov.daniil/T1/regular.hpp
@@ -1,26 +1,22 @@
 #ifndef REGULAR_HPP
 #define REGULAR_HPP
 
-#include <cstddef>
 #include "shape.hpp"
-#include "triangle.hpp"
 
 namespace vyzhanov
 {
   class Regular: public Shape
   {
   public:
-    Regular (const point_t& a, const point_t& b, const point_t& c);
+    Regular(const point_t& p1, const point_t& p2, const point_t& p3);
     virtual ~Regular() = default;
     virtual double getArea() const;
     virtual rectangle_t getFrameRect() const;
-    virtual void move(const point_t& pos);
+    virtual void move(const point_t& point);
     virtual void move(double dx, double dy);
-    virtual void scale(const double ratio);
+    virtual void scale(double ratio);
   private:
-    Triangle mainTri_;
-    point_t points_[3];
+    point_t a_, b_, c_;
   };
 }
-
 #endif
