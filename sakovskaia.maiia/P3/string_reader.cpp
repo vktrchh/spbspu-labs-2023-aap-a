059f7587d6eb52d440868860722f9a413c76839b
MayaSakovskaya <zloyejik05@gmail.com>
2024-03-15 20:05:09 +0300

    input errors have been processed

--- sakovskaia.maiia/P3/string_reader.cpp
+++ sakovskaia.maiia/P3/string_reader.cpp
@@ -13,21 +13,39 @@ char * sakovskaia::readString(std::istream & input)
   input >> std::noskipws;
   while ((input >> c) && (c != '\n'))
   {
+    if (!input)
+    {
+      delete [] buffer;
+      throw std::logic_error("Error in input");
+    }
     buffer[i++] = c;
     if (i == (size - 1))
     {
       size_t new_size = size + 10;
-      new_buffer = new char[new_size];
-      for (size_t j = 0; j < i; j++)
+      try
       {
-        new_buffer[j] = buffer[j];
+        new_buffer = new char[new_size];
+        for (size_t j = 0; j < i; j++)
+        {
+          new_buffer[j] = buffer[j];
+        }
+        delete [] buffer;
+        buffer = new_buffer;
+        size = new_size;
       }
-      delete [] buffer;
-      buffer = new_buffer;
-      size = new_size;
+      catch (const std::bad_alloc &e)
+      {
+        delete [] buffer;
+        buffer = nullptr;
+      }
     }
-    buffer[i] = "\n";
-    return buffer;
+    buffer[i] = '\n';
+    if ((buffer[0] == '\n') || (buffer[0] == '\0'))
+    {
+      delete [] buffer;
+      throw std::logic_error("String is empty");
+    }
   }
   input >> std::skipws;
+  return buffer;
 }
