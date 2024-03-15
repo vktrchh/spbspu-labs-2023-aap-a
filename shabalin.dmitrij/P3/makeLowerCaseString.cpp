296f051872b8d01d580c0898da8e9b08ff0307de
dmitei <dima9990860349@icloud.com>
2024-03-15 04:27:25 +0300

    delete a lot of comments

--- shabalin.dmitrij/P3/makeLowerCaseString.cpp
+++ shabalin.dmitrij/P3/makeLowerCaseString.cpp
@@ -1,27 +1,6 @@
 #include "makeLowerCaseString.h"
 #include <cstddef>
 #include <cctype>
-/*
-char *shabalin::makeLowerCaseString(char *str, char *result)
-{
-  if (str == nullptr || *str == '\0')
-  {
-    return 0;
-  }
-  for (size_t i = 0; str[i] != '\0'; ++i)
-  {
-    if(std::isupper(str[i]))
-    {
-      str[i] = std::tolower(str[i]);
-    }
-  }
-  for (size_t j = 0; str[j] != '\0'; ++j)
-  {
-    result[j] = str[j];
-  }
-  return result;
-}
-*/
 
 void shabalin::makeLowerCaseString(char* str)
 {
