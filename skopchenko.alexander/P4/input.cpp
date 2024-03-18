20c5816d185a3d08e5cc536e24fadc167479b99a
polykisa <s.sanek2005@gmail.com>
2024-03-18 11:03:53 +0300

    CG + unnecessary code removal

--- skopchenko.alexander/P4/input.cpp
+++ skopchenko.alexander/P4/input.cpp
@@ -3,20 +3,11 @@
 
 void skopchenko::input(std::istream& iFile, int* matrix, size_t rows, size_t cols)
 {
-    size_t count = 0;
-
-    for (size_t i = 0; i < rows * cols; i++)
-    {
-        if (!(iFile >> matrix[i]))
-        {
-            throw std::logic_error("Bad input\n");
-        }
-        count++;
-    }
-
-    if (count < rows * cols)
+  for (size_t i = 0; i < rows * cols; i++)
+  {
+    if (!(iFile >> matrix[i]))
     {
       throw std::logic_error("Bad input\n");
     }
-
+  }
 }
