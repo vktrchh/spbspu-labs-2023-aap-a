a8d99153bbd0fabe035eef618b1235c5fdbd8ac9
MayaSakovskaya <zloyejik05@gmail.com>
2024-03-18 14:41:37 +0300

    trying to fix

--- sakovskaia.maiia/P3/string_analysis.cpp
+++ sakovskaia.maiia/P3/string_analysis.cpp
@@ -19,10 +19,9 @@ int sakovskaia::checkRep(const char * const string)
 void sakovskaia::printRep(const char * const string, char * answer)
 {
   size_t a = 0;
-  size_t flag = 0;
   for (size_t i = 0; string[i] != '\n'; i++)
   {
-    flag = 0;
+    size_t flag = 0;
     for (size_t j = 0; string[j] != '\n'; j++)
     {
       if (string[i] == answer[j])
