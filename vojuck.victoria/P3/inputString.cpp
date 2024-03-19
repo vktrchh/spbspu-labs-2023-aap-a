a5ed70944cd547375a61dc303e97a25777f01619
vktrchh <viskas6969@gmail.com>
2024-03-19 04:12:03 +0300

    std::isspace instead of c == '\n'

--- vojuck.victoria/P3/inputString.cpp
+++ vojuck.victoria/P3/inputString.cpp
@@ -14,7 +14,7 @@ char * vojuck::inputString(std::istream& input, int& size)
       throw std::logic_error("input error");
     }
     array[i++] = c;
-    if (c == '\n')
+    if (std::isspace(c))
     {
       if (i == 1)
       {
